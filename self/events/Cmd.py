2fb5022918bf93e7a6fb01cdc0e9e28090f7f1d0
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-18 00:22:02 +0330

    Update Cmd.py

--- self/events/Cmd.py
+++ self/events/Cmd.py
@@ -12,15 +12,6 @@ def Cmd(
     def decorator(func):
         async def wrapper(event):
             try:
-                await client.bot.send_message("TheaBoLi", "Hi")
-                selfall = client.DB.get_key("SELF_ALL_MODE") or "off"
-                if selfmode and selfall == "off": return
-                selfchats = client.DB.get_key("SELF_MODE") or []
-                if selfmode and event.chat_id in selfchats: return
-                event.reply_message = await event.get_reply_message()
-                event.is_sudo = True if event.sender_id == client.me.id else False
-                event.is_ch = True if event.is_channel and not event.is_group else False
-                if sudo and not event.is_sudo and not event.is_ch: return
                 await func(event)
             except:
                 stext = f"{client.str} The Lastest Error:\n\n{format_exc()}"
