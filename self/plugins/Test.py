88f17fa955f569373f488489f10c7cee4232777d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-18 00:51:53 +0330

    Update Test.py

--- self/plugins/Test.py
+++ self/plugins/Test.py
@@ -1,6 +1,18 @@
 from self import client
-from telethon import events
+import os
 
-@client.on(events.NewMessage(pattern=f"(?i)^\{client.cmd}ping$"))
-async def test(event):
-    await event.edit(f"**{client.str} Pong‌ !!**")
+@client.Cmd(pattern=f"(?i)^\{client.cmd}log (m|c|i)$")
+async def logs(event):
+    await event.edit(f"**{client.str} Processing . . .**")
+    type = str(event.pattern_match.group(1))
+    if type == "m" and os.path.exists("CmdError.txt"):
+        await event.respond(f"**{client.str} Log File!**", file="CmdError.txt")
+        await event.delete()
+    elif type == "c" and os.path.exists("CallbackError.txt"):
+        await event.respond(f"**{client.str} Log File!**", file="CallbackError.txt")
+        await event.delete()
+    elif type == "i" and os.path.exists("InlineError.txt"):
+        await event.respond(f"**{client.str} Log File!**", file="InlineError.txt")
+        await event.delete()
+    else:
+        await event.edit(f"**{client.str} The Log File Is Not Available!**")
