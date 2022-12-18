a5226f00d04cfe2f5eff6b8edb1cec90ad3b8541
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-18 16:50:04 +0330

    Update database.py

--- self/database.py
+++ self/database.py
@@ -16,7 +16,14 @@ class RedisDB:
     def __init__(self):
         URL = (config.REDIS_URL).split(":")[0]
         PORT = (config.REDIS_URL).split(":")[-1]
-        self.db = Redis("127.0.0.1", 6379, 7, charset='UTF-8', decode_responses=True)
+        self.db = Redis(
+                host=URL,
+                password=config.REDIS_PASSWORD,
+                port=int(PORT),
+                decode_responses=True,
+                socket_timeout=5,
+                retry_on_timeout=True,
+           )
         self.set = self.db.set
         self.get = self.db.get
         self.keys = self.db.keys
