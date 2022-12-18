7204115f6aeb3bd0d7e1f1b654ba3c5027931c62
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-18 22:41:28 +0330

    Update Cmd.py

--- self/events/Cmd.py
+++ self/events/Cmd.py
@@ -1,6 +1,7 @@
 from self import client
 from telethon import events
 from traceback import format_exc
+import re
 
 def Cmd(
     pattern=None,
@@ -9,6 +10,13 @@ def Cmd(
     selfmode=True,
     **kwargs,
 ):
+    cmds = client.DB.get_key("SELF_CMDS") or []
+    if pattern:
+        finds = re.findall("\w+", pattern)
+        for find in finds:
+            if len(find) > 1 and not find in cmds:
+                cmds.append(find)
+    client.DB.set_key("SELF_CMDS", cmds)
     def decorator(func):
         async def wrapper(event):
             try:
@@ -19,6 +27,12 @@ def Cmd(
                 event.reply_message = await event.get_reply_message()
                 event.is_sudo = True if event.sender_id == client.me.id else False
                 event.is_ch = True if event.is_channel and not event.is_group else False
+                cmds = client.DB.get_key("SELF_CMDS") or []
+                event.is_cmd = False
+                for cmd in cmds:
+                    if cmd in event.text:
+                        event.is_cmd = True
+                        pass
                 if sudo and not event.is_sudo and not event.is_ch: return
                 await func(event)
             except:
