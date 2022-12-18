33b64669d7d5f6d50c4c7b73945f83ec9171fa2a
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-18 23:24:49 +0330

    Update helper.py

--- FidoSelf/functions/helper.py
+++ FidoSelf/functions/helper.py
@@ -1,21 +1,7 @@
-from self import client
+from FidoSelf import client
 import asyncio
 import time
 import math
-
-async def edit_reply(event, text=None, **kwargs):
-    result = await event.edit(text=text, **kwargs)
-    if not result:
-        result = await event.reply(text=text, **kwargs)
-    return result
-
-async def down_media(event, message, **kwargs):
-    start = time.time()
-    file_name = None
-    if message.media:
-        file_name = message.media.document.attributes[-1].file_name
-    callback = lambda current, total: asyncio.get_event_loop().create_task(progress(event, current, total, start, "down", file_name=file_name))
-    return await client.download_media(message, progress_callback=callback, **kwargs)
 
 async def progress(event, current, total, start, type, file_name=None):
     if type == "down":
@@ -38,27 +24,6 @@ async def progress(event, current, total, start, type, file_name=None):
 **{client.str} ETA:** ( `{client.utils.convert_time(eta) or "---"}` )
 """
         await event.edit(text)
-
-def media_type(event):
-    if not event:
-        return None
-    if event.photo:
-        return "photo"
-    elif event.video and not event.gif and not event.video_note:
-        return "video"
-    elif event.video and event.gif:
-        return "gif"
-    elif event.video and event.video_note:
-        return "video note"
-    elif event.audio:
-        return "audio"
-    elif event.voice:
-        return "voice"
-    elif event.sticker and event.sticker.mime_type == "image/webp":
-        return "sticker"
-    elif event.sticker and event.sticker.mime_type == "application/x-tgsticker":
-        return "animated sticker"
-    return None
 
 def mention(info):
     if info.username:
