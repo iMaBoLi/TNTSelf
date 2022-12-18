b6dd76fdd74f3914f2c499de7d6868632e75b84c
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-18 23:17:39 +0330

    Create Cmd.py

---
+++ FidoSelf/events/Cmd.py
@@ -0,0 +1,52 @@
+from self import client
+from telethon import events
+from traceback import format_exc
+import re
+
+def Cmd(
+    pattern=None,
+    sudo=True,
+    edits=True,
+    selfmode=True,
+    realm=False,
+    backch=False,
+    **kwargs,
+):
+    cmds = client.DB.get_key("SELF_CMDS") or []
+    if pattern:
+        finds = re.findall("\w+", pattern)
+        for find in finds:
+            if len(find) > 1 and not find in cmds:
+                cmds.append(find)
+    client.DB.set_key("SELF_CMDS", cmds)
+    def decorator(func):
+        async def wrapper(event):
+            try:
+                selfall = client.DB.get_key("SELF_ALL_MODE") or "on"
+                if selfmode and selfall == "off": return
+                selfchats = client.DB.get_key("SELF_MODE") or []
+                if selfmode and event.chat_id in selfchats: return
+                event.reply_message = await event.get_reply_message()
+                event.is_sudo = True if event.sender_id == client.me.id else False
+                event.is_ch = True if event.is_channel and not event.is_group else False
+                if sudo and not event.is_sudo and not event.is_ch: return
+                cmds = client.DB.get_key("SELF_CMDS") or []
+                event.is_cmd = False
+                for cmd in cmds:
+                    if cmd in event.text: 
+                        event.is_cmd = True
+                arealm = client.DB.get_key("REALM_CHAT") or False
+                if realm and not arealm:
+                    return await event.edit(f"**{client.str} The Realm Chat Not Saved!**")
+                abackch = client.DB.get_key("BACKUP_CHANNEL") or False
+                if backch and not abackch:
+                    return await event.edit(f"**{client.str} The BackUp Channel Not Saved!**")
+                await func(event)
+            except:
+                stext = f"{client.str} The Lastest Error:\n\n{format_exc()}"
+                open("CmdError.txt", "w").write(str(stext))
+        client.add_event_handler(wrapper, events.NewMessage(pattern=pattern, **kwargs))
+        if edits:
+            client.add_event_handler(wrapper, events.MessageEdited(pattern=pattern, **kwargs))
+        return wrapper
+    return decorator
