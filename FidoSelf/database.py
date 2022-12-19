003e48105631aad2d22d608f8c630f13e93c5fd2
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-19 11:18:17 +0330

    Update database.py

--- FidoSelf/database.py
+++ FidoSelf/database.py
@@ -79,8 +79,8 @@ class SqlDB:
         self.connection = psycopg2.connect(dsn=self.url)
         self.connection.autocommit = True
         self.cursor = self.connection.cursor()
-        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS FidoSelf (_ varchar(70))")
-        self.re_cache()
+        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS FidoDB (_ varchar(70))")
+        self.recache()
 
     @property
     def name(self):
@@ -88,17 +88,17 @@ class SqlDB:
 
     @property
     def usage(self):
-        self.cursor.execute("SELECT pg_size_pretty(pg_relation_size('FidoSelf')) AS size")
+        self.cursor.execute("SELECT pg_size_pretty(pg_relation_size('FidoDB')) AS size")
         data = self.cursor.fetchall()
         return int(data[0][0].split()[0])
 
-    def re_cache(self):
+    def recache(self):
         self.cache = {}
         for key in self.keys():
             self.cache.update({key: self.get_key(key)})
 
     def keys(self):
-        self.cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name  = 'fidoself'") 
+        self.cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name  = 'fidodb'") 
         data = self.cursor.fetchall()
         return [key[0] for key in data]
 
@@ -111,7 +111,7 @@ class SqlDB:
 
     def get(self, variable):
         try:
-            self.cursor.execute(f"SELECT {variable} FROM FidoSelf")
+            self.cursor.execute(f"SELECT {variable} FROM FidoDB")
         except psycopg2.errors.UndefinedColumn:
             return None
         data = self.cursor.fetchall()
@@ -124,19 +124,19 @@ class SqlDB:
 
     def set_key(self, key, value):
         try:
-            self.cursor.execute(f"ALTER TABLE FidoSelf DROP COLUMN IF EXISTS {key}")
+            self.cursor.execute(f"ALTER TABLE FidoDB DROP COLUMN IF EXISTS {key}")
         except:
             pass
         self.cache.update({key: value})
-        self.cursor.execute(f"ALTER TABLE FidoSelf ADD {key} TEXT")
-        self.cursor.execute(f"INSERT INTO FidoSelf ({key}) values (%s)", (str(value),))
+        self.cursor.execute(f"ALTER TABLE FidoDB ADD {key} TEXT")
+        self.cursor.execute(f"INSERT INTO FidoDB ({key}) values (%s)", (str(value),))
         return True
 
     def del_key(self, key):
-        if key in self._cache:
+        if key in self.cache:
             del self.cache[key]
         try:
-            self.cursor.execute(f"ALTER TABLE FidoSelf DROP COLUMN {key}")
+            self.cursor.execute(f"ALTER TABLE FidoDB DROP COLUMN {key}")
         except psycopg2.errors.UndefinedColumn:
             return False
         return True
@@ -144,7 +144,7 @@ class SqlDB:
     def clean(self):
         self.cache.clear()
         for key in self.keys():
-            if not key == "_":
+            if str(key) != "_":
                 self.del_key(key)
         return True
 
