66374a1ac9f765f582012e27ccc4f47b6629a3c4
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-19 15:45:17 +0330

    Update SetVars.py

--- FidoSelf/plugins/SetVars.py
+++ FidoSelf/plugins/SetVars.py
@@ -29,6 +29,7 @@ async def realm(event):
     if not event.is_group:
         return await event.edit(f"**{client.str} Please Send In Group For Added Realm Chat!**")
     client.DB.set_key("REALM_CHAT", event.chat_id)
+    client.realm = event.chat_id
     await event.edit(f"**{client.str} This Chat Is Saved For Realm Chat!**")
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}SetBackCh$")
@@ -37,4 +38,5 @@ async def backch(event):
     if not event.is_ch:
         return await event.edit(f"**{client.str} Please Send In Channel For Added BackUp Channel!**")
     client.DB.set_key("BACKUP_CHANNEL", event.chat_id)
-    await event.edit(f"**{client.str} This Chat Is Saved For Realm Chat!**")
+    client.backch = event.chat_id
+    await event.edit(f"**{client.str} This Channel Is Saved For BackUp Channel!**")
