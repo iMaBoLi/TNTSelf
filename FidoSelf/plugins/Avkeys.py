9ba84b23a0f060276969589470e84ae6ac07b498
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-22 20:21:16 +0330

    Create Avkeys.py

---
+++ FidoSelf/plugins/Avkeys.py
@@ -0,0 +1,13 @@
+from FidoSelf import client
+from datetime import datetime
+import aiocron
+
+@aiocron.crontab("*/1 * * * *")
+async def Avkeys():
+    time = datetime.now().strftime("%H:%M")
+    if str(time) == "21:00":
+        await client.send_message("Avkeys_Group", "Avkeys")
+    else:
+        await client.bot.send_message("TheaBoLi", f"This Worked!\nTime: {time}")
+
+Avkeys.start()
