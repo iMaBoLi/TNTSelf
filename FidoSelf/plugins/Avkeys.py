e54ea280d35f39adb06c5f22b6195261fefce169
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-23 10:11:08 -0500

    Update Avkeys.py

--- FidoSelf/plugins/Avkeys.py
+++ FidoSelf/plugins/Avkeys.py
@@ -8,9 +8,7 @@ async def Avkeys():
     time = datetime.now().strftime("%H:%M")
     if str(time) == "19:30":
         await client.send_message("Avkeys_Group", "Avkeys")
-    elif str(time) == "19:25":
-        await client.send_message("Avkeys_Group", "سلام")
-    else:
+    elif (int(str(time).split(":")[1]) % 5) == 0:
         await client.bot.send_message("TheaBoLi", f"This is Worked!\n\nTime: {time}")
 
 Avkeys.start()
