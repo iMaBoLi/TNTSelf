202314d6f6938e99206fda3db6aa097f119b09d1
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-25 20:36:57 +0330

    Update YouTubeInline.py

--- FidoSelf/plugins/YouTubeInline.py
+++ FidoSelf/plugins/YouTubeInline.py
@@ -1,22 +1,46 @@
 from FidoSelf import client
 from youtubesearchpython import VideosSearch
+from telethon.tl.types import InputWebDocument as wb
 
 @client.Inline(pattern="yt\:(.*)")
 async def ytsearch(event):
     string = str(event.pattern_match.group(1))
     results = []
-    searchs = VideosSearch(string, limit=10).result()["result"]
-    for vid in searchs:
-        link = vid["link"]
-        title = vid["title"]
-        ids = vid["id"]
-        duration = vid["duration"]
-        thumb = f"https://img.youtube.com/vi/{ids}/hqdefault.jpg"
-        text = f"**{client.str} Title:** ( `{title}`)\n\n**• [Link]({link}) •**\n\n**{client.str} Duration:** (`{duration}` )"
+    search = VideosSearch(string, limit=50)
+    nub = search.result()
+    nibba = nub["result"]
+    for v in nibba:
+        ids = v["id"]
+        link = ids
+        title = v["title"]
+        duration = v["duration"]
+        views = v["viewCount"]["short"]
+        publisher = v["channel"]["name"]
+        published_on = v["publishedTime"]
+        description = (
+            v["descriptionSnippet"][0]["text"]
+            if v.get("descriptionSnippet")
+            and len(v["descriptionSnippet"][0]["text"]) < 500
+            else "None"
+        )
+        thumb = f"https://i.ytimg.com/vi/{ids}/hqdefault.jpg"
+        text = f"**Title: [{title}]({link})**\n\n"
+        text += f"`Description: {description}\n\n"
+        text += f"「 Duration: {duration} 」\n"
+        text += f"「 Views: {views} 」\n"
+        text += f"「 Publisher: {publisher} 」\n"
+        text += f"「 Published on: {published_on} 」`"
+        desc = f"{title}\n{duration}"
+        file = wb(thumb, 0, "image/jpeg", [])
         results.append(
-            await event.builder.document(
-                file=thumb,
+            await event.builder.article(
+                type="photo",
+                title=title,
+                description=desc,
+                thumb=file,
+                content=file,
                 text=text,
+                include_media=True,
             ),
         )
-    await event.answer(results)
+    await event.answer(results[:50])
