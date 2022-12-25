78826d12612eccd6537778512a4f94e0d4e270d0
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-25 18:42:33 +0330

    Update logs.py

--- FidoSelf/plugins/logs.py
+++ FidoSelf/plugins/logs.py
@@ -5,14 +5,14 @@ import os
 async def logs(event):
     await event.edit(f"**{client.str} Processing . . .**")
     type = str(event.pattern_match.group(1))
-    if type == "m" and os.path.exists("CmdError.txt"):
-        await event.respond(f"**{client.str} Log File!**", file="CmdError.txt")
+    if type == "m" and os.path.exists("CmdError.log"):
+        await event.respond(f"**{client.str} Log File!**", file="CmdError.log")
         await event.delete()
-    elif type == "c" and os.path.exists("CallbackError.txt"):
-        await event.respond(f"**{client.str} Log File!**", file="CallbackError.txt")
+    elif type == "c" and os.path.exists("CallbackError.log"):
+        await event.respond(f"**{client.str} Log File!**", file="CallbackError.log")
         await event.delete()
-    elif type == "i" and os.path.exists("InlineError.txt"):
-        await event.respond(f"**{client.str} Log File!**", file="InlineError.txt")
+    elif type == "i" and os.path.exists("InlineError.log"):
+        await event.respond(f"**{client.str} Log File!**", file="InlineError.log")
         await event.delete()
     else:
         await event.edit(f"**{client.str} The Log File Is Not Available!**")
