dab19ee05f9cffcfad4c4cecaeda3ddba0eb20d7
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-26 09:48:41 +0330

    Update helper.py

--- FidoSelf/functions/helper.py
+++ FidoSelf/functions/helper.py
@@ -29,3 +29,25 @@ def mention(info):
     if info.username:
         return "@" + info.username
     return f"[{info.first_name}](tg://user?id={info.id})"
+
+def convert_date(gy, gm, gd):
+   g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+   if (gm > 2):
+       gy2 = gy + 1
+   else:
+       gy2 = gy
+   days = 355666 + (365 * gy) + ((gy2 + 3) // 4) - ((gy2 + 99) // 100) + ((gy2 + 399) // 400) + gd + g_d_m[gm - 1]
+   jy = -1595 + (33 * (days // 12053))
+   days %= 12053
+   jy += 4 * (days // 1461)
+   days %= 1461
+   if (days > 365):
+       jy += (days - 1) // 365
+       days = (days - 1) % 365
+   if (days < 186):
+       jm = 1 + (days // 31)
+       jd = 1 + (days % 31)
+   else:
+      jm = 7 + ((days - 186) // 30)
+      jd = 1 + ((days - 186) % 30)
+   return [jy, jm, jd]
