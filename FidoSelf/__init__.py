28b089e2e8f556f51ba02f1f24d620fff361404b
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-27 00:14:07 +0330

    Update __init__.py

--- FidoSelf/__init__.py
+++ FidoSelf/__init__.py
@@ -1,21 +1,20 @@
-from FidoSelf.functions.BaseClient import FidoClient
+from telethon import TelegramClient
 from telethon.sessions import StringSession
 from FidoSelf import config
 import time
 
 START_TIME = time.time()
 
-client = FidoClient(
+client = TelegramClient(
     session=StringSession(str(config.SESSION)),
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     device_model="FidoSelf",
     app_version="0.4.1",
-)
+).start()
 
-client.bot = FidoClient(
+client.bot = TelegramClient(
     session=None,
     api_id=config.API_ID,
     api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-)
+).start(bot_token=config.BOT_TOKEN)
