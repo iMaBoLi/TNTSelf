04869b87ab60e77f774dcd4212932334cdabd1a6
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-26 13:25:35 +0330

    Update misc.py

--- FidoSelf/functions/misc.py
+++ FidoSelf/functions/misc.py
@@ -24,8 +24,19 @@ async def addvars():
     setattr(client, "realm", client.DB.get_key("REALM_CHAT"))
     setattr(client, "backch", client.DB.get_key("BACKUP_CHANNEL"))
     setattr(client, "mention", mention)
+    down_foshs()
 
 def stimezone():
     tzone = client.DB.get_key("TIME_ZONE") or "Asia/Tehran"
     os.environ["TZ"] = str(tzone)
     time.tzset()
+
+def down_foshs():
+    ffile = client.DB.get_key("ORGFOSHS_FILE") or {}
+    if ffile:
+        get = await client.get_messages(int(ffile["chat_id"]), ids=int(ffile["msg_id"]))
+        await get.downlaod_file("ORGFOSHS.txt")
+    ffile = client.DB.get_key("FRIENDFOSHS_FILE") or {}
+    if ffile:
+        get = await client.get_messages(int(ffile["chat_id"]), ids=int(ffile["msg_id"]))
+        await get.downlaod_file("FRIENDFOSHS.txt")
