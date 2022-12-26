8832e7f1b006fce15eb93adcf1cce6805ce20f7c
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-27 00:27:59 +0330

    Create client.py

---
+++ FidoSelf/functions/client.py
@@ -0,0 +1,20 @@
+from telethon import TelegramClient
+
+class FidoClient(TelegramClient):
+    def __init__(
+        self,
+        session,
+        api_id=None,
+        api_hash=None,
+        bot_token=None,
+        *args,
+        **kwargs,
+    ):
+        kwargs["api_id"] = api_id
+        kwargs["api_hash"] = api_hash
+        super().__init__(session, **kwargs)
+        self.run_in_loop(self.start_client(bot_token=bot_token))
+        self.dc_id = self.session.dc_id
+
+    async def start_client(self, **kwargs):
+        await self.start(**kwargs)
