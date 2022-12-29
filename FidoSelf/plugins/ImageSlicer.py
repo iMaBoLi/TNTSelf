572ff80c836e7761d2dec1a4f83a48b26a318e53
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-29 18:36:15 +0330

    Update ImageSlicer.py

--- FidoSelf/plugins/ImageSlicer.py
+++ FidoSelf/plugins/ImageSlicer.py
@@ -2,14 +2,18 @@ from FidoSelf import client
 import image_slicer
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}Slice (\d*)$")
-async def ssticker(event):
+async def sliceimage(event):
     await event.edit(f"**{client.str} Processing . . .**")
     tile = event.pattern_match.group(1)
     if event.is_reply and event.reply_message.photo:
         photo = await event.reply_message.download_media()
         tiles = image_slicer.slice(photo, int(tile))
         photos = [str(tile).split(" - ")[1].replace(">", "") for tile in tiles]
-        for phs in list(client.utils.chunks(photos))
-            await event.respond(f"**{client.str} The Photo Was Sliced To** ( `{len(photos)}` ) **Tiles!**")
-        return await event.delete()
-    await event.edit(f"**{client.str} Please Reply To Photo!**")
+        for phs in list(client.utils.chunks(photos)):
+            await event.respond(f"**{client.str} The Photo Was Sliced To** ( `{len(photos)}` ) **Tiles!**", file=phs)
+        await event.delete()
+        os.remove(photo)
+        for ph in photos:
+            os.remove(ph)
+    else:
+        await event.edit(f"**{client.str} Please Reply To Photo!**")
