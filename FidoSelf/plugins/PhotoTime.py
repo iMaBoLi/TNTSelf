cc789e3c538615199d932dbda40b5b65a2e1c43a
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-30 01:37:22 +0330

    Update PhotoTime.py

--- FidoSelf/plugins/PhotoTime.py
+++ FidoSelf/plugins/PhotoTime.py
@@ -1,6 +1,6 @@
 from FidoSelf import client
 from telethon import Button
-from FidoSelf.plugins.ManageTime import COLORS
+from FidoSelf.functions.vars import COLORS
 import os
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}Photo (On|Off)$")
@@ -18,7 +18,7 @@ async def addphoto(event):
         return await event.edit(f"**{client.str} Please Reply To Photo!**")
     phname = str(event.pattern_match.group(1))
     phname = phname + ".png"
-    photos = client.DB.get_key("PHOTOS")
+    photos = client.DB.get_key("PHOTOS") or {}
     if phname in photos:
         return await event.edit(f"**{client.str} The Photo Name** ( `{phname}` ) **Already In Photo List!**")
     if not client.backch:
@@ -36,7 +36,7 @@ async def addphoto(event):
 @client.Cmd(pattern=f"(?i)^\{client.cmd}DelPhoto (.*)$")
 async def delphoto(event):
     await event.edit(f"**{client.str} Processing . . .**")
-    photos = client.DB.get_key("PHOTOS")
+    photos = client.DB.get_key("PHOTOS") or {}
     phname = str(event.pattern_match.group(1))
     if phname not in photos:
         return await event.edit(f"**{client.str} The Photo** ( `{phname}` ) **Not In Photo List!**")
@@ -47,7 +47,7 @@ async def delphoto(event):
 @client.Cmd(pattern=f"(?i)^\{client.cmd}GetPhoto (.*)$")
 async def getphoto(event):
     await event.edit(f"**{client.str} Processing . . .**")
-    photos = client.DB.get_key("PHOTOS")
+    photos = client.DB.get_key("PHOTOS") or {}
     phname = str(event.pattern_match.group(1))
     if phname not in photos:
         return await event.edit(f"**{client.str} The Photo** ( `{phname}` ) **Not In Photo List!**")
@@ -61,7 +61,7 @@ async def getphoto(event):
 @client.Cmd(pattern=f"(?i)^\{client.cmd}PhotoList$")
 async def photolist(event):
     await event.edit(f"**{client.str} Processing . . .**")
-    photos = client.DB.get_key("PHOTOS")
+    photos = client.DB.get_key("PHOTOS") or {}
     if not photos:
         return await event.edit(f"**{client.str} The Photo List Is Empty!**")
     text = f"**{client.str} The Photo List:**\n\n"
@@ -74,7 +74,7 @@ async def photolist(event):
 @client.Cmd(pattern=f"(?i)^\{client.cmd}CleanPhotoList$")
 async def cleanphotos(event):
     await event.edit(f"**{client.str} Processing . . .**")
-    photos = client.DB.get_key("PHOTOS")
+    photos = client.DB.get_key("PHOTOS") or {}
     if not photos:
         return await event.edit(f"**{client.str} The Photo List Is Already Empty!**")
     client.DB.del_key("PHOTOS")
@@ -95,7 +95,7 @@ async def addphoto(event):
 async def photo(event):
     work = str(event.data_match.group(1).decode('utf-8'))
     data = (str(event.data_match.group(2).decode('utf-8'))).split(":")
-    photos = client.DB.get_key("PHOTOS")
+    photos = client.DB.get_key("PHOTOS") or {}
     phname = data[0]
     where = data[1]
     if work == "size":
@@ -146,7 +146,7 @@ async def photo(event):
 @client.Callback(data="photoclose\:(.*)")
 async def closephoto(event):
     phname = str(event.data_match.group(1).decode('utf-8'))
-    photos = client.DB.get_key("PHOTOS")
+    photos = client.DB.get_key("PHOTOS") or {}
     del photos[phname]
     client.DB.set_key("PHOTOS", photos)
     await event.edit(text=f"**{client.str} The Photo Panel Successfuly Closed!**")
@@ -155,7 +155,7 @@ async def closephoto(event):
 async def savefontfile(event):
     await event.edit(f"**{client.str} Processing . . .**")
     fname = str(event.pattern_match.group(1))
-    fonts = client.DB.get_key("FONTS")
+    fonts = client.DB.get_key("FONTS") or {}
     if len(fonts) > 10:
         return await event.edit(f"**{client.str} Sorry, You Cannot Save More Than 10 Fonts!**")
     if not event.reply_message:
@@ -177,7 +177,7 @@ async def savefontfile(event):
 async def delfontfile(event):
     await event.edit(f"**{client.str} Processing . . .**")
     fname = str(event.pattern_match.group(1))
-    fonts = client.DB.get_key("FONTS")
+    fonts = client.DB.get_key("FONTS") or {}
     if fname not in fonts:
         return await event.edit(f"**{client.str} The Font** ( `{fname}` ) **Not In Fonts List!**")
     del fonts[fname]
@@ -187,7 +187,7 @@ async def delfontfile(event):
 @client.Cmd(pattern=f"(?i)^\{client.cmd}FontList$")
 async def fontlist(event):
     await event.edit(f"**{client.str} Processing . . .**")
-    fonts = client.DB.get_key("FONTS")
+    fonts = client.DB.get_key("FONTS") or {}
     if not fonts:
         return await event.edit(f"**{client.str} The Font File List Is Empty!**")
     text = f"**{client.str} The Font File List:**\n\n"
@@ -200,7 +200,7 @@ async def fontlist(event):
 @client.Cmd(pattern=f"(?i)^\{client.cmd}CleanFontList$")
 async def cleanfonts(event):
     await event.edit(f"**{client.str} Processing . . .**")
-    fonts = os.listdir(client.path + "fonts/")
+    fonts = client.DB.get_key("FONTS") or {}
     if not fonts:
         return await event.edit(f"**{client.str} The Font File List Is Already Empty!**")
     client.DB.del_key("FONTS")
@@ -209,7 +209,7 @@ async def cleanfonts(event):
 @client.Cmd(pattern=f"(?i)^\{client.cmd}AddTextTime ([\s\S]*)$")
 async def addtexttime(event):
     await event.edit(f"**{client.str} Processing . . .**")
-    texttimes = client.DB.get_key("TEXT_TIMES")
+    texttimes = client.DB.get_key("TEXT_TIMES") or []
     newtexttime = str(event.pattern_match.group(1))
     if newtexttime in texttimes:
         return await event.edit(f"**{client.str} The Text Time** ( `{newtexttime}` ) **Already In Text Time List!**")  
@@ -217,25 +217,25 @@ async def addtexttime(event):
     for word in newtexttime:
         if word not in trtexts:
             newtexttime = newtexttime.replace(word, "")
-    texttimes += [newtexttime]
+    texttimes.append(newtexttime)
     client.DB.set_key("TEXT_TIMES", texttimes)
     await event.edit(f"**{client.str} The Text Time** ( `{newtexttime}` ) **Added To Text Time List!**")  
     
 @client.Cmd(pattern=f"(?i)^\{client.cmd}DelTextTime ([\s\S]*)$")
 async def deltexttime(event):
     await event.edit(f"**{client.str} Processing . . .**")
-    texttimes = client.DB.get_key("TEXT_TIMES")
+    texttimes = client.DB.get_key("TEXT_TIMES") or []
     newtexttime = str(event.pattern_match.group(1))
     if newtexttime not in texttimes:
         return await event.edit(f"**{client.str} The Text Time** ( `{newtexttime}` ) **Not In Text Time List!**")  
-    texttimes = texttimes.remove(newtexttime)
+    texttimes.remove(newtexttime)
     client.DB.set_key("TEXT_TIMES", texttimes)
     await event.edit(f"**{client.str} The Text Time** ( `{newtexttime}` ) **Deleted From Text Time List!**")  
     
 @client.Cmd(pattern=f"(?i)^\{client.cmd}TextTimeList$")
 async def texttimelist(event):
     await event.edit(f"**{client.str} Processing . . .**")
-    texttimes = client.DB.get_key("TEXT_TIMES")
+    texttimes = client.DB.get_key("TEXT_TIMES") or []
     if not texttimes:
         return await event.edit(f"**{client.str} The Text Time List Is Empty!**")
     text = f"**{client.str} The Text Time List:**\n\n"
@@ -248,8 +248,8 @@ async def texttimelist(event):
 @client.Cmd(pattern=f"(?i)^\{client.cmd}CleanTextTimeList$")
 async def cleantexttimes(event):
     await event.edit(f"**{client.str} Processing . . .**")
-    texttimes = client.DB.get_key("TEXT_TIMES")
+    texttimes = client.DB.get_key("TEXT_TIMES") or []
     if not texttimes:
         return await event.edit(f"**{client.str} The Text Time List Is Already Empty!**")
-    client.DB.set_key("TEXT_TIMES", [])
+    client.DB.del_key("TEXT_TIMES")
     await event.edit(f"**{client.str} The Text Time List Is Cleared!**")
