d1c8acb890e0677631737caa6c42cc3ae05cf17c
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-29 20:49:00 +0330

    Update Cmd.py

--- FidoSelf/events/Cmd.py
+++ FidoSelf/events/Cmd.py
@@ -10,7 +10,7 @@ def Cmd(
     selfmode=True,
     **kwargs,
 ):
-    cmds = client.DB.get_key("SELF_CMDS")
+    cmds = client.DB.get_key("SELF_CMDS") or []
     if pattern:
         finds = re.findall("\w+", pattern)
         for find in finds:
@@ -20,15 +20,15 @@ def Cmd(
     def decorator(func):
         async def wrapper(event):
             try:
-                selfall = client.DB.get_key("SELF_ALL_MODE")
+                selfall = client.DB.get_key("SELF_ALL_MODE") or "on"
                 if selfmode and selfall == "off": return
-                selfchats = client.DB.get_key("SELF_MODE")
+                selfchats = client.DB.get_key("SELF_MODE") or []
                 if selfmode and event.chat_id in selfchats: return
                 event.reply_message = await event.get_reply_message()
                 event.is_sudo = True if event.sender_id == client.me.id else False
                 event.is_ch = True if event.is_channel and not event.is_group else False
                 if sudo and not event.is_sudo and not event.is_ch: return
-                cmds = client.DB.get_key("SELF_CMDS")
+                cmds = client.DB.get_key("SELF_CMDS") or []
                 event.is_cmd = False
                 for cmd in cmds:
                     if cmd in event.text: 
