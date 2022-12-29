a7638d90533c40b8e3fa0d8c4780eca02fabc0a6
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-29 18:47:36 +0330

    Update ImageSlicer.py

--- FidoSelf/plugins/ImageSlicer.py
+++ FidoSelf/plugins/ImageSlicer.py
@@ -1,12 +1,13 @@
 from FidoSelf import client
 import image_slicer
+import os
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}Slice (\d*)$")
+@client.Cmd(pattern=f"(?i)^\{client.cmd}SSlice (\d*)$")
 async def sliceimage(event):
     await event.edit(f"**{client.str} Processing . . .**")
     tile = event.pattern_match.group(1)
     if event.is_reply and event.reply_message.photo:
-        photo = await event.reply_message.download_media()
+        photo = await event.reply_message.download_media("SliceImage.jpg")
         tiles = image_slicer.slice(photo, int(tile))
         photos = [str(tile).split(" - ")[1].replace(">", "") for tile in tiles]
         for phs in list(client.utils.chunks(photos, 10)):
