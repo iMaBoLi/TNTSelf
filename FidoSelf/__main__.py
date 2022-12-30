dce65a2e4971277e7d38e07631f0e450492b368d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-30 12:23:54 +0330

    Update __main__.py

--- FidoSelf/__main__.py
+++ FidoSelf/__main__.py
@@ -20,7 +20,7 @@ async def setup():
                 text = f"**❌ Unloaded Plugin :**\n\n"
                 text += f"{client.str} `{plug}` - ( `{NOT_LOADED_PLUGS[plug]}` )\n"
                 await send.reply(text)
-        res = await client.runcmd('git log --pretty=format:"%an: %s" -5')
+        res = await client.utils.runcmd('git log --pretty=format:"%an: %s" -5')
         await send.reply(f"**{client.str} The 5 Lastest Github Commits:**\n\n`{res[0]}`")
     except:
         pass
