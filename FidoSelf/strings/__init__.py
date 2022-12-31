ac34308cc53c0665370b1a10520ee9f6f06a4610
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-31 15:55:31 +0330

    Update __init__.py

--- FidoSelf/strings/__init__.py
+++ FidoSelf/strings/__init__.py
@@ -2,7 +2,7 @@ from FidoSelf import client
 
 def get_string(string):
     lang = client.DB.get_key("LANGUAGE") or "en"
-    STRS = open(f"FidoSelf/strings/{str(lang)}.txt", "r").read()
+    STRS = open(f"FidoSelf/strings/{str(lang)}.yml", "r").read()
     STRING = eval(STRS)
     for page in string.split("_"):
         STRING = STRING[page]
