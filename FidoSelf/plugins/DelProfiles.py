6651779e5dc77dea8d4e439c59095cdbfc766f45
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-31 23:51:47 +0330

    Update DelProfiles.py

--- FidoSelf/plugins/DelProfiles.py
+++ FidoSelf/plugins/DelProfiles.py
@@ -1,17 +1,17 @@
 from FidoSelf import client
 from telethon import functions, types
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}DelProfile ?(\-\d*)?$")
+@client.Cmd(pattern=f"(?i)^\{client.cmd}DelProfile ?((\-)?\d*)?$")
 async def delprofiles(event):
     await event.edit(client.get_string("Wait"))
     pphoto = await client.get_profile_photos("me")
     if not pphoto:
-        return await event.edit(f"**{client.str} The Profile Photos Is Empty!**")
+        return await event.edit(client.get_string("DelProfiles_1"))
     prof = event.pattern_match.group(1)
     if not prof:
         pphoto = pphoto[0]
         await client(functions.photos.DeletePhotosRequest(id=[types.InputPhoto(id=pphoto.id, access_hash=pphoto.access_hash, file_reference=pphoto.file_reference)]))
-        await event.edit(f"**{client.str} The Last Profile Photo Has Been Deleted!**")
+        await event.edit(client.get_string("DelProfiles_2"))
     elif str(prof).startswith("-"):
         prof = prof.replace("-", "")
         if int(prof) > len(pphoto):
@@ -19,10 +19,10 @@ async def delprofiles(event):
         for con in range(0, int(prof)):
             profs = pphoto[con]
             await client(functions.photos.DeletePhotosRequest(id=[types.InputPhoto(id=profs.id, access_hash=profs.access_hash, file_reference=profs.file_reference)]))
-        await event.edit(f"**{client.str} The** ( `{str(prof)}` ) **Of Your Profile Photos Has Been Deleted!**")
+        await event.edit(client.get_string("DelProfiles_3").format(prof))
     else:        
         if int(prof) > len(pphoto):
             return await event.edit(f"**{client.str} This Profile Photo Is Not Available!**")
         pphoto = pphoto[int(prof)-1]
         await client(functions.photos.DeletePhotosRequest(id=[types.InputPhoto(id=pphoto.id, access_hash=pphoto.access_hash, file_reference=pphoto.file_reference)]))
-        await event.edit(f"**{client.str} The Profile Photo** ( `{str(prof)}` ) **Has Been Deleted!**")
+        await event.edit(client.get_string("DelProfiles_4").format(prof))
