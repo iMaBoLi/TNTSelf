e8c150294bfc64b52363bf3322d79ba8edecf433
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-31 21:30:15 +0330

    Update Translate.py

--- FidoSelf/plugins/Translate.py
+++ FidoSelf/plugins/Translate.py
@@ -5,11 +5,11 @@ from googletrans import Translator
 async def translator(event):
     await event.edit(client.get_string("Wait"))
     if not event.reply_message or not event.reply_message.text:
-        return await event.edit(client.get_string("Translate_1"))
+        return await event.edit(client.get_string("Reply_T"))
     dest = event.text.split()[1]
     try:
         translator = Translator()
         trjome = translator.translate(event.reply_message.text, dest=dest.lower())
     except ValueError:
-        await event.edit(client.get_string("Translate_2"))
-    await event.edit(client.get_string("Translate_3").format(trjome.src, dest.lower(), trjome.text))
+        await event.edit(client.get_string("Translate_1"))
+    await event.edit(client.get_string("Translate_2").format(trjome.src, dest.lower(), trjome.text))
