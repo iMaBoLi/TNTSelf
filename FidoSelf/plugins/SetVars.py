16d23493b22605087630bb427943150302501a01
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-31 22:55:43 +0330

    Update SetVars.py

--- FidoSelf/plugins/SetVars.py
+++ FidoSelf/plugins/SetVars.py
@@ -1,5 +1,14 @@
 from FidoSelf import client
 
+@client.Cmd(pattern=f"(?i)^\{client.cmd}Set(en|fa)$")
+async def language(event):
+    await event.edit(client.get_string("Wait"))
+    mode = event.pattern_match.group(1).lower()
+    client.DB.set_key("LANGUAGE", mode)
+    client.lang = mode
+    text = client.get_string("Lang")
+    await event.edit(text)
+
 @client.Cmd(pattern=f"(?i)^\{client.cmd}SetStr (.*)$")
 async def messagesstarter(event):
     await event.edit(client.get_string("Wait"))
