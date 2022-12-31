4b0d6ca620068a43c7797e72cc8543f2d17a9bcb
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-31 16:35:15 +0330

    Update __init__.py

--- FidoSelf/strings/__init__.py
+++ FidoSelf/strings/__init__.py
@@ -2,7 +2,7 @@ from FidoSelf import client
 
 def get_string(string):
     lang = client.DB.get_key("LANGUAGE") or "en"
-    STRS = open(f"FidoSelf/strings/{str(lang)}.yml", "r").read()
+    STRS = open(f"FidoSelf/strings/strings/{str(lang)}.yml", "r").read()
     STRING = eval(STRS)
     for page in string.split("_"):
         STRING = STRING[page]
@@ -11,3 +11,11 @@ def get_string(string):
         newstr = newstr.replace("{STR}", client.str)
         newstr = newstr.replace("{CMD}", client.cmd)    
     return newstr
+
+def get_cmd(cmd):
+    lang = client.DB.get_key("LANGUAGE") or "en"
+    STRS = open(f"FidoSelf/strings/commands/{str(lang)}.yml", "r").read()
+    STRING = eval(STRS)
+    STRING = STRING[cmd]
+    STRING = f"(?i)^\{client.cmd}{STRING}$"
+    return STRING
