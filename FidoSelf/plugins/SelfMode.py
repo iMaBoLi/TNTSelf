cac14fcc778b5a255f6c34d3a51165a9aa419db8
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-31 16:09:50 +0330

    Update SelfMode.py

--- FidoSelf/plugins/SelfMode.py
+++ FidoSelf/plugins/SelfMode.py
@@ -7,7 +7,7 @@ async def selfallmode(event):
     client.DB.set_key("SELF_ALL_MODE", mode)
     client.DB.del_key("SELF_MODE")
     change = client.get_string("Change_1") if mode == "on" else client.get_string("Change_2")
-    await event.edit(client.get_string("SelfMode_1"))
+    await event.edit(client.get_string("SelfMode_1").format(change))
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}Self (On|Off)$", selfmode=False)
 async def selfmode(event):
@@ -23,4 +23,4 @@ async def selfmode(event):
         if event.chat_id in chats:
             chats.remove(event.chat_id)
             client.DB.set_key("SELF_MODE", chats)
-    await event.edit(client.get_string("SelfMode_2"))
+    await event.edit(client.get_string("SelfMode_2").format(change))
