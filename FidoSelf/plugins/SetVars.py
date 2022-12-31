f60b71dd6a968810b19fc600ba51e713e3bdc3b3
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-31 16:26:52 +0330

    Update SetVars.py

--- FidoSelf/plugins/SetVars.py
+++ FidoSelf/plugins/SetVars.py
@@ -2,7 +2,7 @@ from FidoSelf import client
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}SetStr (.*)$")
 async def messagesstarter(event):
-    await event.edit(client.get_string("Wait").format(client.str))
+    await event.edit(client.get_string("Wait"))
     string = event.pattern_match.group(1)
     client.DB.set_key("MESSAGES_STARTER", str(string))
     client.str = str(string)
@@ -10,7 +10,7 @@ async def messagesstarter(event):
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}SetCmd (.*)$")
 async def cmdmessage(event):
-    await event.edit(client.get_string("Wait").format(client.str))
+    await event.edit(client.get_string("Wait"))
     cmd = event.pattern_match.group(1)
     client.DB.set_key("SELF_CMD", str(cmd))
     client.cmd = str(cmd)
@@ -18,14 +18,14 @@ async def cmdmessage(event):
 
 @client.Cmd(pattern=f"(?i)^.DelCmd$")
 async def dcmdmessage(event):
-    await event.edit(client.get_string("Wait").format(client.str))
+    await event.edit(client.get_string("Wait"))
     client.DB.set_key("SELF_CMD", ".")
     client.cmd = "."
     await event.edit(client.get_string("Vars_3"))
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}SetRealm$")
 async def realm(event):
-    await event.edit(client.get_string("Wait").format(client.str))
+    await event.edit(client.get_string("Wait"))
     if not event.is_group:
         return await event.edit(client.get_string("Vars_4"))
     client.DB.set_key("REALM_CHAT", event.chat_id)
@@ -34,7 +34,7 @@ async def realm(event):
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}SetBackCh$")
 async def backch(event):
-    await event.edit(client.get_string("Wait").format(client.str))
+    await event.edit(client.get_string("Wait"))
     if not event.is_ch:
         return await event.edit(client.get_string("Vars_6"))
     client.DB.set_key("BACKUP_CHANNEL", event.chat_id)
