2170fb44688ed31a6c93e79fa11e08836d6dea38
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-31 20:19:47 +0330

    Update TimerSave.py

--- FidoSelf/plugins/TimerSave.py
+++ FidoSelf/plugins/TimerSave.py
@@ -7,7 +7,7 @@ async def timersave(event):
     mode = event.pattern_match.group(1).lower()
     client.DB.set_key("TIMER_MODE", mode)
     change = client.get_string("Change_1") if mode == "on" else client.get_string("Change_2")
-    await event.edit(f"**{client.str} The Download Timers Photo adn Video Mode Has Been {change}!**")
+    await event.edit(client.get_string("TimerSave_1").format(change))
 
 @client.Cmd(sudo=False)
 async def dtimers(event):
@@ -15,10 +15,10 @@ async def dtimers(event):
     if event.is_private and event.media and event.photo and event.media.to_dict()["_"] == "MessageMediaPhoto" and event.media.ttl_seconds and mode == "on":
         down = await event.download_media(f"{event.sender_id}.jpg")
         mention = client.mention(await event.get_sender())
-        await client.send_file(client.realm, down, caption=f"**{client.str} The Photo Has Been Downloaded!**\n\n**{client.str} User:** ( {mention} )\n**{client.str} Timer:** ( `{client.utils.convert_time(event.media.ttl_seconds)}` )")
+        await client.send_file(client.realm, down, caption=client.get_string("TimerSave_2").format(mention, client.utils.convert_time(event.media.ttl_seconds)))
         os.remove(down)
     if event.is_private and event.media and event.video and event.media.to_dict()["_"] == "MessageMediaDocument" and event.media.ttl_seconds and mode == "on":
         down = await event.download_media(f"{event.sender_id}.mp4")
         mention = client.mention(await event.get_sender())
-        await client.send_file(client.realm, down, caption=f"**{client.str} The Video Has Been Downloaded!**\n\n**{client.str} User:** ( {mention} )\n**{client.str} Timer:** ( `{client.utils.convert_time(event.media.ttl_seconds)}` )")
+        await client.send_file(client.realm, down, caption=client.get_string("TimerSave_3").format(mention, client.utils.convert_time(event.media.ttl_seconds)))
         os.remove(down)
