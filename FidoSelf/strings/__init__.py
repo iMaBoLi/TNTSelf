99a41a43a84fa9a7651177c591751304a67909b3
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-31 15:15:40 +0330

    Update __init__.py

--- FidoSelf/strings/__init__.py
+++ FidoSelf/strings/__init__.py
@@ -1,10 +1,9 @@
 from FidoSelf import client
-import json
 
 def get_string(string):
     lang = client.DB.get_key("LANGUAGE") or "en"
     STRS = open(f"FidoSelf/strings/{str(lang)}.txt", "r").read()
-    STRING = json.loads(STRS)
+    STRING = eval(STRS)
     for page in string.split("_"):
         STRING = STRING[page]
     newstr = STRING
