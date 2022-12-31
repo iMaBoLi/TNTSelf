1d1bbba2ecb249419d4fb817e9d39a0ebfef16f4
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2022-12-31 12:52:43 +0330

    Update ls.py

--- FidoSelf/plugins/ls.py
+++ FidoSelf/plugins/ls.py
@@ -23,7 +23,7 @@ def get_file_icon(name):
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}ls(?:\s|$)([\s\S]*)$")
 async def ls(event):
-    await event.edit(client.get_string("Wait_1").format(client.str))
+    await event.edit(client.get_string("Wait").format(client.str))
     input = "".join(event.text.split(maxsplit=1)[1:])
     path = input or os.getcwd()
     if not os.path.exists(path):
