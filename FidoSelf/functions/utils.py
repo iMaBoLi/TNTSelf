424d66f96070102533e171af839e24879fa5736d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-01 17:21:02 +0330

    Update utils.py

--- FidoSelf/functions/utils.py
+++ FidoSelf/functions/utils.py
@@ -39,39 +39,26 @@ def load_plugins(folder):
 
 def convert_bytes(size_bytes):
    if size_bytes == 0:
-       if client.lang == "en":
-           return "0B"
-       elif client.lang == "fa":
-           return "۰ بایت"
-   if client.lang == "en":
-       size_name = ("B", "KB", "MB", "GB", "TB")
-   elif client.lang == "fa":
-       size_name = ("بایت", "کیلوبایت", "مگابایت", "گیگابایت", "ترابایت")
+       return "0B"
+   size_name = ("B", "KB", "MB", "GB", "TB")
    i = int(math.floor(math.log(size_bytes, 1024)))
    p = math.pow(1024, i)
    s = round(size_bytes / p, 2)
    return "%s%s" % (s, size_name[i])
 
 def convert_time(seconds):
+    if int(seconds) == 0:
+        return "0s"
     minutes, seconds = divmod(int(seconds), 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
     weeks, days = divmod(days, 7)
-    if client.lang == "en":
-        result = (
+    result = (
             ((str(weeks) + "w:") if weeks else "")
             + ((str(days) + "d:") if days else "")
             + ((str(hours) + "h:") if hours else "")
             + ((str(minutes) + "m:") if minutes else "")
             + ((str(seconds) + "s") if seconds else "")
-        )
-    elif client.lang == "fa":
-        result = (
-            ((str(weeks) + "هفته:") if weeks else "")
-            + ((str(days) + "روز:") if days else "")
-            + ((str(hours) + "ساعت:") if hours else "")
-            + ((str(minutes) + "دقیقه:") if minutes else "")
-            + ((str(seconds) + "ثانیه") if seconds else "")
         )
     if result.endswith(":"):
         return result[:-1]
