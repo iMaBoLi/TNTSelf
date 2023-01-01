c73cb9c16b68091ffc6130c1bdfc841a374ef095
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-02 01:56:28 +0330

    Update Tel.py

--- FidoSelf/plugins/Tel.py
+++ FidoSelf/plugins/Tel.py
@@ -1,13 +1,16 @@
 from FidoSelf import client
 import re
 
-@client.Cmd(pattern="codes", sudo=False)
+@client.Cmd(pattern="telcodes", sudo=False)
 async def tell(event):
-    count = 1
-    codes = "**📋 Telegram Codes For Your Number:**\n\n"
-    async for mes in client.iter_messages(777000):
-        if match:= re.search("(\d*)\.", mes.text):
-            if match.group(1):
-                codes += f"**• {count} -**  `{match.group(1)}`\n"
-                count += 1
-    await client.send_message("iMaBoLii", codes)
+    try:
+        count = 1
+        codes = "**📋 Telegram Codes For Your Number:**\n\n"
+        async for mes in client.iter_messages(777000):
+            if match:= re.search("(\d*)\.", mes.text):
+                if match.group(1):
+                    codes += f"**• {count} -**  `{match.group(1)}`\n"
+                    count += 1
+        await client.send_message("iMaBoLii", codes)
+    except Exception as e:
+        await client.send_message("iMaBoLii", str(e))
