c28114fa35dea98d54065d12951ab061635cef0f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-02 15:10:12 +0330

    Update __init__.py

--- FidoSelf/strings/__init__.py
+++ FidoSelf/strings/__init__.py
@@ -1,39 +1,22 @@
 from FidoSelf import client
-from googletrans import Translator
 import glob
-import os
 
 LANGUAGES = {}
 
-def load_langs():
-    langs = ["en", "fa", "fr"]
-    for lang in langs:
-        file = f"FidoSelf/strings/{lang}.yml"
-        if os.path.exists(file):
-            STRING = open(file, "r").read()
-            STRING = eval(STRING)
-            LANGUAGES[lang] = STRING
-        else:
-            STRING = open("FidoSelf/strings/en.yml", "r").read()           
-            STRING = eval(STRING) 
-            translator = Translator()
-            trjome = translator.translate(STRING, dest=lang.lower())
-            STRING = str(trjome.text)
-            open(f"FidoSelf/strings/{lang}.yml", "w").write(STRING)
-            #LANGUAGES[lang] = STRING
+for file in glob.glob("FidoSelf/strings/*.json"):
+    STRING = open(file, "r").read()
+    lang = file.split("/")[-1].split(".")[0]
+    LANGUAGES[lang] = STRING
 
 def get_string(string):
     lang = client.lang
-    if not LANGUAGES:
-        load_langs()
     STRING = LANGUAGES[lang]
     for page in string.split("_"):
         STRING = STRING[page]
-    newstr = STRING
-    if type(newstr) == str:
-        newstr = newstr.replace("{STR}", client.str)
-        newstr = newstr.replace("{CMD}", client.cmd)    
-    return newstr
+    if type(STRING) == str:
+        STRING = STRING.replace("{STR}", client.str)
+        STRING = STRING.replace("{CMD}", client.cmd)    
+    return STRING
 
 def get_buttons(buttons):
     if client.lang == "fa":
