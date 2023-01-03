815068f996ae9ff0233b04871e6167d440138a5e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-03 15:17:58 +0330

    Create PMeme.py

---
+++ FidoSelf/plugins/PMeme.py
@@ -0,0 +1,33 @@
+from FidoSelf import client
+from telethon import Button
+
+@client.Cmd(pattern=f"(?i)^\{client.cmd}Pmeme (.*)$")
+async def persianmeme(event):
+    await event.edit(client.get_string("Wait"))
+    query = event.pattern_match.group(1)
+    res = await client.inline_query(client.bot.me.username, f"pmeme:{query}")
+    await res[0].click(event.chat_id)
+    await event.delete()
+
+@client.Inline(pattern="pmeme\:(.*)")
+async def inlinememe(event):
+    query = str(event.pattern_match.group(1))
+    results = await client.inline_query("Persian_Meme_Bot", query)
+    text = f"**{client.str} The Persian Meme Results For Query:** ( `{}` )"
+    buttons = []
+    con = 0
+    for result in results[:20]:
+        title = result.title[:10]
+        type = result.type
+        buttons.append(Button.inline(f"• {title} - {type} •", data=f"getpmeme:{query}:{con}))
+        con += 1
+    buttons = list(client.utils.chunks(buttons, 2))
+    await event.answer([event.builder.article(f"{client.str} Smart Self - PMeme", text=text, buttons=buttons)])
+
+@client.Callback(data="getpmeme\:(.*)\:(.*)")
+async def getpmeme(event):
+    query = str(event.data_match.group(1).decode('utf-8'))
+    count = int(event.data_match.group(2).decode('utf-8'))
+    results = await client.inline_query("Persian_Meme_Bot", query)
+    await res[count].click(event.chat_id)
+    await event.delete()
