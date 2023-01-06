f6305baffa7206f3f43d116e8d51c22d6957edd8
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-07 00:31:48 +0330

    Update TestClass.py

--- FidoSelf/plugins/TestClass.py
+++ FidoSelf/plugins/TestClass.py
@@ -1,9 +1,20 @@
 from FidoSelf import client
+from telethon.errors import (
+    ChatAdminRequiredError,
+)
 
-class TestClass:
-    def __init__(self):
-        self.client = client
-    def ping(self):
-        @self.client.Cmd(pattern="Kosk")
-        async def ping(event):
-            await self.client.send_message(event.chat_id, "**• Im Online!**")
+@client.Cmd(pattern=f"(?i)^\{client.cmd}Kick ?(.*)?")
+async def kick(event):
+    await event.edit(client.get_string("Wait"))
+    event = await client.get_ids(event)
+    info = await client.get_entity(event.userid)
+    if not event.userid:
+        return await event.edit(client.get_string("Reply_UU"))
+    try:
+        await client.edit_permissions(event.chat_id, event.userid, view_messages=False)
+        await client.edit_permissions(event.chat_id, event.userid)
+    except ChatAdminRequiredError:
+        return await event.edit(client.get_string("Kick_1"))
+    except Exception as e:
+        return await event.edit(client.get_string("Kick_2").format(str(e)))
+    await event.edit(client.get_string("Kick_3").format(client.mention(info)))
