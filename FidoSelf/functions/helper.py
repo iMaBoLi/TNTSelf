8feaf3901ad9a9d3ca72bbffef8a47e3254d0a77
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-07 15:04:48 +0330

    Update helper.py

--- FidoSelf/functions/helper.py
+++ FidoSelf/functions/helper.py
@@ -1,7 +1,20 @@
 from FidoSelf import client
+from functools import partial, wraps
+from concurrent.futures import ThreadPoolExecutor
 import asyncio
+import multiprocessing
 import time
 import math
+
+def run_async(function):
+    @wraps(function)
+    async def wrapper(*args, **kwargs):
+        return await asyncio.get_event_loop().run_in_executor(
+            ThreadPoolExecutor(max_workers=multiprocessing.cpu_count() * 5),
+            partial(function, *args, **kwargs),
+        )
+    return wrapper
+
 
 async def progress(event, current, total, start, type, file_name=None):
     if type == "down":
