b624f48651f10db2719f253c95e9e5116e1f38a8
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-08 01:36:48 +0330

    Update VideoShot.py

--- FidoSelf/plugins/VideoShot.py
+++ FidoSelf/plugins/VideoShot.py
@@ -1,6 +1,7 @@
 from FidoSelf import client
 from telethon import functions, types
 import time
+import os
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}VShot ((\-)?\d*)$")
 async def videoshot(event):
@@ -8,19 +9,37 @@ async def videoshot(event):
     data = event.pattern_match.group(1)
     if not event.is_reply or not event.reply_message.video:
         return await event.edit(client.get_string("Reply_V"))
+    newtime = time.time()
+    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down"))
+    file = await event.reply_message.download_media(progress_callback=callback)
+    duration = event.reply_message.video.attributes[0].duration
     if str(data).startswith("-"):
         count = int(data.replace("-", ""))
-        newtime = time.time()
-        callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down"))
-        file = await event.reply_message.download_media(progress_callback=callback)
-        duration = event.reply_message.video.attributes[0].duration
-        newdur = (duration - 10) / count
+        newdur = duration / count
         files = []
-        lastdur = 5
+        lastdur = 0
+        await event.edit(client.get_string("VideoShot_1").format(str(con)))
         for con in range(count):
             out = f"Shot-{con}.jpg"
             cmd = f"ffmpeg -i {file} -ss {lastdur} -vframes 1 {out}"
             await client.utils.runcmd(cmd)
             files.append(out)
             lastdur += newdur
-        await client.send_file(event.chat_id, files)
+        await event.edit(client.get_string("VideoShot_2").format(str(con)))
+        caption = client.get_string("VideoShot_3").format(str(con))
+        for shots in list(client.utils.chunks(files, 9)):
+            await client.send_file(event.chat_id, shots, caption=caption)
+        os.remove(file)
+        for file in files:
+            os.remove(file)
+        await event.delete()
+    else:
+        if int(data) > duration:
+            data = duration
+        await event.edit(client.get_string("VideoShot_4").format(data))
+        out = f"Shot-{data}.jpg"
+        cmd = f"ffmpeg -i {file} -ss {int(data)} -vframes 1 {out}"
+        await client.utils.runcmd(cmd)
+        caption = client.get_string("VideoShot_5").format(str(data))
+        await client.send_file(event.chat_id, out, caption=caption)
+        await event.delete()
