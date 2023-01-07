ba4bbf074e38a28851704e55a70eaf523d13de2e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-07 13:15:38 +0330

    Create YtDl.py

---
+++ FidoSelf/plugins/YtDl.py
@@ -0,0 +1,31 @@
+from FidoSelf import client
+from FidoSelf.functions.async import run_async
+from yt_dlp import YoutubeDL
+
+@run_async
+async def del(url, opts):
+    return YoutubeDL(opts).download([url])
+
+@client.Cmd(pattern="\.dl (.*)")
+async def ytdltest(event):
+    await event.edit(".....!")
+    OPTS = {
+    "format": "bestaudio",
+    "addmetadata": True,
+    "key": "FFmpegMetadata",
+    "prefer_ffmpeg": True,
+    "geo_bypass": True,
+    "outtmpl": f"%(id)s.mp3",
+    "logtostderr": False,
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "best",
+        },
+        {"key": "FFmpegMetadata"},
+     ],
+    }
+    url = event.pattern_match.group(1)
+    await del(url, OPTS)
+    await event.edit("Ok!")
