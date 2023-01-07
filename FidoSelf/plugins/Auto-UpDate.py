9a4c4826c4938be784edd9343f41de6946b7e6f6
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-07 11:24:33 +0330

    Update Auto-UpDate.py

--- FidoSelf/plugins/Auto-UpDate.py
+++ FidoSelf/plugins/Auto-UpDate.py
@@ -6,8 +6,8 @@ import random
 async def autoupdater():
     git = Git()
     file = "FidoSelf/plugins/__Updater__.py"
-    rand = random.randint(11111, 99999)
-    text = f"# Auto Updater For Your Self!\n\n# Random Number Is: {str(rand)}\n\nGood Luck!!"
+    rand = random.randint(11111111, 99999999)
+    text = f"# Auto Updater For Your Self!\n\n# Random Number Is: {str(rand)}\n\n# Good Luck!!"
     open(file, "w").write(text)
     git.create(file, file)
     git.update(file, file)
