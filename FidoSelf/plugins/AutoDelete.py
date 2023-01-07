04824087518d0f148a58db01f505ac5515ed4686
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-07 22:16:02 +0330

    Update AutoDelete.py

--- FidoSelf/plugins/AutoDelete.py
+++ FidoSelf/plugins/AutoDelete.py
@@ -3,25 +3,25 @@ import asyncio
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}AutoDelete (On|Off)$")
 async def autodelete(event):
-    await event.edit(f"**{client.str} Processing . . .**")
+    await event.edit(client.get_string("Wait"))
     mode = event.pattern_match.group(1).lower()
     client.DB.set_key("AUTO_DELETE_MODE", mode)
-    change = "Actived" if mode == "on" else "DeActived"
-    await event.edit(f"**{client.str} The Auto Delete Messages Mode Has Been {change}!**")
+    change = client.get_string("Change_1") if mode == "on" else client.get_string("Change_2")
+    await event.edit(client.get_string("AutoDelete_1").format(change))
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}SetAutoDeleteSleep (\d*)$")
 async def setautodeletesleep(event):
-    await event.edit(f"**{client.str} Processing . . .**")
+    await event.edit(client.get_string("Wait"))
     sleep = event.pattern_match.group(1)
     sleep = int(sleep) * 60
     client.DB.set_key("AUTO_DELETE_SLEEP", sleep)
-    await event.edit(f"**{client.str} The Auto Delete Messages Sleep Has Been Set To {client.utils.convert_time(int(sleep))}!**")
+    await event.edit(client.get_string("AutoDelete_2").format(client.utils.convert_time(sleep)))
 
 @client.Cmd()
 async def autodeletes(event):
     mode = client.DB.get_key("AUTO_DELETE_MODE")
     if mode == "on":
-        sleep = client.DB.get_key("AUTO_DELETE_SLEEP") or 5
+        sleep = client.DB.get_key("AUTO_DELETE_SLEEP") or 300
         client.loop.create_task(delete(event, sleep))
 
 async def delete(event, sleep):
