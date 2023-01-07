4b850879de2708e389d3b70589d81ff88d3ed15e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-07 23:40:11 +0330

    Update EditPhoto.py

--- FidoSelf/plugins/EditPhoto.py
+++ FidoSelf/plugins/EditPhoto.py
@@ -3,27 +3,32 @@ from PIL import Image
 from PIL.ImageFilter import BLUR, CONTOUR, DETAIL, EDGE_ENHANCE_MORE, EMBOSS, SMOOTH_MORE, SHARPEN
 import os
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}S(Blur|Contour|Detail|Emboss|Edge|Smooth|Sharpen)$")
+@client.Cmd(pattern=f"(?i)^\{client.cmd}S(Bw|Blur|Contour|Detail|Emboss|Edge|Smooth|Sharpen)$")
 async def editphoto(event):
     await event.edit(client.get_string("Wait"))
-    modes = {
-        "Blur": BLUR,
-        "Contour": CONTOUR,
-        "Detail": DETAIL,
-        "Emboss": EMBOSS,
-        "Edge": EDGE_ENHANCE_MORE,
-        "Smooth": SMOOTH_MORE,
-        "Sharpen": SHARPEN,
-    }
-    mode = event.pattern_match.group(1).title()
-    mode = modes[mode]
     if not event.is_reply or not event.reply_message.photo:
         return await event.edit(client.get_string("Reply_P"))
     photo = await event.reply_message.download_media()
+    mode = event.pattern_match.group(1).title()
     newfile = f"EditImage-{str(mode)}.jpg"
-    img = Image.open(photo)
-    newimg = img.filter(mode)
-    newimg.save(newfile)
+    if mode == "Bw":
+        img = Image.open(photo)
+        newimg = img.convert("1")
+        newimg.save(newfile)
+    else:
+        modes = {
+            "Blur": BLUR,
+            "Contour": CONTOUR,
+            "Detail": DETAIL,
+            "Emboss": EMBOSS,
+            "Edge": EDGE_ENHANCE_MORE,
+            "Smooth": SMOOTH_MORE,
+            "Sharpen": SHARPEN,
+        }
+        mode = modes[mode]
+        img = Image.open(photo)
+        newimg = img.filter(mode)
+        newimg.save(newfile)
     await event.respond(file=newfile)        
     os.remove(photo)
     os.remove(newfile)
