8066e3a81bd75f14799efc89402e02c07249f6ae
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-08 15:14:28 +0330

    Create AudioConverter.py

---
+++ FidoSelf/plugins/AudioConverter.py
@@ -0,0 +1,23 @@
+from FidoSelf import client
+from telethon import types
+import time
+import os
+
+@client.Cmd(pattern=f"(?i)^\{client.cmd}S(Audio|Voice)$")
+async def audioconverter(event):
+    await event.edit(client.get_string("Wait"))
+    mode = event.pattern_match.group(1).title()
+    if not event.is_reply or not (event.reply_message.audio and event.reply_message.voice):
+        return await event.edit(client.get_string("Reply_AV"))
+    if event.reply_message.file.size > client.MAX_SIZE:
+        return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
+    newtime = time.time()
+    file_name = event.reply_message.file.name or "---"
+    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down", file_name))
+    audio = await event.reply_message.download_media(progress_callback=callback)
+    newtime = time.time()
+    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up", file_name))
+    voice == False if mode == "Audio" else True
+    await client.send_file(event.chat_id, video, attributes=attributes, progress_callback=callback, voice_note=voice)
+    os.remove(audio)
+    await event.delete()
