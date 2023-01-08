9435fef80b517227729f2ba9c3f523e6662ce178
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-08 20:47:05 +0330

    Update FloodText.py

--- FidoSelf/plugins/FloodText.py
+++ FidoSelf/plugins/FloodText.py
@@ -2,8 +2,8 @@ from FidoSelf import client
 import random
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}Flood \'(\d*)\' ?([\s\S]*)?")
-async def Flood(event):
-    count = event.pattern_match.group(1)
+async def flood(event):
+    count = int(event.pattern_match.group(1))
     texts = event.pattern_match.group(2)
     if not texts and not event.is_reply:
         texts = "1,2,3,4,5,6,7,8,9"
