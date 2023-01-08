7cd392b006275cd04d4d23b80125f2e6d42fc2b8
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-08 14:39:03 +0330

    Update VideoConverter.py

--- FidoSelf/plugins/VideoConverter.py
+++ FidoSelf/plugins/VideoConverter.py
@@ -3,9 +3,10 @@ from telethon import types
 import time
 import os
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}SVNote$")
+@client.Cmd(pattern=f"(?i)^\{client.cmd}SV(Note|Normal)$")
 async def videoshot(event):
     await event.edit(client.get_string("Wait"))
+    mode = event.pattern_match.group(1).title()
     if not event.is_reply or not event.reply_message.video:
         return await event.edit(client.get_string("Reply_V"))
     if event.reply_message.file.size > client.MAX_SIZE:
@@ -14,8 +15,10 @@ async def videoshot(event):
     file_name = event.reply_message.file.name or "---"
     callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down", file_name))
     video = await event.reply_message.download_media(progress_callback=callback)
-    duration = event.reply_message.video.attributes[0].duration
-    attributes = [types.DocumentAttributeVideo(duration=event.reply_message.file.duration, w=event.reply_message.file.width, h=event.reply_message.file.height, round_message=True)]
+    if mode == "Normal":
+        attributes = [types.DocumentAttributeVideo(duration=event.reply_message.file.duration, w=event.reply_message.file.width, h=event.reply_message.file.height, round_message=False, supports_streaming=True)]
+    elif mode == "Note":
+        attributes = [types.DocumentAttributeVideo(duration=event.reply_message.file.duration, w=event.reply_message.file.width, h=event.reply_message.file.height, round_message=True)]
     newtime = time.time()
     callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up", file_name))
     await client.send_file(event.chat_id, video, attributes=attributes, progress_callback=callback)
