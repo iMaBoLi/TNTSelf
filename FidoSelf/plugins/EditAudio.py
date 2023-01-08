21ad9f829ef2646ac57a361086a8f1f5d4e35e88
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-08 12:03:59 +0330

    Create EditAudio.py

---
+++ FidoSelf/plugins/EditAudio.py
@@ -0,0 +1,49 @@
+from FidoSelf import client
+from telethon import types
+import os
+import time
+
+@client.Cmd(pattern=f"(?i)^\{client.cmd}SDur (\d*)$")
+async def setduration(event):
+    await event.edit(client.get_string("Wait"))
+    dur = int(event.pattern_match.group(1))
+    if dur >= 2147483647:
+        dur = 2147483647
+    if not event.is_reply or not event.reply_message.audio:
+        return await event.edit(client.get_string("Reply_A"))
+    if event.reply_message.file.size > client.MAX_SIZE:
+        return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
+    newtime = time.time()
+    file_name = event.reply_message.file.name or "---"
+    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down", file_name))
+    audio = await event.reply_message.download_media(progress_callback=callback)
+    await event.edit(client.get_string("EditAudio_1").format(dur))
+    attributes = [types.DocumentAttributeAudio(duration=dur)] 
+    newtime = time.time()
+    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up"))
+    caption = client.get_string("EditAudio_2").format(dur)
+    await client.send_file(event.chat_id, audio, caption=caption, progress_callback=callback, attributes=attributes)        
+    os.remove(audio)
+    await event.delete()
+
+@client.Cmd(pattern=f"(?i)^\{client.cmd}SEAudio (.*)\:(.*)$")
+async def editaudio(event):
+    await event.edit(client.get_string("Wait"))
+    title = str(event.pattern_match.group(1))
+    performer = str(event.pattern_match.group(2))
+    if not event.is_reply or not event.reply_message.audio:
+        return await event.edit(client.get_string("Reply_A"))
+    if event.reply_message.file.size > client.MAX_SIZE:
+        return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
+    newtime = time.time()
+    file_name = event.reply_message.file.name or "---"
+    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down", file_name))
+    audio = await event.reply_message.download_media(progress_callback=callback)
+    await event.edit(client.get_string("EditAudio_3").format(title, performer))
+    attributes = [types.DocumentAttributeAudio(title=title, performer=performer)] 
+    newtime = time.time()
+    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up"))
+    caption = client.get_string("EditAudio_4").format(title, performer)
+    await client.send_file(event.chat_id, audio, caption=caption, progress_callback=callback, attributes=attributes)        
+    os.remove(audio)
+    await event.delete()
