32cddee71bc9271f06a25a8d16d86a66fcb080c5
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-09 00:31:20 +0330

    Update __init__.py

--- FidoSelf/__init__.py
+++ FidoSelf/__init__.py
@@ -1,17 +1,19 @@
 from telethon import TelegramClient
 from telethon.sessions import StringSession
+from logging import INFO, getLogger, basicConfig, FileHandler, StreamHandler
 from FidoSelf import config
 from FidoSelf.database import DB
 import time
 
 START_TIME = time.time()
+__version__ = "0.7.3"
 
 client = TelegramClient(
     session=StringSession(str(config.SESSION)),
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     device_model="FidoSelf",
-    app_version="0.4.1",
+    app_version=__version__,
 ).start()
 
 client.bot = TelegramClient(
@@ -19,3 +21,13 @@ client.bot = TelegramClient(
     api_id=config.API_ID,
     api_hash=config.API_HASH,
 ).start()
+
+client.LOGS = getLogger("FidoLogs")
+
+file = "Fido.log"
+basicConfig(
+    format="%(asctime)s | %(name)s [%(levelname)s] : %(message)s",
+    level=INFO,
+    datefmt="%m/%d/%Y - %H:%M:%S",
+    handlers=[FileHandler(file), StreamHandler()],
+)
