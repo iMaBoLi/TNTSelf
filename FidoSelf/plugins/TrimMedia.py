b9d15a025137546a2ad90e1817bc0ec154424c20
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-09 23:07:31 +0330

    Update TrimMedia.py

--- FidoSelf/plugins/TrimMedia.py
+++ FidoSelf/plugins/TrimMedia.py
@@ -1,4 +1,5 @@
 from FidoSelf import client
+from moviepy.editor import VideoFileClip
 import os
 import time
 
@@ -12,24 +13,37 @@ async def trimmedia(event):
         medias = client.get_string("ReplyMedia")
         media = medias["Video"] + " - " + medias["Music"]
         rtype = medias[mtype]
+        if mtype == "Empty":
+            return await event.edit(client.get_string("ReplyMedia_Not").format(media))
         return await event.edit(client.get_string("ReplyMedia_Main").format(rtype, media))
     if event.reply_message.file.size > client.MAX_SIZE:
         return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
     newtime = time.time()
     callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down"))
-    audio = await event.reply_message.download_media(progress_callback=callback)
+    file = await event.reply_message.download_media(progress_callback=callback)
     if ee > event.reply_message.file.duration:
         ee = event.reply_message.file.duration
     if ss >= ee:
         ss = ee - 30
-    await event.edit(client.get_string("TrimAudio_1").format(ss, ee))
-    newfile = f"TrimedAudio-{saudio}-{eaudio}.mp3"
-    cmd = f'ffmpeg -i "{audio}" -preset ultrafast -ss {ss} -to {ee} -vn -acodec copy "{newfile}" -y'
-    await client.utils.runcmd(cmd)
-    newtime = time.time()
-    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up"))
-    caption = client.get_string("TrimAudio_2").format(saudio, eaudio)
-    await client.send_file(event.chat_id, newfile, caption=caption, progress_callback=callback)        
-    os.remove(audio)
-    os.remove(newfile)
+    if mtype == "Video":
+        await event.edit(client.get_string("TrimMedia_1").format(ss, ee))
+        newfile = f"TrimedVideo-{ss}-{ee}.mp4"
+        clip = VideoFileClip(file).cutout(ss, ee)
+        clip.write_videofile(newfile)
+        newtime = time.time()
+        callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up"))
+        caption = client.get_string("TrimMedia_2").format(saudio, eaudio)
+        await client.send_file(event.chat_id, newfile, caption=caption, progress_callback=callback)        
+        os.remove(newfile)
+    elif mtype == "Music":
+        await event.edit(client.get_string("TrimMedia_3").format(ss, ee))
+        newfile = f"TrimedAudio-{ss}-{ee}.mp3"
+        cmd = f'ffmpeg -i "{file}" -preset ultrafast -ss {ss} -to {ee} -vn -acodec copy "{newfile}" -y'
+        await client.utils.runcmd(cmd)
+        newtime = time.time()
+        callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up"))
+        caption = client.get_string("TrimAMedia_4").format(saudio, eaudio)
+        await client.send_file(event.chat_id, newfile, caption=caption, progress_callback=callback)        
+        os.remove(newfile)
+    os.remove(file)
     await event.delete()
