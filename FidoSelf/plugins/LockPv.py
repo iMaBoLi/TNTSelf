814a88cf3b868a95a35d2cab6434456cee47603e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-09 10:23:31 +0330

    Create LockPv.py

---
+++ FidoSelf/plugins/LockPv.py
@@ -0,0 +1,17 @@
+from FidoSelf import client
+from telethon import functions
+
+@client.Cmd(pattern=f"(?i)^\{client.cmd}LockPv (On|Off)$")
+async def lockpv(event):
+    await event.edit(client.get_string("Wait"))
+    mode = event.pattern_match.group(1).lower()
+    client.DB.set_key("LOCK_PV", mode)
+    change = client.get_string("Change_1") if mode == "on" else client.get_string("Change_2")
+    await event.edit(client.get_string("LockPv_1").format(change))
+
+@client.Cmd(sudo=False, edits=False)
+async def locker(event):
+    if not event.is_private or event.is_white: return
+    mode = client.DB.get_key("LOCK_PV") or "off"
+    if mode == "on":
+        await client(functions.contacts.BlockRequest(event.sender_id))
