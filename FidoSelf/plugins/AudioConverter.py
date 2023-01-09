a99e7601498c9916fe029c9f9ee5c6d365351dd7
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-09 23:21:50 +0330

    Update AudioConverter.py

--- FidoSelf/plugins/AudioConverter.py
+++ FidoSelf/plugins/AudioConverter.py
@@ -12,6 +12,8 @@ async def audioconverter(event):
         medias = client.get_string("ReplyMedia")
         media = medias["Music"] + " - " + medias["Voice"]
         rtype = medias[mtype]
+        if mtype == "Empty":
+            return await event.edit(client.get_string("ReplyMedia_Not").format(media))
         return await event.edit(client.get_string("ReplyMedia_Main").format(rtype, media))
     if event.reply_message.file.size > client.MAX_SIZE:
         return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
@@ -19,10 +21,11 @@ async def audioconverter(event):
         newtime = time.time()
         file_name = event.reply_message.file.name or "---"
         callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down", file_name))
-        audio = await event.reply_message.download_media(progress_callback=callback)
+        voice = await event.reply_message.download_media(progress_callback=callback)
         newtime = time.time()
         callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up", file_name))
-        await client.send_file(event.chat_id, audio, progress_callback=callback, voice_note=False)
+        await client.send_file(event.chat_id, voice, progress_callback=callback, voice_note=False)
+        os.remove(voice)
     elif mode == "Voice" and mtype in ["Music"]:
         newtime = time.time()
         file_name = event.reply_message.file.name or "---"
@@ -31,10 +34,10 @@ async def audioconverter(event):
         newtime = time.time()
         callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up", file_name))
         await client.send_file(event.chat_id, audio, progress_callback=callback, voice_note=True)
+        os.remove(audio)
     else:
         medias = client.get_string("ReplyMedia")
         media = medias["Voice"] if mode == "Music" else medias["Music"]
         rtype = medias[mtype]
         await event.edit(client.get_string("ReplyMedia_Main").format(rtype, media))
-    os.remove(audio)
     await event.delete()
