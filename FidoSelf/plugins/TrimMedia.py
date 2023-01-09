051a152131b17ac57324db73c46419703bee9d50
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-09 21:28:35 +0330

    Update and rename TrimAudio.py to TrimMedia.py

--- FidoSelf/plugins/TrimMedia.py
+++ FidoSelf/plugins/TrimMedia.py
@@ -3,29 +3,28 @@ import os
 import time
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}STrim (\d*)\-(\d*)$")
-async def trimaudio(event):
+async def trimmedia(event):
     await event.edit(client.get_string("Wait"))
-    saudio = int(event.pattern_match.group(1))
-    eaudio = int(event.pattern_match.group(2))
+    ss = int(event.pattern_match.group(1))
+    ee = int(event.pattern_match.group(2))
     mtype = client.mediatype(event.reply_message)
-    if not event.is_reply or mtype not in ["Music"]:
+    if not event.is_reply or mtype not in ["Video", "Music"]:
         medias = client.get_string("ReplyMedia")
-        media = medias["Music"]
+        media = medias["Video"] + " - " + medias["Music"]
         rtype = medias[mtype]
         return await event.edit(client.get_string("ReplyMedia_Main").format(rtype, media))
     if event.reply_message.file.size > client.MAX_SIZE:
         return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
     newtime = time.time()
-    file_name = event.reply_message.file.name or "---"
-    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down", file_name))
+    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down"))
     audio = await event.reply_message.download_media(progress_callback=callback)
-    if eaudio > event.reply_message.file.duration:
-        eaudio = event.reply_message.file.duration
-    if saudio >= eaudio:
-        saudio = eaudio - 30
-    await event.edit(client.get_string("TrimAudio_1").format(saudio, eaudio))
+    if ee > event.reply_message.file.duration:
+        ee = event.reply_message.file.duration
+    if ss >= ee:
+        ss = ee - 30
+    await event.edit(client.get_string("TrimAudio_1").format(ss, ee))
     newfile = f"TrimedAudio-{saudio}-{eaudio}.mp3"
-    cmd = f'ffmpeg -i "{audio}" -preset ultrafast -ss {saudio} -to {eaudio} -vn -acodec copy "{newfile}" -y'
+    cmd = f'ffmpeg -i "{audio}" -preset ultrafast -ss {ss} -to {ee} -vn -acodec copy "{newfile}" -y'
     await client.utils.runcmd(cmd)
     newtime = time.time()
     callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up"))
