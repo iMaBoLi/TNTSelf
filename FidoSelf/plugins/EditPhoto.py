482dc6b172c5b4f1d2a0285603c1d17b25bac2fc
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-09 21:10:23 +0330

    Update EditPhoto.py

--- FidoSelf/plugins/EditPhoto.py
+++ FidoSelf/plugins/EditPhoto.py
@@ -6,11 +6,15 @@ import os
 @client.Cmd(pattern=f"(?i)^\{client.cmd}S(Bw|Blur|Contour|Detail|Emboss|Edge|Smooth|Sharpen)$")
 async def editphoto(event):
     await event.edit(client.get_string("Wait"))
-    if not event.is_reply or not event.reply_message.photo:
-        return await event.edit(client.get_string("Reply_P"))
-    photo = await event.reply_message.download_media()
     mode = event.pattern_match.group(1).title()
-    newfile = f"EditImage-{str(mode)}.jpg"
+    mtype = client.mediatype(event.reply_message)
+    if not event.is_reply or mtype not in ["Photo"]:
+        medias = client.get_string("ReplyMedia")
+        media = medias["Photo"]
+        rtype = medias[mtype]
+        return await event.edit(client.get_string("ReplyMedia_Main").format(rtype, media))
+    photo = await event.reply_message.download_media()
+    newfile = f"EditPhoto-{str(mode)}.jpg"
     if mode == "Bw":
         img = Image.open(photo)
         newimg = img.convert("1")
@@ -29,7 +33,9 @@ async def editphoto(event):
         img = Image.open(photo)
         newimg = img.filter(mode)
         newimg.save(newfile)
-    await event.respond(file=newfile)        
+    mode = "BlackWhite" if mode == "Bw" else mode
+    caption = client.get_string("EditPhoto_1").format(mode)
+    await client.send_file(event.chat_id, newfile, caption=caption)        
     os.remove(photo)
     os.remove(newfile)
     await event.delete()
