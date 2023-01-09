6171f3838f8c6ee6595a4d4174d0c0350e95d6c4
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-09 16:39:53 +0330

    Update logs.py

--- FidoSelf/plugins/logs.py
+++ FidoSelf/plugins/logs.py
@@ -1,17 +1,17 @@
 from FidoSelf import client
 import os
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}log (m|c|i)$")
+@client.Cmd(pattern=f"(?i)^\{client.cmd}Logs \-(c|g)$")
 async def logs(event):
     type = str(event.pattern_match.group(1))
-    if type == "m" and os.path.exists("CmdError.log"):
-        await event.respond(f"**{client.str} Log File!**", file="CmdError.log")
+    if type == "c" and os.path.exists("Fido.log"):
+        await event.respond(f"**{client.str} The Console Logs File!**", file="Fido.log")
         await event.delete()
-    elif type == "c" and os.path.exists("CallbackError.log"):
-        await event.respond(f"**{client.str} Log File!**", file="CallbackError.log")
-        await event.delete()
-    elif type == "i" and os.path.exists("InlineError.log"):
-        await event.respond(f"**{client.str} Log File!**", file="InlineError.log")
+    elif type == "g":
+        git = await client.utils.runcmd('git log --pretty=format:"[%an]: %s" -20')
+        open("Github.log", "w").write(str(git[0]))
+        await event.respond(f"**{client.str} The Github Commits Logs File!**", file="Github.log")
+        os.remove("Github.log")
         await event.delete()
     else:
         await event.edit(f"**{client.str} The Log File Is Not Available!**")
