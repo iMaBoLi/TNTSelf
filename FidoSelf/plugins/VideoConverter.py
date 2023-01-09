231ff3c497030b2698322625e7ee10a9f7643e78
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-09 18:17:16 +0330

    Update VideoConverter.py

--- FidoSelf/plugins/VideoConverter.py
+++ FidoSelf/plugins/VideoConverter.py
@@ -7,11 +7,7 @@ import os
 async def videoconverter(event):
     await event.edit(client.get_string("Wait"))
     mode = event.pattern_match.group(1).title()
-    if not event.is_reply or mtype not in ["Video", "Gif", "VideoNote"]:
-        medias = client.get_string("ReplyMedia")
-        media = medias["Video"] + " - " + medias["Gif"] + " - " + medias["VideoNote"]
-        rtype = medias[mtype]
-        return await event.edit(client.get_string("ReplyMedia_Main").format(rtype, media))
+    mtype = client.mediatype(event.reply_message)
     if event.reply_message.file.size > client.MAX_SIZE:
         return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
     newtime = time.time()
@@ -20,16 +16,26 @@ async def videoconverter(event):
     video = await event.reply_message.download_media(progress_callback=callback)
     newtime = time.time()
     callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up"))
-    if mode == "Normal":
+    if mode == "Normal" and mtype in ["VideoNote", "Gif"]:
         attributes = [types.DocumentAttributeVideo(duration=event.reply_message.file.duration, w=event.reply_message.file.width, h=event.reply_message.file.height, round_message=False, supports_streaming=True)]
         await client.send_file(event.chat_id, video, attributes=attributes, progress_callback=callback)
-    elif mode == "Note":
+    elif mode == "Note" and mtype in ["Video", "Gif"]:
         attributes = [types.DocumentAttributeVideo(duration=event.reply_message.file.duration, w=event.reply_message.file.width, h=event.reply_message.file.height, round_message=True)]
         await client.send_file(event.chat_id, video, attributes=attributes, progress_callback=callback)
-    elif mode == "Gif":
+    elif mode == "Gif" and mtype in ["VideoNote", "Video"]:
         newfile = video + ".gif"
         os.rename(video, newfile)
         await client.send_file(event.chat_id, newfile, progress_callback=callback)
         os.remove(newfile)
+    else:
+        medias = client.get_string("ReplyMedia")
+        rtype = medias[mtype]
+        if mode == "Normal":
+            media = medias["Gif"] + " - " + medias["VideoNote"]
+        elif mode == "Note":
+            media = medias["Video"] + " - " + medias["Gif"]
+        elif mode == "Gif":
+            media = medias["Video"] + " - " + medias["VideoNote"]
+        return await event.edit(client.get_string("ReplyMedia_Main").format(rtype, media))
     os.remove(video)
     await event.delete()
