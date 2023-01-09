a222741161cb05b45195da5e17e9c5abaf1da6e6
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-09 17:40:02 +0330

    Update ImageSlicer.py

--- FidoSelf/plugins/ImageSlicer.py
+++ FidoSelf/plugins/ImageSlicer.py
@@ -6,15 +6,18 @@ import os
 async def sliceimage(event):
     await event.edit(client.get_string("Wait"))
     tile = event.pattern_match.group(1)
-    if event.is_reply and event.reply_message.photo:
-        photo = await event.reply_message.download_media("SliceImage.jpg")
-        tiles = image_slicer.slice(photo, int(tile))
-        photos = [str(tile).split(" - ")[1].replace(">", "") for tile in tiles]
-        for phs in list(client.utils.chunks(photos, 9)):
-            await event.respond(client.get_string("ImageSlicer_1").format(len(photos)), file=phs)
-        await event.delete()
-        os.remove(photo)
-        for ph in photos:
-            os.remove(ph)
-    else:
-        await event.edit(client.get_string("Reply_P"))
+    mtype = client.mediatype(event.reply_message)
+    if not event.is_reply or mtype not in ["Photo"]:
+        medias = client.get_string("ReplyMedia")
+        media = medias["Photo"]
+        rtype = medias[mtype]
+        return await event.edit(client.get_string("ReplyMedia_Main").format(rtype, media))
+    photo = await event.reply_message.download_media("SliceImage.jpg")
+    tiles = image_slicer.slice(photo, int(tile))
+    photos = [str(tile).split(" - ")[1].replace(">", "") for tile in tiles]
+    for phs in list(client.utils.chunks(photos, 9)):
+        await event.respond(client.get_string("ImageSlicer_1").format(len(photos)), file=phs)
+    os.remove(photo)
+    for ph in photos:
+        os.remove(ph)
+    await event.delete()
