d0deebe5e8a84851e57d029ddc2cd0391cd3b4e6
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-10 13:56:51 +0330

    Update and rename VideoConverter.py to VideoConvert.py

--- FidoSelf/plugins/VideoConvert.py
+++ FidoSelf/plugins/VideoConvert.py
@@ -4,43 +4,51 @@ import time
 import os
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}SV(Note|Normal|Gif)$")
-async def videoconverter(event):
+async def videoconvert(event):
     await event.edit(client.get_string("Wait"))
     mode = event.pattern_match.group(1).title()
     mtype = client.mediatype(event.reply_message)
-    if not event.is_reply or not mtype in ["Video", "Gif", "VideoNote"]:
-        medias = client.get_string("ReplyMedia")
-        rtype = medias[mtype]
-        media = medias["Video"] + " - " + medias["Gif"] + " - " + medias["VideoNote"]
-        return await event.edit(client.get_string("ReplyMedia_Main").format(rtype, media))
-    if event.reply_message.file.size > client.MAX_SIZE:
-        return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
-    newtime = time.time()
-    file_name = event.reply_message.file.name or "---"
-    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down", file_name))
-    video = await event.reply_message.download_media(progress_callback=callback)
-    newtime = time.time()
-    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up"))
     if mode == "Normal" and mtype in ["VideoNote", "Gif"]:
+        if event.reply_message.file.size > client.MAX_SIZE:
+            return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
+        callback = event.progress(download=True)
+        video = await event.reply_message.download_media(progress_callback=callback)
         attributes = [types.DocumentAttributeVideo(duration=event.reply_message.file.duration, w=event.reply_message.file.width, h=event.reply_message.file.height, round_message=False, supports_streaming=True)]
+        callback = event.progress(upload=True)
         await client.send_file(event.chat_id, video, attributes=attributes, progress_callback=callback)
+        os.remove(video)
+        await event.delete()
     elif mode == "Note" and mtype in ["Video", "Gif"]:
+        if event.reply_message.file.size > client.MAX_SIZE:
+            return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
+        callback = event.progress(download=True)
+        video = await event.reply_message.download_media(progress_callback=callback)
         attributes = [types.DocumentAttributeVideo(duration=event.reply_message.file.duration, w=event.reply_message.file.width, h=event.reply_message.file.height, round_message=True)]
+        callback = event.progress(upload=True)
         await client.send_file(event.chat_id, video, attributes=attributes, progress_callback=callback)
+        os.remove(video)
+        await event.delete()
     elif mode == "Gif" and mtype in ["VideoNote", "Video"]:
+        if event.reply_message.file.size > client.MAX_SIZE:
+            return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
+        callback = event.progress(download=True)
+        video = await event.reply_message.download_media(progress_callback=callback)
         newfile = video + ".gif"
         os.rename(video, newfile)
+        callback = event.progress(upload=True)
         await client.send_file(event.chat_id, newfile, progress_callback=callback)
         os.remove(newfile)
+        os.remove(video)
+        await event.delete()
     else:
         medias = client.get_string("ReplyMedia")
-        rtype = medias[mtype]
         if mode == "Normal":
             media = medias["Gif"] + " - " + medias["VideoNote"]
         elif mode == "Note":
             media = medias["Video"] + " - " + medias["Gif"]
         elif mode == "Gif":
             media = medias["Video"] + " - " + medias["VideoNote"]
-        return await event.edit(client.get_string("ReplyMedia_Main").format(rtype, media))
-    os.remove(video)
-    await event.delete()
+        if mtype == "Empty":
+            return await event.edit(client.get_string("ReplyMedia_Not").format(media))
+        else:      
+            return await event.edit(client.get_string("ReplyMedia_Main").format(medias[mtype], media))
