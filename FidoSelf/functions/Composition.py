54600195542902c85eabc01f1f65f964f543633a
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-11 01:02:16 +0330

    Update Composition.py

--- FidoSelf/functions/Composition.py
+++ FidoSelf/functions/Composition.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from math import floor
 
 class Composition:
     def __init__(
@@ -16,7 +17,7 @@ class Composition:
     def create(self, outfile):
         img = Image.new(self.mode, (self.size + 10, self.size + 10), self.background)
         sizes = self.get_sizes()
-        count = 1
+        count = 0
         for photo in self.photos:
             image = Image.open(photo)
             size = (round(sizes[count]["size"][0]), round(sizes[count]["size"][1]))
@@ -28,58 +29,66 @@ class Composition:
         return outfile
 
     def get_sizes(self):
+        rows = round(self.tiles / 2)
+        lines = self.tiles - rows
+        SIZE = {}
+        for count in range(self.tiles):
+            
+            SIZE.update({count: {"size": [],"where": [,rows]}})
+        return SIZE
+
         SIZES = {
             1: {
                 1: {
-                    "size": [self.size, self.size],
+                    "size": [self.size, rows],
                     "where": [0, 0],
                 },
             },
             2: {
                 1: {
-                    "size": [self.size, self.size / 2],
+                    "size": [self.size, rows],
                     "where": [0, 0],
                 },
                 2: {
-                    "size": [self.size, self.size / 2],
+                    "size": [self.size, rows],
                     "where": [0, self.size / 2],
                 },
             },
             3: {
                 1: {
-                    "size": [self.size / 2, self.size / 2],
+                    "size": [self.size / 2, rows],
                     "where": [0, 0],
                 },
                 2: {
-                    "size": [self.size / 2, self.size / 2],
+                    "size": [self.size / 2, rows],
                     "where": [self.size / 2, 0],
                 },
                 3: {
-                    "size": [self.size, self.size / 2],
+                    "size": [self.size, rows],
                     "where": [0, self.size / 2],
                 },
             },
             4: {
                 1: {
-                    "size": [self.size / 2, self.size / 2],
+                    "size": [self.size / 2, rows],
                     "where": [0, 0],
                 },
                 2: {
-                    "size": [self.size / 2, self.size / 2],
+                    "size": [self.size / 2, rows],
                     "where": [self.size / 2, 0],
                 },
                 3: {
-                    "size": [self.size / 2, self.size / 2],
+                    "size": [self.size / 2, rows],
                     "where": [0, self.size / 2],
                 },
                 4: {
-                    "size": [self.size / 2, self.size / 2],
+                    "size": [self.size / 2, rows],
                     "where": [self.size / 2, self.size / 2],
                 },
             },
             5: {
                 1: {
-                    "size": [self.size / 3, self.size / 2],
+                    "size": [self.size / 3, rows],
                     "where": [0, 0],
                 },
                 2: {
@@ -121,6 +130,36 @@ class Composition:
                     "where": [self.size / 3, self.size / 2],
                 },
                 6: {
+                    "size": [self.size / 3, self.size / 2],
+                    "where": [self.size / 3 * 2, self.size / 2],
+                },
+            },
+            7: {
+                1: {
+                    "size": [self.size / 4, self.size / 2],
+                    "where": [0, 0],
+                },
+                2: {
+                    "size": [self.size / 4, self.size / 2],
+                    "where": [self.size / 4, 0],
+                },
+                3: {
+                    "size": [self.size / 4, self.size / 2],
+                    "where": [self.size / 4 * 2, 0],
+                },
+                4: {
+                    "size": [self.size / 4, self.size / 2],
+                    "where": [self.size / 4 * 3, 0],
+                },
+                5: {
+                    "size": [self.size / 3, self.size / 2],
+                    "where": [0, self.size / 2],
+                },
+                6: {
+                    "size": [self.size / 3, self.size / 2],
+                    "where": [self.size / 3, self.size / 2],
+                },
+                7: {
                     "size": [self.size / 3, self.size / 2],
                     "where": [self.size / 3 * 2, self.size / 2],
                 },
