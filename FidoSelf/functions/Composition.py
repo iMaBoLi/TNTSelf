a01a98ab8b598761bb9d9f41269e06cc5ce91f48
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-11 00:19:51 +0330

    Create Composition.py

---
+++ FidoSelf/functions/Composition.py
@@ -0,0 +1,104 @@
+from PIL import Image
+
+class Composition:
+    def __init__(
+        self,
+        photos,
+        background=(255, 255, 255),
+    ):
+
+        self.photos = photos
+        self.background = background
+        self.mode = "RGB"
+        self.size = 1024
+        self.tiles = len(self.photos)
+
+    def create(self, outfile):
+        img = Image.new(self.mode, (self.size, self.size), self.background)
+        if self.tiles > len(self.SIZES)
+        sizes = self.get_sizes()
+        count = 1
+        for photo in self.photos:
+            image = Image.open(photo)
+            size = (round(sizes[count]["size"][0]), round(sizes[count]["size"][1])
+            where = (round(sizes[count]["where"][0]), round(sizes[count]["where"][1])
+            newimage = image.resize(size)
+            img.paste(newimage, where)
+            count += 1
+        img.save(outfile)
+        return outfile
+
+    def get_sizes(self):
+        SIZES = {
+            1: {
+                1: {
+                    "size": [self.size, self.size],
+                    "where": [0, 0]},
+                },
+            },
+            2: {
+                1: {
+                    "size": [self.size, self.size / 2],
+                    "where": [0, 0],
+                },
+                2: {
+                    "size": [self.size, self.size / 2],
+                    "where": [0, self.size / 2],
+                },
+            },
+            3: {
+                1: {
+                    "size": [self.size / 2, self.size / 2],
+                    "where": [0, 0],
+                },
+                2: {
+                    "size": [self.size / 2, self.size / 2],
+                    "where": [self.size / 2, 0],
+                },
+                3: {
+                    "size": [self.size, self.size / 2],
+                    "where": [0, self.size / 2],
+                },
+            },
+            4: {
+                1: {
+                    "size": [self.size / 2, self.size / 2],
+                    "where": [0, 0],
+                },
+                2: {
+                    "size": [self.size / 2, self.size / 2],
+                    "where": [self.size / 2, 0],
+                },
+                3: {
+                    "size": [self.size / 2, self.size / 2],
+                    "where": [0, self.size / 2],
+                },
+                4: {
+                    "size": [self.size / 2, self.size / 2],
+                    "where": [self.size / 2, self.size / 2],
+                },
+            },
+            5: {
+                1: {
+                    "size": [self.size / 3, self.size / 3],
+                    "where": [0, 0],
+                },
+                2: {
+                    "size": [self.size / 3, self.size / 3],
+                    "where": [self.size / 2, 0],
+                },
+                3: {
+                    "size": [self.size / 3, self.size / 3],
+                    "where": [self.size / 3, 0],
+                },
+                4: {
+                    "size": [self.size / 2, self.size / 2],
+                    "where": [self.size / 2, self.size / 2],
+                },
+                5: {
+                    "size": [self.size / 2, self.size / 2],
+                    "where": [self.size / 2, self.size / 2],
+                },
+            },
+        }
+        return self.SIZES[self.tiles]
