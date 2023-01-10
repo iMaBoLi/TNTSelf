43da94c9e1690b600faea7de9b757892ba8f9810
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-10 11:26:22 +0330

    Update and rename AudioConverter.py to AudioConvert.py

--- FidoSelf/plugins/AudioConvert.py
+++ FidoSelf/plugins/AudioConvert.py
@@ -6,16 +6,18 @@ import os
 async def audioconverter(event):
     await event.edit(client.get_string("Wait"))
     mode = event.pattern_match.group(1).title()
-    mtype = client.mediatype(event.reply_message)
-    if event.reply_message.file.size > client.MAX_SIZE:
-        return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
+    mtype = event.reply_message.mediatype()
     if mode == "Music" and mtype in ["Voice"]:
+        if event.reply_message.file.size > client.MAX_SIZE:
+            return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
         callback = event.progress("download")
         voice = await event.reply_message.download_media(progress_callback=callback)
         callback = event.progress("upload")
         await client.send_file(event.chat_id, voice, progress_callback=callback, voice_note=False)
         os.remove(voice)
     elif mode == "Voice" and mtype in ["Music"]:
+        if event.reply_message.file.size > client.MAX_SIZE:
+            return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
         callback = event.progress("download")
         audio = await event.reply_message.download_media(progress_callback=callback)
         callback = event.progress("upload")
@@ -24,9 +26,8 @@ async def audioconverter(event):
     else:
         medias = client.get_string("ReplyMedia")
         media = medias["Voice"] if mode == "Music" else medias["Music"]
-        rtype = medias[mtype]
         if mtype == "Empty":
             await event.edit(client.get_string("ReplyMedia_Not").format(media))
         else: 
-            await event.edit(client.get_string("ReplyMedia_Main").format(rtype, media))
+            await event.edit(client.get_string("ReplyMedia_Main").format(medias[mtype], media))
     await event.delete()
