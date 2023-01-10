c0582ec9606565f841a9f7da48eb14025e85e97e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-10 14:59:39 +0330

    Update utils.py

--- FidoSelf/functions/utils.py
+++ FidoSelf/functions/utils.py
@@ -8,9 +8,6 @@ import glob
 import random
 import os
 
-LOADED_PLUGS = []
-NOT_LOADED_PLUGS = {}
-
 def shuffle(list, count=5):
     for i in range(count):
         r = random.random()
@@ -37,18 +34,20 @@ def reverse(mylist):
     return result
 
 def load_plugins(folder):
+    plugs = []
+    notplugs = {}
     files = sorted(glob.glob(f"{folder}/*.py"))
     for file in files:
         try:
             filename = file.replace("/", ".").replace(".py" , "")
             load = import_module(filename)
-            LOADED_PLUGS.append(os.path.basename(file))
+            plugs.append(os.path.basename(file))
         except:
-            NOT_LOADED_PLUGS.update({os.path.basename(file): format_exc()})
+            notplugs.update({os.path.basename(file): format_exc()})
+    return plugs, notplugs
 
 def convert_bytes(size_bytes):
-   if size_bytes == 0:
-       return "0B"
+   if size_bytes == 0: return "0B"
    size_name = ("B", "KB", "MB", "GB", "TB")
    i = int(math.floor(math.log(size_bytes, 1024)))
    p = math.pow(1024, i)
@@ -56,15 +55,12 @@ def convert_bytes(size_bytes):
    return "%s%s" % (s, size_name[i])
 
 def convert_time(seconds):
-    if int(seconds) == 0:
-        return "0s"
+    if int(seconds) == 0: return "0s"
     minutes, seconds = divmod(int(seconds), 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
-    weeks, days = divmod(days, 7)
     result = (
-            ((str(weeks) + "w:") if weeks else "")
-            + ((str(days) + "d:") if days else "")
+            ((str(days) + "d:") if days else "")
             + ((str(hours) + "h:") if hours else "")
             + ((str(minutes) + "m:") if minutes else "")
             + ((str(seconds) + "s") if seconds else "")
