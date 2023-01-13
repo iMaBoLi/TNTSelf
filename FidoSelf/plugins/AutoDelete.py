d1e72cfb556cecbb3b47287b5c4c7bb6a3effd79
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-13 19:52:23 +0330

    Update AutoDelete.py

--- FidoSelf/plugins/AutoDelete.py
+++ FidoSelf/plugins/AutoDelete.py
@@ -33,3 +33,20 @@ async def trydelete(event):
             await event.delete()
         except:
             pass
+
+category = "Manager"
+filename = str(__file__).split("/")[-1].split(".")[0]
+note = "Delete Automatic Messages After A Time!"
+client.HELP.update({
+    "category": category,
+    "note": note,
+    "commands": {
+        "AutoDelete": {
+            "{CMD}AutoDelete On": "To Active Auto Delete Message",
+            "{CMD}AutoDelete Off": "To DeActive Auto Delete Message",
+        },
+        "AutoDeleteSleep": {
+            "{CMD}SetAutoDeleteSleep <Min>": "Set Auto Delete Message Sleep Time",
+        },
+    },
+})
