397ff01204f0287cbcc9001daedc3a8a9d92259f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-13 23:50:40 +0330

    Update AutoDelete.py

--- FidoSelf/plugins/AutoDelete.py
+++ FidoSelf/plugins/AutoDelete.py
@@ -1,14 +1,6 @@
 from FidoSelf import client
 import asyncio
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}AutoDelete (On|Off)$")
-async def autodelete(event):
-    await event.edit(client.get_string("Wait"))
-    mode = event.pattern_match.group(1).lower()
-    client.DB.set_key("AUTO_DELETE_MODE", mode)
-    change = client.get_string("Change_1") if mode == "on" else client.get_string("Change_2")
-    await event.edit(client.get_string("AutoDelete_1").format(change))
-
 @client.Cmd(pattern=f"(?i)^\{client.cmd}SetAutoDeleteSleep (\d*)$")
 async def setautodeletesleep(event):
     await event.edit(client.get_string("Wait"))
