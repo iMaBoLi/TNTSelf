dee3673db5c223f998828394ea1296b3fea2356f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-15 00:19:16 +0330

    Update MainHelp.py

--- FidoSelf/plugins/MainHelp.py
+++ FidoSelf/plugins/MainHelp.py
@@ -1,19 +1,18 @@
 from FidoSelf import client
 from telethon import Button
+from googletrans import Translator
 import math
 
-CATS = {
-    "Settings": "Settings ⚙️",
-    "Manager": "Manager 👮",
-    "Tools": "Tools 🔧",
-    "Account": "Account 💎",
-    "Groups": "Groups 👥",
-    "Time": "Time ⏰",
-}
+def translate(text):
+    if client.lang == "en":
+        return text
+    translator = Translator()
+    trjome = translator.translate(text, dest=client.lang)
+    return trjome.text
 
 def get_help_buttons():
     buttons = []
-    #CATS = client.get_string("Cats")
+    CATS = client.get_string("Categorys")
     for cat in CATS:
         buttons.append(Button.inline(f"• {CATS[cat]} •", data=f"gethelp:{cat}:1"))
     buttons = list(client.utils.chunks(buttons, 2))
@@ -27,6 +26,15 @@ def get_plugins(cat):
         if client.HELP[plugin]["category"] == cat:
             plugins.update({plugin: client.HELP[plugin]})
     return plugins
+
+def get_cmds_count(cat=None):
+    plugins = client.HELP if not cat else get_plugins(cat)
+    count = 0
+    for plugin in plugins:
+        for command in client.HELP[plugin]["commands"]:
+            for com in client.HELP[plugin]["commands"][command]:
+                count += 1
+    return count
 
 def get_pages_button(cat, page):
     buttons = []
@@ -67,7 +75,8 @@ async def help(event):
 
 @client.Inline(pattern="selfmainhelp")
 async def inlinehelp(event):
-    text = "Ok"
+    CATS = client.get_string("Categorys")
+    text = client.get_string("Help_1").format(len(CATS), get_cmds_count())
     buttons = get_help_buttons()
     await event.answer([event.builder.article(f"{client.str} FidoSelf - Help", text=text, buttons=buttons)])
 
@@ -75,27 +84,29 @@ async def inlinehelp(event):
 async def gethelp(event):
     cat = str(event.data_match.group(1).decode('utf-8'))
     page = int(event.data_match.group(2).decode('utf-8'))
-    #text = client.get_string("Help_2").format(CATS[cat])
     buttons = get_cat_buttons(cat, page)
-    await event.edit(buttons=buttons)
+    text = client.get_string("Help_2").format(count, get_cmds_count(cat))
+    await event.edit(text=text, buttons=buttons)
 
 @client.Callback(data="gethelpplugin\:(.*)\:(.*)")
 async def getplugin(event):
     plugin = str(event.data_match.group(1).decode('utf-8'))
     page = int(event.data_match.group(2).decode('utf-8'))
-    text = f"**{client.str} Plugin Name:** ( `{plugin}` )\n"
+    text = client.get_string("Help_3").format(plugin)
+    CATS = client.get_string("Categorys")
     info = client.HELP[plugin]
-    text += f'**{client.str} Category:** ( `{info["category"]}` )\n'
-    text += f'**{client.str} Note:** ( `{info["note"]}` )\n'
+    text += client.get_string("Help_4").format(CATS[info["category"]])
+    text += client.get_string("Help_5").format(translate(info["note"]))
     for command in info["commands"]:
-        text += f'\n**{client.str} {command}:**\n'
+        text += f'  \n**{client.str} {command}:**\n'
         for com in info["commands"][command]:
             ncom = com.replace("{CMD}", client.cmd)
-            text += f'    `{ncom}` - **{info["commands"][command][com]}\n'
+            cominfo = translate(info["commands"][command][com])
+            text += f'    `{ncom}` - **{cominfo}**\n'
     #buttons = get_plugin_buttons(page) 
     await event.edit(text=text) 
 
 @client.Callback(data="closehelp")
 async def closehelp(event):
-    text = "Closed!"
+    text = client.get_string("Help_6")
     await event.edit(text=text)
