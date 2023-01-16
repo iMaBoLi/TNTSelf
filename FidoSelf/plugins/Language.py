62bbdc35efdf60d9d1d81b8ac06a992880e82d51
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-16 16:02:30 +0330

    Update Language.py

--- FidoSelf/plugins/Language.py
+++ FidoSelf/plugins/Language.py
@@ -1,5 +1,5 @@
 from FidoSelf import client
-from FidoSelf.strings import LANGUAGES, LANGS, install_lang
+from FidoSelf.strings import LANGUAGES, LANGS, MAINLANGS, install_lang
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}SetLang (.*)$")
 async def setlanguage(event):
@@ -16,6 +16,8 @@ async def setlanguage(event):
 async def installlanguage(event):
     await event.edit(client.get_string("Wait"))
     lang = event.pattern_match.group(1).lower()
+    if lang in MAINLANGS:
+        return await event.edit(client.get_string("Language_15"))
     if lang not in LANGS:
         return await event.edit(client.get_string("Language_3").format(lang))
     langname = f"{lang} - {LANGS[lang]}"
@@ -34,6 +36,8 @@ async def installlanguage(event):
 async def removelanguage(event):
     await event.edit(client.get_string("Wait"))
     lang = event.pattern_match.group(1).lower()
+    if lang in MAINLANGS:
+        return await event.edit(client.get_string("Language_15"))
     if lang not in LANGUAGES:
         return await event.edit(client.get_string("Language_7").format(lang))
     langname = f"{lang} - {LANGS[lang]}"
@@ -44,10 +48,25 @@ async def removelanguage(event):
     del LANGUAGES[lang]
     await event.edit(client.get_string("Language_8").format(langname))
 
+@client.Cmd(pattern=f"(?i)^\{client.cmd}ReloadLangs$")
+async def removelanguage(event):
+    await event.edit(client.get_string("Wait"))
+    others = client.DB.get_key("INSTALL_LANGS") or []
+    if not others:
+        return await event.edit(client.get_string("Language_9"))
+    await event.edit(client.get_string("Language_10"))
+    langnames = "\n"
+    for lang in others:
+        install_lang(lang)
+        langname = f"{lang} - {LANGS[lang]}"
+        await event.edit(client.get_string("Language_11").format(langname))
+        langnames += langname + "\n"
+    await event.edit(client.get_string("Language_12").format(langnames))
+
 @client.Cmd(pattern=f"(?i)^\{client.cmd}LangList$")
 async def alllanguages(event):
     await event.edit(client.get_string("Wait"))
-    text = client.get_string("Language_9")
+    text = client.get_string("Language_13")
     row = 1
     for lang in LANGS:
         text += f"**{row}-** `{lang}`|{LANGS[lang]}\n"
@@ -57,7 +76,7 @@ async def alllanguages(event):
 @client.Cmd(pattern=f"(?i)^\{client.cmd}Languages$")
 async def languages(event):
     await event.edit(client.get_string("Wait"))
-    text = client.get_string("Language_10")
+    text = client.get_string("Language_14")
     row = 1
     for lang in LANGUAGES:
         text += f"**{row}-** `{lang}`|{LANGS[lang]}\n"
@@ -75,6 +94,7 @@ client.HELP.update({
             "{CMD}SetLang <Text>": "To Setting Bot Language To Other Languages",
             "{CMD}InstallLang <Text>": "To Install New Language In Bot",
             "{CMD}RemoveLang <Text>": "To Remove A Language In Bot",
+            "{CMD}ReloadLangs": "To Reload And Install Saved Languages In Bot",
             "{CMD}Languages": "To Show Installed Languages",
             "{CMD}LangList": "To Show Available Languages",
         },
