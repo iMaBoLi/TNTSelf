6552a76fcc8e61834fcea611407047af8670dd05
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-16 15:49:28 +0330

    Update Language.py

--- FidoSelf/plugins/Language.py
+++ FidoSelf/plugins/Language.py
@@ -16,41 +16,51 @@ async def setlanguage(event):
 async def installlanguage(event):
     await event.edit(client.get_string("Wait"))
     lang = event.pattern_match.group(1).lower()
+    if lang not in LANGS:
+        return await event.edit(client.get_string("Language_3").format(lang))
+    langname = f"{lang} - {LANGS[lang]}"
+    await event.edit(client.get_string("Language_4").format(langname))
     install = install_lang(lang)
+    langs = client.DB.get_key("INSTALL_LANGS") or []
+    if lang not in langs:
+        langs.append(lang)
+        client.DB.set_key("INSTALL_LANGS", langs)
     if install == "Installed":
-        langs = client.DB.get_key("INSTALL_LANGS") or []
-        if lang not in langs:
-            langs.append(lang)
-            client.DB.set_key("INSTALL_LANGS", langs)
-        langname = f"{lang} - {LANGS[lang]}"
-        return await event.edit(client.get_string("Language_3").format(langname))
+        return await event.edit(client.get_string("Language_5").format(langname))
     elif install == "Updated":
-        langs = client.DB.get_key("INSTALL_LANGS") or []
-        if lang not in langs:
-            langs.append(lang)
-            client.DB.set_key("INSTALL_LANGS", langs)
-        langname = f"{lang} - {LANGS[lang]}"
-        return await event.edit(client.get_string("Language_4").format(langname))
-    elif install == "NotFound":
-        return await event.edit(client.get_string("Language_5").format(lang))
+        return await event.edit(client.get_string("Language_6").format(langname))
+
+@client.Cmd(pattern=f"(?i)^\{client.cmd}RemoveLang (.*)$")
+async def removelanguage(event):
+    await event.edit(client.get_string("Wait"))
+    lang = event.pattern_match.group(1).lower()
+    if lang not in LANGUAGES:
+        return await event.edit(client.get_string("Language_7").format(lang))
+    langname = f"{lang} - {LANGS[lang]}"
+    langs = client.DB.get_key("INSTALL_LANGS") or []
+    if lang in langs:
+        langs.remove(lang)
+        client.DB.set_key("INSTALL_LANGS", langs)
+    del LANGUAGES[lang]
+    await event.edit(client.get_string("Language_8").format(langname))
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}LangList$")
 async def alllanguages(event):
     await event.edit(client.get_string("Wait"))
-    text = client.get_string("Language_6")
+    text = client.get_string("Language_9")
     row = 1
     for lang in LANGS:
-        text += f"**{row}-** `{lang}` | **{LANGS[lang]}**\n"
+        text += f"**{row}-** `{lang}`|{LANGS[lang]}\n"
         row += 1
     await event.edit(text)
 
 @client.Cmd(pattern=f"(?i)^\{client.cmd}Languages$")
 async def languages(event):
     await event.edit(client.get_string("Wait"))
-    text = client.get_string("Language_7")
+    text = client.get_string("Language_10")
     row = 1
     for lang in LANGUAGES:
-        text += f"**{row}-** `{lang}` | **{LANGS[lang]}**\n"
+        text += f"**{row}-** `{lang}`|{LANGS[lang]}\n"
         row += 1
     await event.edit(text)
 
@@ -64,6 +74,7 @@ client.HELP.update({
         "commands": {
             "{CMD}SetLang <Text>": "To Setting Bot Language To Other Languages",
             "{CMD}InstallLang <Text>": "To Install New Language In Bot",
+            "{CMD}RemoveLang <Text>": "To Remove A Language In Bot",
             "{CMD}Languages": "To Show Installed Languages",
             "{CMD}LangList": "To Show Available Languages",
         },
