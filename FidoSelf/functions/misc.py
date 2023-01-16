eae773be71f7c5982bf026746dd84ea72dbba7ad
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-16 23:38:25 +0330

    Update misc.py

--- FidoSelf/functions/misc.py
+++ FidoSelf/functions/misc.py
@@ -9,8 +9,7 @@ from FidoSelf.database import DB
 import time
 import os
 
-MAX_SIZE = 104857600 * 5
-PLUGINSHELP = {}
+MAX_SIZE = 104857600 * 10
 
 async def addvars():
     setattr(client, "utils", utils)
@@ -26,14 +25,15 @@ async def addvars():
     setattr(client, "mention", mention)
     setattr(client, "progress", progress)
     setattr(client, "mediatype", mediatype)
-    setattr(client, "HELP", PLUGINSHELP)
+    setattr(client, "HELP", {})
     setattr(client, "MAX_SIZE", MAX_SIZE)
     setattr(client, "me", (await client.get_me()))
     setattr(client.bot, "me", (await client.bot.get_me()))
     setattr(client, "str", client.DB.get_key("MESSAGES_STARTER") or "✥")
     setattr(client, "cmd", client.DB.get_key("SELF_CMD") or ".")
     setattr(client, "realm", client.DB.get_key("REALM_CHAT"))
-    setattr(client, "backch", client.DB.get_key("BACKUP_CHANNEL"))
+    setattr(client, "backup", client.DB.get_key("BACKUP_CHANNEL"))
+    setattr(client, "support", client.DB.get_key("SUPPORT_CHANNEL"))
 
 def stimezone():
     tzone = client.DB.get_key("TIME_ZONE") or "Asia/Tehran"
