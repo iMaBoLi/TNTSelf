997c72b0f3c72e294155735364b0baca99b5f90d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-18 23:00:20 +0330

    Update NameTime.py

--- FidoSelf/plugins/NameTime.py
+++ FidoSelf/plugins/NameTime.py
@@ -1,6 +1,6 @@
 from FidoSelf import client
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}Name (On|Off)$")
+@client.Command(pattern=f"(?i)^\{client.cmd}Name (On|Off)$")
 async def name(event):
     await event.edit(client.get_string("Wait"))
     mode = event.pattern_match.group(1).lower()
@@ -8,7 +8,7 @@ async def name(event):
     change = client.get_string("Change_1") if mode == "on" else client.get_string("Change_2")
     await event.edit(f"**{client.str} The Name Mode Has Been {change}!**")
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}AddName (.*)$")
+@client.Command(pattern=f"(?i)^\{client.cmd}AddName (.*)$")
 async def addname(event):
     await event.edit(client.get_string("Wait"))
     names = client.DB.get_key("NAMES") or []
@@ -19,7 +19,7 @@ async def addname(event):
     client.DB.set_key("NAMES", names)
     await event.edit(f"**{client.str} The Name** ( `{newname}` ) **Added To Name List!**")  
     
-@client.Cmd(pattern=f"(?i)^\{client.cmd}DelName (.*)$")
+@client.Command(pattern=f"(?i)^\{client.cmd}DelName (.*)$")
 async def delname(event):
     await event.edit(client.get_string("Wait"))
     names = client.DB.get_key("NAMES") or []
@@ -30,7 +30,7 @@ async def delname(event):
     client.DB.set_key("NAMES", names)
     await event.edit(f"**{client.str} The Name** ( `{newname}` ) **Deleted From Name List!**")  
     
-@client.Cmd(pattern=f"(?i)^\{client.cmd}NameList$")
+@client.Command(pattern=f"(?i)^\{client.cmd}NameList$")
 async def namelist(event):
     await event.edit(client.get_string("Wait"))
     names = client.DB.get_key("NAMES") or []
@@ -43,7 +43,7 @@ async def namelist(event):
         row += 1
     await event.edit(text)
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}CleanNameList$")
+@client.Command(pattern=f"(?i)^\{client.cmd}CleanNameList$")
 async def cleannames(event):
     await event.edit(client.get_string("Wait"))
     names = client.DB.get_key("NAMES") or []
