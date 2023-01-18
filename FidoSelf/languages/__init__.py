2c7cf003bb9e363b481ca4c85e1192724a88c271
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-19 00:02:51 +0330

    Update __init__.py

--- FidoSelf/languages/__init__.py
+++ FidoSelf/languages/__init__.py
@@ -1,6 +1,4 @@
 from FidoSelf import client
-from googletrans import Translator
-from googletrans.constants import LANGUAGES as LANGS
 
 LANGUAGES = {}
 MAINLANGS = ["en", "fa"]
@@ -12,72 +10,16 @@ def load_langs():
         STRING = eval(STRING)
         LANGUAGES[main] = STRING
         client.LOGS.info(f"• Language ( {main} ) Successfuly Added!")
-    if client.lang not in MAINLANGS:
-        install_lang(client.lang)
-        client.LOGS.info(f"• Language ( {client} ) Successfuly Added By Translate!")
-
-RMSTRS = {
-    "\n": "%%",
-    "?": "©©",
-    ".": "®®",
-    "( `{}` )": "$$",
-    "( {} )": "€€",
-    "`{}`": "¥¥",
-    "{}": "££",
-}
-
-def translate(text, lang):
-    translator = Translator()
-    for STR in RMSTRS:
-        text = text.replace(STR, RMSTRS[STR]) 
-    trjome = translator.translate(text, dest=lang) 
-    trtext = trjome.text
-    for STR in RMSTRS:
-        trtext = trtext.replace(RMSTRS[STR], STR)
-    return trtext
-
-def install_lang(dest):
-    if dest not in LANGS:
-        return "NotFound"
-    mode = "Installed"
-    if dest in LANGUAGES:
-        del LANGUAGES[dest]
-        mode = "Updated"
-    translator = Translator()
-    NewLang = {}
-    for object in LANGUAGES["en"]:
-        result = LANGUAGES["en"][object]
-        if isinstance(result, dict):
-            newlist = {}
-            for key in result:
-                trtext = translate(result[key], dest)
-                newlist.update({key: trtext})
-            NewLang.update({object: newlist})
-        elif isinstance(result, str):
-            trtext = translate(result, dest)
-            NewLang.update({object: trtext})
-    LANGUAGES.update({dest: NewLang})
-    return mode
 
 def get_string(string):
     lang = client.lang
-    try:
-        STRING = LANGUAGES[lang]
-        for page in string.split("_"):
-            STRING = STRING[page]
-        if isinstance(STRING, str):
-            STRING = STRING.replace("{STR}", client.str)
-            STRING = STRING.replace("{CMD}", client.cmd)
-        return STRING
-    except:
-        STRING = LANGUAGES["en"]
-        for page in string.split("_"):
-            STRING = STRING[page]
-        if isinstance(STRING, str):
-            STRING = translate(STRING, lang)
-            STRING = STRING.replace("{STR}", client.str)
-            STRING = STRING.replace("{CMD}", client.cmd)
-        return STRING
+    SRRING = LANGUAGES[lang]
+    for page in string.split("_"):
+        STRING = STRING[page]
+    if isinstance(STRING, str):
+        STRING = STRING.replace("{STR}", client.str)
+        STRING = STRING.replace("{CMD}", client.cmd)
+    return STRING
 
 def get_buttons(buttons):
     if client.lang in ["fa", "ar"]:
