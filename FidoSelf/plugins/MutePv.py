fe6bc8260dbf0cb1e986c11b3f0b2ea8b43a3603
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-18 22:59:44 +0330

    Update MutePv.py

--- FidoSelf/plugins/MutePv.py
+++ FidoSelf/plugins/MutePv.py
@@ -1,6 +1,6 @@
 from FidoSelf import client
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}MutePv (On|Off)$")
+@client.Command(pattern=f"(?i)^\{client.cmd}MutePv (On|Off)$")
 async def mutepv(event):
     await event.edit(client.get_string("Wait"))
     mode = event.pattern_match.group(1).lower()
@@ -8,7 +8,7 @@ async def mutepv(event):
     change = client.get_string("Change_1") if mode == "on" else client.get_string("Change_2")
     await event.edit(client.get_string("MutePv_1").format(change))
 
-@client.Cmd(sudo=False, edits=False)
+@client.Command(sudo=False, edits=False)
 async def muter(event):
     if not event.is_private or event.is_white or event.is_sudo or event.is_bot: return
     mode = client.DB.get_key("MUTE_PV") or "off"
