73d21f838c4a1b7a1a98909d37d1bef9f08a6df8
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-18 22:59:54 +0330

    Update Cleaner.py

--- FidoSelf/plugins/Cleaner.py
+++ FidoSelf/plugins/Cleaner.py
@@ -20,7 +20,7 @@ for LIST in LISTS:
         Pattern += Pat + "|"
 Pattern = Pattern[:-1]
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}Clean ({Pattern})$")
+@client.Command(pattern=f"(?i)^\{client.cmd}Clean ({Pattern})$")
 async def cleaner(event):
     await event.edit(client.get_string("Wait"))
     inlist = event.pattern_match.group(1).title()
