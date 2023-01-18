a9524b32b27f53227e9a3f1af16ebeb1c6145652
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-18 22:59:46 +0330

    Update AutoReplace.py

--- FidoSelf/plugins/AutoReplace.py
+++ FidoSelf/plugins/AutoReplace.py
@@ -1,6 +1,6 @@
 from FidoSelf import client
 
-@client.Cmd()
+@client.Command()
 async def replace(event):
     if event.is_cmd or not event.text: return
     mode = client.DB.get_key("AUTO_REPLACE_MODE") or "on"
