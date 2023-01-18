fba0c232ceca525ca0b8c62bf931b8e6b9004502
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-18 13:17:09 +0330

    Update TextSay.py

--- FidoSelf/plugins/TextSay.py
+++ FidoSelf/plugins/TextSay.py
@@ -1,40 +1,27 @@
 from FidoSelf import client
 import asyncio
 
-@client.Cmd(pattern=f"(?i)^\{client.cmd}AutoSay (On|off)$")
-async def autosay(event):
-    await event.edit(client.get_string("Wait"))
-    mode = event.pattern_match.group(1).lower()
-    change = client.get_string("Change_1") if change == "on" else client.get_string("Change_2")
-    client.DB.set_key("AUTO_SAY_MODE", mode)
-    await event.edit(client.get_string("TextSay_1").format(change))
-
-@client.Cmd(pattern=f"(?i)^\{client.cmd}SetSaySleep (\d*)$")
-async def saysleep(event):
-    await event.edit(client.get_string("Wait"))
-    sleep = event.pattern_match.group(1)
-    client.DB.set_key("SAY_SLEEP", str(sleep))
-    await event.edit(client.get_string("TextSay_2").format(client.utils.convert_time(int(sleep))))
+@client.Cmd(pattern=f"(?i)^\{client.cmd}TSay ([\S\s]*)$")
+async def tsay(event):
+    text = event.pattern_match.group(1)
+    sleep = client.DB.get_key("SAY_SLEEP") or 1
+    sleep = int(sleep) if str(sleep).isdigit() else float(sleep)
+    new = ""
+    for par in text:
+        new += par
+        await event.edit(new)
+        await asyncio.sleep(sleep)
 
 @client.Cmd(edits=False)
-async def say(event):
+async def autosay(event):
     if event.is_cmd or not event.text: return
     mode = client.DB.get_key("AUTO_SAY_MODE") or "off"
-    sleep = client.DB.get_key("SAY_SLEEP") or "1"
+    sleep = client.DB.get_key("SAY_SLEEP") or 1
+    sleep = int(sleep) if str(sleep).isdigit() else float(sleep)
     if mode == "off": return
     text = str(event.text)
     new = ""
     for par in text:
         new += par
         await event.edit(new)
-        await asyncio.sleep(int(sleep))
-
-@client.Cmd(pattern=f"(?i)^\{client.cmd}TSay ([\S\s]*)$")
-async def tsay(event):
-    text = event.pattern_match.group(1)
-    sleep = client.DB.get_key("SAY_SLEEP") or "1"
-    new = ""
-    for par in text:
-        new += par
-        await event.edit(new)
-        await asyncio.sleep(int(sleep))
+        await asyncio.sleep(sleep)
