dd65a1416748975827acddace8f0015deb6e00c8
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-19 23:38:20 +0330

    Update Inline.py

--- FidoSelf/events/Inline.py
+++ FidoSelf/events/Inline.py
@@ -4,14 +4,14 @@ from traceback import format_exc
 
 def Inline(
     pattern=None,
-    sudo=True,
+    onlysudo=True,
     **kwargs,
 ):
     def decorator(func):
         async def wrapper(event):
             try:
                 event.is_sudo = True if event.sender_id == client.me.id else False
-                if sudo and not event.is_sudo:
+                if onlysudo and not event.is_sudo:
                     text = client.get_string("OtherUse_Inline")
                     return await event.answer([event.builder.article(f"{client.str} FidoSelf - NotForYou", text=text)])
                 await func(event)
