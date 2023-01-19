a83e9a6992d61288c37f77e6c1c06a8253acf4c8
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-20 00:27:00 +0330

    Update language.py

--- FidoSelf/functions/language.py
+++ FidoSelf/functions/language.py
@@ -1,11 +1,25 @@
 from FidoSelf import client
 
+MAINSTRINGS = {
+    "EN": {
+        "wait": "^{STR} Please Wait . . .$", 
+    },
+    "FA": {
+        "wait": "^{STR} لطفا صبر کنید . . .$", 
+    }
+}
+
 def get_string(STRINGS, string):
     lang = client.lang or "EN"
-    STRING = STRINGS[lang][string]
+    if string in MAINSTRINGS[lang]:
+        STRING = MAINSTRINGS[lang][string]
+    else:
+        STRING = STRINGS[lang][string]
     REPLACES = {
         "{STR}": client.STR,
         "{CMD}": client.CMD,
+        "^": "**",
+        "$": "**",
     }
     for replace in REPLACES:
         STRING = STRING.replace(replace, REPLACES[replace])
