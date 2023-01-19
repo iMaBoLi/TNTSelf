f062265ad7b2bc2e92e542d5943756f59d2456a7
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-20 00:13:00 +0330

    Update and rename Bot.py to CheckBot.py

--- FidoSelf/plugins/Bot.py
+++
@@ -1,5 +0,0 @@
-from FidoSelf import client
-
-@client.Command(pattern=f"(?i)^\{client.cmd}(Ping|Bot|Self)$")
-async def ping(event):
-    await event.edit(client.get_string("Ping"))
---
+++ FidoSelf/plugins/CheckBot.py
@@ -0,0 +1,32 @@
+from FidoSelf import client
+
+STRINGS = {
+    "EN": {
+        "ping": "**{STR} IM Online Forever!**",
+        "bot": "**{STR} Bot Is Online!**",
+    },
+    "FA": {
+        "ping": "**{STR} من همیشه آنلاین هستم!**",
+        "bot": "**{STR} ربات آنلاین است!**",
+    },
+}
+
+@client.Command(
+    commands={
+        "EN": "Ping",
+        "FA": "پینگ",
+     }
+)
+async def ping(event):
+    text = client.get_string(STRINGS, "ping")
+    await event.edit(text)
+
+@client.Command(
+    commands={
+        "EN": "Bot",
+        "FA": "ربات",
+     }
+)
+async def bot(event):
+    text = client.get_string(STRINGS, "bot")
+    await event.edit(text)
