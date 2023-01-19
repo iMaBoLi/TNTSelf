56217eb615068f8314002c744f857c2995561483
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-20 00:05:11 +0330

    Update runcodes.py

--- FidoSelf/plugins/runcodes.py
+++ FidoSelf/plugins/runcodes.py
@@ -1,6 +1,7 @@
 from FidoSelf import client
-from FidoSelf.functions.utils import runcmd
+from FidoSelf.functions import *
 from telethon import events, functions, types, Button
+from datetime import datetime
 import traceback
 import requests
 import asyncio
@@ -10,6 +11,7 @@ import io
 import glob
 import re
 import json
+import time
 
 async def runner(code , event):
     chat = await event.get_chat()
@@ -18,13 +20,13 @@ async def runner(code , event):
     exec("async def coderunner(event , local, chat_id, reply): "+ "".join(f"\n {l}" for l in code.split("\n")))
     return await locals()["coderunner"](event , local, chat.id, reply)
 
-@client.Command(pattern=f"(?i)^\{client.cmd}run(?:\s|$)([\s\S]*)$", sudo=False)
+@client.bot.on(events.NewMessage(pattern=f"(?i)^(\.|\/|\,)Run(?:\s|$)([\s\S]*)$")
 async def runcodes(event):
-    await event.edit(f"`{client.str} Running ...`")
+    edit = await event.reply(f"`• Running ...`")
     if event.text[4:]:
         cmd = "".join(event.text.split(maxsplit=1)[1:])
     else:
-        return await event.edit(f"**{client.str} What Should I Run ?**")
+        return await edit.edit(f"**• What Should I Run ?**")
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = io.StringIO()
@@ -32,55 +34,24 @@ async def runcodes(event):
     stdout, stderr, exc = None, None, None
     try:
         await runner(cmd , event)
-    except Exception as e:
-        exc = traceback.format_exc()
+    except:
+        exec = traceback.format_exc()
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
-    result = None
-    if exc:
-        result = exc
-        res = "Errors"
+    result = "Success‌!"
+    if exec:
+        result = exec
     elif stderr:
         result = stderr
-        res = "Errors"
     elif stdout:
         result = stdout
-        res = "Results"
+    if len(result) < 4096:
+        await event.edit(f"**• Results:**\n\n`{result`}")
     else:
-        result = "Success‌!"
-        res = "Results"
-    out = f"""
-**{client.str} Code :** 
-`{event.text}`
-
-**{client.str} {res} :** 
-`{result}`
-"""
-    if len(out) < 4096:
-        await event.edit(out)
-    else:
-        f = open(f"{res}.txt", "w")
-        f.write(str(result))
-        try:
-            await client.send_file(event.chat_id, f"{res}.txt" , caption=f"""
-**{client.str} Code :** 
-`{event.text}`
-
-**{client.str} {res} :** 
-**In File‌!**
-""")
-        except:
-            out = f"""
-{client.str} Code:
-{event.text}
-
-{client.str} {res}: 
-{result}
-"""
-            f = open(f"{res}.txt", "w")
-            f.write(str(out))
-            await client.send_file(event.chat_id, f"{res}.txt" , caption=f"**{client.str} {res} :** \n\n**In File‌!**")
-        os.remove(f"{res}.txt")
-        await event.delete()
+        file = "OutPut.txt"
+        open(file, "w").write(str(result))
+        await client.send_file(event.chat_id, file , caption="**• Code OutPut!**")
+        os.remove(file)
+        await edit.delete()
