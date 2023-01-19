b9e7d65b581b677dc11cc2ce185cb1227f72a57f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-19 23:48:09 +0330

    Update misc.py

--- FidoSelf/functions/misc.py
+++ FidoSelf/functions/misc.py
@@ -1,27 +1,20 @@
 from FidoSelf import client
 from FidoSelf.events import Command, Callback, Inline
 from FidoSelf.functions import *
-import time
-import os
 
 async def addvars():
-    setattr(client, "utils", utils)
     setattr(client, "Command", Command)
     setattr(client, "Callback", Callback)
     setattr(client, "Inline", Inline)
     setattr(client, "DB", DB)
-    setattr(client, "lang", client.DB.get_key("LANGUAGE") or "EN")
+    setattr(client, "LANG", client.DB.get_key("LANGUAGE") or "EN")
     setattr(client, "get_string", get_string)
-    setattr(client, "config", config)
-    setattr(client, "vars", add_vars)
-    setattr(client, "mention", mention)
-    setattr(client, "progress", progress)
-    setattr(client, "mediatype", mediatype)
+    setattr(client, "add_vars", add_vars)
     setattr(client, "HELP", {})
     setattr(client, "MAX_SIZE", MAX_SIZE)
     setattr(client, "me", (await client.get_me()))
     setattr(client.bot, "me", (await client.bot.get_me()))
-    setattr(client, "str", client.DB.get_key("MESSAGES_STARTER") or "✥")
-    setattr(client, "cmd", client.DB.get_key("SELF_CMD") or ".")
-    setattr(client, "realm", client.DB.get_key("REALM_CHAT") or client.me.id)
-    setattr(client, "backch", client.DB.get_key("BACKUP_CHANNEL"))
+    setattr(client, "STR", client.DB.get_key("MESSAGES_STARTER") or "✥")
+    setattr(client, "CMD", client.DB.get_key("SELF_CMD") or "")
+    setattr(client, "REALM", client.DB.get_key("REALM_CHAT") or client.me.id)
+    setattr(client, "BACKUP", client.DB.get_key("BACKUP_CHANNEL"))
