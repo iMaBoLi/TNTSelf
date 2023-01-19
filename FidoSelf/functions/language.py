3c5c720961d419d194bc3a288f6134c0a9c7ffd7
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-20 00:19:10 +0330

    Update language.py

--- FidoSelf/functions/language.py
+++ FidoSelf/functions/language.py
@@ -1,13 +1,12 @@
 from FidoSelf import client
 
-REPLACES = {
-    "{STR}": client.STR,
-    "{CMD}": client.CMD,
-}
-
 def get_string(STRINGS, string):
     lang = client.lang or "EN"
     STRING = STRINGS[lang][string]
+    REPLACES = {
+        "{STR}": client.STR,
+        "{CMD}": client.CMD,
+    }
     for replace in REPLACES:
         STRING = STRING.replace(replace, REPLACES[replace])
     return STRING
