858af344f0d4f4e88937efa07d98614a630ea9d2
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-19 23:14:04 +0330

    Create __init__.py

---
+++ FidoSelf/functions/__init__.py
@@ -0,0 +1,8 @@
+from FidoSelf import client
+from FidoSelf.functions.database import DB
+from FidoSelf import config
+from FidoSelf.functions.language import get_string
+from FidoSelf.functions.helper import progress, mention, mediatype
+from FidoSelf.functions.vars import add_vars
+
+MAX_SIZE = 104857600 * 10
