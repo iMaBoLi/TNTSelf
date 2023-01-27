4f7fbed58eabdf499ecfd1f5fb8964c64d5b99ce
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-27 23:21:03 +0330

    Update __main__.py

--- FidoSelf/__main__.py
+++ FidoSelf/__main__.py
@@ -10,27 +10,29 @@ async def setup():
     plugs, notplugs = load_plugins(client.PLUGINS)
     client.LOGS.info(f"• Successfully Installed {len(plugs)} Plugin From Main Plugins!")
     client.LOGS.info(f"• Not Installed {len(notplugs)} Plugin From Main Plugins!")
-    try:
-        send = await client.bot.send_message(client.REALM, f"**👋 Fido Self Has Been Start Now !**\n\n**🧒 UserMode :** {client.mention(client.me)}\n**🤖 Manager :** {client.mention(client.bot.me)}")
-        if plugs:
-            text = "**✅ Loaded Plugins :**\n\n"
-            for plug in plugs:
-                text += f"`{plug}`\n"
-            await send.reply(text)
-        if notplugs:
-            text = "**❌ Unloaded Plugins :**\n\n"
-            ftext = ""
-            for plug in notplugs:
-                text += f"`{plug}`\n"
-                ftext += f"{notplugs[plug]}\n\n"
-            await send.reply(text)
-            file = "NotPlugs.txt"
-            open(file, "w").write(ftext)
-            #await send.reply(file=file)
-        res = await runcmd('git log --pretty=format:"[%an]: %s" -20')
-        await send.reply(f"**• Github Commits:**\n\n`{res[0]}`")
-    except:
-        pass
+    update = client.DB.get_key("UPDATE") or True
+    if update:
+        try:
+            send = await client.bot.send_message(client.REALM, f"**👋 Fido Self Has Been Start Now !**\n\n**🧒 UserMode :** {client.mention(client.me)}\n**🤖 Manager :** {client.mention(client.bot.me)}")
+            if plugs:
+                text = "**✅ Loaded Plugins :**\n\n"
+                for plug in plugs:
+                    text += f"`{plug}`\n"
+                await send.reply(text)
+            if notplugs:
+                text = "**❌ Unloaded Plugins :**\n\n"
+                ftext = ""
+                for plug in notplugs:
+                    text += f"`{plug}`\n"
+                    ftext += f"{notplugs[plug]}\n\n"
+                await send.reply(text)
+                file = "NotPlugs.txt"
+                open(file, "w").write(ftext)
+                #await send.reply(file=file)
+            res = await runcmd('git log --pretty=format:"[%an]: %s" -20')
+            await send.reply(f"**• Github Commits:**\n\n`{res[0]}`")
+        except:
+            pass
     client.LOGS.info(f"• Python Version: {platform.python_version()}")
     client.LOGS.info(f"• Telethon Version: {telever}")
     client.LOGS.info(f"• FidoSelf Version: {client.__version__}")
