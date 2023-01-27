dab5b0c76a11daa804bc001fdc7195e5864c5418
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-27 20:30:18 +0330

    Update Language.py

--- FidoSelf/plugins/Language.py
+++ FidoSelf/plugins/Language.py
@@ -21,7 +21,7 @@ async def language(event):
     text = client.get_string("wait")
     await event.edit(text)
     lang = event.pattern_match.group(1).upper()
-    if lang == client.lang:
+    if lang == client.LANG:
         text = client.get_string("already", STRINGS)
         return await event.edit(text)
     client.DB.set_key("LANGUAGE", lang)
