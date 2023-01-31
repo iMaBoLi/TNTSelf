f0d4e5610c926acdfae050bafb4cc854b26dfad8
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-01-31 09:01:23 +0330

    Update SetChats.py

--- FidoSelf/plugins/SetChats.py
+++ FidoSelf/plugins/SetChats.py
@@ -1,21 +1,45 @@
 from FidoSelf import client
 
-@client.Command(pattern=f"(?i)^\{client.cmd}Set(Realm|Backup)$")
-async def setchats(event):
-    await event.edit(client.get_string("Wait"))
+STRINGS = {
+    "EN": {
+        "notrealm": "^{STR} Please Send In Group To Set Realm Chat!$",
+        "realm": "^{STR} This Chat Was Set For Realm Chat!$",
+        "notbackup": "^{STR} Please Send In Channel To Set Backup Channel!$",
+        "backup": "^{STR} This Channel Was Set For Backup Channel!$",    },
+    "FA": {
+        "notrealm": "^{STR} لطفا داخل یک گروه ارسال کنید تا ریلم چت تنظیم شود!$",
+        "realm": "^{STR} این چت برای ریلم چت تنظیم شد!$",
+        "notbackup": "^{STR} لطفا داخل یک چنل ارسال کنید تا چنل پشتیبان تنظیم شود!$",
+        "backup": "^{STR} این چت برای چنل پشتیبان تنظیم شد!$",
+    },
+}
+
+@client.Command(
+    commands={
+        "EN": "Set (Realm|BackUp)",
+        "FA": "تنظیم )ریلم|پشتیبان)",
+     }
+)
+async def ping(event):
+    text = client.get_string("wait")
+    await event.edit(text)
     mode = event.pattern_match.group(1).title()
-    if mode == "Realm":
+    if mode in ["Realm", "ریلم"]:
         if not event.is_group:
-            return await event.edit(client.get_string("SetChats_1"))
+            text = client.get_string("notrealm", STRINGS)
+            return await event.edit(text)
         client.DB.set_key("REALM_CHAT", event.chat_id)
         client.realm = event.chat_id
-        await event.edit(client.get_string("SetChats_2"))
-    elif mode == "Backup":
+        text = client.get_string("realm", STRINGS)
+        await event.edit(text)
+    elif mode == ["Backup", "پشتیبان"]:
         if not event.is_ch:
-            return await event.edit(client.get_string("SetChats_3"))
+            text = client.get_string("notbackup", STRINGS)
+            return await event.edit(text)
         client.DB.set_key("BACKUP_CHANNEL", event.chat_id)
         client.backch = event.chat_id
-        await event.edit(client.get_string("SetChats_4"))
+        text = client.get_string("backup", STRINGS)
+        await event.edit(text)
 
 category = "Settings"
 plugin = "SetChats"
