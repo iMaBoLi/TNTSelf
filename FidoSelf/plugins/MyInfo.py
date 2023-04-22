19d8bf501c3cc96fa3f8408c5f43e6d8975d0fbb
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-04-22 14:57:50 +0330

    Update MyInfo.py

--- FidoSelf/plugins/MyInfo.py
+++ FidoSelf/plugins/MyInfo.py
@@ -1,13 +1,16 @@
 from FidoSelf import client
+from telethon import functions
 
 STRINGS = {
     "EN": {
+        "info": "**{STR} Your Profile Info:**\n\n   **ID:** ( `{}` )\n   **Name:** ( `{}` )\n   **UserName:** ( `{}` )\n   **Biography:** ( `{}` )",
         "id": "**{STR} Your ID:** ( `{RES}` )",
         "name": "**{STR} Your Name:** ( `{RES}` )",
         "phone": "**{STR} Your Phone:** ( `{RES}` )",
         "profile": "**{STR} Your Profile Picture!**",
     },
     "FA": {
+        "اطلاعات": "**{STR} اطلاعات شما:**\n\n   **آیدی:** ( `{}` )\n   **اسم:** ( `{}` )\n   **نام کاربری:** ( `{}` )\n   **بیوگرافی:** ( `{}` )",
         "آیدی": "**{STR} آیدی شما:** ( `{RES}` )",
         "اسم": "**{STR} اسم شما:** ( `{RES}` )",
         "شماره": "**{STR} شماره شما:** ( `{RES}` )",
@@ -24,24 +27,33 @@ STRINGS = {
 async def myinfo(event):
     type = event.pattern_match.group(1)
     info = await client.get_me()
+    mypic = await client.get_profile_photos("me")
+    name = f"{info.first_name} {info.last_name}" if info.last_name else info.first_name,
+    if type.upper() in ["INFO", "اطلاعات"]:
+        text = client.get_string(type.lower(), STRINGS)
+        uinfo = (await client(functions.users.GetFullUserRequest("me"))).full_user
+        text = text.format(info.id, name, info.username, uinfo.about)
+        await event.respond(text, file=mypic[0])
+        await event.delete()
     infos = {
         "EN": {
             "ID": info.id,
-            "NAME": f"{info.first_name} {info.last_name}" if info.last_name else info.first_name,
-            "PHONE": info.phone,
-            "PROFILE": (await client.get_profile_photos("me"))[0],
+            "NAME": name,
+            "PHONE": "+" + info.phone,
+            "PROFILE": mypic[0],
         },
         "FA": {
             "آیدی": info.id,
-            "اسم": f"{info.first_name} {info.last_name}" if info.last_name else info.first_name,
-            "شماره": info.phone,
-            "پروفایل": (await client.get_profile_photos("me"))[0],
+            "اسم": name,
+            "شماره": "+" + info.phone,
+            "پروفایل": mypic[0],
         },
     }
     result = infos[client.LANG][type.upper()]
     text = client.get_string(type.lower(), STRINGS)
-    if type.upper() == "PROFILE":
-        await event.edit(text, file=result)
+    if type.upper() in ["PROFILE", "پروفایل"]:
+        await event.respond(text, file=result)
+        await event.delete()
     else:
         text = text.format(RES=result)
         await event.edit(text)
