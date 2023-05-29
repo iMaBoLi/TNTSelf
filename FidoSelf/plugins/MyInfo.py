634bbfd77ae022bfe6e15b3e94c6a6274a89d7e1
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-05-29 17:26:58 +0530

    Update MyInfo.py

--- FidoSelf/plugins/MyInfo.py
+++ FidoSelf/plugins/MyInfo.py
@@ -6,6 +6,8 @@ STRINGS = {
         "info": "**{STR} Your Profile Info:**\n\n   **ID:** ( `{}` )\n   **Name:** ( `{}` )\n   **UserName:** ( `{}` )\n   **Biography:** ( `{}` )",
         "id": "**{STR} Your ID:** ( `{RES}` )",
         "name": "**{STR} Your Name:** ( `{RES}` )",
+        "username": "**{STR} Your UserName:** ( `{RES}` )",
+        "bio": "**{STR} Your Biography:** ( `{RES}` )",
         "phone": "**{STR} Your Phone:** ( `{RES}` )",
         "profile": "**{STR} Your Profile Picture!**",
     },
@@ -13,6 +15,8 @@ STRINGS = {
         "اطلاعات": "**{STR} اطلاعات شما:**\n\n   **آیدی:** ( `{}` )\n   **اسم:** ( `{}` )\n   **نام کاربری:** ( `{}` )\n   **بیوگرافی:** ( `{}` )",
         "آیدی": "**{STR} آیدی شما:** ( `{RES}` )",
         "اسم": "**{STR} اسم شما:** ( `{RES}` )",
+        "یوزرنیم": "**{STR} یوزرنیم شما:** ( `{RES}` )",
+        "بیو": "**{STR} بیوگرافی شما:** ( `{RES}` )",
         "شماره": "**{STR} شماره شما:** ( `{RES}` )",
         "پروفایل": "**{STR} عکس پروفایل شما!**",
     },
@@ -20,19 +24,20 @@ STRINGS = {
 
 @client.Command(
     commands={
-        "EN": "My(iNfo|iD|Name|Phone|Profile)",
-        "FA": "(اطلاعات|آیدی|اسم|شماره|پروفایل) من",
+        "EN": "My(iNfo|iD|Name|Bio|Username|Phone|Profile)",
+        "FA": "(اطلاعات|آیدی|اسم|بیو|یوزرنیم|شماره|پروفایل) من",
      }
 )
 async def myinfo(event):
     type = event.pattern_match.group(1)
     info = await client.get_me()
     mypic = await client.get_profile_photos("me")
-    name = f"{info.first_name} {info.last_name}" if info.last_name else info.first_name,
+    name = f"{info.first_name} {info.last_name}" if info.last_name else info.first_name
+    username = f"@{info.username}" if info.usrename else "---"
     if type.upper() in ["INFO", "اطلاعات"]:
         text = client.get_string(type.lower(), STRINGS)
         uinfo = (await client(functions.users.GetFullUserRequest("me"))).full_user
-        text = text.format(info.id, name, info.username, uinfo.about)
+        text = text.format(info.id, name, username, uinfo.about)
         await event.respond(text, file=mypic[0])
         await event.delete()
     infos = {
@@ -41,12 +46,16 @@ async def myinfo(event):
             "NAME": name,
             "PHONE": "+" + info.phone,
             "PROFILE": mypic[0],
+            "USERNAME": username,
+            "BIO": uinfo.about,
         },
         "FA": {
             "آیدی": info.id,
             "اسم": name,
             "شماره": "+" + info.phone,
             "پروفایل": mypic[0],
+            "یوزرنیم": username,
+            "بیو": uinfo.about,
         },
     }
     result = infos[client.LANG][type.upper()]
