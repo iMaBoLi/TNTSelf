55a09fabea82512521a18960752152aa9ce86b7d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-05-31 19:28:28 +0530

    Update logs.py

--- FidoSelf/plugins/logs.py
+++ FidoSelf/plugins/logs.py
@@ -10,14 +10,14 @@ import os
 )
 async def logs(event):
     type = str(event.pattern_match.group(1))
-    if type == "c" and os.path.exists("Fidog.log"):
-        await event.respond(f"**{client.str} The Console Logs File!**", file="Fido.log")
+    if type == "c" and os.path.exists("Fido.log"):
+        await event.respond("**The Console Logs File!**", file="Fido.log")
         await event.delete()
     elif type == "g":
         git = await client.utils.runcmd('git log --pretty=format:"[%an]: %s"')
         open("Github.log", "w").write(str(git[0]))
-        await event.respond(f"**{client.str} The Github Commits Logs File!**", file="Github.log")
+        await event.respond("**The Github Commits Logs File!**", file="Github.log")
         os.remove("Github.log")
         await event.delete()
     else:
-        await event.edit(f"**{client.str} The Log File Is Not Available!**")
+        await event.edit("**The Log File Is Not Available!**")
