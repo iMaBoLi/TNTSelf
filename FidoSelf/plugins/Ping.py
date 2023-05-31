6910d18a234fb58de9a7cc1412059e17a2d23ef8
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-01 01:14:49 +0530

    Update and rename Bot.py to Ping.py

---
+++ FidoSelf/plugins/Ping.py
@@ -0,0 +1,27 @@
+from FidoSelf import client
+from datetime import datetime
+
+STRINGS = {
+    "EN": {
+        "ping": "**{STR} PonG!** [ `{ping}` ]",
+    },
+    "FA": {
+        "ping": "**{STR}پونگ! [ `{ping}` ]**",
+    },
+}
+
+@client.Command(
+    commands={
+        "EN": "Ping",
+        "FA": "پینگ",
+     }
+)
+async def ping(event):
+    start = datetime.now()
+    await event.edit("**!!!**")
+    end = datetime.now()
+    tms = (end - start).microseconds / 1000
+    ms = round((tms - 0.6) / 3, 1)
+    text = client.get_string("ping", STRINGS)
+    text = text.format(ping=ping)
+    await event.edit(text)
