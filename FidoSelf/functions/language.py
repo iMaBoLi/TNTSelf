ee391c67d34d77aacb2fd7e7e747fef06624491f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-05-31 19:38:22 +0530

    Update language.py

--- FidoSelf/functions/language.py
+++ FidoSelf/functions/language.py
@@ -1,13 +1,21 @@
 from FidoSelf import client
 
+MAINSTRINGS = {}
+
+def ADDSTRINGS():
+    files = ["EN.json", "FA.json"]
+    for file in files:
+        STRINGS = open(file, "r").read()
+        STRINGS = eval(STRINGS)
+        MAINSTRINGS += STRINGS
+
 def get_string(string, STRINGS=None, LANG=None):
     lang = LANG or client.LANG
     if STRINGS:
         STRING = STRINGS[lang][string]
     else:
-        langfile = lang + ".json"
-        MAINSTRINGS = open("FidoSelf/languages" + langfile, "r").read()
-        MAINSTRINGS = eval(MAINSTRINGS)
+        if not MAINSTRINGS:
+            ADDSTRINGS()
         STRING = MAINSTRINGS[string]
     REPLACES = {
         "{STR}": client.STR,
