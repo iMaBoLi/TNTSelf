0f02c0bc5c589e819372ea84aa6f1042c6fba7b1
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-05-31 19:14:05 +0530

    Update language.py

--- FidoSelf/functions/language.py
+++ FidoSelf/functions/language.py
@@ -1,29 +1,17 @@
 from FidoSelf import client
 
-MAINSTRINGS = {
-    "EN": {
-        "wait": "^{STR} Please Wait . . .$", 
-        "On": "Actived",
-        "Off": "DeActived",
-    },
-    "FA": {
-        "wait": "^{STR} لطفا صبر کنید . . .$", 
-        "On": "فعال شد",
-        "Off": "غیر فعال شد",
-    }
-}
-
 def get_string(string, STRINGS=None, LANG="EN"):
     lang = client.LANG or LANG
     if STRINGS:
         STRING = STRINGS[lang][string]
     else:
-        STRING = MAINSTRINGS[lang][string]
+        langfile = lang + ".json"
+        MAINSTRINGS = open(langfile, "r").read()
+        MAINSTRINGS = eval(MAINSTRINGS)
+        STRING = MAINSTRINGS[string]
     REPLACES = {
         "{STR}": client.STR,
         "{CMD}": client.CMD,
-        "^": "**",
-        "$": "**",
     }
     for replace in REPLACES:
         STRING = STRING.replace(replace, REPLACES[replace])
