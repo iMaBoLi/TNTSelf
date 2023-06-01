96f390de2ffdeb665794951694ca7808b04bd431
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-02 03:20:55 +0530

    Create strings.py

---
+++ FidoSelf/data/strings.py
@@ -0,0 +1,31 @@
+STRINGS = {
+    "wait": "**Please Wait ⏳**",
+    "On": "Actived",
+    "Off": "DeActived",
+    "replyMain": "**The Replyed Media Is** ( `{}` ) **Please Reply To** ( `{}` )",
+    "replyNot": "**You Have Not Replied Please Reply To** ( `{}` )",
+    "replyNotAll": "**You Have Not Replied Please Reply To A Message!**",
+    "replyMessage": "Message",
+    "replyText": "Text",
+    "replyPhoto": "Photo",
+    "replyVideo": "Video",
+    "replyVideoNote": "Video Note",
+    "replyGif": "Gif",
+    "replyVoice": "Voice",
+    "replyMusic": "Music",
+    "replySticker": "Sticker",
+    "replyASticker": "Animated Sticker",
+    "replyFile": "File",
+    "replyEmpty": "Empty",
+	
+	
+	
+	
+	
+
+	
+	
+	
+
+	
+}
