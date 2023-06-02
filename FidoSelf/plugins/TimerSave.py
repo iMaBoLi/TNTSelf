61f643c353d781c8c5bac9aa65d9bcf95aba6340
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-03 02:59:00 +0530

    update FidoSelf/plugins/TimerSave.py

--- FidoSelf/plugins/TimerSave.py
+++ FidoSelf/plugins/TimerSave.py
@@ -1,14 +1,19 @@
 from FidoSelf import client
 import os
 
-@client.Command(sudo=False)
+STRINGS = {
+    "caption": "**The Timer Media Was Saved!**\n\n**User:** ( `{}` )\n**Timer:** ( `{}` )",
+}
+
+@client.Command(onlysudo=False)
 async def dtimermedias(event):
     mode = client.DB.get_key("TIMER_MODE")
-    mtype = client.mediatype(event)
+    mtype = client.functions.mediatype(event)
     if event.is_private and mode == "on" and mtype in ["Photo", "Video"] and event.media.ttl_seconds:
         file = await event.download_media()
         sender = await event.get_sender()
-        mention = client.mention(sender)
-        ttl = client.utils.convert_time(event.media.ttl_seconds)
-        await client.send_file(client.realm, file, caption=client.get_string("TimerSave").format(mention, ttl))
+        mention = client.functions.mention(sender)
+        ttl = client.functions.convert_time(event.media.ttl_seconds)
+        caption = STRINGS["caption"].format(mention, ttl)
+        await client.send_file(client.realm, file, caption=caption)
         os.remove(file)
