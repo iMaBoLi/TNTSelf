c77ddaed75f749520cfb6b20ff10d2115a548db7
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-03 03:25:43 +0530

    update FidoSelf/plugins/DelProfiles.py

--- FidoSelf/plugins/DelProfiles.py
+++ FidoSelf/plugins/DelProfiles.py
@@ -1,17 +1,25 @@
 from FidoSelf import client
 from telethon import functions, types
 
-@client.Command(pattern=f"(?i)^\{client.cmd}DelProfile ?((\-)?\d*)?$")
+STRINGS = {
+    "not": "**The Profile Photos Is Not Found!**",
+    "last": "**The Last Profile Photo Has Been Deleted!**",
+    "count": "**The** ( `{}` ) **Of Your Profile Photos Has Been Deleted!**",
+    "nota": "**This Profile Photo Is Not Available!**",
+    "one": "**The Profile Photo** ( `{}` ) **Has Been Deleted!**",
+}
+
+@client.Command(command="DelProfile ?((\-)?\d*)?")
 async def delprofiles(event):
-    await event.edit(client.get_string("Wait"))
+    await event.edit(client.STRINGS["wait"])
     pphoto = await client.get_profile_photos("me")
     if not pphoto:
-        return await event.edit(client.get_string("DelProfiles_1"))
+        return await event.edit(STRINGS["not"])
     prof = event.pattern_match.group(1)
     if not prof:
         pphoto = pphoto[0]
         await client(functions.photos.DeletePhotosRequest(id=[types.InputPhoto(id=pphoto.id, access_hash=pphoto.access_hash, file_reference=pphoto.file_reference)]))
-        await event.edit(client.get_string("DelProfiles_2"))
+        await event.edit(STRINGS["last"])
     elif str(prof).startswith("-"):
         prof = prof.replace("-", "")
         if int(prof) > len(pphoto):
@@ -19,10 +27,10 @@ async def delprofiles(event):
         for con in range(0, int(prof)):
             profs = pphoto[con]
             await client(functions.photos.DeletePhotosRequest(id=[types.InputPhoto(id=profs.id, access_hash=profs.access_hash, file_reference=profs.file_reference)]))
-        await event.edit(client.get_string("DelProfiles_3").format(prof))
+        await event.edit(STRINGS["count"].format(prof))
     else:        
         if int(prof) > len(pphoto):
-            return await event.edit(f"**{client.str} This Profile Photo Is Not Available!**")
+            return await event.edit(STRINGS["nota"])
         pphoto = pphoto[int(prof)-1]
         await client(functions.photos.DeletePhotosRequest(id=[types.InputPhoto(id=pphoto.id, access_hash=pphoto.access_hash, file_reference=pphoto.file_reference)]))
-        await event.edit(client.get_string("DelProfiles_4").format(prof))
+        await event.edit(STRINGS["one"].format(prof))
