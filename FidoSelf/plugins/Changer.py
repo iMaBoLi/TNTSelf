482a117f02fbacf46cff715693b1f543bbf4f4f1
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-03 03:17:29 +0530

    update FidoSelf/plugins/Changer.py

--- FidoSelf/plugins/Changer.py
+++ FidoSelf/plugins/Changer.py
@@ -1,107 +1,45 @@
 from FidoSelf import client
 
 STRINGS = {
-    "EN": {
-        "Main": "**{STR} The {} Has Been {}!**",
-        "Name": "Name Mode",
-        "Bio": "Bio Mode",
-        "Photo": "Photo Mode",
-        "Dtimer": "Download Timer Medias",
-        "Quicks": "Quicks Mode",
-        "Monshi": "Monshi Self",
-        "Autodelete": "Auto Delete Messages",
-        "Autoreplace": "Auto Replace Mode",
-        "Autosay": "Auto Say Mode",
-        "Mutepv": "Mute Pv",
-        "Lockpv": "Lock Pv",
-        "Antispampv": "Anti Spam Pv",
-        "Readall": "MarkRead All",
-        "Readpv": "MarkRead Pvs",
-        "Readgp": "MarkRead Groups",
-        "Readch": "MarkRead Channels",
-    },
-    "FA": {
-        "Main": "**{STR} حالت {} باموفقیت {}!**",
-        "Name": "اسم",
-        "Bio": "بیو",
-        "Photo": "عکس",
-        "Dtimer": "دانلود مدیا های زماندار",
-        "Quicks": "پاسخ سریع",
-        "Monshi": "منشی سلف",
-        "Autodelete": "پاک کردن خودکار پیام ها",
-        "Autoreplace": "جایگزین کردن خودکار پیام ها",
-        "Autosay": "ادیت کردن خودکار پیام ها",
-        "Mutepv": "سکوت پیوی",
-        "Lockpv": "قفل پیوی",
-        "Antispampv": "آنتی اسپم پیوی",
-        "Readall": "خواندن همه پیام ها",
-        "Readpv": "خواندن پیام های پیوی",
-        "Readgp": "خواندن پیام های گروه",
-        "Readch": "خواندن پیام های چنل",
-    },
-}
-
-Modes = {
-    "EN": {
-        "Name": "NAME_MODE",
-        "Bio": "BIO_MODE",
-        "Photo": "PHOTO_MODE",
-        "Dtimer": "TIMER_MODE",
-        "Quicks": "QUICKS_MODE",
-        "Monshi": "MONSHI_MODE",
-        "Autodelete": "AUTO_DELETE_MODE",
-        "Autoreplace": "AUTO_REPLACE_MODE",
-        "Autosay": "AUTO_SAY_MODE",
-        "Mutepv": "MUTE_PV",
-        "Lockpv": "LOCK_PV",
-        "Antispampv": "ANTISPAM_PV",
-        "Readall": "READALL_MODE",
-        "Readpv": "READPV_MODE",
-        "Readgp": "READGP_MODE",
-        "Readch": "READCH_MODE",
-    },
-    "FA": {
-        "اسم": "NAME_MODE",
-        "بیو": "BIO_MODE",
-        "عکس": "PHOTO_MODE",
-        "دانلود تایمردار": "TIMER_MODE",
-        "پاسخ ها": "QUICKS_MODE",
-        "منشی": "MONSHI_MODE",
-        "حذف خودکار": "AUTO_DELETE_MODE",
-        "جایگزین خودکار": "AUTO_REPLACE_MODE",
-        "تایپ خودکار": "AUTO_SAY_MODE",
-        "سکوت پیوی": "MUTE_PV",
-        "قفل پیوی": "LOCK_PV",
-        "آنتی اسپم پیوی": "ANTISPAM_PV",
-        "خواندن همه": "READALL_MODE",
-        "خواندن پیوی": "READPV_MODE",
-        "خواندن گروه": "READGP_MODE",
-        "خواندن چنل": "READCH_MODE",
-    },
+    "Change": "**The {} Has Been {}!**",
+    "Not": "**The {} iS Already {}!**",
+    "Modes": {
+        "Name": ["Name Mode", "NAME_MODE"],
+        "Bio": ["Bio Mode", "BIO_MODE"],
+        "Photo": ["Photo Mode", "PHOTO_MODE"],
+        "Dtimer": ["Download Timer Medias", "TIMER_MODE"],
+        "Quicks": ["Quicks Mode", "QUICKS_MODE"]
+        "Monshi": ["Monshi Self", "MONSHI_MODE"],
+        "Autodelete": ["Auto Delete Messages", "AUTO_DELETE_MODE"],
+        "Autoreplace": ["Auto Replace Mode", "AUTO_REPLACE_MODE"],
+        "Autosay": ["Auto Say Mode", "AUTO_SAY_MODE"],
+        "Mutepv": ["Mute Pv", "MUTE_PV"],
+        "Lockpv": ["Lock Pv", "LOCK_PV"],
+        "Antispampv": ["Anti Spam Pv", "ANTISPAM_PV"],
+        "Readall": ["MarkRead All", "READALL_MODE"],
+        "Readpv": ["MarkRead Pvs", "READPV_MODE"],
+        "Readgp": ["MarkRead Groups", "READGP_MODE"],
+        "Readch": ["MarkRead Channels", "READCH_MODE"],
+    }
 }
 
 ENPAT = ""
-for mode in Modes["EN"]:
+for mode in STRINGS["Modes"]:
     ENPAT += mode + "|"
 ENPAT = ENPAT[:-1]
 
-FAPAT = ""
-for mode in Modes["FA"]:
-    FAPAT += mode + "|"
-FAPAT = FAPAT[:-1]
-
-@client.Command(
-    commands={
-        "EN": f"({ENPAT}) (On|Off)",
-        "FA": f"({FAPAT}) (روشن|خاموش)",
-     }
-)
+@client.Command(command="({ENPAT}) (On|Off)")
 async def changer(event):
-    await event.edit(client.get_string("wait"))
+    await event.edit(client.STRINGS["wait"])
     Mode = event.pattern_match.group(1).title()
-    Change = event.pattern_match.group(2).lower()   
-    client.DB.set_key(Modes[client.LANG][Mode], Change)
-    Mode = client.get_string(Mode, STRINGS)
-    Change = client.get_string("On") if Change == "on" else client.get_string("Off")
-    Main = client.get_string("Main", STRINGS)
-    await event.edit(Main.format(Mode, Change))
+    Change = event.pattern_match.group(2).lower()
+    EDMode = STRINGS["Modes"][Mode][0]
+    DBMode = STRINGS["Modes"][Mode][1]
+    stats = client.DB.get_key(DBMode)
+    if stats == Change:
+        text = STRINGS["Not"].format(EDMode, Change)
+        return await event.edit(text)
+    client.DB.set_key(DBMode, Change)
+    Change = client.STRINGS["On"] if Change == "on" else client.STRINGS["Off"]
+    text = STRINGS["Change"].format(EDMode, Change)
+    await event.edit(text)
