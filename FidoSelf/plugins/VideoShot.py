fde818dd11eeadea1237f8286a24910d04ba717d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-04 00:02:07 +0530

    update FidoSelf/plugins/VideoShot.py

--- FidoSelf/plugins/VideoShot.py
+++ FidoSelf/plugins/VideoShot.py
@@ -3,20 +3,28 @@ from telethon import functions, types
 import time
 import os
 
-@client.Command(pattern=f"(?i)^\{client.cmd}VShot ((\-)?\d*)$")
+STRINGS = {
+    "taking": "**Taking** ( `{}` ) **Screen Shot From Your Video ...**",
+    "taked": "**Taked** ( `{}` ) **Screen Shot From Your Video ...**",
+    "sending": "**Sending** ( `{}` ) **Screen Shots ...**",
+    "sended": "**Taked** ( `{}` ) **Screen Shot From Your Video!**",
+    "takingdur": "**Taking Screen Shot From Duration** ( `{}` ) **From Your Video ...**",
+    "takeddur": "**Taked Screen Shot From Duration** ( `{}` ) **From Your Video!**",
+}
+
+@client.Command(command="VShot ((\-)?\d*)")
 async def videoshot(event):
-    await event.edit(client.get_string("Wait"))
+    await event.edit(client.STRINGS["wait"])
     data = event.pattern_match.group(1)
-    mtype = client.mediatype(event.reply_message)
-    if not event.is_reply or mtype not in ["Video"]:
-        medias = client.get_string("ReplyMedia")
-        media = medias["Video"] 
-        if mtype == "Empty":
-            return await event.edit(client.get_string("ReplyMedia_Not").format(media))
-        else:
-            return await event.edit(client.get_string("ReplyMedia_Main").format(medias[mtype], media))
+    mtype = client.functions.mediatype(event.reply_message)
+    if not event.is_reply or mtype not in ["Photo"]:
+        medias = client.STRINGS["replyMedia"]
+        media = medias["Photo"]
+        rtype = medias[mtype]
+        text = client.STRINGS["replyMedia"]["Main"].format(rtype, media)
+        return await event.edit(text)
     if event.reply_message.file.size > client.MAX_SIZE:
-        return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
+        return await event.edit(client.STRINGS["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
     callback = event.progress(download=True)
     file = await event.reply_message.download_media(progress_callback=callback)
     duration = event.reply_message.file.duration
@@ -28,18 +36,18 @@ async def videoshot(event):
             count = duration
         files = []
         lastdur = 0
-        await event.edit(client.get_string("VideoShot_1").format(count))
+        await event.edit(STRINGS["taking"].format(count))
         for con in range(count):
             out = f"Shot-{con}.jpg"
             cmd = f"ffmpeg -i {file} -ss {lastdur} -vframes 1 {out}"
-            await client.utils.runcmd(cmd)
+            await client.functions.runcmd(cmd)
             files.append(out)
             lastdur += newdur
             if con == 0 or ((con + 1) % 3) == 0:
-                await event.edit(client.get_string("VideoShot_2").format(con + 1))
-        await event.edit(client.get_string("VideoShot_3").format(count))
-        caption = client.get_string("VideoShot_4").format(count)
-        for shots in list(client.utils.chunks(files, 9)):
+                await event.edit(STRINGS["taked"].format(con + 1))
+        await event.edit(STRINGS["sending"].format(count))
+        caption = STRINGS["sended"].format(count)
+        for shots in list(client.functions.chunks(files, 9)):
             await client.send_file(event.chat_id, shots, caption=caption)
         os.remove(file)
         for file in files:
@@ -48,11 +56,11 @@ async def videoshot(event):
     else:
         if int(data) > duration:
             data = duration - 1
-        await event.edit(client.get_string("VideoShot_5").format(data))
+        await event.edit(STRINGS["takingdur"].format(data))
         out = f"Shot-{data}.jpg"
         cmd = f"ffmpeg -i {file} -ss {int(data)} -vframes 1 {out}"
-        await client.utils.runcmd(cmd)
-        caption = client.get_string("VideoShot_6").format(str(data))
+        await client.functions.runcmd(cmd)
+        caption = STRINGS["takeddur"].format(str(data))
         await client.send_file(event.chat_id, out, caption=caption)
         os.remove(file)
         os.remove(out)
