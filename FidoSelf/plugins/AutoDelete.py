14d33ed8637b5234ffde969a95568cbcb606e04c
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-03 18:32:49 +0530

    update FidoSelf/plugins/AutoDelete.py

--- FidoSelf/plugins/AutoDelete.py
+++ FidoSelf/plugins/AutoDelete.py
@@ -1,7 +1,7 @@
 from FidoSelf import client
 import asyncio
 
-@client.Command(pattern=f"(?i)^\{client.cmd}SetAutoDeleteSleep (\d*)$")
+@client.Command(command="SetAutoDeleteSleep (\d*)")
 async def setautodeletesleep(event):
     await event.edit(client.get_string("Wait"))
     sleep = event.pattern_match.group(1)
@@ -9,7 +9,7 @@ async def setautodeletesleep(event):
     client.DB.set_key("AUTO_DELETE_SLEEP", sleep)
     await event.edit(client.get_string("AutoDelete_2").format(client.utils.convert_time(sleep)))
 
-@client.Command(edits=False)
+@client.Command(allow_edits=False)
 async def autodeletes(event):
     if event.is_cmd: return
     client.loop.create_task(trydelete(event))
@@ -23,17 +23,3 @@ async def trydelete(event):
         await event.delete()
     except:
         pass
-
-category = "Tools"
-plugin = "AutoDelete"
-note = "Delete Your Messages After A Time!"
-client.HELP.update({
-    plugin: {
-        "category": category,
-        "note": note,
-        "commands": {
-            "{CMD}AutoDelete <On|Off>": "To Active Or DeActive Auto Delete Mode",
-            "{CMD}Sleep AutoDelete <Min>": "To Set Sleep For Auto Delete Mode",
-        },
-    }
-})
