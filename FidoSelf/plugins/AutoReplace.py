2ff654e790c2a593a5d3d06a1992b289bcde033f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-03 17:03:35 +0530

    update FidoSelf/plugins/AutoReplace.py

--- FidoSelf/plugins/AutoReplace.py
+++ FidoSelf/plugins/AutoReplace.py
@@ -1,25 +1,12 @@
 from FidoSelf import client
 
-@client.Command()
+@client.Command(notcmd=True)
 async def replace(event):
-    #if event.is_cmd or not event.text: return
+    if event.is_cmd or not event.text: return
     mode = client.DB.get_key("AUTO_REPLACE_MODE") or "on"
     if mode == "on":
         try:
-            text = await client.vars(str(event.text), event)
+            text = await client.AddVars(str(event.text), event)
             await event.edit(text)
         except:
             pass
-
-category = "Tools"
-plugin = "AutoReplace"
-note = "Replace Variebels In Your Messages!"
-client.HELP.update({
-    plugin: {
-        "category": category,
-        "note": note,
-        "commands": {
-            "{CMD}AutoReplace <On|Off>": "To Active Or DeActive Auto Replace Mode",
-        },
-    }
-})
