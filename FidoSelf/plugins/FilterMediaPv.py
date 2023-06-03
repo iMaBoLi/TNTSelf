c9f3560b4f4f231a2af900d99d6d74caa8919ef5
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-03 18:49:19 +0530

    update FidoSelf/plugins/FilterMediaPv.py

--- FidoSelf/plugins/FilterMediaPv.py
+++ FidoSelf/plugins/FilterMediaPv.py
@@ -1,65 +1,80 @@
 from FidoSelf import client
 from telethon import Button
 
+STRINGS = {
+    "menu": "**Please Use The Buttons Below To Control The Filter Medias In Pv:**",
+    "types": {
+        "FILTERPV_TEXT": "Text",
+        "FILTERPV_PHOTO": "Photo",
+        "FILTERPV_VIDEO": "Video",
+        "FILTERPV_GIF": "Gif",
+        "FILTERPV_VOICE": "Voice",
+        "FILTERPV_MUSIC": "Music",
+        "FILTERPV_STICKER": "Sticker",
+        "FILTERPV_ANISTICKER": "Animated Sticker",
+        "FILTERPV_FILE": "File",
+        "FILTERPV_LINK": "Link",
+    },
+}
+
 def get_filter_buttons():
     buttons = []
-    TYPES = client.get_string("FilterPvType")
+    TYPES = STRINGS["types"]
     for type in TYPES:
         last = client.DB.get_key(type) or "off"
         smode = "( ✔️ )" if last == "on" else "( ✖️ )"
         cmode = "off" if last == "on" else "on"
         buttons.append(Button.inline(f"• {TYPES[type]} - {smode} •", data=f"setfilterpv:{type}:{cmode}"))
-    buttons = list(client.utils.chunks(buttons, 2))
-    buttons = client.get_buttons(buttons)
+    buttons = list(client.functions.chunks(buttons, 2))
     return buttons
 
-@client.Command(pattern=f"(?i)^\{client.cmd}SFilterPv$")
+@client.Command(command="AddPvFilter")
 async def filterpvmedia(event):
-    await event.edit(client.get_string("Wait"))
+    await event.edit(client.STRINGS["wait"])
     res = await client.inline_query(client.bot.me.username, "filtermediapv")
     await res[0].click(event.chat_id)
     await event.delete()
 
 @client.Inline(pattern="filtermediapv")
 async def inlinefilterpanel(event):
-    text = client.get_string("FilterMediaPv_1")
+    text = STRINGS["menu"]
     buttons = get_filter_buttons()
-    await event.answer([event.builder.article(f"{client.str} FidoSelf - Filter Media Pv", text=text, buttons=buttons)])
+    await event.answer([event.builder.article("FidoSelf - Filter Media Pv", text=text, buttons=buttons)])
 
 @client.Callback(data="setfilterpv\:(.*)\:(.*)")
 async def setfilterpvs(event):
     mode = event.data_match.group(1).decode('utf-8')
     change = event.data_match.group(2).decode('utf-8')
     client.DB.set_key(mode, change)
-    text = client.get_string("FilterMediaPv_1")
+    text = STRINGS["menu"]
     buttons = get_filter_buttons()
     await event.edit(text=text, buttons=buttons)
 
-@client.Command(sudo=False, edits=False)
+@client.Command(onlysudo=False, allowedits=False)
 async def mediafilter(event):
     if not event.is_private or event.is_white or event.is_sudo or event.is_bot: return
-    TYPES = client.get_string("FilterPvType")
+    TYPES = STRINGS["types"]
     MODES = {}
     for type in TYPES:
         mode = client.DB.get_key(type) or "off" 
         MODES.update({type: mode})
     if event.text and MODES["FILTERPV_TEXT"] == "on":
         await event.delete()
-    elif client.mediatype(event) == "photo" and MODES["FILTERPV_PHOTO"] == "on":
+    elif client.functions.mediatype(event) == "Photo" and MODES["FILTERPV_PHOTO"] == "on":
         await event.delete()
-    elif client.mediatype(event) == "video" and MODES["FILTERPV_VIDEO"] == "on":
+    elif client.functions.mediatype(event) == "Video" and MODES["FILTERPV_VIDEO"] == "on":
         await event.delete()
-    elif client.mediatype(event) == "gif" and MODES["FILTERPV_GIF"] == "on":
+    elif client.functions.mediatype(event) == "Gif" and MODES["FILTERPV_GIF"] == "on":
         await event.delete()
-    elif client.mediatype(event) == "voice" and MODES["FILTERPV_VOICE"] == "on":
+    elif client.functions.mediatype(event) == "Voice" and MODES["FILTERPV_VOICE"] == "on":
         await event.delete()
-    elif client.mediatype(event) == "music" and MODES["FILTERPV_MUSIC"] == "on":
+    elif client.functions.mediatype(event) == "Music" and MODES["FILTERPV_MUSIC"] == "on":
         await event.delete()
-    elif client.mediatype(event) == "sticker" and MODES["FILTERPV_STICKER"] == "on":
+    elif client.functions.mediatype(event) == "Sticker" and MODES["FILTERPV_STICKER"] == "on":
         await event.delete()
-    elif client.mediatype(event) == "animated sticker" and MODES["FILTERPV_ANISTICKER"] == "on":
+    elif client.functions.mediatype(event) == "ASticker" and MODES["FILTERPV_ANISTICKER"] == "on":
         await event.delete()
-    elif client.mediatype(event) == "file" and MODES["FILTERPV_FILE"] == "on":
+    elif client.functions.mediatype(event) == "File" and MODES["FILTERPV_FILE"] == "on":
         await event.delete()
     elif event.text and MODES["FILTERPV_LINK"] == "on":
         if event.entities:
