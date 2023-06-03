c4b4625ee5c22d9ed9d4659398beb288346bb366
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-03 14:17:00 +0530

    update FidoSelf/plugins/SetVars.py

--- FidoSelf/plugins/SetVars.py
+++ FidoSelf/plugins/SetVars.py
@@ -1,77 +1,26 @@
 from FidoSelf import client
 
-@client.Command(pattern=f"(?i)^\{client.cmd}SetStr (.*)$")
-async def messagesstarter(event):
-    await event.edit(client.get_string("Wait"))
-    string = event.pattern_match.group(1)
-    client.DB.set_key("MESSAGES_STARTER", str(string))
-    client.str = str(string)
-    await event.edit(client.get_string("Vars_1").format(string))
+STRINGS = {
+    "norealm": "**Please Send In Group For Added Realm Chat!**",
+    "setrealm": "**This Chat Is Saved For Realm Chat!**",
+    "noback": "**Please Send In Channel For Added BackUp Or Support Channel!**",
+    "setback": "**This Channel Is Saved For BackUp Channel!**",
+}
 
-@client.Command(pattern=f"(?i)^\{client.cmd}SetCmd (.*)$")
-async def cmdmessage(event):
-    await event.edit(client.get_string("Wait"))
-    cmd = event.pattern_match.group(1)
-    client.DB.set_key("SELF_CMD", str(cmd))
-    client.cmd = str(cmd)
-    await event.edit(client.get_string("Vars_2").format(cmd))
-
-@client.Command(pattern=f"(?i)^.DelCmd$")
-async def dcmdmessage(event):
-    await event.edit(client.get_string("Wait"))
-    client.DB.set_key("SELF_CMD", ".")
-    client.cmd = "."
-    await event.edit(client.get_string("Vars_3"))
-
-@client.Command(pattern=f"(?i)^\{client.cmd}SetRealm$")
+@client.Command(command="SetRealm")
 async def realm(event):
-    await event.edit(client.get_string("Wait"))
+    await event.edit(client.STRINGS["wait"])
     if not event.is_group:
-        return await event.edit(client.get_string("Vars_4"))
+        return await event.edit(client.STRINGS["norealm"])
     client.DB.set_key("REALM_CHAT", event.chat_id)
     client.realm = event.chat_id
-    await event.edit(client.get_string("Vars_5"))
+    await event.edit(client.STRINGS["setrealm"])
 
-@client.Command(pattern=f"(?i)^\{client.cmd}SetBackCh$")
+@client.Command(command="SetBackCh")
 async def backch(event):
-    await event.edit(client.get_string("Wait"))
+    await event.edit(client.STRINGS["wait"])
     if not event.is_ch:
-        return await event.edit(client.get_string("Vars_6"))
+        return await event.edit(client.STRINGS["noback"])
     client.DB.set_key("BACKUP_CHANNEL", event.chat_id)
     client.backch = event.chat_id
-    await event.edit(client.get_string("Vars_7"))
-
-@client.Command(pattern=f"(?i)^\{client.cmd}SetHelpEmoji (.*)$")
-async def helpemoji(event):
-    await event.edit(client.get_string("Wait"))
-    string = event.pattern_match.group(1)
-    client.DB.set_key("HELP_EMOJI", str(string))
-    await event.edit(client.get_string("Vars_8").format(string))
-
-category = "Settings"
-plugin = "SetChats"
-note = "Set Coustom Realm Chat And BackUp Channel!"
-client.HELP.update({
-    plugin: {
-        "category": category,
-        "note": note,
-        "commands": {
-            "{CMD}SetRealm": "Set Realm Chat",
-            "{CMD}SetBackCh": "Set BackUp Channel",
-        },
-    }
-})
-plugin = "Variebels"
-note = "Set Command Starter And Message Starter!"
-client.HELP.update({
-    plugin: {
-        "category": category,
-        "note": note,
-        "commands": {
-            "{CMD}SetCmd <Text>": "Set Commands Stater Sambol",
-            ".DelCmd": "Delete Commands Stater Sambol",
-            "{CMD}SetStr <Text>": "Set Message Starter Sambol",
-            "{CMD}SetHelpEmoji <Text>": "Set Emoji In Help Sambol",
-        },
-    }
-})
+    await event.edit(client.STRINGS["setback"])
