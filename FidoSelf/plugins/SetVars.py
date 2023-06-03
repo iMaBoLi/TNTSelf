858ce19adc736ed4888bffc2bf93d0361892d5cf
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-03 14:21:13 +0530

    update FidoSelf/plugins/SetVars.py

--- FidoSelf/plugins/SetVars.py
+++ FidoSelf/plugins/SetVars.py
@@ -11,16 +11,16 @@ STRINGS = {
 async def realm(event):
     await event.edit(client.STRINGS["wait"])
     if not event.is_group:
-        return await event.edit(client.STRINGS["norealm"])
+        return await event.edit(STRINGS["norealm"])
     client.DB.set_key("REALM_CHAT", event.chat_id)
     client.realm = event.chat_id
-    await event.edit(client.STRINGS["setrealm"])
+    await event.edit(STRINGS["setrealm"])
 
 @client.Command(command="SetBackCh")
 async def backch(event):
     await event.edit(client.STRINGS["wait"])
     if not event.is_ch:
-        return await event.edit(client.STRINGS["noback"])
+        return await event.edit(STRINGS["noback"])
     client.DB.set_key("BACKUP_CHANNEL", event.chat_id)
     client.backch = event.chat_id
-    await event.edit(client.STRINGS["setback"])
+    await event.edit(STRINGS["setback"])
