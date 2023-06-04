4d57291c2536f0c0d67c331b2089ccbfe1d3dd8f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-04 13:50:43 +0330

    update FidoSelf/functions/Config.py

---
+++ FidoSelf/functions/Config.py
@@ -0,0 +1,19 @@
+#Infos
+API_ID = 
+API_HASH = ""
+SESSION = ""
+BOT_SESSION = ""
+
+#Database
+REDIS_URL = ""
+REDIS_PASSWORD = ""
+
+#Expire
+START_TIME = 
+REMAIN_TIME = 
+
+#Keys
+OCR_APIKEY = ""
+
+#Other
+COMMANDS_STARTER = "."
