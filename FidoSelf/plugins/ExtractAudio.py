a9766301dd5cea8c63c77bb5b1da091f4df2724f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-04 15:10:35 +0530

    update FidoSelf/plugins/ExtractAudio.py

--- FidoSelf/plugins/ExtractAudio.py
+++ FidoSelf/plugins/ExtractAudio.py
@@ -1,25 +1,31 @@
 from FidoSelf import client
 import os
-import time
 
-@client.Command(pattern=f"(?i)^\{client.cmd}SAudio$")
+STRINGS = {
+    "exing": "**Extracting Audio From Video ...**",
+    "exed": "**The Audio Was Extracted From Video!**",
+}
+
+@client.Command(command="ExAudio")
 async def exaudio(event):
-    await event.edit(client.get_string("Wait"))
-    if not event.is_reply or not event.reply_message.video:
-        return await event.edit(client.get_string("Reply_V"))
+    await event.edit(client.STRINGS["wait"])
+    mtype = client.functions.mediatype(event.reply_message)
+    if not event.is_reply or mtype not in ["Video"]:
+        medias = client.STRINGS["replyMedia"]
+        media = medias["Video"]
+        rtype = medias[mtype]
+        text = client.STRINGS["replyMedia"]["Main"].format(rtype, media)
+        return await event.edit(text)
     if event.reply_message.file.size > client.MAX_SIZE:
-        return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
-    newtime = time.time()
-    file_name = event.reply_message.file.name or "---"
-    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down", file_name))
+        return await event.edit(client.STRINGS["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
+    callback = event.progress(download=True)
     video = await event.reply_message.download_media(progress_callback=callback)
-    await event.edit(client.get_string("ExAudio_1"))
+    await event.edit(STRINGS["exing"])
     newfile = f"EcAudio-{video}.mp3"
     cmd = f'ffmpeg -i {video} -vn -acodec copy {newfile}'
-    await client.utils.runcmd(cmd)
-    newtime = time.time()
-    callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up"))
-    caption = client.get_string("ExAudio_2")
+    await client.functions.runcmd(cmd)
+    callback = event.progress(upload=True)
+    caption = STRINGS["exed"]
     await client.send_file(event.chat_id, newfile, caption=caption, progress_callback=callback)        
     os.remove(video)
     os.remove(newfile)
