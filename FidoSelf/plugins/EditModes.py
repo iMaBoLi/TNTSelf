ae23ed7773d21c91d87fcdee44eaa1a2a3f4bdde
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-04 15:22:30 +0330

    Update EditModes.py

--- FidoSelf/plugins/EditModes.py
+++ FidoSelf/plugins/EditModes.py
@@ -1,19 +1,6 @@
 from FidoSelf import client
 
-@client.Command(pattern=f"(?i)^\{client.cmd}E(Bold|Mono|Italic|Underline|Strike|Spoiler|Hashtag) (On|off)$")
-async def bio(event):
-    await event.edit(client.get_string("Wait"))
-    mode = event.pattern_match.group(1).lower()
-    change = event.pattern_match.group(2).lower()
-    changer = client.get_string("Change_1") if change == "on" else client.get_string("Change_2")
-    if change == "on":
-        client.DB.set_key("EDIT_MODE", mode.title())
-    else:
-        client.DB.del_key("EDIT_MODE")
-    mode = client.get_string(f"Edits_{mode.title()}")
-    await event.edit(client.get_string("EditMode_1").format(mode.title(), changer))
-
-@client.Command(edits=False)
+@client.Command(notcmd=True, alowedits=False)
 async def editmodes(event):
     if event.is_cmd or not event.text: return
     mode = client.DB.get_key("EDIT_MODE") or ""
