3f1d82ec4c044aeedd18ac23bf1c7e481b1a53de
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-04 15:02:14 +0330

    update FidoSelf/plugins/MainPanel.py

--- FidoSelf/plugins/MainPanel.py
+++ FidoSelf/plugins/MainPanel.py
@@ -41,51 +41,51 @@ STRINGS = {
     "Random2": "Random V2",
 }
 
-PAGES_COUNT = 3
-
-def get_pages_button(page):
+def get_pages_button():
     buttons = []
-    if page > 1:
-        buttons.append(Button.inline(client.STRINGS["inline"]["Back"], data=f"panelpage:{page-1}"))
-    if page < PAGES_COUNT:
-        buttons.append(Button.inline(client.STRINGS["inline"]["Next"], data=f"panelpage:{page+1}"))
-    buttons.append(Button.inline(client.STRINGS["inline"]["Close"], data="closepanel"))
+    PAGES_COUNT = 5
+    for page in range(1, PAGES_COUNT):
+        name = client.functions.create_font(page, 3)
+        buttons.append(Button.inline(f"( {name} )", data=f"panelpage:{page}"))
     return buttons
 
-def get_mode_buttons(page):
+def get_mode_buttons():
     buttons = []
     MODES = STRINGS["Modes"]
     for mode in MODES:
         if mode in ["SELF_ALL_MODE", "QUICKS_MODE", "AUTO_REPLACE_MODE"] and not client.DB.get_key(mode):
-            buttons.append([Button.inline(f"• {MODES[mode]} •", data=f"setmode:{page}:{mode}:off"), Button.inline(client.STRINGS["inline"]["On"], data=f"setmode:{page}:{mode}:off")])
+            buttons.append([Button.inline(f"• {MODES[mode]} •", data=f"setmode:{mode}:off"), Button.inline(client.STRINGS["inline"]["On"], data=f"setmode:{mode}:off")])
         else:
             gmode = client.DB.get_key(mode) or "off"
             cmode = "on" if gmode == "off" else "off"
-            buttons.append([Button.inline(f"• {MODES[mode]} •", data=f"setmode:{page}:{mode}:{cmode}"), Button.inline((client.STRINGS["inline"]["On"] if gmode == "on" else client.STRINGS["inline"]["Off"]), data=f"setmode:{page}:{mode}:{cmode}")])
-    pgbts = get_pages_button(page)
+            buttons.append([Button.inline(f"• {MODES[mode]} •", data=f"setmode:{mode}:{cmode}"), Button.inline((client.STRINGS["inline"]["On"] if gmode == "on" else client.STRINGS["inline"]["Off"]), data=f"setmode:{mode}:{cmode}")])
+    pgbts = get_pages_button()
     buttons.append(pgbts)
+    buttons.append([Button.inline(client.STRINGS["inline"]["Close"], data="closepanel")])
     return buttons
 
-def get_time_buttons(page):
+def get_time_buttons():
     newtime = datetime.now().strftime("%H:%M")
     last = client.DB.get_key("TIME_FONT") or 1
     buttons = []
-    buttons.append([Button.inline(f'• {STRINGS["Random1"]} •', data=f"setfonttime:{page}:random"), Button.inline((client.STRINGS["inline"]["On"] if str(last) == "random" else client.STRINGS["inline"]["Off"]), data=f"setfonttime:{page}:random")])
-    buttons.append([Button.inline(f'• {STRINGS["Random2"]} •', data=f"setfonttime:{page}:random2"), Button.inline((client.STRINGS["inline"]["On"] if str(last) == "random2" else client.STRINGS["inline"]["Off"]), data=f"setfonttime:{page}:random2")])
+    buttons.append([Button.inline(f'• {STRINGS["Random1"]} •', data=f"setfonttime:random"), Button.inline((client.STRINGS["inline"]["On"] if str(last) == "random" else client.STRINGS["inline"]["Off"]), data=f"setfonttime:random")])
+    buttons.append([Button.inline(f'• {STRINGS["Random2"]} •', data=f"setfonttime:random2"), Button.inline((client.STRINGS["inline"]["On"] if str(last) == "random2" else client.STRINGS["inline"]["Off"]), data=f"setfonttime:random2")])
     for font in FONTS:
-        buttons.append([Button.inline(f"• {create_font(newtime, font)} •", data=f"setfonttime:{page}:{font}"), Button.inline((client.STRINGS["inline"]["On"] if str(last) == str(font) else client.STRINGS["inline"]["Off"]), data=f"setfonttime:{page}:{font}")])
-    pgbts = get_pages_button(page)
+        buttons.append([Button.inline(f"• {create_font(newtime, font)} •", data=f"setfonttime:{font}"), Button.inline((client.STRINGS["inline"]["On"] if str(last) == str(font) else client.STRINGS["inline"]["Off"]), data=f"setfonttime:{font}")])
+    pgbts = get_pages_button()
     buttons.append(pgbts)
+    buttons.append([Button.inline(client.STRINGS["inline"]["Close"], data="closepanel")])
     return buttons
 
-def get_edit_buttons(page):
+def get_edit_buttons():
     last = client.DB.get_key("EDIT_MODE")
     buttons = []
     EDITS = STRINGS["Edits"]
     for edit in EDITS:
-        buttons.append([Button.inline(f"• {EDITS[edit]} •", data=f"seteditmode:{page}:{edit}"), Button.inline((client.STRINGS["inline"]["On"] if str(last) == str(edit) else client.STRINGS["inline"]["Off"]), data=f"seteditmode:{page}:{edit}")])
-    pgbts = get_pages_button(page)
+        buttons.append([Button.inline(f"• {EDITS[edit]} •", data=f"seteditmode:{edit}"), Button.inline((client.STRINGS["inline"]["On"] if str(last) == str(edit) else client.STRINGS["inline"]["Off"]), data=f"seteditmode:{edit}")])
+    pgbts = get_pages_button()
     buttons.append(pgbts)
+    buttons.append([Button.inline(client.STRINGS["inline"]["Close"], data="closepanel")])
     return buttons
 
 @client.Command(command="Panel")
@@ -98,7 +98,7 @@ async def addecho(event):
 @client.Inline(pattern="selfmainpanel")
 async def inlinepanel(event):
     text = STRINGS["modepage"]
-    buttons = get_mode_buttons(1)
+    buttons = get_mode_buttons()
     await event.answer([event.builder.article("FidoSelf - Panel", text=text, buttons=buttons)])
 
 @client.Callback(data="panelpage\:(.*)")
@@ -106,45 +106,42 @@ async def panelpages(event):
     page = int(event.data_match.group(1).decode('utf-8'))
     if page == 1:
         text = STRINGS["modepage"]
-        buttons = get_mode_buttons(page)
+        buttons = get_mode_buttons()
         await event.edit(text=text, buttons=buttons)
     elif page == 2:
         text = STRINGS["fontpage"]
-        buttons = get_time_buttons(page)
+        buttons = get_time_buttons()
         await event.edit(text=text, buttons=buttons)
     elif page == 3:
         text = STRINGS["editpage"]
-        buttons = get_edit_buttons(page)
+        buttons = get_edit_buttons()
         await event.edit(text=text, buttons=buttons)
 
-@client.Callback(data="setmode\:(.*)\:(.*)\:(.*)")
+@client.Callback(data="setmode\:(.*)\:(.*)")
 async def setmode(event):
-    page = int(event.data_match.group(1).decode('utf-8'))
-    mode = event.data_match.group(2).decode('utf-8')
-    change = event.data_match.group(3).decode('utf-8')
+    mode = event.data_match.group(1).decode('utf-8')
+    change = event.data_match.group(2).decode('utf-8')
     client.DB.set_key(mode, change)
     text = STRINGS["modepage"]
-    buttons = get_mode_buttons(page)
+    buttons = get_mode_buttons()
     await event.edit(text=text, buttons=buttons)
 
-@client.Callback(data="setfonttime\:(.*)\:(.*)")
+@client.Callback(data="setfonttime\:(.*)")
 async def setfonttime(event):
-    page = int(event.data_match.group(1).decode('utf-8'))
-    font = event.data_match.group(2).decode('utf-8')
+    font = event.data_match.group(1).decode('utf-8')
     client.DB.set_key("TIME_FONT", str(font))
-    buttons = get_time_buttons(page)
+    buttons = get_time_buttons()
     await event.edit(buttons=buttons)
 
 @client.Callback(data="seteditmode\:(.*)\:(.*)")
 async def seteditmode(event):
-    page = int(event.data_match.group(1).decode('utf-8'))
-    edit = event.data_match.group(2).decode('utf-8')
+    edit = event.data_match.group(1).decode('utf-8')
     last = client.DB.get_key("EDIT_MODE")
     if str(last) == str(edit):
         client.DB.set_key("EDIT_MODE", False)
     else:
         client.DB.set_key("EDIT_MODE", str(edit))
-    buttons = get_edit_buttons(page)
+    buttons = get_edit_buttons()
     await event.edit(buttons=buttons)
 
 @client.Callback(data="closepanel")
