2670f2833aa295b16e0f6f013fbc28f27033788d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-05 19:34:30 +0330

    update FidoSelf/plugins/AiImage.py

--- FidoSelf/plugins/AiImage.py
+++ FidoSelf/plugins/AiImage.py
@@ -6,14 +6,14 @@ STRINGS = {
     "caption": "**The Image For Query** ( `{}` ) **And StyleID** ( `{}` ) **Created!**",
 }
 
-@client.Command(command="GPhoto (.*)\:?(\d*)?")
+@client.Command(command="GPhoto \'(\d*)\' (.*)")
 async def generatephoto(event):
     await event.edit(client.STRINGS["wait"])
     client.loop.create_task(generate(event))
     
 async def generate(event):
-    query = event.pattern_match.group(1)
-    style = event.pattern_match.group(2) or 84
+    style = event.pattern_match.group(1)
+    query = event.pattern_match.group(2)
     await event.edit(STRINGS["gen"].format(query, style))
     file = Somnium.Generate(query, style)
     caption = STRINGS["caption"].format(query, style)
