f09a4d70399522d7c084078c9c5624a5083573b2
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-05 12:46:02 +0330

    update FidoSelf/plugins/Foshs.py

---
+++ FidoSelf/plugins/Foshs.py
@@ -0,0 +1,44 @@
+from FidoSelf import client
+import os
+
+STRINGS = {
+    "save": "**The Enemy Foshs File Has Been Saved!**",
+    "del": "**The Enemy Foshs File Has Been Deleted!**",
+    "nsave": "**The Enemy Foshs File Is Not Saved!**",
+    "file": "**The Foshs File!**\n**Count:** ( `{}` )",
+}
+
+@client.Command(command="AddFosh")
+async def savefoshfile(event):
+    await event.edit(client.STRINGS["wait"])
+    mtype = client.functions.mediatype(event.reply_message)
+    if not event.is_reply or mtype not in ["File"]:
+        medias = client.STRINGS["replyMedia"]
+        media = medias["File"]
+        rtype = medias[mtype]
+        text = client.STRINGS["replyMedia"]["Main"].format(rtype, media)
+        return await event.edit(text)
+    format = str(event.reply_message.media.document.attributes[0].file_name).split(".")[-1]
+    if format != "txt":
+        return await event.edit(STRINGS["txt"])
+    info = await event.reply_message.save()
+    client.DB.set_key("FOSHS_FILE", info)
+     await event.edit(STRINGS["save"])
+
+@client.Command(command="DelFosh")
+async def delfoshfile(event):
+    await event.edit(client.STRINGS["wait"])
+    client.DB.del_key("FOSHS_FILE")
+    await event.edit(STRINGS["del"])
+
+@client.Command(command="GetFosh")
+async def getfoshfile(event):
+    await event.edit(client.STRINGS["wait"])
+    foshs = client.DB.get_key("FOSHS_FILE")
+    if not foshs:
+        return await event.edit(STRINGS["nsave"])
+    get = await client.get_messages(int(foshs["chat_id"]), ids=int(foshs["msg_id"]))
+    file = await get.download_media()
+    lines = len(open(file, "r").read_lines())
+    await event.respond(STRINGS["file"].format(lines), file=file)
+    os.remove(file)
