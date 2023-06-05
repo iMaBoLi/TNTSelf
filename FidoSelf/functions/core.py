743b2f0eec2205bafeeefae81583b9a9372abcf7
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-05 10:35:31 +0330

    update FidoSelf/functions/core.py

--- FidoSelf/functions/core.py
+++ FidoSelf/functions/core.py
@@ -1,10 +1,14 @@
 from FidoSelf import client
+from telethon.types import Message
 import re
 
-def check_cmd(text):
+def check_cmd(event):
+    if not event.text: return False
     commands = client.DB.get_key("SELFCOMMANDS") or []
     for command in commands:
-        search = re.search(command, text)
+        search = re.search(command, event.text)
         if search:
             return True
     return False
+
+setattr(Message, "checkCmd", check_cmd)
