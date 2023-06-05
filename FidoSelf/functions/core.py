30fec201e93151fd728bba328d549c841c94260c
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-06 01:19:26 +0330

    update FidoSelf/functions/core.py

--- FidoSelf/functions/core.py
+++ FidoSelf/functions/core.py
@@ -16,15 +16,34 @@ setattr(Message, "checkCmd", check_cmd)
 async def DownloadFiles():
     foshs = client.DB.get_key("FOSHS_FILE")
     if foshs:
-        get = await client.get_messages(int(foshs["chat_id"]), ids=int(foshs["msg_id"]))
-        await get.download_media("FOSHS.txt")
+        try:
+            get = await client.get_messages(int(foshs["chat_id"]), ids=int(foshs["msg_id"]))
+            await get.download_media("FOSHS.txt")
+        except:
+            pass
+
+    cover = client.DB.get_key("FILE_COVER")
+    if cover:
+        try:
+            get = await client.get_messages(int(cover["chat_id"]), ids=int(cover["msg_id"]))
+            await get.download_media("Cover.png")
+        except:
+            pass
+
     photos = client.DB.get_key("PHOTOS")
     if photos:
         for photo in photos:
-            get = await client.get_messages(int(photos[photo]["chat_id"]), ids=int(PHOTOS[photo]["msg_id"]))
-            await get.download_media(photo)
+            try:
+                get = await client.get_messages(int(photos[photo]["chat_id"]), ids=int(PHOTOS[photo]["msg_id"]))
+                await get.download_media(photo)
+            except:
+                pass
+            
     fonts = client.DB.get_key("FONTS")
     if fonts:
         for font in fonts:
-            get = await client.get_messages(int(fonts[font]["chat_id"]), ids=int(fonts[font]["msg_id"]))
-            await get.download_media(font)
+            try:
+                get = await client.get_messages(int(fonts[font]["chat_id"]), ids=int(fonts[font]["msg_id"]))
+                await get.download_media(font)
+            except:
+                pass
