03faa9e0ecd31dda71967ceab77a12076e31a799
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-05 16:45:27 +0330

    update FidoSelf/plugins/Rotater.py

--- FidoSelf/plugins/Rotater.py
+++ FidoSelf/plugins/Rotater.py
@@ -3,36 +3,43 @@ from PIL import Image
 import os
 import time
 
-@client.Command(pattern=f"(?i)^\{client.cmd}SRotate (\d*)$")
+STRINGS = {
+    "proted": "**The Photo Was Rotated To** ( `{}°` )",
+    "vrot": "**Rotating Video To** ( `{}°` ) **...*",
+    "vroted": "**The Video Was Rotated To** ( `{}°` )",
+}
+
+@client.Command(command="SRotate (\d*)")
 async def rotate(event):
-    await event.edit(client.get_string("Wait"))
+    await event.edit(client.STRINGS["wait"])
     darge = int(event.pattern_match.group(1))
-    if event.is_reply and event.reply_message.photo:
-        photo = await event.reply_message.download_media()
+    mtype = client.functions.mediatype(event.reply_message)
+    if not event.is_reply or mtype not in ["File", "Music"]:
+        medias = client.STRINGS["replyMedia"]
+        media = medias["File"] + " - " + medias["Music"]
+        rtype = medias[mtype]
+        text = client.STRINGS["replyMedia"]["Main"].format(rtype, media)
+        return await event.edit(text)
+    if event.reply_message.file.size > client.MAX_SIZE:
+        return await event.edit(client.STRINGS["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
+    if mtype == "Photo":
+        file = await event.reply_message.download_media()
         newfile = f"RotatedImage-{str(darge)}.jpg"
-        img = Image.open(photo)
+        img = Image.open(file)
         newimg = img.rotate(darge)
         newimg.save(newfile)
-        await event.respond(client.get_string("Rotater_1").format(str(darge)), file=newfile)        
-        os.remove(photo)
-        os.remove(newfile)
-        await event.delete()
-    elif event.is_reply and event.reply_message.video:
-        if event.reply_message.file.size > client.MAX_SIZE:
-            return await event.edit(client.get_string("LargeSize").format(client.utils.convert_bytes(client.MAX_SIZE)))
-        newtime = time.time()
-        callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "down"))
-        video = await event.reply_message.download_media(progress_callback=callback)
-        await event.edit(client.get_string("Rotater_2").format(str(darge)))
+        await event.respond(STRINGS["proted"].format(str(darge)), file=newfile)        
+    elif mtype == "Video":
+        callback = event.progress(download=True)
+        file = await event.reply_message.download_media(progress_callback=callback)
+        await event.edit(STRINGS["vrot"].format(str(darge)))
         newfile = f"RotatedVideo-{str(darge)}.mp4"
-        cmd = f'ffmpeg -i {video} -vf "rotate={darge}" {newfile}'
+        cmd = f'ffmpeg -i {file} -vf "rotate={darge}" {newfile}'
         await client.utils.runcmd(cmd)
         newtime = time.time()
-        callback = lambda start, end: client.loop.create_task(client.progress(event, start, end, newtime, "up"))
-        caption = client.get_string("Rotater_3").format(str(darge))
+        callback = event.progress(upload=True)
+        caption = STRINGS["vroted"].format(str(darge))
         await client.send_file(event.chat_id, newfile, caption=caption, progress_callback=callback)        
-        os.remove(video)
-        os.remove(newfile)
-        await event.delete()
-    else:
-        await event.edit(client.get_string("Reply_PV"))
+    os.remove(file)
+    os.remove(newfile)
+    await event.delete()
