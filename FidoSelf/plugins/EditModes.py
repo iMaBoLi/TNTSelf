bd8ce76bad2fdc28bde7b18f02c0ce586164678d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-05 12:02:52 +0330

    update FidoSelf/plugins/EditModes.py

--- FidoSelf/plugins/EditModes.py
+++ FidoSelf/plugins/EditModes.py
@@ -1,8 +1,6 @@
 from FidoSelf import client
 
 EDITS ={
-    "Sign",
-    "Poker",
     "Bold",
     "Mono",
     "Italic",
@@ -14,24 +12,23 @@ EDITS ={
 
 @client.Command(alowedits=False)
 async def editmodes(event):
-    if not event.text: return
-    if event.checkCmd(): return
-    mode = client.DB.get_key("EDITALL_MODE") or ""
+    if not event.text or event.checkCmd(): return
+    allmode = client.DB.get_key("EDITALL_MODE") or ""
+    chats = client.DB.get_key("EDITCHATS_MODE") or {}
     lasttext = str(event.text)
-    if not mode:
-        return
-    elif mode == "Bold":
+    if allmode == "Bold" or (event.chat_id in chats and chats[event.chat_id] == "Bold"):
         await event.edit("**" + lasttext + "**")
-    elif mode == "Mono":
+    elif allmode == "Mono" or (event.chat_id in chats and chats[event.chat_id] == "Mono"):
         await event.edit("`" + lasttext + "`")
-    elif mode == "Italic":
+    elif allmode == "Italic" or (event.chat_id in chats and chats[event.chat_id] == "Italic"):
         await event.edit("__" + lasttext + "__")
-    elif mode == "Underline":
+    elif allmode == "Underline" or (event.chat_id in chats and chats[event.chat_id] == "Underline"):
         await event.edit("<u>" + lasttext + "</u>", parse_mode="HTML")
-    elif mode == "Strike":
+    elif allmode == "Strike" or (event.chat_id in chats and chats[event.chat_id] == "Strike"):
         await event.edit("~~" + lasttext + "~~")
-    elif mode == "Spoiler":
+    elif allmode == "Spoiler" or (event.chat_id in chats and chats[event.chat_id] == "Spoiler"):
         await event.edit("||" + lasttext + "||")
-    elif mode == "Hashtag":
+    elif allmode == "Hashtag" or  or (event.chat_id in chats and chats[event.chat_id] == "Hashtag"):
         lasttext = lasttext.replace(" ", "_")
+        lasttext = lasttext.replace("\n", "_")
         await event.edit("#" + lasttext)
