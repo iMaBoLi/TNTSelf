8c9be4d0b6d91df066835ef64df680f27828cd2e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-05 18:41:03 +0330

    update FidoSelf/plugins/OcrApi.py

--- FidoSelf/plugins/OcrApi.py
+++ FidoSelf/plugins/OcrApi.py
@@ -1,6 +1,34 @@
 from FidoSelf import client
 import requests, os
 
+STRINGS = {
+    "setap": "**The Ocr ApiKey** ( `{}` ) **Has Been Saved!**",
+    "notsave": "**The Ocr ApiKey Is Not Saved!**",
+    "notlang": "**The Entered Language Is Not Found!**",
+    "notcom": "**The Extract Text Not Completed!**\n**Error:** ( `{}` )",
+    "com": "**The Extract Text Completed!**\n**Language:** ( `{}` )\n\n**Result:** ( `{}` )",
+    "langs": "**The Available OcrApi Languages:**\n\n",
+}
+LANGS = {
+    "ara": "Arabic",
+    "eng": "English",
+    "fre": "French",
+    "kor": "Korean",
+    "ita": "Italian",
+    "jpn": "Japanese",
+    "chs": "Chinese",
+    "ger": "German",
+    "spa": "Spanish",
+    "swe": "Swedish",
+    "tur": "Turkish",
+    "bul": "Bulgarian",
+    "pol": "Polish",
+    "por": "Portugal",
+    "rus": "Russian",
+    "hrv": "Croatian",
+    "hin": "Hindi",
+}
+
 def ocr_file(file, language):
     payload = {
         "isOverlayRequired": True,
@@ -18,38 +46,40 @@ def ocr_file(file, language):
         return False, raw['ErrorMessage'][0]
     return True, raw['ParsedResults'][0]['ParsedText']
 
-@client.Command(pattern=f"(?i)^\{client.cmd}OcrApi (.*)$")
+@client.Command(command="SetOcrKey (.*)")
 async def saveocrapi(event):
-    await event.edit(client.get_string("Wait"))
+    await event.edit(client.STRINGS["wait"])
     api = event.pattern_match.group(1)
     client.DB.set_key("OCR_API_KEY", api)
-    await event.edit(client.get_string("OcrApi_1").format(api))
+    await event.edit(STRINGS["setapi"].format(api))
 
-@client.Command(pattern=f"(?i)^\{client.cmd}Ocr (.*)$")
+@client.Command(command="Ocr ?(.*)?")
 async def ocrapi(event):
-    await event.edit(client.get_string("Wait"))
-    lang = event.pattern_match.group(1)
-    LANGS = client.get_string("OcrLangs")
+    await event.edit(client.STRINGS["wait"])
+    lang = event.pattern_match.group(1) or "eng"
+    mtype = client.functions.mediatype(event.reply_message)
+    if not event.is_reply or mtype not in ["Photo"]:
+        medias = client.STRINGS["replyMedia"]
+        media = medias["Photo"]
+        rtype = medias[mtype]
+        text = client.STRINGS["replyMedia"]["Main"].format(rtype, media)
+        return await event.edit(text)
     if not client.DB.get_key("OCR_API_KEY"):
-        return await event.edit(client.get_string("OcrApi_2"))
+        return await event.edit(STRINGS["notsave"])
     if not lang in LANGS:
-        return await event.edit(client.get_string("OCrApi_3"))
-    if not event.is_reply or not event.photo:
-        return await event.edit(client.get_string("Reply_P"))
-    photo = f"{event.reply_message.id}.png"
-    await event.reply_message.download_media(photo)
+        return await event.edit(STRINGS["notlang"])
+    photo = await event.reply_message.download_media()
     stat, res = ocr_file(photo, lang)
     if not stat:
-        text = client.get_string("OcrApi_4").format(res)
+        text = STRINGS["notcom"].format(res)
         return await event.edit(text)
-    await event.edit(client.get_string("OcrApi_5").format(LANGS[lang], res))   
+    await event.edit(STRINGS["com"].format(LANGS[lang], res))   
     os.remove(photo)
 
-@client.Command(pattern=f"(?i)^\{client.cmd}OcrLangs$")
+@client.Command(command="OcrLangs")
 async def ocrlangs(event):
-    await event.edit(client.get_string("Wait"))
-    text = client.get_string("OcrApi_6")
-    LANGS = client.get_string("OcrLangs")
+    await event.edit(client.STRINGS["wait"])
+    text = STRINGS["langs"]
     for lang in LANGS:
         text += f"• `{lang}` - **{LANGS[lang]}**\n"
     await event.edit(text)
