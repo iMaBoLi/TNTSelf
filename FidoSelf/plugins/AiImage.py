78c7c821386ec318a0191d8954f4e86939e43a52
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-05 19:40:46 +0330

    update FidoSelf/plugins/AiImage.py

--- FidoSelf/plugins/AiImage.py
+++ FidoSelf/plugins/AiImage.py
@@ -4,6 +4,7 @@ from somnium import Somnium
 STRINGS = {
     "gen": "**Creating Image For Query** ( `{}` ) **And StyleID** ( `{}` ) **...**",
     "caption": "**The Image For Query** ( `{}` ) **And StyleID** ( `{}` ) **Created!**",
+    "styles": "**The Styles For Creating Photo:** ( {} )\n\n**Use From StylesID For Create Photos!**",
 }
 
 @client.Command(command="GPhoto \'(\d*)\' (.*)")
@@ -19,3 +20,9 @@ async def generate(event):
     caption = STRINGS["caption"].format(query, style)
     await client.send_file(event.chat_id, file, caption=caption)
     await event.delete()
+    
+@client.Command(command="GStyles")
+async def getstyles(event):
+    await event.edit(client.STRINGS["wait"])
+    link = "https://graph.org/List-Of-Styles-06-05"
+    await event.edit(client.STRINGS["styles"].format(link))
