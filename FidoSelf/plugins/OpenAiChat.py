249f22060ef98124ab8944c236232ef544014824
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-05 21:27:37 +0330

    update FidoSelf/plugins/OpenAiChat.py

--- FidoSelf/plugins/OpenAiChat.py
+++ FidoSelf/plugins/OpenAiChat.py
@@ -1,33 +1,29 @@
 from FidoSelf import client
 import openai
+from FidoSelf.functions.AiCreator import AiCreate
 
 STRINGS = {
     "setapi": "**The OpenAi ApiKey** ( `{}` ) **Has Been Saved!**",
-    "get": "**Geeting Ai Chat Result For Query** ( `{}` ) **...**",
-    "notres": "**Not Ai Chat Result For Query** ( `{}` )",
-    "result": "**Query** ( `{}` ):\n\n**Result:** ( `{}` )",
+    "get": "**Geeting OpenAi Result For Question** ( `{}` ) **...**",
+    "result": "**Question** ( `{}` ):\n\n**ChatGPT:** ( `{}` )",
 }
 
-openai.api_key = client.DB.get_key("OPENAI_APIKEY")
-conversations = {}
+CONVERSATIONS = {}
 
-def generate_gpt_response(input_text, chat_id):
-    openai.api_key = client.DB.get_key("OPENAI_APIKEY")
-    global conversations
-    model = "gpt-3.5-turbo"
-    messages = conversations.get(chat_id, [])
-    messages.append({"role": "user", "content": input_text})
-    try:
-        response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-        )
-        generated_text = response.choices[0].message.content.strip()
-        messages.append({"role": "assistant", "content": generated_text})
-        conversations[chat_id] = messages
-    except Exception as e:
-        generated_text = f"`Error generating GPT response: {str(e)}`"
-    return generated_text
+async def gpt_response(query, chat_id):
+    if not openai.api_key:
+        openai.api_key = client.DB.get_key("OPENAI_APIKEY")
+    global CONVERSATIONS
+    messages = CONVERSATIONS.get(chat_id, [])
+    messages.append({"role": "user", "content": query})
+    response = await openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+    )
+    result = response.choices[0].message.content.strip()
+    messages.append({"role": "assistant", "content": result})
+    CONVERSATIONS[chat_id] = messages
+    return result, CONVERSATIONS[chat_id]
 
 @client.Command(command="SetAiKey (.*)")
 async def saveaiapi(event):
@@ -39,11 +35,10 @@ async def saveaiapi(event):
 @client.Command(command="GOText (.*)")
 async def aichat(event):
     await event.edit(client.STRINGS["wait"])
-    client.loop.create_task(generate(event))
-    
-async def generate(event):
     query = event.pattern_match.group(1)
     await event.edit(STRINGS["get"].format(query))
-    result = generate_gpt_response(query, event.chat_id)
+    result, con = await gpt_response(query, event.chat_id)
+    file = AiCreate(con, "ChatGPT.html")
     text = STRINGS["result"].format(query, result)
-    await event.edit(text)
+    await event.respond(text, file=file)
+    await event.delete()
