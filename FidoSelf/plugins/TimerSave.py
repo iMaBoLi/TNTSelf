0f6c890ea28f1d219ad617a63201b410f63c48c4
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-06 13:24:48 +0330

    update FidoSelf/plugins/TimerSave.py

--- FidoSelf/plugins/TimerSave.py
+++ FidoSelf/plugins/TimerSave.py
@@ -10,7 +10,7 @@ async def dtimermedias(event):
     mode = client.DB.get_key("TIMER_MODE")
     mtype = client.functions.mediatype(event)
     if event.is_private and mode == "on" and mtype in ["Photo", "Video"] and event.media.ttl_seconds:
-        file = await event.download_media()
+        file = await event.download_media(client.PATH)
         sender = await event.get_sender()
         mention = client.functions.mention(sender)
         ttl = client.functions.convert_time(event.media.ttl_seconds)
