4522edaedaf3886ed634507fc4953b23bef5826f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-06 16:27:18 +0330

    update FidoSelf/plugins/TextSay.py

--- FidoSelf/plugins/TextSay.py
+++ FidoSelf/plugins/TextSay.py
@@ -1,27 +1,17 @@
 from FidoSelf import client
 import asyncio
 
-@client.Command(pattern=f"(?i)^\{client.cmd}TSay ([\S\s]*)$")
+@client.Command(command="TSay ([\S\s]*)")
 async def tsay(event):
     text = event.pattern_match.group(1)
-    sleep = client.DB.get_key("SAY_SLEEP") or 1
-    sleep = int(sleep) if str(sleep).isdigit() else float(sleep)
     new = ""
     for par in text:
         new += par
         await event.edit(new)
-        await asyncio.sleep(sleep)
-
-@client.Command(edits=False)
-async def autosay(event):
-    if event.is_cmd or not event.text: return
-    mode = client.DB.get_key("AUTO_SAY_MODE") or "off"
-    sleep = client.DB.get_key("SAY_SLEEP") or 1
-    sleep = int(sleep) if str(sleep).isdigit() else float(sleep)
-    if mode == "off": return
-    text = str(event.text)
-    new = ""
-    for par in text:
-        new += par
+        await asyncio.sleep(0.2)
+        await event.edit(new + "|")
+        await asyncio.sleep(0.2)
         await event.edit(new)
-        await asyncio.sleep(sleep)
+        await asyncio.sleep(0.2)
+        await event.edit(new + "|")
+        await asyncio.sleep(0.2)
