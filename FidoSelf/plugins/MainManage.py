07abb5e98c6300d3560221346cdce71219a101de
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-07 10:51:35 +0330

    update FidoSelf/plugins/MainManage.py

--- FidoSelf/plugins/MainManage.py
+++ FidoSelf/plugins/MainManage.py
@@ -16,21 +16,21 @@ STRINGS = {
     "infouser": "**User Info:**\n\n**Mention:** ( {} )\n**ID:** ( `{}` )\n**First Name:** ( `{}` )\n**Last Name:** ( `{}` )\n**Username :** ( `{}` )\n**Contact:** ( `{}` )\n**Mutual Contact:** ( `{}` )\n**Status:** ( `{}` )\n**Common Chats:** ( `{}` )\n**Bio:** ( `{}` )",
 }
 
-async def get_manage_buttons(userid):
+async def get_manage_buttons(userid, chatid):
     buttons = []
     MANAGES = STRINGS["Manages"]
-    buttons.append([Button.inline(f'• {MANAGES["INFO"]} •', data=f"getinfo:{userid}")])
+    buttons.append([Button.inline(f'• {MANAGES["INFO"]} •', data=f"getinfo:{chatid}:{userid}")])
     info = await client(functions.users.GetFullUserRequest(userid))
     info = info.full_user
     smode = MANAGES["UNBLOCK"] if info.blocked else MANAGES["BLOCK"]
     cmode = "unblock" if info.blocked else "block"
-    buttons.append([Button.inline(f"• {smode} •", data=f"{cmode}:{userid}")])
+    buttons.append([Button.inline(f"• {smode} •", data=f"{cmode}:{chatid}:{userid}")])
     obuts = []
     for manage in ["BLACKS", "WHITES", "ECHOS"]:
         lists = client.DB.get_key(manage) or []
         smode = "( ✔️ )" if userid in lists else "( ✖️ )"
         cmode = "del" if userid in lists else "add"
-        obuts.append(Button.inline(f"• {MANAGES[manage]} - {smode} •", data=f"setuser:{userid}:{manage}:{cmode}"))
+        obuts.append(Button.inline(f"• {MANAGES[manage]} - {smode} •", data=f"setuser:{chatid}:{userid}:{manage}:{cmode}"))
     obuts = list(client.functions.chunks(obuts, 2))
     for but in obuts:
         buttons.append(but)
@@ -45,25 +45,27 @@ async def managepanel(event):
         return await event.edit(client.STRINGS["getid"]["IU"])
     elif not result and not userid:
         return await event.edit(client.STRINGS["getid"]["UUP"])
-    res = await client.inline_query(client.bot.me.username, f"managepanel:{userid}")
+    res = await client.inline_query(client.bot.me.username, f"managepanel:{chatid}:{userid}")
     if event.is_reply:
         await res[0].click(event.chat_id, reply_to=event.reply_message.id)
     else:
         await res[0].click(event.chat_id)
     await event.delete()
 
-@client.Inline(pattern="managepanel\:(.*)")
+@client.Inline(pattern="managepanel\:(.*)\:(.*)")
 async def inlinemanagepanel(event):
-    userid = int(event.pattern_match.group(1))
+    chatid = int(event.pattern_match.group(1))
+    userid = int(event.pattern_match.group(2))
     text = STRINGS["menu"]
-    buttons = await get_manage_buttons(userid)
+    buttons = await get_manage_buttons(userid, chatid})
     await event.answer([event.builder.article("FidoSelf - Manage", text=text, buttons=buttons)])
 
-@client.Callback(data="setuser\:(.*)\:(.*)\:(.*)")
+@client.Callback(data="setuser\:(.*)\:(.*)\:(.*)\:(.*)")
 async def setusermanage(event):
-    userid = int(event.data_match.group(1).decode('utf-8'))
-    mode = event.data_match.group(2).decode('utf-8')
-    change = event.data_match.group(3).decode('utf-8')
+    chatid = int(event.data_match.group(1).decode('utf-8'))
+    userid = int(event.data_match.group(2).decode('utf-8'))
+    mode = event.data_match.group(3).decode('utf-8')
+    change = event.data_match.group(4).decode('utf-8')
     info = await client.get_entity(userid)
     lists = client.DB.get_key(mode) or []
     if change == "add":
@@ -72,24 +74,26 @@ async def setusermanage(event):
     elif change == "del":
         lists.remove(userid)
         client.DB.set_key(mode, lists)
-    buttons = await get_manage_buttons(userid)
+    buttons = await get_manage_buttons(userid, chatid)
     await event.edit(buttons=buttons)
 
-@client.Callback(data="(block|unblock)\:(.*)")
+@client.Callback(data="(block|unblock)\:(.*)\:(.*)")
 async def closemanagepanel(event):
     change = str(event.data_match.group(1).decode('utf-8'))
-    userid = int(event.data_match.group(2).decode('utf-8'))
+    chatid = int(event.data_match.group(2).decode('utf-8'))
+    userid = int(event.data_match.group(3).decode('utf-8'))
     if change == "block":
         await client(functions.contacts.BlockRequest(userid))
     elif change == "unblock":
         await client(functions.contacts.UnblockRequest(userid))
     await asyncio.sleep(0.3)
-    buttons = await get_manage_buttons(userid)    
+    buttons = await get_manage_buttons(userid, chatid)    
     await event.edit(buttons=buttons)
 
-@client.Callback(data="getinfo\:(.*)")
+@client.Callback(data="getinfo\:(.*)\:(.*)")
 async def getinfo(event):
-    userid = int(event.data_match.group(1).decode('utf-8'))
+    chatid = int(event.data_match.group(1).decode('utf-8'))
+    userid = int(event.data_match.group(2).decode('utf-8'))
     uinfo = await client.get_entity(userid)
     info = await client(functions.users.GetFullUserRequest(userid))
     info = info.full_user
@@ -99,10 +103,10 @@ async def getinfo(event):
     username = f"@{uinfo.username}" if uinfo.username else "---"
     userinfo = STRINGS["infouser"].format(client.mention(uinfo), uinfo.id, uinfo.first_name, (uinfo.last_name or "---"), username, contact, mcontact,status, info.common_chats_count, (info.about or "---"))
     if info.profile_photo:
-        await client.send_file(event.chat_id, info.profile_photo, caption=userinfo)
+        await client.send_file(chatid, info.profile_photo, caption=userinfo)
     else:
-        await client.send_message(event.chat_id, userinfo)
-    buttons = await get_manage_buttons(userid)    
+        await client.send_message(chatid, userinfo)
+    buttons = await get_manage_buttons(userid, chatid)    
     await event.edit(buttons=buttons)
 
 @client.Callback(data="closemanage")
