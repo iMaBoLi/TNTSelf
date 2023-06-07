736b5c1a4cc965b5f516fa17bcbeb408b01ffc6b
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-08 00:03:02 +0330

    update FidoSelf/plugins/Sticker.py

--- FidoSelf/plugins/Sticker.py
+++ FidoSelf/plugins/Sticker.py
@@ -1,38 +1,75 @@
 from FidoSelf import client
-from telethon import functions, utils
+from telethon import functions, types, utils
+from telethon.errors.rpcerrorlist import PackShortNameOccupiedError
 
-@client.Command(command="Spack (.*)")
-async def Spack(event):
+STRINGS = {
+    "notall": "**The Short Name** ( `{}` ) **iS Already Used in Other Packs!**",
+    "creating": "**The Pack** ( `{}` ) **iS Creating ...**",
+    "adding": "**The Stickers On Pack** ( `{}` ) **iS Adding ...**",
+    "created": "**The Pack iS Copied!**\n\n**Title:** ( `{}` )\n**Link:** ( `{}` )\n**Stickers Count:** ( `{}` )",
+}
+
+@client.Command(command="Cpack (.*)\:(.*)")
+async def copypack(event):
     await event.edit(client.STRINGS["wait"])
-    packname = event.pattern_match.group(1)
-    reply = event.reply_message
-    id = reply.media.document.attributes[1].stickerset.id
-    hash = reply.media.document.attributes[1].stickerset.access_hash
+    packtitle = event.pattern_match.group(1)
+    packname = event.pattern_match.group(2)
+    if not event.is_reply or mtype not in ["Sticker", "ASticker"]:
+        medias = client.STRINGS["replyMedia"]
+        media = medias["Sticker"] + " - " + medias["ASticker"]
+        rtype = medias[mtype]
+        text = client.STRINGS["replyMedia"]["Main"].format(rtype, media)
+        return await event.edit(text)
+    id = event.reply_message.media.document.attributes[1].stickerset.id
+    hash = event.reply_message.media.document.attributes[1].stickerset.access_hash
     stickers = await client(
         functions.messages.GetStickerSetRequest(
-            stickerset=types.InputStickerSetID(id=id, access_hash=hash), hash=0
+            stickerset=types.InputStickerSetID(
+                id=id,
+                access_hash=hash
+            ),
+            hash=0,
         )
     )
     stickers = stickers.documents
-    anim = True if reply.document.mime_type in ["image/webp"] else False
-    vid = True if reply.document.mime_type in ["video/webm", "application/x-tgsticker"] else False
     stiks = []
     for sticker in stickers:
         doc = utils.get_input_document(sticker)
         stiks.append(
             types.InputStickerSetItem(
                 document=doc,
-                emoji=reply.document.attributes[-2].alt,
+                emoji=event.reply_message.document.attributes[-2].alt,
+            )
         )
     short_name = packname.replace(" ", "_")
     short_name = f"{short_name}_by_{client.bot.me.username}"
-    result = await client.bot(
-        functions.stickers.CreateStickerSetRequest(
-                user_id=event.sender_id,
-                title=packname,
-                short_name=short_name,
-                animated=anim,
-                videos=vid,
-                stickers=stiks,
+    anim = True if event.reply_message.document.mime_type in ["image/webp"] else False
+    await event.edit(STRINGS["creating"].format(packtitle))
+    try:
+        result = await client.bot(
+            functions.stickers.CreateStickerSetRequest(
+                    user_id=event.sender_id,
+                    title=packtitle,
+                    short_name=short_name,
+                    animated=anim,
+                    stickers=stiks[:10],
+                )
+            )
+    except PackShortNameOccupiedError:
+        return await event.edit(STRINGS["notall"].format(packname))
+    id = result.set.id
+    id = result.set.access_hash
+    await event.edit(STRINGS["adding"].format(packtitle))
+    for sticker in stiks[10:]:
+        result = await client.bot(
+            functions.stickers.AddStickerToSetRequest(
+                stickerset=types.InputStickerSetID(
+                    id=id,
+                    access_hash=access_hash,
+                ),
+                sticker=sticker,
             )
         )
+    link = f"https://t.me/addstickers/{short_name}"
+    text = STRINGS["created"].format(packtitle, link, len(stiks))
+    await event.edit(text)
