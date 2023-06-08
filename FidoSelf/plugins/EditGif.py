9fe502ef2a63465dd8caf8f68c8bd482f35e0e2f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-08 13:46:14 +0330

    update FidoSelf/plugins/EditGif.py

--- FidoSelf/plugins/EditGif.py
+++ FidoSelf/plugins/EditGif.py
@@ -2,6 +2,7 @@ from FidoSelf import client
 import os
 
 STRINGS = {
+    "coning": "**Converting To** ( `{}` ) **...**",
     "caption": "**The Filter** ( `{}` ) **iS Added To Gif!**",
 }
 
@@ -10,21 +11,24 @@ async def editgif(event):
     await event.edit(client.STRINGS["wait"])
     filter = event.pattern_match.group(1).title()
     mtype = client.functions.mediatype(event.reply_message)
-    if not event.is_reply or mtype not in ["Gif"]:
+    if not event.is_reply or mtype not in ["Video", "Gif"]:
         medias = client.STRINGS["replyMedia"]
-        media = medias["Gif"]
+        media = medias["Video"] + " - " + medias["Gif"]
         rtype = medias[mtype]
         text = client.STRINGS["replyMedia"]["Main"].format(rtype, media)
         return await event.edit(text)
     callback = event.progress(download=True)
-    gif = await event.reply_message.download_media(client.PATH, progress_callback=callback)
-    outfile = client.PATH + "EditGif.gif"
+    file = await event.reply_message.download_media(client.PATH, progress_callback=callback)
+    type = "BlackWhite" if filter == "Bw" else "Invert(Negative)"
+    await event.edit(STRINGS["coning"].format(type))
+    if mtype == "Video":
+        outfile = client.PATH + "EditVideo.mp4"
+    elif mtype == "Gif":
+        outfile = client.PATH + "EditGif.gif"
     if filter == "Bw":
-        cmd = f'ffmpeg -i "{gif}" -vf format=gray {outfile} -y'
-        type = "BlackWhite"
+        cmd = f'ffmpeg -i "{file}" -vf format=gray {outfile} -y'
     elif filter == "Inv":
-        cmd = f'ffmpeg -i "{gif}" -vf lutyuv="y=negval:u=negval:v=negval" {outfile} -y'
-        type = "Invert(Negative)"
+        cmd = f'ffmpeg -i "{file}" -vf lutyuv="y=negval:u=negval:v=negval" {outfile} -y'
     await client.functions.runcmd(cmd)
     caption = STRINGS["caption"].format(type)
     await client.send_file(event.chat_id, outfile, caption=caption, supports_streaming=True)
