84929f90a13a00f083ce13966e4e5a71ba9ef3a6
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-11 00:48:19 +0330

    update FidoSelf/plugins/YouTube.py

--- FidoSelf/plugins/YouTube.py
+++ FidoSelf/plugins/YouTube.py
@@ -1,24 +1,44 @@
 from FidoSelf import client
 from telethon import types
-from FidoSelf.functions.youtube import yt_downloader
+import os
 
-@client.Command(command="Yt(Video|Music) (.*) (.*)")
+STRINGS = {
+    "linkinv": "**The Entered Youtube Link Is Invalid!**",
+    "downingvid": "**Downloadig Video** ( `{}` ) **...**",
+    "downingaud": "**Downloadig Audio** ( `{}` ) **...**",
+    "caption": "**Title:** ( `{}` )\n**Uploader:** ( `{}` )\n**Views:** ( `{}` )\n**Duration:** ( `{}` )\n**Description:** ( `{}` )",
+}
+
+@client.Command(command="Yt(Video|Music) (.*)")
 async def ytdownload(event):
     await event.edit(client.STRINGS["wait"])
     type = event.pattern_match.group(1).lower()
-    quality = event.pattern_match.group(2)
-    link = event.pattern_match.group(3)
-    info = await yt_downloader(link, type, quality)
-    attributes = [types.DocumentAttributeVideo(duration=int(info["duration"]), w=720, h=720, supports_streaming=True)]
-    media = types.InputMediaUploadedDocument(
-        file=info["OUTFILE"],
-        mime_type="video/mp4",
-        attributes=attributes,
-    )
+    link = event.pattern_match.group(2)
+    if not client.functions.YOUTUBE_REGEX.search(link):
+        return await event.edit(client.STRINGS["linkinv"])
+    ytinfo = client.functions.yt_info(link)
+    if type == "video":
+        await event.edit(client.STRINGS["downingvid"].format(ytinfo["title"]))
+        info = await client.functions.yt_downloader(link, type, "720")
+        duration = int(ytinfo["duration"])
+        attributes = [types.DocumentAttributeVideo(duration=duration, w=720, h=720, supports_streaming=True)]
+    else:
+        await event.edit(client.STRINGS["downingaud"].format(ytinfo["title"]))
+        info = await client.functions.yt_downloader(link, type, "320k")
+        duration = int(ytinfo["duration"])
+        title = ytinfo["title"]
+        performer = ytinfo["uploader"]
+        attributes = [types.DocumentAttributeAudio(duration=duration, title=title, performer=performer)]
+    caption = STRINGS["caption"].format(ytinfo["title"], ytinfo["uploader"], ytinfo["view_count"], ytinfo["duration_string"], ytinfo["description"])
     callback = event.progress(upload=True)
     await client.send_file(
         event.chat_id,
-        file=media,
+        file=info["OUTFILE"],
         thumb=info["THUMBNAIL"],
+        attributes=attributes,
+        caption=caption,
         progress_callback=callback,
     )
+    os.remove(info["OUTFILE"])
+    os.remove(info["THUMBNAIL"])
+    await event.delete()
