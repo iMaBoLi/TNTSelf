dea109fd115aa218b3f4ee5c23de6419d0f3fb1c
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-10 23:57:05 +0330

    update FidoSelf/functions/youtube.py

--- FidoSelf/functions/youtube.py
+++ FidoSelf/functions/youtube.py
@@ -1,19 +1,24 @@
 from FidoSelf import client
 from youtubesearchpython import VideosSearch
 from yt_dlp import YoutubeDL
-from PIL import Image
+import requests
 import os
 import re
 
 YOUTUBE_URL = "https://www.youtube.com/watch?v="
 YOUTUBE_REGEX = re.compile(r"(?:youtube\.com|youtu\.be)/(?:[\w-]+\?v=|embed/|v/|shorts/)?([\w-]{11})")
 
-VIDEO = "yt-dlp -o '{outfile}' --write-thumbnail --add-metadata --embed-thumbnail -f 'best[height>={quality}]' {link}"
-SONG = "yt-dlp -o '{outfile}' --write-thumbnail --add-metadata --embed-thumbnail --extract-audio --audio-format mp3 --audio-quality {quality} {link}"
+VIDEO = "yt-dlp -o '{outfile}' -f 'best[height>={quality}]' {link}"
+SONG = "yt-dlp -o '{outfile}' --extract-audio --audio-format mp3 --audio-quality {quality} {link}"
+THUMB = "yt-dlp -o '{outfile}' --write-thumbnail --skip-download {link}"
 
 async def yt_downloader(link, type, quality):
     info = YoutubeDL().extract_info(link, download=False)
     videoid = info["id"]
+    thumb = client.PATH + "youtube/" + videoid
+    cmd = THUMB.format(outfile=outfile, link=link)
+    await client.functions.runcmd(cmd)
+    thumb = convert_thumb(thumb)
     if type == "video":
         outfile = client.PATH + "youtube/" + videoid + ".mp4"
         cmd = VIDEO.format(outfile=outfile, quality=quality, link=link)
@@ -22,7 +27,6 @@ async def yt_downloader(link, type, quality):
         outfile = client.PATH + "youtube/" + videoid + ".mp3"
         cmd = SONG.format(outfile=outfile, quality=quality, link=link)
         await client.functions.runcmd(cmd)
-    thumb = convert_thumb(outfile)
     info["OUTFILE"] = outfile
     info["THUMBNAIL"] = thumb
     return info
