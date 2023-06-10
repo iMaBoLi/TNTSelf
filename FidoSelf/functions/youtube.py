9fdafc7dbaf97981216d16537cd86e326403d346
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-10 23:10:38 +0330

    update FidoSelf/functions/youtube.py

--- FidoSelf/functions/youtube.py
+++ FidoSelf/functions/youtube.py
@@ -1,40 +1,37 @@
 from FidoSelf import client
 from youtubesearchpython import VideosSearch
-import yt_dlp
+from yt_dlp import YoutubeDL
+from PIL import Image
 import os
 import re
 
 YOUTUBE_URL = "https://www.youtube.com/watch?v="
 YOUTUBE_REGEX = re.compile(r"(?:youtube\.com|youtu\.be)/(?:[\w-]+\?v=|embed/|v/|shorts/)?([\w-]{11})")
 
-VIDEO = "yt-dlp --force-ipv4 --write-thumbnail --add-metadata --embed-thumbnail -o '{outfile}' -f 'best[height=={quality}}]' {link}"
-SONG = "yt-dlp --force-ipv4 --write-thumbnail --add-metadata --embed-thumbnail -o '{outfile}' --extract-audio --audio-format mp3 --audio-quality {quality} {link}"
-THUMB = "yt-dlp --force-ipv4 -o '{outfile}' --write-thumbnail --skip-download {link}"
+VIDEO = "yt-dlp -o '{outfile}' --write-thumbnail --add-metadata --embed-thumbnail -f 'best[height=={quality}}]' {link}"
+SONG = "yt-dlp -o '{outfile}' --write-thumbnail --add-metadata --embed-thumbnail --extract-audio --audio-format mp3 --audio-quality {quality} {link}"
 
-async def yt_downloader(link, type, quality, outfile):
+async def yt_downloader(link, type, quality):
+    info = YoutubeDL().extract_info(link, download=False)
+    videoid = info["id"]
     if type == "video":
-        cmd = VIDEO.format(link=link, quality=quality, outfile=outfile)
+        outfile = client.PATH + "youtube/" + videoid + ".mp4"
+        cmd = VIDEO.format(outfile=outfile, quality=quality, link=link)
         await client.functions.runcmd(cmd)
-        return outfile
     elif type == "music":
-        cmd = SONG.format(link=link, quality=quality, outfile=outfile)
+        outfile = client.PATH + "youtube/" + videoid + ".mp3"
+        cmd = SONG.format(outfile=outfile, quality=quality, link=link)
         await client.functions.runcmd(cmd)
-        return outfile
+    thumb = convert_thumb(client.PATH + "youtube/" + videoid)
+    info["OUTFILE"] = outfile
+    info["THUMBNAIL"] = thumb
+    return info
         
 def get_videoid(url):
     match = YOUTUBE_REGEX.search(url)
     return match.group(1)
-
-def get_downinfo(type):
-    choice = "bestvideo+bestaudio/best"
-    disp = "best(video+audio)"
-    if type == "mkv":
-        choice = "bestvideo+bestaudio/best"
-        disp = "best(video+audio)"
-    elif type == "mp4":
-        choice = "bestvideo[ext=webm]+251/bestvideo[ext=mp4]+(258/256/140/bestaudio[ext=m4a])/bestvideo[ext=webm]+(250/249)/best"
-        disp = "best(video+audio)[webm/mp4]"
-    elif type == "mp3":
-        choice = "320"
-        disp = "320 Kbps"
-    return choice, disp
+    
+def convert_thumb(file):
+    img = Image.open(file + ".webp")
+    img.save(file + ".jpg", format="jpeg")  
+    return file + ".jpg"
