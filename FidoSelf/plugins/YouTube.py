9332233646b9267c53ecec7021cd54a3c364e798
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-11 17:02:55 +0330

    update FidoSelf/plugins/YouTube.py

--- FidoSelf/plugins/YouTube.py
+++ FidoSelf/plugins/YouTube.py
@@ -10,22 +10,23 @@ STRINGS = {
     "ytclick": "**Click To Follow Button To Get Search Results For Query:** ( `{}` )",
 }
 
-@client.Command(command="Yt(Video|Music) (.*)")
+@client.Command(command="Yt(Video|Music) (.*) ?(.*)?")
 async def ytdownload(event):
     await event.edit(client.STRINGS["wait"])
     type = event.pattern_match.group(1).lower()
     link = event.pattern_match.group(2)
+    quality = event.pattern_match.group(3)
     if not client.functions.YOUTUBE_REGEX.search(link):
         return await event.edit(client.STRINGS["linkinv"])
     ytinfo = client.functions.yt_info(link)
     if type == "video":
         await event.edit(STRINGS["downingvid"].format(ytinfo["title"]))
-        info = await client.functions.yt_downloader(link, type, "720")
+        info = await client.functions.yt_downloader(link, type, (quality or "720"))
         duration = int(ytinfo["duration"])
         attributes = [types.DocumentAttributeVideo(duration=duration, w=720, h=720, supports_streaming=True)]
     else:
         await event.edit(STRINGS["downingaud"].format(ytinfo["title"]))
-        info = await client.functions.yt_downloader(link, type, "320k")
+        info = await client.functions.yt_downloader(link, type, (quality or "720"))
         duration = int(ytinfo["duration"])
         title = ytinfo["title"]
         performer = ytinfo["uploader"]
