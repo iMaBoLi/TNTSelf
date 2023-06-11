e0982d2291023f81fe1515c72fc8fc933a722f61
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-11 20:51:06 +0330

    update FidoSelf/plugins/YouTube.py

--- FidoSelf/plugins/YouTube.py
+++ FidoSelf/plugins/YouTube.py
@@ -6,46 +6,11 @@ STRINGS = {
     "linkinv": "**The Entered Youtube Link Is Invalid!**",
     "downingvid": "**Downloadig Video** ( `{}` ) **...**",
     "downingaud": "**Downloadig Audio** ( `{}` ) **...**",
-    "caption": "**Title:** ( `{}` )\n**Uploader:** ( `{}` )\n**Views:** ( `{}` )\n**Duration:** ( `{}` )\n**Description:** ( `{}` )",
+    "ytdown": "**Title:** ( `{}` )\n**Uploader:** ( `{}` )\n**Views:** ( `{}` )\n**Duration:** ( `{}` )\n**Description:** ( `{}` )",
     "ytclick": "**Click To Follow Button To Get Search Results For Query:** ( `{}` )",
     "ytsearch": "**Link:** ( {} )\n**Title:** ( `{}` )\n**Uploader:** ( `{}` )\n**Views:** ( `{}` )\n**Duration:** ( `{}` )\n**Description:** ( `{}` )",
+    "com": "**The Download And Upload Completed!**",
 }
-
-@client.Command(command="Yt(Video|Music) (.*) ?(.*)?")
-async def ytdownload(event):
-    await event.edit(client.STRINGS["wait"])
-    type = event.pattern_match.group(1).lower()
-    link = event.pattern_match.group(2)
-    quality = event.pattern_match.group(3)
-    if not client.functions.YOUTUBE_REGEX.search(link):
-        return await event.edit(client.STRINGS["linkinv"])
-    ytinfo = client.functions.yt_info(link)
-    if type == "video":
-        await event.edit(STRINGS["downingvid"].format(ytinfo["title"]))
-        info = await client.functions.yt_downloader(link, type, (quality or "720"))
-        duration = int(ytinfo["duration"])
-        attributes = [types.DocumentAttributeVideo(duration=duration, w=720, h=720, supports_streaming=True)]
-    else:
-        await event.edit(STRINGS["downingaud"].format(ytinfo["title"]))
-        info = await client.functions.yt_downloader(link, type, (quality or "720"))
-        duration = int(ytinfo["duration"])
-        title = ytinfo["title"]
-        performer = ytinfo["uploader"]
-        attributes = [types.DocumentAttributeAudio(duration=duration, title=title, performer=performer)]
-    description = str(ytinfo["description"])[:100] + " ..."
-    caption = STRINGS["caption"].format(ytinfo["title"], ytinfo["uploader"], ytinfo["view_count"], ytinfo["duration_string"], description)
-    callback = event.progress(upload=True)
-    await client.send_file(
-        event.chat_id,
-        file=info["OUTFILE"],
-        thumb=info["THUMBNAIL"],
-        attributes=attributes,
-        caption=caption,
-        progress_callback=callback,
-    )
-    os.remove(info["OUTFILE"])
-    os.remove(info["THUMBNAIL"])
-    await event.delete()
     
 @client.Command(command="YtDown (.*)")
 async def ytdown(event):
@@ -64,41 +29,42 @@ async def ytdowninline(event):
     chatid = event.pattern_match.group(1)
     videoid = event.pattern_match.group(2)
     link = client.functions.YOUTUBE_URL + videoid
+    ytinfo = client.functions.yt_info(link)
+    text = STRINGS["ytdown"].format()
     videos, audios = client.functions.get_formats(link)
     vidbuttons = []
     for video in videos:
         vid = videos[video]
         size = client.functions.convert_bytes(vid["filesize"])
-        name = vid["format"] + " - " + size
+        name = "🎞️" + vid["format"] + " - " + size
         ext = vid["ext"]
-        vidbuttons.append(Button.inline(name, data=f"ytdownload:{chatid}:{videoid}:{video}:{ext}:video"))
+        vidbuttons.append(Button.inline(name, data=f"ytdownload:{chatid}:{videoid}:{video}:{ext}"))
     vidbuttons = list(client.functions.chunks(vidbuttons, 2))
     audbuttons = []
     for audio in audios:
         aud = audios[audio]
         size = client.functions.convert_bytes(aud["filesize"])
-        name = aud["format"] + " - " + size
+        name = "🎤" + aud["format"] + " - " + size
         ext = vid["ext"]
-        audbuttons.append(Button.inline(name, data=f"ytdownload:{chatid}:{videoid}:{video}:{ext}:music"))
+        audbuttons.append(Button.inline(name, data=f"ytdownload:{chatid}:{videoid}:{video}:{ext}"))
     audbuttons = list(client.functions.chunks(audbuttons, 2))
     buttons = vidbuttons + audbuttons
-    await event.answer([event.builder.article("FidoSelf - YtSearch", text=link, buttons=buttons)])
+    await event.answer([event.builder.article("FidoSelf - YouTube Downloader", text=text, buttons=buttons)])
 
-@client.Callback(data="ytdownload\:(.*)\:(.*)\:(.*)\:(.*)\:(.*)")
+@client.Callback(data="ytdownload\:(.*)\:(.*)\:(.*)\:(.*)")
 async def ytdownload(event):
     chatid = event.data_match.group(1).decode('utf-8')
     videoid = event.data_match.group(2).decode('utf-8')
     format = event.data_match.group(3).decode('utf-8')
     ext = event.data_match.group(4).decode('utf-8')
-    type = event.data_match.group(5).decode('utf-8')
     link = client.functions.YOUTUBE_URL + videoid
     down = await client.functions.yt_downloader(link, format, ext)
     ytinfo = client.functions.yt_info(link)
-    if type == "video":
+    if ext == "mp4":
         await event.edit(STRINGS["downingvid"].format(ytinfo["title"]))
         duration = int(ytinfo["duration"])
         attributes = [types.DocumentAttributeVideo(duration=duration, w=720, h=720, supports_streaming=True)]
-    else:
+    elif ext == "m4a":
         await event.edit(STRINGS["downingaud"].format(ytinfo["title"]))
         duration = int(ytinfo["duration"])
         title = ytinfo["title"]
@@ -117,7 +83,7 @@ async def ytdownload(event):
     )
     os.remove(down["OUTFILE"])
     os.remove(down["THUMBNAIL"])
-    await event.delete()
+    await event.edit(STRINGS["com"])
     
 @client.Command(command="YtSearch (.*)")
 async def ytsearch(event):
@@ -139,7 +105,7 @@ async def ytsearchclick(event):
 async def ytsearch(event):
     query = event.pattern_match.group(1)
     answers = []
-    searchs = client.functions.yt_search(query, limit=10)
+    searchs = client.functions.yt_search(query, limit=20)
     for search in searchs:
         link = search["link"]
         description = str(search["descriptionSnippet"][0]["text"])[:100] + " ..."
@@ -155,7 +121,6 @@ async def ytsearch(event):
             text=text,
             buttons=buttons,
             thumb=thumb,
-            content=thumb,
         )
         answers.append(answer)
     await event.answer(answers)
