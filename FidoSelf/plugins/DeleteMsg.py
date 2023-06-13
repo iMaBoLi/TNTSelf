f34f52f1bf7e54fb5497d1e5e2be324e562e02e6
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-13 23:36:13 +0330

    update FidoSelf/plugins/DeleteMsg.py

--- FidoSelf/plugins/DeleteMsg.py
+++ FidoSelf/plugins/DeleteMsg.py
@@ -5,36 +5,24 @@ STRINGS = {
     "userdel": "**The** ( `{}` ) **Message From User** ( {} ) **In This Chat Was Deleted!**",
 }
 
-@client.Command(command="Del (\d*)")
+@client.Command(command="Del ?(\d*)?")
 async def deletemsg(event):
     await event.edit(client.STRINGS["wait"])
-    limit = int(event.pattern_match.group(1))
-    if event.is_private:
+    limit = event.pattern_match.group(1)
+    if not limit:
         if event.is_reply:
-            mention = client.mention(event.reply_message.sender)
-            count = 0
-            async for message in client.iter_messages(event.chat_id, from_user=event.reply_message.sender_id, limit=limit):
-                await message.delete(revoke=True)
-                count += 1
-            return await event.edit(STRINGS["userdel"].format(count, mention))
-        else:
-            count = 0
-            async for message in client.iter_messages(event.chat_id, limit=limit):
-                await message.delete(revoke=True)
-                count += 1
-            return await event.edit(STRINGS["chatdel"].format(count))
+            await event.reply_message.delete(revoke=True)
+        return await event.delete(revoke=True)
+    if event.is_reply:
+        mention = client.mention(event.reply_message.sender)
+        count = 0
+        async for message in client.iter_messages(event.chat_id, from_user=event.reply_message.sender_id, limit=limit):
+            await message.delete(revoke=True)
+            count += 1
+        await event.edit(STRINGS["userdel"].format(count, mention))
     else:
-        if event.is_reply:
-            
-            mention = client.mention(event.reply_message.sender)
-            count = 0
-            async for message in client.iter_messages(event.chat_id, from_user=event.reply_message.sender_id, limit=limit):
-                await message.delete(revoke=True)
-                count += 1
-            return await event.edit(STRINGS["userdel"].format(count, mention))
-        else:
-            count = 0
-            async for message in client.iter_messages(event.chat_id, limit=limit):
-                await message.delete(revoke=True)
-                count += 1
-            return await event.edit(STRINGS["chatdel"].format(count))
+        count = 0
+        async for message in client.iter_messages(event.chat_id, limit=limit):
+            await message.delete(revoke=True)
+            count += 1
+        await event.edit(STRINGS["chatdel"].format(count))
