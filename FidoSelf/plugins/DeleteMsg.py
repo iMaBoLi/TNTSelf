ea8e2f413ba97565e0aec3cf5e9d6ad3b7bd45d0
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-13 23:38:28 +0330

    update FidoSelf/plugins/DeleteMsg.py

--- FidoSelf/plugins/DeleteMsg.py
+++ FidoSelf/plugins/DeleteMsg.py
@@ -16,13 +16,13 @@ async def deletemsg(event):
     if event.is_reply:
         mention = client.mention(event.reply_message.sender)
         count = 0
-        async for message in client.iter_messages(event.chat_id, from_user=event.reply_message.sender_id, limit=limit):
+        async for message in client.iter_messages(event.chat_id, from_user=event.reply_message.sender_id, offset_id=(event.id - 1), limit=limit):
             await message.delete(revoke=True)
             count += 1
         await event.edit(STRINGS["userdel"].format(count, mention))
     else:
         count = 0
-        async for message in client.iter_messages(event.chat_id, limit=limit):
+        async for message in client.iter_messages(event.chat_id, offset_id=(event.id - 1), limit=limit):
             await message.delete(revoke=True)
             count += 1
         await event.edit(STRINGS["chatdel"].format(count))
