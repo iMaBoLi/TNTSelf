ebff9c76d0c01a374a4e3ac3757217b9f1812cf6
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-13 15:33:30 +0330

    update FidoSelf/plugins/CountryInfo.py

--- FidoSelf/plugins/CountryInfo.py
+++ FidoSelf/plugins/CountryInfo.py
@@ -3,7 +3,7 @@ from countryinfo import CountryInfo
 
 STRINGS = {
     "notcon": "**The Country Name** ( `{}` ) **Is Not Finded!**",
-    "country": "**Country Info:** ( `{}` )\n\n**Capital:** ( `{}` )\n**Population:** ( `{}` )\n**Area:** ( `{}` )\n**Region:** ( `{}` )\n**Calling Codes:** ( `{}` )\n**TimeZones:** ( `{}` )",
+    "country": "**Country Info:** ( `{}` )\n\n**Capital:** ( `{}` )\n**Population:** ( `{}` )\n**Area:** ( `{}` )\n**Region:** ( `{}` )\n**Calling Codes:** ( `{}` )\n**TimeZones:** ( `{}` )\n**Borders:** ( `{}` )",
 }
 
 @client.Command(command="SCountry (.*)")
@@ -20,10 +20,14 @@ async def chatcounts(event):
     ccodes = ""
     for ccode in info["callingCodes"]:
         ccodes += ccode + " - "
-    ccodes = ccodes[:-2]
+    ccodes = ccodes[:-3]
     tzones = ""
     for tzone in info["timezones"]:
         tzones += tzone + " - "
-    tzones = tzones[:-2]
-    text = STRINGS["country"].format(name, info["capital"], info["population"], info["area"], region, ccodes, tzones)
+    tzones = tzones[:-3]
+    borders = ""
+    for border in info["borders"]:
+        borders += border + " - "
+    borders = borders[:-3]
+    text = STRINGS["country"].format(name, info["capital"], info["population"], info["area"], region, ccodes, tzones, borders)
     await event.edit(text)
