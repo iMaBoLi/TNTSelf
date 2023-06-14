e81a24091f81a72d501cc30dcd21a805ace283c7
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-14 16:45:46 +0330

    update FidoSelf/functions/data.py

---
+++ FidoSelf/functions/data.py
@@ -0,0 +1,26 @@
+ACTIONS = [
+    "bandari",
+    "typing",
+    "game",
+    "photo",
+    "audio",
+    "video",
+    "file",
+    "sticker",
+    "contact",
+    "location",
+    "record-video",
+    "record-audio",
+    "record-round",
+]
+
+EDITS ={
+    "Bold",
+    "Mono",
+    "Italic",
+    "Underline",
+    "Strike",
+    "Spoiler",
+    "Hashtag",
+    "Mention",
+}
