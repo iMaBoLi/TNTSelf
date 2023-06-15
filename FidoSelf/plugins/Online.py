ff6dbf35816e1ee095e699287ffba8a06020ba34
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-15 21:21:40 +0330

    update FidoSelf/plugins/Online.py

--- FidoSelf/plugins/Online.py
+++ FidoSelf/plugins/Online.py
@@ -1,6 +1,18 @@
 from FidoSelf import client
 from telethon import functions
 
+__INFO__ = {
+    "Category": "Account",
+    "Plugname": "Online",
+    "Pluginfo": {
+        "Help": "To Set Online Mode For Account!",
+        "Commands": {
+            "{CMD}Online <On-Off>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
 @client.Command(onlysudo=False)
 async def online(event):
     mode = client.DB.get_key("ONLINE_MODE") or "off"
