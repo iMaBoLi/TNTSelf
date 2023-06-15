ccbdd470bec515cde38d0f9311f1ef988c4ee460
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-15 17:50:52 +0330

    update FidoSelf/functions/help.py

--- FidoSelf/functions/help.py
+++ FidoSelf/functions/help.py
@@ -1,2 +1,9 @@
+from FidoSelf import client
+
 def AddInfo(info):
-    pass
+    category = info["Category"]
+    pluginname = info["Plugname"]
+    plugininfo = info["Pluginfo"]
+    if not client.HELP[category]:
+        client.HELP.update({category: {}})
+    client.HELP[category][pluginname] = plugininfo
