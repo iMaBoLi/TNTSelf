68e6d809c2ef852e8d71c398857eea48a264d0df
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-15 23:00:52 +0330

    update FidoSelf/plugins/CountryInfo.py

--- FidoSelf/plugins/CountryInfo.py
+++ FidoSelf/plugins/CountryInfo.py
@@ -1,6 +1,18 @@
 from FidoSelf import client
 from countryinfo import CountryInfo
 
+__INFO__ = {
+    "Category": "Practical",
+    "Plugname": "Country Info",
+    "Pluginfo": {
+        "Help": "To Get Information About Country!",
+        "Commands": {
+            "{CMD}SCountry <Name>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
 STRINGS = {
     "notcon": "**The Country Name** ( `{}` ) **Is Not Finded!**",
     "country": "**Country Info:** ( `{}` )\n\n**Spellings:** ( `{}` )\n**Capital:** ( `{}` )\n**Population:** ( `{}` )\n**Area:** ( `{}` )\n**Region:** ( `{}` )\n**Currencies:** ( `{}` )\n**Calling Codes:** ( `{}` )\n**TimeZones:** ( `{}` )\n**Borders:** ( `{}` )\n\n**Provinces:** ( `{}` )",
