5e1f3489f77358383213a5df86dc00b5b62497f5
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-15 21:29:01 +0330

    update FidoSelf/plugins/LockPv.py

--- FidoSelf/plugins/LockPv.py
+++ FidoSelf/plugins/LockPv.py
@@ -1,6 +1,18 @@
 from FidoSelf import client
 from telethon import functions
 
+__INFO__ = {
+    "Category": "Private",
+    "Plugname": "LockPv",
+    "Pluginfo": {
+        "Help": "To Lock Pv And Block Users In Pv!",
+        "Commands": {
+            "{CMD}LockPv <On-Off>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
 @client.Command(onlysudo=False, alowedits=False)
 async def lockepv(event):
     if not event.is_private or event.is_white or event.is_sudo or event.is_bot: return
