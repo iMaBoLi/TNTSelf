3137318a50c87fcffcb79aecdf06c617ea79b501
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-15 17:33:33 +0330

    update FidoSelf/plugins/Contacts.py

---
+++ FidoSelf/plugins/Contacts.py
@@ -0,0 +1,21 @@
+from FidoSelf import client
+from telethon import functions
+
+STRINGS = {
+    "notdel": "**The Contact With Name** ( `{}` ) **Is Not Founded!**",
+    "delcon": "**The** ( `{}` ) **Contact By Name** ( `{}` ) **From Contacts Was Deleted!**",
+}
+
+@client.Command(command="DelC (.*)")
+async def delcontacts(event):
+    await event.edit(client.STRINGS["wait"])
+    name = event.pattern_match.group(1)
+    contacts = await client(functions.contacts.GetContactsRequest(hash=0))
+    count = 0
+    for contact in contacts.users:
+        if contact.first_name == name:
+            await client(functions.contacts.DeleteContactsRequest(id=[contact.id]))
+            count += 1
+    if not count:
+        return await event.edit(STRINGS["notdel"].format(name))
+    await event.edit(STRINGS["delcon"].format(count, name))
