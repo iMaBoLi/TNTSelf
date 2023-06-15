f2477fa1f7c9e6b7bd529aa33400883d8661ebcc
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-15 21:28:26 +0330

    update FidoSelf/plugins/MutePv.py

--- FidoSelf/plugins/MutePv.py
+++ FidoSelf/plugins/MutePv.py
@@ -1,5 +1,17 @@
 from FidoSelf import client
 
+__INFO__ = {
+    "Category": "Private",
+    "Plugname": "MutePv",
+    "Pluginfo": {
+        "Help": "To Mute Users In Pv!",
+        "Commands": {
+            "{CMD}MutePv <On-Off>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
 @client.Command(onlysudo=False, alowedits=False)
 async def mutepv(event):
     if not event.is_private or event.is_white or event.is_sudo or event.is_bot: return
