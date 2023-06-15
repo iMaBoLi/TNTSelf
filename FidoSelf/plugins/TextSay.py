d554399f2ece3568f61f923c4596e2fa29569b98
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-15 20:56:40 +0330

    update FidoSelf/plugins/TextSay.py

--- FidoSelf/plugins/TextSay.py
+++ FidoSelf/plugins/TextSay.py
@@ -1,17 +1,30 @@
 from FidoSelf import client
 import asyncio
 
+__INFO__ = {
+    "Category": "Funs",
+    "Plugname": "Text Say",
+    "Pluginfo": {
+        "Help": "To Say Your Message Texts!",
+        "Commands": {
+            "{CMD}TSay <Text>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
 @client.Command(command="TSay ([\S\s]*)")
 async def tsay(event):
-    text = event.pattern_match.group(1)
-    new = ""
-    for par in text:
-        new += par
-        await event.edit(new)
+    OText = event.pattern_match.group(1)
+    Text = ""
+    for Part in OText:
+        Text += Part
+        await event.edit(Text)
+        await asyncio.sleep(0.5)
+        await event.edit(Text + "|")
+        await asyncio.sleep(0.5)
+        await event.edit(Text)
         await asyncio.sleep(0.2)
-        await event.edit(new + "|")
+        await event.edit(Text + "|")
         await asyncio.sleep(0.2)
-        await event.edit(new)
-        await asyncio.sleep(0.2)
-        await event.edit(new + "|")
-        await asyncio.sleep(0.2)
+    await event.edit(Text)
