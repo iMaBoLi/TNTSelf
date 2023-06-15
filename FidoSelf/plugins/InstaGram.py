ae1f69447f6ef36813efb33be88d25c06d627d8b
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-15 17:24:06 +0330

    update FidoSelf/plugins/InstaGram.py

--- FidoSelf/plugins/InstaGram.py
+++ FidoSelf/plugins/InstaGram.py
@@ -1,9 +1,12 @@
 from FidoSelf import client
-import instagrapi
+from instagrapi import Client
+from instagrapi.exceptions import BadPassword, ChallengeError
 import os
 
 STRINGS = {
     "session": "**The Instagram Account Session File!**\n\n**Username:** ( `{}` )\n**Password:** ( `{}` )",
+    "invpass": "**The Instagram Account Password In Incorrect!**\n\n**Username:** ( `{}` )\n**Password:** ( `{}` )",
+    "banacc": "**The Instagram Account Is Blocked!**\n\n**Username:** ( `{}` )\n**Password:** ( `{}` )",
 }
 
 @client.Command(command="Cinsta (.*)\:(.*)\:?(.*)?")
@@ -11,12 +14,14 @@ async def createsession(event):
     await event.edit(client.STRINGS["wait"])
     username = event.pattern_match.group(1)
     password = event.pattern_match.group(2)
-    verpass = event.pattern_match.group(3)
-    insta = instagrapi.Client()
-    if verpass:
+    verpass = event.pattern_match.group(3) or None
+    insta = Client()
+    try:
         insta.login(username, password, verification_code=verpass)
-    else:
-        insta.login(username, password)
+    except BadPassword:
+        return await event.edit(STRINGS["invpass"].format(username, password)
+    except ChallengeError:
+        return await event.edit(STRINGS["banacc"].format(username, password)
     sesfile = client.PATH + username + "-" + password + ".json"
     insta.dump_settings(sesfile)
     caption = STRINGS["session"].format(username, password)
