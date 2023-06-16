728d49494c76d77eeb724f5bb3cbcad87e84fd5b
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-17 01:36:54 +0330

    update FidoSelf/plugins/AntiEdit.py

--- FidoSelf/plugins/AntiEdit.py
+++ FidoSelf/plugins/AntiEdit.py
@@ -2,30 +2,37 @@ from FidoSelf import client
 
 __INFO__ = {
     "Category": "Manage",
-    "Plugname": "Anti Forward",
+    "Plugname": "Anti Edit",
     "Pluginfo": {
-        "Help": "To Delete Forwarded Messages And Send Whitout Forward!",
+        "Help": "To Delete Edited Messages And Send Whitout Edit!",
         "Commands": {
-            "{CMD}AntiForward <On-Off>": None,
+            "{CMD}AntiEdit <On-Off>": None,
         },
     },
 }
 client.functions.AddInfo(__INFO__)
 
 STRINGS = {
-    "change": "**The Anti Forward Mode Has Been {}!**",
+    "change": "**The Anti Edit Mode Has Been {}!**",
 }
 
-@client.Command(command="AntiForward (On|Off)")
+@client.Command(command="AntiEdit (On|Off)")
 async def setanti(event):
     await event.edit(client.STRINGS["wait"])
     change = event.pattern_match.group(1).lower()
-    client.DB.set_key("ANTIFORWARD_MODE", change)
+    client.DB.set_key("ANTIEDIT_MODE", change)
     ShowChange = client.STRINGS["On"] if change == "on" else client.STRINGS["Off"]
     await event.edit(STRINGS["change"].format(ShowChange))
 
 @client.Command()
 async def getUodate(event):
-    file = "Out.txt"
-    open(file, "w").write(str(event))
-    await event.respond(file=file)
+    if event.checkCmd(): return
+    antimode = client.DB.get_key("ANTIEDIT_MODE") or "off"
+    if antimode == "on":
+        if event.to_dict()["original_update"]["_"] in ["UpdateEditMessage", "UpdateEditChannelMessage"]:
+            getmsg = await client.get_messages(event.chat_id, ids=event.id)
+            if getmsg.reply_to:
+                await event.respond(getmsg, reply_to=getmsg.reply_to.reply_to_msg_id)
+            else:
+                await event.respond(getmsg)
+            await event.delete()
