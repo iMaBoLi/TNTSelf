24999bf9e9f4d24362c213bc368fec253b22d5bb
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-18 00:05:43 +0330

    Update FidoSelf/events/Command.py

--- FidoSelf/events/Command.py
+++ FidoSelf/events/Command.py
@@ -3,6 +3,30 @@ from telethon import events
 from traceback import format_exc
 import re
 
+SPAMS = {}
+
+def checkspam(userid):
+    bantime = 30
+    maxtime = 6
+    if userid not in SPAMS:
+        SPAMS[userid] = {"next_time": time.time() + maxtime, "messages": 1, "banned": 0, "bans_count": 0}
+        uspam = SPAMS[userid]
+    else:
+        uspam = SPAMS[userid]
+        uspam["messages"] += 1
+    if uspam["banned"] >= time.time():
+        return True
+    else:
+        if uspam["next_time"] >= time.time():
+            if uspam["messages"] >= 10:
+                SPAMS[userid]["banned"] = time.time() + (SPAMS[userid]["bans_count"] * bantime)
+                SPAMS[userid]["bans_count"] += 1
+                return True
+        else:
+            SPAMS[userid]["messages"] = 1
+            SPAMS[userid]["next_time"] = time.time() + maxtime
+            return False
+
 def Command(
     pattern=None,
     command=None,
@@ -23,9 +47,10 @@ def Command(
         async def wrapper(event):
             try:
                 event.is_sudo = True if event.sender_id == client.me.id else False
+                antispam = client.DB.get_key("ANTI_SPAM") or "off"
+                if not event.is_sudo and antispam == "on" and checkspam(event.sender_id): return
                 event.is_ch = True if event.is_channel and not event.is_group else False
-                if onlysudo and not event.is_sudo and not event.is_ch:
-                    return
+                if onlysudo and not event.is_sudo and not event.is_ch: return
                 event.reply_message = await event.get_reply_message()
                 event.is_bot = False
                 if hasattr(event, "sender") and hasattr(event.sender, "bot"):
