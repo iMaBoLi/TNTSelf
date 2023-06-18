1ffd6bb0115f4805d6cd6c3d443c4e21b75c1dbf
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-19 00:44:27 +0330

    Update Sign.py

--- FidoSelf/plugins/Sign.py
+++ FidoSelf/plugins/Sign.py
@@ -1,1 +1,48 @@
-FidoSelf/plugins/Sign.py
+from FidoSelf import client
+
+__INFO__ = {
+    "Category": "Funs",
+    "Plugname": "Sign",
+    "Pluginfo": {
+        "Help": "To Sign A Text On Your Messages!",
+        "Commands": {
+            "{CMD}Sign <On-Off>": None,
+            "{CMD}SetSign <Text>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+STRINGS = {
+    "change": "**The Sign Mode Has Been {}!**",
+    "setsign": "**The Sign Text Was Set To** ( `{}` )",
+}
+
+@client.Command(command="Sign (On|Off)")
+async def signmode(event):
+    await event.edit(client.STRINGS["wait"])
+    change = event.pattern_match.group(1).lower()
+    client.DB.set_key("SIGN_MODE", change)
+    ShowChange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
+    await event.edit(STRINGS["change"].format(ShowChange))
+
+@client.Command(command="SetSign ([\s\S]*)")
+async def setsign(event):
+    await event.edit(client.STRINGS["wait"])
+    type = event.pattern_match.group(1).lower()
+    stext = event.pattern_match.group(2)
+    client.DB.set_key("SIGN_TEXT", stext)
+    text = STRINGS["setsign"].format(stext)
+    await event.edit(text)
+
+@client.Command(alowedits=False)
+async def sign(event):
+    if event.checkCmd(): return
+    mode = client.DB.get_key("SIGN_MODE") or "OFF"
+    if mode == "ON":
+        stext = client.DB.get_key("SIGN_TEXT") or "- Signed By Me!"
+        if event.text:
+            ntext = event.text + "\n\n" + stext
+        else:
+            ntext = stext
+        await event.edit(ntext)
