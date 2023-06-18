0d32954274a1730aa1950a52d462896cf9f4db18
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-19 00:02:11 +0330

    Update logs.py

--- FidoSelf/plugins/logs.py
+++ FidoSelf/plugins/logs.py
@@ -1,10 +1,1 @@
-from FidoSelf import client
-import os
-
-@client.Command(command="Logs")
-async def logs(event):
-    if os.path.exists("Fido.log"):
-        await event.respond("**The Console Logs File!**", file="Fido.log")
-        await event.delete()
-    else:
-        await event.edit("**The Log File Is Not Available!**")
+FidoSelf/plugins/logs.py
