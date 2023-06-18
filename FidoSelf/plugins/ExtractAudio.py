3f1fc49e7c991e2c7a623f2691d0375e90900251
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-19 00:44:18 +0330

    Update ExtractAudio.py

--- FidoSelf/plugins/ExtractAudio.py
+++ FidoSelf/plugins/ExtractAudio.py
@@ -1,1 +1,43 @@
-FidoSelf/plugins/ExtractAudio.py
+from FidoSelf import client
+import os
+
+__INFO__ = {
+    "Category": "Tools",
+    "Plugname": "Extract Audio",
+    "Pluginfo": {
+        "Help": "To Extraxt Audio From Video!",
+        "Commands": {
+            "{CMD}ExAudio <Reply(Video)>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+STRINGS = {
+    "exing": "**Extracting Audio From Video ...**",
+    "exed": "**The Audio Was Extracted From Video!**",
+}
+
+@client.Command(command="ExAudio")
+async def exaudio(event):
+    await event.edit(client.STRINGS["wait"])
+    mtype = client.functions.mediatype(event.reply_message)
+    if not event.is_reply or mtype not in ["Video"]:
+        medias = client.STRINGS["replyMedia"]
+        media = medias["Video"]
+        rtype = medias[mtype]
+        text = client.STRINGS["replyMedia"]["Main"].format(rtype, media)
+        return await event.edit(text)
+    if event.reply_message.file.size > client.MAX_SIZE:
+        return await event.edit(client.STRINGS["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
+    callback = event.progress(download=True)
+    video = await event.reply_message.download_media(client.PATH, progress_callback=callback)
+    await event.edit(STRINGS["exing"])
+    newfile = client.PATH + f"ExAudio-{video}.acc"
+    cmd = f"ffmpeg -i {video} -vn -acodec copy {newfile}"
+    callback = event.progress(upload=True)
+    caption = STRINGS["exed"]
+    await client.send_file(event.chat_id, newfile, caption=caption, progress_callback=callback)        
+    os.remove(video)
+    os.remove(newfile)
+    await event.delete()
