ebf6eaeea8d2f004bd5a6dc3eb6f477f3ecc623a
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-18 18:50:42 +0330

    Update FidoSelf/plugins/UsersHelp.py

--- FidoSelf/plugins/UsersHelp.py
+++ FidoSelf/plugins/UsersHelp.py
@@ -2,58 +2,49 @@ from FidoSelf import client
 from telethon import Button, events
 from .MainHelp import STRINGS, CATS
 
-@client.bot.on(events.InlineQuery(pattern="GetHelp\:(.*)"))
-async def inlinehelp(event):
-    userid = int(event.pattern_match.group(1))
-    if event.sender_id != userid: return
-    info = await client.get_entity(event.sender_id)
+@client.bot.on(events.NewMessage(pattern="(?i)^\/Gethelp$", incoming=True))
+async def gethelp(event):
+    reply = await event.reply(client.STRINGS["wait"])
     text = STRINGS["main"].format(client.mention(info))
+    userid = event.sender_id
     buttons = []
     for category in CATS:
         plugcount = len(client.HELP[category])
         ShowName = CATS[category].format(plugcount)
-        buttons.append(Button.inline(ShowName, data=f"OTHERGetCategory:{userid}:{category}"))
+        buttons.append(Button.inline(ShowName, data=f"OtherGetCategory:{category}"))
     buttons = list(client.functions.chunks(buttons, 2))
-    buttons.append([Button.inline(client.STRINGS["inline"]["Close"], data=f"OTHERCloseHelp:{userid}")])
-    await event.answer([event.builder.article("FidoSelf - Help", text=text, buttons=buttons)])
-
-@client.bot.on(events.CallbackQuery(data=b"OTHERHelp\:(.*)"))
+    buttons.append([Button.inline(client.STRINGS["inline"]["Close"], data="OtherCloseHelp")])
+    await event.reply(text, buttons=buttons)
+    await reply.delete()
+    
+@client.bot.on(events.CallbackQuery(data="OtherHelp"))
 async def callhelp(event):
-    userid = int(event.data_match.group(1).decode('utf-8'))
-    if event.sender_id != userid:
-        return await event.answer("• This Is Not For You!", alert=True)
     info = await client.get_entity(event.sender_id)
     text = STRINGS["main"].format(client.mention(info))
     buttons = []
     for category in CATS:
         plugcount = len(client.HELP[category])
         ShowName = CATS[category].format(plugcount)
-        buttons.append(Button.inline(ShowName, data=f"OTHERGetCategory:{userid}:{category}"))
+        buttons.append(Button.inline(ShowName, data=f"OtherGetCategory:{category}"))
     buttons = list(client.functions.chunks(buttons, 2))
-    buttons.append([Button.inline(client.STRINGS["inline"]["Close"], data=f"OTHERCloseHelp:{userid}")])
+    buttons.append([Button.inline(client.STRINGS["inline"]["Close"], data="OtherCloseHelp")])
     await event.edit(text=text, buttons=buttons)
 
-@client.bot.on(events.CallbackQuery(data=b"OTHERGetCategory\:(.*)\:(.*)"))
+@client.bot.on(events.CallbackQuery(data="OtherGetCategory\:(.*)"))
 async def getcategory(event):
-    userid = int(event.data_match.group(1).decode('utf-8'))
-    category = str(event.data_match.group(2).decode('utf-8'))
-    if event.sender_id != userid:
-        return await event.answer("• This Is Not For You!", alert=True)
+    category = str(event.data_match.group(1).decode('utf-8'))
     buttons = []
     for plugin in client.HELP[category]:
-        buttons.append(Button.inline(f"• {plugin} •", data=f"OTHERGetHelp:{userid}:{plugin}:{category}"))
+        buttons.append(Button.inline(f"• {plugin} •", data=f"OtherGetHelp:{plugin}:{category}"))
     buttons = client.functions.chunker(buttons, sizes=[3,2])
-    buttons.append([Button.inline(client.STRINGS["inline"]["Back"], data=f"OTHERHelp:{userid}"), Button.inline(client.STRINGS["inline"]["Close"], data=f"OTHERCloseHelp:{userid}")])
+    buttons.append([Button.inline(client.STRINGS["inline"]["Back"], data="OtherHelp"), Button.inline(client.STRINGS["inline"]["Close"], data="OtherCloseHelp")])
     text = STRINGS["category"].format(client.mention(client.me), category)
     await event.edit(text=text, buttons=buttons)
 
-@client.bot.on(events.CallbackQuery(data=b"OTHERGetHelp\:(.*)\:(.*)\:(.*)"))
+@client.bot.on(events.CallbackQuery(data="OtherGetHelp\:(.*)\:(.*)"))
 async def getplugin(event):
-    userid = int(event.data_match.group(1).decode('utf-8'))
-    plugin = event.data_match.group(2).decode('utf-8')
-    category = event.data_match.group(3).decode('utf-8')
-    if event.sender_id != userid:
-        return await event.answer("• This Is Not For You!", alert=True)
+    plugin = event.data_match.group(1).decode('utf-8')
+    category = event.data_match.group(2).decode('utf-8')
     info = client.HELP[category][plugin]
     text = "**꥟ " + info["Help"] + "**\n"
     text += "⊰ ┈───╌ ❊ ╌───┈ ⊱" + "\n\n"
@@ -64,13 +55,10 @@ async def getplugin(event):
         if info["Commands"][command]:
             text += "    **› " + info["Commands"][command] + "**\n"
         text += "\n" + "─────── ⋆ ───────" + "\n\n"
-    buttons = [[Button.inline(client.STRINGS["inline"]["Back"], data=f"OTHERGetCategory:{userid}:{category}"), Button.inline(client.STRINGS["inline"]["Close"], data=f"OTHERCloseHelp:{userid}")]]
+    buttons = [[Button.inline(client.STRINGS["inline"]["Back"], data=f"OtherGetCategory:{category}"), Button.inline(client.STRINGS["inline"]["Close"], data="OtherCloseHelp")]]
     await event.edit(text=text, buttons=buttons) 
 
-@client.bot.on(events.CallbackQuery(data=f"OTHERCloseHelp\:(.*)"))
+@client.bot.on(events.CallbackQuery(data="OtherCloseHelp"))
 async def closehelp(event):
-    userid = int(event.data_match.group(1).decode('utf-8'))
-    if event.sender_id != userid:
-        return await event.answer("• This Is Not For You!", alert=True)
     text = STRINGS["closehelp"]
     await event.edit(text=text)
