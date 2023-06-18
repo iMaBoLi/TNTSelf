8aa3e2b61b7441ca5428df992307cc3ded5f2b73
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-19 00:44:32 +0330

    Update EmojiGame.py

--- FidoSelf/plugins/EmojiGame.py
+++ FidoSelf/plugins/EmojiGame.py
@@ -1,1 +1,34 @@
-FidoSelf/plugins/EmojiGame.py
+from FidoSelf import client
+from telethon.tl import types
+
+__INFO__ = {
+    "Category": "Funs",
+    "Plugname": "Emojis",
+    "Pluginfo": {
+        "Help": "To Send Emoji Games In Chats!",
+        "Commands": {
+            "{CMD}SDice": None,
+            "{CMD}SDart": None,
+            "{CMD}SBasket": None,
+            "{CMD}SFoot": None,
+            "{CMD}SBoll": None,
+            "{CMD}SSlot": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+@client.Command(command="S(Dice|Dart|Basket|Foot|Boll|Slot)")
+async def gameemojis(event):
+    await event.delete()
+    emojis = {
+        "Dice": "🎲",
+        "Dart": "🎯",
+        "Basket": "🏀",
+        "Foot": "⚽️",
+        "Boll": "🎳",
+        "Slot": "🎰",
+    }
+    emoji = event.pattern_match.group(1).title()
+    emoji = emojis[emoji]
+    await client.send_file(event.chat_id, types.InputMediaDice(emoji))
