921b307c8fdd58bd7a734c8f366dcd0eedf74755
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-19 00:44:53 +0330

    Update MutePv.py

--- FidoSelf/plugins/MutePv.py
+++ FidoSelf/plugins/MutePv.py
@@ -1,1 +1,31 @@
-FidoSelf/plugins/MutePv.py
+from FidoSelf import client
+
+__INFO__ = {
+    "Category": "Private",
+    "Plugname": "MutePv",
+    "Pluginfo": {
+        "Help": "To Mute Users In Pv!",
+        "Commands": {
+            "{CMD}MutePv <On-Off>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+STRINGS = {
+    "change": "**The Mute Pv Mode Has Been {}!**",
+}
+@client.Command(command="MutePv (On|Off)")
+async def mutepvmode(event):
+    await event.edit(client.STRINGS["wait"])
+    change = event.pattern_match.group(1).lower()
+    client.DB.set_key("MUTE_PV", change)
+    ShowChange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
+    await event.edit(STRINGS["change"].format(ShowChange))
+
+@client.Command(onlysudo=False, alowedits=False)
+async def mutepv(event):
+    if not event.is_private or event.is_white or event.is_sudo or event.is_bot: return
+    mode = client.DB.get_key("MUTE_PV") or "OFF"
+    if mode == "ON":
+        await event.delete()
