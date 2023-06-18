405735730685e5a724c045de27724ca976e3febb
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-19 00:44:52 +0330

    Update AntiEdit.py

--- FidoSelf/plugins/AntiEdit.py
+++ FidoSelf/plugins/AntiEdit.py
@@ -1,1 +1,39 @@
-FidoSelf/plugins/AntiEdit.py
+from FidoSelf import client
+from telethon import events, types
+
+__INFO__ = {
+    "Category": "Manage",
+    "Plugname": "Anti Edit",
+    "Pluginfo": {
+        "Help": "To Delete Edited Messages And Send Whitout Edit!",
+        "Commands": {
+            "{CMD}AntiEdit <On-Off>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+STRINGS = {
+    "change": "**The Anti Edit Mode Has Been {}!**",
+}
+
+@client.Command(command="AntiEdit (On|Off)")
+async def setanti(event):
+    await event.edit(client.STRINGS["wait"])
+    change = event.pattern_match.group(1).lower()
+    client.DB.set_key("ANTIEDIT_MODE", change)
+    ShowChange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
+    await event.edit(STRINGS["change"].format(ShowChange))
+
+@client.Command()
+async def antiedit(event):
+    if event.original_update.to_dict()["_"] not in ["UpdateEditMessage", "UpdateEditChannelMessage"]: return
+    if event.checkCmd() or event.via_bot_id: return
+    antimode = client.DB.get_key("ANTIEDIT_MODE") or "OFF"
+    if antimode == "ON":
+        getmsg = await client.get_messages(event.chat_id, ids=event.id)
+        if getmsg.reply_to:
+            await event.respond(getmsg, reply_to=getmsg.reply_to.reply_to_msg_id)
+        else:
+            await event.respond(getmsg)
+        await event.delete()
