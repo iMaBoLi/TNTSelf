a44af5df25c16101858d9282193005bf3decfdd8
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-19 00:44:43 +0330

    Update Ping.py

--- FidoSelf/plugins/Ping.py
+++ FidoSelf/plugins/Ping.py
@@ -1,1 +1,30 @@
-FidoSelf/plugins/Ping.py
+from FidoSelf import client
+from datetime import datetime
+
+__INFO__ = {
+    "Category": "Setting",
+    "Plugname": "Ping",
+    "Pluginfo": {
+        "Help": "Return Ping Of Your Self!",
+        "Commands": {
+            "{CMD}Ping": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+STRINGS = {
+    "bping": "**!!!**",
+    "ping": "**PonG!** [ `{ping}` ]",
+}
+
+@client.Command(command="Ping")
+async def ping(event):
+    start = datetime.now()
+    await event.edit(STRINGS["bping"])
+    end = datetime.now()
+    tms = (end - start).microseconds / 10000
+    ping = round(tms / 3, 2)
+    text = STRINGS["ping"]
+    text = text.format(ping=ping)
+    await event.edit(text)
