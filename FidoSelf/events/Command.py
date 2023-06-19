0208f1c1a4713e071fb703660f90d6232279a855
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-19 17:50:09 +0330

    Update FidoSelf/events/Command.py

--- FidoSelf/events/Command.py
+++ FidoSelf/events/Command.py
@@ -13,31 +13,23 @@ def Command(
 ):
     if command and not pattern:
         pattern = f"(?i)^\.{command}$"
-        
     if pattern and pattern not in client.COMMANDS:
         client.COMMANDS.append(pattern)
-
     def decorator(func):
         async def wrapper(event):
             try:
                 event.is_sudo = True if event.sender_id == client.me.id else False
                 event.is_ch = True if event.is_channel and not event.is_group else False
-                chat = await event.get_chat()
-                if onlysudo and not event.is_sudo and (event.is_ch and not (chat.creator or chat.admin_rights)): return
                 event.reply_message = await event.get_reply_message()
-                event.is_bot = False
+                chat = await event.get_chat()
                 sender = await event.get_sender()
-                if not isinstance(sender, types.User) or sender.bot:
-                    event.is_bot = True
-                event.is_black = False
+                if onlysudo and not event.is_sudo and (event.is_ch and not (chat.creator or chat.admin_rights)): return
+                event.is_bot = True if (not isinstance(sender, types.User) or sender.bot) else False
                 blacks = client.DB.get_key("BLACKS") or []
-                if event.sender_id in blacks:
-                    event.is_black = True
-                if not event.is_sudo and event.is_black: return
-                event.is_white = False
+                event.is_black = True if event.sender_id in blacks else False
                 whites = client.DB.get_key("WHITES") or []
-                if event.sender_id in whites:
-                    event.is_white = True
+                event.is_white = True if event.sender_id in whites else False
+                if not event.is_sudo and event.is_black: return
                 if event.via_bot_id: return
                 await func(event)
             except:
