6cc19eb21296148e9a87ae11ea87a4442cb83c23
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-20 01:04:30 +0330

    Update FidoSelf/plugins/Translate.py

--- FidoSelf/plugins/Translate.py
+++ FidoSelf/plugins/Translate.py
@@ -22,13 +22,8 @@ STRINGS = {
 @client.Command(command="Str (.*)")
 async def translator(event):
     await event.edit(client.STRINGS["wait"])
-    mtype = client.functions.mediatype(event.reply_message)
-    if not event.is_reply or not event.reply_message.text:
-        medias = client.STRINGS["replyMedia"]
-        media = medias["Text"]
-        rtype = medias[mtype]
-        text = client.STRINGS["replyMedia"]["Main"].format(rtype, media)
-        return await event.edit(text)
+    reply, mtype = event.checkReply("Text")
+    if reply: return await event.edit(reply)
     dest = event.pattern_match.group(1).lower()
     if dest not in googletrans.LANGUAGES:
         return await event.edit(STRINGS["notlang"].format(dest))
