7f6264c58eca6a863aada0a9d94b4ba23fbb487e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-20 11:03:17 +0330

    Update FidoSelf/plugins/GetProfiles.py

--- FidoSelf/plugins/GetProfiles.py
+++ FidoSelf/plugins/GetProfiles.py
@@ -21,11 +21,9 @@ STRINGS = {
 @client.Command(command="GProfiles ?(.*)?")
 async def delprofiles(event):
     await event.edit(client.STRINGS["wait"])
-    result, userid = await event.userid(event.pattern_match.group(1))
-    if not result and str(userid) == "Invalid":
-        return await event.edit(client.STRINGS["getid"]["IU"])
-    elif not result and not userid:
-        return await event.edit(client.STRINGS["getid"]["UUP"])
+    userid = await event.userid(event.pattern_match.group(1))
+    if not userid:
+        return await event.edit(client.STRINGS["getuserID"])
     info = await client.get_entity(userid)
     mention = client.functions.mention(info)
     photos = await client.get_profile_photos(userid)
