219a09abf0f6c29ab3327f8059879fc2c6d629d4
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-21 11:57:47 +0330

    Update FidoSelf/plugins/GoodBy.py

--- FidoSelf/plugins/GoodBy.py
+++ FidoSelf/plugins/GoodBy.py
@@ -91,7 +91,7 @@ async def autogoodby(event):
     if not event.user_left and not event.user_kicked: return
     goodbymode = client.DB.get_key("GOODBY_MODE") or "OFF"
     chats = client.DB.get_key("GOODBY_CHATS") or {}
-    if event.chat_id in chats: return
+    if event.chat_id not in chats: return
     if goodbymode == "ON":
         info = chats[event.chat_id]
         getmsg = await client.get_messages(int(info["chat_id"]), ids=int(info["msg_id"]))
