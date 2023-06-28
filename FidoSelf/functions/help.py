dfdc76279aa552d15adefa2bef80e10578d10a30
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-28 10:03:19 +0330

    Delete help.py

--- FidoSelf/functions/help.py
+++
@@ -1,9 +0,0 @@
-from FidoSelf import client
-
-def AddInfo(info):
-    category = info["Category"]
-    pluginname = info["Plugname"]
-    plugininfo = info["Pluginfo"]
-    if category not in client.HELP:
-        client.HELP.update({category: {}})
-    client.HELP[category][pluginname] = plugininfo
