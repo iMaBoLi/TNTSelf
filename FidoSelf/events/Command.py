27cbbc1d56b5696eb16c4e55c8833768c937562e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-30 00:01:57 +0330

    update FidoSelf/events/Command.py

--- FidoSelf/events/Command.py
+++ FidoSelf/events/Command.py
@@ -32,9 +32,8 @@ def Command(
                 await func(event)
             except:
                 client.LOGS.error(format_exc())
-        addhandler = client.add_event_handler(wrapper, events.NewMessage(pattern=pattern, **kwargs))
-        client.HANDLERS.append(func)
-        client.HANDLERS.append(wrapper)
+        client.add_event_handler(wrapper, events.NewMessage(pattern=pattern, **kwargs))
+        client.HANDLERS.update({func.__name__: func})
         if allowedits:
             client.add_event_handler(wrapper, events.MessageEdited(pattern=pattern, **kwargs))
         return wrapper
