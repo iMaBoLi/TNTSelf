298f98ce6c6c290b84ba87a839b1927e4c45575d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-29 17:35:03 +0330

    update FidoSelf/plugins/MainHelp.py

--- FidoSelf/plugins/MainHelp.py
+++ FidoSelf/plugins/MainHelp.py
@@ -35,15 +35,15 @@ CATS = {
 
 def gethelp(category, plugin):
     info = client.HELP[category][plugin]
-    text = "**꥟ " + info["Help"] + "**\n"
-    text += "⊰ ┈───╌ ❊ ╌───┈ ⊱" + "\n\n"
+    text = "**꥟ " + info["Help"] + "**\n\n"
+    text += "⊰ ┈───╌ ❊ ╌───┈ ⊱" + "\n"
     for command in info["Commands"]:
         cname = command.replace("{CMD}", ".")
         share = f"http://t.me/share/text?text={cname.split(' ')[0]}"
         text += f"[🔗]({share})" + ": " + f"`{cname}`" + "\n"
         if info["Commands"][command]:
-            text += "    **› " + info["Commands"][command] + "**\n"
-    text += "─────── ⋆ ───────" + "\n"
+            text += "  __› " + info["Commands"][command] + "__\n"
+    text += "⊰ ┈───╌ ❊ ╌───┈ ⊱"
     return text
 
 def search_plugin(pluginname):
@@ -100,7 +100,7 @@ async def getcategory(event):
     buttons = []
     for plugin in client.HELP[category]:
         buttons.append(Button.inline(f"• {plugin} •", data=f"GetHelp:{plugin}:{category}"))
-    buttons = client.functions.chunker(buttons, sizes=[2,1,2])
+    buttons = list(client.functions.chunks(buttons, 2))
     buttons.append([Button.inline(client.STRINGS["inline"]["Back"], data="Help"), Button.inline(client.STRINGS["inline"]["Close"], data="CloseHelp")])
     text = STRINGS["category"].format(client.mention(client.me), category)
     await event.edit(text=text, buttons=buttons)
