41b5ec50a4c835ced7a8aa971d1111ab2e7a51ab
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-29 09:49:38 +0330

    Update FidoSelf/plugins/Searcher.py

--- FidoSelf/plugins/Searcher.py
+++ FidoSelf/plugins/Searcher.py
@@ -32,20 +32,20 @@ async def searcher(event):
     query = str(event.pattern_match.group(2))
     filters = {
         "All": None,
-        "Photo": types.InputMessagesFilterPhotos(),
-        "Video": types.InputMessagesFilterVideo(),
-        "Gif": types.InputMessagesFilterGif(),
-        "Voice": types.InputMessagesFilterVoice(),
-        "Music": types.InputMessagesFilterMusic(),
-        "File": types.InputMessagesFilterDocument(),
-        "Url": types.InputMessagesFilterUrl(),
+        "Photo": types.InputMessagesFilterPhotos,
+        "Video": types.InputMessagesFilterVideo,
+        "Gif": types.InputMessagesFilterGif,
+        "Voice": types.InputMessagesFilterVoice,
+        "Music": types.InputMessagesFilterMusic,
+        "File": types.InputMessagesFilterDocument,
+        "Url": types.InputMessagesFilterUrl,
     }
     filter = event.pattern_match.group(1).title()
     addfilter = filters[filter]
     text = STRINGS["result"].format((query or "---"), filter)
     count = 1
-    async for message in client.iter_messages(event.chat_id, search=query, filter=filter, limit=40):
-        link = await client(functions.channels.ExportMessageLinkRequest(channel=event.chat_id, id=message.id, thread=True))
+    async for message in client.iter_messages(event.chat_id, search=query, filter=addfilter, limit=40):
+        link = await client(functions.channels.ExportMessageLinkRequest(channel=message.chat_id, id=message.id, thread=True))
         name = STRINGS["click"]
         text += f"**{count} -** [{name}]({link.link})\n"
         count += 1
