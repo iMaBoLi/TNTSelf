6d1959ca7e0d5ea37447775c982504b84938a739
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-30 20:14:30 +0330

    update FidoSelf/plugins/FontFile.py

--- FidoSelf/plugins/FontFile.py
+++ FidoSelf/plugins/FontFile.py
@@ -1,83 +1,54 @@
 from FidoSelf import client
+import os
 
 __INFO__ = {
     "Category": "Account",
-    "Plugname": "Font File",
+    "Plugname": "Font",
     "Pluginfo": {
         "Help": "To Save Font File For Profile Time!",
         "Commands": {
-            "{CMD}NewFont <Name>": "Save Font File White Name!",
-            "{CMD}DeleteFont <Name>": "Delete Font File White Name!",
-            "{CMD}FontList": None,
-            "{CMD}CleanFontList": None,
+            "{CMD}SetFont <Reply(File)>": None,
+            "{CMD}DelFont": None,
+            "{CMD}GetFont": None,
         },
     },
 }
 client.functions.AddInfo(__INFO__)
 
 STRINGS = {
-    "more": "**Sorry, You Cannot Save More Than 10 Fonts!**",
-    "ttf": "**Please Reply To Font File With .TTF Format!**",
-    "newnot": "**The Font** ( `{}` ) **Already In Font List!**",
-    "newadd": "**The Font** ( `{}` ) **Added To Font List!**",
-    "delnot": "**The Font** ( `{}` ) **Not In Font List!**",
-    "del": "**The Font** ( `{}` ) **Deleted From Font List!**",
-    "empty": "**The Font List Is Empty!**",
-    "list": "**The Font List:**\n\n",
-    "aempty": "**The Font List Is Already Empty!**",
-    "clean": "**The Font List Is Cleaned!**",
+    "notttf": "**Please Reply To Font File With .TTF Format!**",
+    "setfont": "**The Font File Has Been Saved!**",
+    "delfont": "**The Font File Has Been Deleted!**",
+    "notsave": "**The Font File Is Not Saved!**",
+    "getfont": "**The Font File For Profile Time!**",
 }
 
-@client.Command(command="NewFont (.*)")
-async def savefontfile(event):
+@client.Command(command="SetFont")
+async def setfontfile(event):
     await event.edit(client.STRINGS["wait"])
     reply, _ = event.checkReply(["File"])
     if reply: return await event.edit(reply)
-    fname = str(event.pattern_match.group(1))
-    fonts = client.DB.get_key("FONTS") or {}
-    if (fname + ".ttf") in fonts:
-        return await event.edit(STRINGS["newnot"].format(fname + ".ttf"))
-    if len(fonts) > 10:
-        return await event.edit(STRINGS["more"])
     format = str(event.reply_message.media.document.attributes[0].file_name).split(".")[-1]
     if format != "ttf":
-        return await event.edit(STRINGS["ttf"])
+        return await event.edit(STRINGS["notttf"])
     info = await event.reply_message.save()
     get = await client.get_messages(int(info["chat_id"]), ids=int(info["msg_id"]))
-    await get.download_media(client.PATH + fname + ".ttf")
-    fonts.update({fname + ".ttf": info})
-    client.DB.set_key("FONTS", fonts)
-    await event.edit(STRINGS["newadd"].format(fname + ".ttf"))
+    await get.download_media(client.PATH + "FontFile.ttf")
+    client.DB.set_key("FONT_FILE", info)
+    await event.edit(STRINGS["setfont"])
 
-@client.Command(command="DelFont (.*)")
+@client.Command(command="DelFont")
 async def delfontfile(event):
     await event.edit(client.STRINGS["wait"])
-    fname = str(event.pattern_match.group(1))
-    fonts = client.DB.get_key("FONTS") or {}
-    if fname not in fonts:
-        return await event.edit(STRINGS["delnot"].format(fname))
-    del fonts[fname]
-    client.DB.set_key("FONTS", fonts)
-    await event.edit(STRINGS["del"].format(fname)) 
-
-@client.Command(command="FontList")
-async def fontlist(event):
+    client.DB.del_key("FONT_FILE")
+    await event.edit(STRINGS["delfont"])
+    
+@client.Command(command="GetFont")
+async def getfontfile(event):
     await event.edit(client.STRINGS["wait"])
-    fonts = client.DB.get_key("FONTS") or {}
-    if not fonts:
-        return await event.edit(STRINGS["empty"])
-    text = STRINGS["list"]
-    row = 1
-    for font in fonts:
-        text += f"**{row} -** `{font}`\n"
-        row += 1
-    await event.edit(text)
-
-@client.Command(command="CleanFontList")
-async def cleanfonts(event):
-    await event.edit(client.STRINGS["wait"])
-    fonts = client.DB.get_key("FONTS") or {}
-    if not fonts:
-        return await event.edit(STRINGS["aempty"])
-    client.DB.del_key("FONTS")
-    await event.edit(STRINGS["clean"])
+    font = client.PATH + "FontFile.ttf"
+    if not os.path.exists(font):
+        return await event.edit(STRINGS["notsave"])
+    caption = STRINGS["getfont"]
+    await event.respond(caption, file=font)
+    await event.delete()
