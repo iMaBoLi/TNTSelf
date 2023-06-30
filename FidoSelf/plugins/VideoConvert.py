e15fca60a929dacddae51e2f4e205f3847c4d47b
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-06-30 10:48:07 +0330

    update FidoSelf/plugins/VideoConvert.py

--- FidoSelf/plugins/VideoConvert.py
+++ FidoSelf/plugins/VideoConvert.py
@@ -1,65 +1,58 @@
 from FidoSelf import client
 from telethon import types
-from moviepy.editor import VideoFileClip
-import time
 import os
 
 __INFO__ = {
     "Category": "Practical",
-    "Plugname": "Video Convert",
+    "Plugname": "Video",
     "Pluginfo": {
-        "Help": "To Convert Your Videos And Gifs!",
+        "Help": "To Convert Video Note To Video!",
         "Commands": {
-            "{CMD}SVNormal <Reply(Gif|VNote)>": "Convert To Normal Video!",
-            "{CMD}SVGif <Reply(Video|VNote)>": "Convert To Gif!",
-            "{CMD}SVNote <Reply(Video|Gif)>": "Convert To Video Note!",
+            "{CMD}SVideo <Reply(VideoNote)>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+__INFO__ = {
+    "Category": "Practical",
+    "Plugname": "VideoNote",
+    "Pluginfo": {
+        "Help": "To Convert Video To Video Note!",
+        "Commands": {
+            "{CMD}SVNote <Reply(Video)>": None,
         },
     },
 }
 client.functions.AddInfo(__INFO__)
 
-@client.Command(command="SV(Note|Normal|Gif)")
+@client.Command(command="SVideo")
 async def videoconvert(event):
     await event.edit(client.STRINGS["wait"])
     mode = event.pattern_match.group(1).title()
-    reply, mtype = event.checkReply(["Video", "Gif", "VideoNote"])
+    reply, mtype = event.checkReply(["VideoNote"])
     if reply: return await event.edit(reply)
     if event.reply_message.file.size > client.MAX_SIZE:
         return await event.edit(client.STRINGS["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
-    if mode == "Normal" and mtype in ["VideoNote", "Gif"]:
-        callback = event.progress(download=True)
-        video = await event.reply_message.download_media(client.PATH, progress_callback=callback)
-        attributes = [types.DocumentAttributeVideo(duration=event.reply_message.file.duration, w=event.reply_message.file.width, h=event.reply_message.file.height, round_message=False, supports_streaming=True)]
-        callback = event.progress(upload=True)
-        await client.send_file(event.chat_id, video, attributes=attributes, progress_callback=callback)
-        os.remove(video)
-        await event.delete()
-    elif mode == "Note" and mtype in ["Video", "Gif"]:
-        callback = event.progress(download=True)
-        video = await event.reply_message.download_media(client.PATH, progress_callback=callback)
-        attributes = [types.DocumentAttributeVideo(duration=event.reply_message.file.duration, w=event.reply_message.file.width, h=event.reply_message.file.height, round_message=True)]
-        callback = event.progress(upload=True)
-        await client.send_file(event.chat_id, video, attributes=attributes, progress_callback=callback)
-        os.remove(video)
-        await event.delete()
-    elif mode == "Gif" and mtype in ["Video", "VideoNote"]:
-        callback = event.progress(download=True)
-        video = await event.reply_message.download_media(client.PATH, progress_callback=callback)
-        newfile = video + ".gif"
-        videoClip = VideoFileClip(video)
-        videoClip.write_gif(newfile)
-        callback = event.progress(upload=True)
-        await client.send_file(event.chat_id, newfile, progress_callback=callback)
-        os.remove(newfile)
-        os.remove(video)
-        await event.delete()
-    else:
-        if mode == "Normal":
-            reply = event.checkReply(["Gif", "VideoNote"])
-            await event.edit(reply)
-        elif mode == "Note":
-            reply = event.checkReply(["Video", "Gif"])
-            await event.edit(reply)
-        elif mode == "Gif":
-            reply = event.checkReply(["Video", "VideoNote"])
-            await event.edit(reply)
+    callback = event.progress(download=True)
+    videonote = await event.reply_message.download_media(client.PATH, progress_callback=callback)
+    attributes = [types.DocumentAttributeVideo(duration=event.reply_message.file.duration, w=event.reply_message.file.width, h=event.reply_message.file.height, round_message=False, supports_streaming=True)]
+    callback = event.progress(upload=True)
+    await client.send_file(event.chat_id, videonote, attributes=attributes, progress_callback=callback)
+    os.remove(videonote)
+    await event.delete()
+
+@client.Command(command="SVNote")
+async def videonotecon(event):
+    await event.edit(client.STRINGS["wait"])
+    mode = event.pattern_match.group(1).title()
+    reply, mtype = event.checkReply(["Video"])
+    if reply: return await event.edit(reply)
+    if event.reply_message.file.size > client.MAX_SIZE:
+        return await event.edit(client.STRINGS["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
+    callback = event.progress(download=True)
+    video = await event.reply_message.download_media(client.PATH, progress_callback=callback)
+    attributes = [types.DocumentAttributeVideo(duration=event.reply_message.file.duration, w=event.reply_message.file.width, h=event.reply_message.file.height, round_message=True)]
+    callback = event.progress(upload=True)
+    await client.send_file(event.chat_id, video, attributes=attributes, progress_callback=callback)
+    os.remove(video)
+    await event.delete()
