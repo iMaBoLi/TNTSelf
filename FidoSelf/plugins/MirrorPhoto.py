003c5455f6af752417adc9dc12121e3c8f4abc4f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-02 14:31:22 +0330

    update FidoSelf/plugins/MirrorPhoto.py

---
+++ FidoSelf/plugins/MirrorPhoto.py
@@ -0,0 +1,34 @@
+from FidoSelf import client
+from PIL import Image, ImageOps
+import os
+
+__INFO__ = {
+    "Category": "Tools",
+    "Plugname": "Mirror Photo",
+    "Pluginfo": {
+        "Help": "To Mirror Your Photo!",
+        "Commands": {
+            "{CMD}SMirror": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+STRINGS = {
+    "mirror": "**The Photo Has Been Mirrored!**",
+}
+
+@client.Command(command="SMirror")
+async def mirrorphoto(event):
+    await event.edit(client.STRINGS["wait"])
+    reply, _ = event.checkReply(["Photo"])
+    if reply: return await event.edit(reply)
+    photo = await event.reply_message.download_media(client.PATH)
+    newphoto = client.PATH + "MirrorPhoto.jpg"
+    img = Image.open(photo)
+    newimg = ImageOps.mirror(img)
+    newimg.save(newphoto)
+    await client.send_file(event.chat_id, newphoto, caption=STRINGS["mirror"])        
+    os.remove(photo)
+    os.remove(newphoto)
+    await event.delete()
