cd58a667edfca226b5c598fb058a0ae077cede8e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-02 11:58:28 +0330

    update FidoSelf/plugins/Updater.py

--- FidoSelf/plugins/Updater.py
+++ FidoSelf/plugins/Updater.py
@@ -20,12 +20,4 @@ async def update(event):
     await event.edit(STRINGS["complete"])
     shutil.rmtree(path)
     os.remove("Fido.zip")
-    await client.disconnect()
-    await client.bot.disconnect()
     await client.functions.runcmd("python3 -m FidoSelf")
-    
-@client.Command(command="Dis")
-async def discon(event):
-    await event.edit(STRINGS["dis"])
-    await client.disconnect()
-    await client.bot.disconnect()
