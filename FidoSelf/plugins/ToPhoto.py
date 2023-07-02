5204fa6bbfe13b98cb64a9b5ced7f36a1211faa0
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-02 16:31:36 +0330

    update FidoSelf/plugins/ToPhoto.py

---
+++ FidoSelf/plugins/ToPhoto.py
@@ -0,0 +1,30 @@
+from FidoSelf import client
+from PIL import Image
+import os
+
+__INFO__ = {
+    "Category": "Tools",
+    "Plugname": "To Photo",
+    "Pluginfo": {
+        "Help": "To Convert Stickers To Photo!",
+        "Commands": {
+            "{CMD}ToPhoto <Reply(Sticker)>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+@client.Command(command="ToPhoto")
+async def tophoto(event):
+    await event.edit(client.STRINGS["wait"])
+    mode = event.pattern_match.group(1).title()
+    if reply:= event.checkReply(["Sticker"]):
+        return await event.edit(reply)
+    sticker = await event.reply_message.download_media(client.PATH)
+    photo = client.PATH + "StickerToPhoto.jpg"
+    img = Image.open(sticker)
+    img.save(newfile, format="jpeg")  
+    await client.send_file(event.chat_id, photo)
+    os.remove(sticker)
+    os.remove(photo)
+    await event.delete()
