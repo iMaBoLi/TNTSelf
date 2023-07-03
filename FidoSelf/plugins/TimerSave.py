375ec06fb86d9068544c04d9e18a967e590e14e2
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-03 12:02:07 +0330

    update FidoSelf/plugins/TimerSave.py

--- FidoSelf/plugins/TimerSave.py
+++ FidoSelf/plugins/TimerSave.py
@@ -29,16 +29,14 @@ async def tsave(event):
     await event.edit(STRINGS["change"].format(ShowChange))
 
 @client.Command(onlysudo=False)
-async def savemedias(event):
+async def timermedias(event):
     if not event.is_private or event.is_bot: return
-    if not event.file or event.checkReply(["Photo", "Video"]): return
+    if not event.media or event.checkReply(["Photo", "Video"]): return
     tmode = client.DB.get_key("TIMER_MODE") or "OFF"
-    if tmode == "ON" and hasattr(event.media, "ttl_seconds") and event.media.ttl_seconds:
-        if event.file.size > client.MAX_SIZE: return
-        file = await event.download_media(client.PATH)
+    if tmode == "ON" and event.media.to_dict()["_"] != "MessageMediaWebPage" and hasattr(event.media, "ttl_seconds") and event.media.ttl_seconds:
         sender = await event.get_sender()
         mention = client.functions.mention(sender)
         ttl = client.functions.convert_time(event.media.ttl_seconds)
         caption = STRINGS["caption"].format(mention, ttl)
-        await client.send_file(client.REALM, file, caption=caption)
+        await client.send_file(client.REALM, event.media, caption=caption)
         os.remove(file)
