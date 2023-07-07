c4456f011cbfb4c0a96f6fe2397608bc93bb766e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-07 11:04:03 +0330

    Update Comment.py

--- FidoSelf/plugins/Comment.py
+++ FidoSelf/plugins/Comment.py
@@ -5,7 +5,7 @@ import aiocron
 import random
 
 __INFO__ = {
-    "Category": "Practical",
+    "Category": "Groups",
     "Name": "Comment",
     "Info": {
         "Help": "To Manage Auto Comment In The Channel Messages!",
