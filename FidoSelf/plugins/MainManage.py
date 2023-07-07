758c594592e9724354e67563bf091e848613af1b
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 00:18:08 +0330

    Update MainManage.py

--- FidoSelf/plugins/MainManage.py
+++ FidoSelf/plugins/MainManage.py
@@ -55,7 +55,7 @@ async def Manage(event):
     await event.edit(client.STRINGS["wait"])
     userid = await event.userid(event.pattern_match.group(1))
     if not userid:
-        return await event.edit(client.STRINGS["getuserID"])
+        return await event.edit(client.STRINGS["user"]["all"])
     chatid = event.chat_id
     res = await client.inline_query(client.bot.me.username, f"Manage:{chatid}:{userid}")
     if event.is_reply:
