8eb88a94e7a909d54996f89e53879e9a52a94393
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 23:32:12 +0330

    update FidoSelf/plugins/Time.py

--- FidoSelf/plugins/Time.py
+++ FidoSelf/plugins/Time.py
@@ -17,7 +17,7 @@ __INFO__ = {
 client.functions.AddInfo(__INFO__)
 
 STRINGS = {
-    "time": "**{STR} \ud804\udc4d Time:** ( `{}` )\n**\ud804\udc4d Date:** ( `{}` )\n**\ud804\udc4d Day:** ( `{}` )\n**\ud804\udc4d Month:** ( `{}` )\n\n**\ud804\udc4d Date:** ( `{}` )\n**\ud804\udc4d Day:** ( `{}` )\n**\ud804\udc4d Month:** ( `{}` )"
+    "time": "**{STR} Time:** ( `{}` )\n**{STR} Date:** ( `{}` )\n**{STR} Day:** ( `{}` )\n**{STR} Month:** ( `{}` )\n\n**{STR} Date:** ( `{}` )\n**{STR} Day:** ( `{}` )\n**{STR} Month:** ( `{}` )"
 }
 
 @client.Command(command="Time")
