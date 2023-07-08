d00c1c09b7a1f74023b83f96586f4ecf27732fb8
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 18:04:36 +0330

    update FidoSelf/plugins/Online.py

--- FidoSelf/plugins/Online.py
+++ FidoSelf/plugins/Online.py
@@ -1,20 +1,23 @@
 from FidoSelf import client
 from telethon import functions
+import aiocron
 
 __INFO__ = {
     "Category": "Setting",
     "Name": "Online",
     "Info": {
-        "Help": "To Set Online Mode For Account!",
+        "Help": "To Setting Online Status For Account!",
         "Commands": {
-            "{CMD}Online <On-Off>": None,
+            "{CMD}Online <On-Off>": {
+                "Help": "To Turn On-Off Online Mode",
+            },
         },
     },
 }
 client.functions.AddInfo(__INFO__)
 
-@client.Command(onlysudo=False)
-async def online(event):
-    mode = client.DB.get_key("ONLINE_MODE") or "OFF"
-    if mode == "ON":
+@aiocron.crontab("*/5 * * * * *")
+async def autosender():
+    onmode = client.DB.get_key("ONLINE_MODE") or "OFF"
+    if onmode == "ON":
         await client(functions.account.UpdateStatusRequest(offline=False))
