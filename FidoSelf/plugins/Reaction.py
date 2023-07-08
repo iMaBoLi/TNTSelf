bbbc31a93b04b280082b2ef03655986385e7459a
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 19:22:49 +0330

    Update Reaction.py

--- FidoSelf/plugins/Reaction.py
+++ FidoSelf/plugins/Reaction.py
@@ -26,7 +26,7 @@ STRINGS = {
 
 @client.Command(command="Reaction (On|Off)")
 async def reactionchat(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     change = event.pattern_match.group(1).upper()
     acChats = client.DB.get_key("REACTION_CHATS") or []
     chatid = event.chat_id
@@ -38,25 +38,25 @@ async def reactionchat(event):
         if chatid in acChats:
             acChats.remove(chatid)
             client.DB.set_key("REACTION_CHATS", acChats)
-    ShowChange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
-    await event.edit(STRINGS["reactchat"].format(ShowChange))
+    showchange = client.getstrings()["On"] if change == "ON" else client.getstrings()["Off"]
+    await event.edit(client.getstrings(STRINGS)["reactchat"].format(showchange))
 
 @client.Command(command="ReactionAll (On|Off)")
 async def reactionall(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     change = event.pattern_match.group(1).upper()
     client.DB.set_key("REACTION_MODE", change)
-    ShowChange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
-    await event.edit(STRINGS["reactall"].format(ShowChange))
+    showchange = client.getstrings()["On"] if change == "ON" else client.getstrings()["Off"]
+    await event.edit(client.getstrings(STRINGS)["reactall"].format(showchange))
 
 @client.Command(command="SetReaction (.*)")
 async def setreaction(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     emoji = event.pattern_match.group(1)
     if emoji not in (await getemojis()) and emoji != "random":
-        return await event.edit(STRINGS["notreact"].format(emoji))
+        return await event.edit(client.getstrings(STRINGS)["notreact"].format(emoji))
     client.DB.set_key("REACTION_EMOJI", emoji)
-    await event.edit(STRINGS["setreact"].format(emoji.title()))
+    await event.edit(client.getstrings(STRINGS)["setreact"].format(emoji.title()))
 
 async def getemojis():
     result = await client(functions.messages.GetAvailableReactionsRequest(hash=0))
