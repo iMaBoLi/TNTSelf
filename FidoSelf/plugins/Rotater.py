11657e1508275f8ef7b9d961a772107e2eec1912
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 19:22:52 +0330

    Update Rotater.py

--- FidoSelf/plugins/Rotater.py
+++ FidoSelf/plugins/Rotater.py
@@ -22,29 +22,29 @@ STRINGS = {
 
 @client.Command(command="SRotate (\d*)")
 async def rotate(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     darge = int(event.pattern_match.group(1))
     if reply:= event.checkReply(["Video", "Photo"]):
         return await event.edit(reply)
     mtype = event.reply_message.mediatype()
     if event.reply_message.file.size > client.MAX_SIZE:
-        return await event.edit(client.STRINGS["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
+        return await event.edit(client.getstrings()["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
     if mtype == "Photo":
         file = await event.reply_message.download_media(client.PATH)
         newfile = client.PATH + f"RotatedImage-{str(darge)}.jpg"
         img = Image.open(file)
         newimg = img.rotate(darge)
         newimg.save(newfile)
-        await event.respond(STRINGS["proted"].format(str(darge)), file=newfile)        
+        await event.respond(client.getstrings(STRINGS)["proted"].format(str(darge)), file=newfile)        
     elif mtype == "Video":
         callback = event.progress(download=True)
         file = await event.reply_message.download_media(client.PATH, progress_callback=callback)
-        await event.edit(STRINGS["vrot"].format(str(darge)))
+        await event.edit(client.getstrings(STRINGS)["vrot"].format(str(darge)))
         newfile = client.PATH + f"RotatedVideo-{str(darge)}.mp4"
         cmd = f'ffmpeg -i {file} -vf "rotate={darge}" {newfile}'
         await client.functions.runcmd(cmd)
         callback = event.progress(upload=True)
-        caption = STRINGS["vroted"].format(str(darge))
+        caption = client.getstrings(STRINGS)["vroted"].format(str(darge))
         await client.send_file(event.chat_id, newfile, caption=caption, progress_callback=callback)        
     os.remove(file)
     os.remove(newfile)
