1aabd3f45861cb0ffd978e730baddedd076886eb
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 19:22:55 +0330

    Update Sign.py

--- FidoSelf/plugins/Sign.py
+++ FidoSelf/plugins/Sign.py
@@ -20,18 +20,18 @@ STRINGS = {
 
 @client.Command(command="Sign (On|Off)")
 async def signmode(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     change = event.pattern_match.group(1).upper()
     client.DB.set_key("SIGN_MODE", change)
-    ShowChange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
-    await event.edit(STRINGS["change"].format(ShowChange))
+    showchange = client.getstrings()["On"] if change == "ON" else client.getstrings()["Off"]
+    await event.edit(client.getstrings(STRINGS)["change"].format(showchange))
 
 @client.Command(command="SetSign ([\s\S]*)")
 async def setsign(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     stext = event.pattern_match.group(1)
     client.DB.set_key("SIGN_TEXT", stext)
-    text = STRINGS["setsign"].format(stext)
+    text = client.getstrings(STRINGS)["setsign"].format(stext)
     await event.edit(text)
 
 @client.Command(allowedits=False)
