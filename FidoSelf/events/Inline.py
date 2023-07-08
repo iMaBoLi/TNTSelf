0ab9ec2c186939cb09e8c2666cba0b3dac8c1530
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 15:33:43 +0330

    update FidoSelf/events/Inline.py

--- FidoSelf/events/Inline.py
+++ FidoSelf/events/Inline.py
@@ -11,7 +11,8 @@ def Inline(
         async def wrapper(event):
             try:
                 event.is_sudo = True if event.sender_id == client.me.id else False
-                if onlysudo and not event.is_sudo:
+                vips = client.DB.get_key("VIP_USERS") or []
+                if onlysudo and not (event.is_sudo or (event.sender_id in vips)):
                     text = client.STRINGS["OtherInline"]
                     return await event.answer([event.builder.article("FidoSelf - NotForYou", text=text)])
                 await func(event)
