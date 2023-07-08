4889583ff19fbcdc1c2d5789041bb7266b17496e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 19:22:53 +0330

    Update Save.py

--- FidoSelf/plugins/Save.py
+++ FidoSelf/plugins/Save.py
@@ -49,51 +49,51 @@ STRINGS = {
 
 @client.Command(command="Save (.*)")
 async def savesaves(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     name = event.pattern_match.group(1)
     if reply:= event.checkReply():
         return await event.edit(reply)
     saves = client.DB.get_key("SAVED_LIST") or {}
     if name in saves:
-        return await event.edit(STRINGS["notall"].format(name))
+        return await event.edit(client.getstrings(STRINGS)["notall"].format(name))
     info = await event.reply_message.save()
     saves.update({name: info})
     client.DB.set_key("SAVED_LIST", saves)
-    await event.edit(STRINGS["save"].format(name))
+    await event.edit(client.getstrings(STRINGS)["save"].format(name))
 
 @client.Command(command="Delete (.*)$")
 async def delsaves(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     name = event.pattern_match.group(1)
     saves = client.DB.get_key("SAVED_LIST") or {}
     if name not in saves:
-        return await event.edit(STRINGS["notin"].format(name))
+        return await event.edit(client.getstrings(STRINGS)["notin"].format(name))
     del saves[name]
     client.DB.set_key("SAVED_LIST", saves)
-    await event.edit(STRINGS["del"].format(name))
+    await event.edit(client.getstrings(STRINGS)["del"].format(name))
 
 @client.Command(command="Get (.*)$")
 async def getsaves(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     name = event.pattern_match.group(1)
     saves = client.DB.get_key("SAVED_LIST") or {}
     if name not in saves:
-        return await event.edit(STRINGS["notin"].format(name))
+        return await event.edit(client.getstrings(STRINGS)["notin"].format(name))
     info = saves[name]
     try:
         message = await client.get_messages(info["chat_id"], ids=info["msg_id"])
         await event.respond(message)
         await event.delete()
     except:
-        await event.edit(STRINGS["notav"].format(name))
+        await event.edit(client.getstrings(STRINGS)["notav"].format(name))
         
 @client.Command(command="SaveList")
 async def savelist(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     saves = client.DB.get_key("SAVED_LIST") or {}
     if not saves:
-        return await event.edit(STRINGS["empty"])
-    text = STRINGS["list"]
+        return await event.edit(client.getstrings(STRINGS)["empty"])
+    text = client.getstrings(STRINGS)["list"]
     row = 1
     for save in saves:
         text += f"**{row} -** `{save}`\n"
@@ -102,9 +102,9 @@ async def savelist(event):
 
 @client.Command(command="CleanSaveList")
 async def cleansaves(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     saves = client.DB.get_key("SAVED_LIST") or {}
     if not saves:
-        return await event.edit(STRINGS["aempty"])
+        return await event.edit(client.getstrings(STRINGS)["aempty"])
     client.DB.del_key("SAVED_LIST")
-    await event.edit(STRINGS["clean"])
+    await event.edit(client.getstrings(STRINGS)["clean"])
