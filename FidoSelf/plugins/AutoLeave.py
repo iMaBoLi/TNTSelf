6e8c90e28aed94d1e06b499d1881fcb0e28a6412
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 19:22:16 +0330

    Update AutoLeave.py

--- FidoSelf/plugins/AutoLeave.py
+++ FidoSelf/plugins/AutoLeave.py
@@ -20,11 +20,11 @@ STRINGS = {
 
 @client.Command(command="AutoLeave (On|Off)")
 async def autoleavemode(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     change = event.pattern_match.group(1).upper()
     client.DB.set_key("AUTOLEAVE_MODE", change)
-    schange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
-    await event.edit(STRINGS["change"].format(schange))
+    schange = client.getstrings()["On"] if change == "ON" else client.getstrings()["Off"]
+    await event.edit(client.getstrings(STRINGS)["change"].format(schange))
     
 @client.on(events.ChatAction())
 async def autoleave(event):
@@ -35,7 +35,7 @@ async def autoleave(event):
             try:
                 chat = await event.get_chat()
                 await client(functions.channels.LeaveChannelRequest(channel=chat.id))
-                text = STRINGS["autoleave"].format(chat.id, (chat.username or chat.title))
+                text = client.getstrings(STRINGS)["autoleave"].format(chat.id, (chat.username or chat.title))
                 await client.send_message(client.REALM, text)
             except:
                 pass
