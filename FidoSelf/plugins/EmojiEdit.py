6d739d53fb51721a140914b73ff51901ebf66285
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 19:22:28 +0330

    Update EmojiEdit.py

--- FidoSelf/plugins/EmojiEdit.py
+++ FidoSelf/plugins/EmojiEdit.py
@@ -20,20 +20,20 @@ STRINGS = {
 
 @client.Command(command="Emoji (On|Off)")
 async def emojimode(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     change = event.pattern_match.group(1).upper()
     client.DB.set_key("EMOJI_MODE", change)
-    ShowChange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
-    await event.edit(STRINGS["change"].format(ShowChange))
+    showchange = client.getstrings()["On"] if change == "ON" else client.getstrings()["Off"]
+    await event.edit(client.getstrings(STRINGS)["change"].format(showchange))
 
 @client.Command(command="SetEmoji (.*)\-(.*)")
 async def setemoji(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     emoji1 = event.pattern_match.group(1)
     emoji2 = event.pattern_match.group(2)
     emojis = emoji1 + "-" + emoji2
     client.DB.set_key("EDIT_EMOJIS", emojis)
-    text = STRINGS["setemoji"].format(emojis)
+    text = client.getstrings(STRINGS)["setemoji"].format(emojis)
     await event.edit(text)
 
 @client.Command(allowedits=False)
