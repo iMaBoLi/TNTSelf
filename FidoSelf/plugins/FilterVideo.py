111b0117fcec04d54018338536358fabe9f26641
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 19:22:31 +0330

    Update FilterVideo.py

--- FidoSelf/plugins/FilterVideo.py
+++ FidoSelf/plugins/FilterVideo.py
@@ -21,21 +21,21 @@ STRINGS = {
 
 @client.Command(command="SV(Bw|Negative)")
 async def editvideo(event):
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(client.getstrings()["wait"])
     vfilter = event.pattern_match.group(1).title()
     if reply:= event.checkReply(["Video"]):
         return await event.edit(reply)
     callback = event.progress(download=True)
     file = await event.reply_message.download_media(client.PATH, progress_callback=callback)
     addfilter = "BlackWhite" if vfilter == "Bw" else "Negative"
-    await event.edit(STRINGS["coning"].format(addfilter))
+    await event.edit(client.getstrings(STRINGS)["coning"].format(addfilter))
     outfile = client.PATH + f"FilterVideo-{str(vfilter)}.mp4"
     if vfilter == "Bw":
         cmd = f'ffmpeg -i "{file}" -vf format=gray {outfile} -y'
     elif vfilter == "Negative":
         cmd = f'ffmpeg -i "{file}" -vf lutyuv="y=negval:u=negval:v=negval" {outfile} -y'
     await client.functions.runcmd(cmd)
-    caption = STRINGS["caption"].format(addfilter)
+    caption = client.getstrings(STRINGS)["caption"].format(addfilter)
     await client.send_file(event.chat_id, outfile, caption=caption, supports_streaming=True)
     os.remove(file)
     os.remove(outfile)
