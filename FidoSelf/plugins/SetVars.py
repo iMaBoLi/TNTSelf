f759a713c8f78aa75bdad6204157df4d57e2eb05
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 07:50:21 +0330

    update FidoSelf/plugins/SetVars.py

--- FidoSelf/plugins/SetVars.py
+++ FidoSelf/plugins/SetVars.py
@@ -30,17 +30,15 @@ __INFO__ = {
 client.functions.AddInfo(__INFO__)
 
 STRINGS = {
-    "norealm": "**Please Send In Group For Set Realm Chat!**",
-    "setrealm": "**This Chat Is Saved For Realm Chat!**",
-    "noback": "**Please Send In Channel For Set BackUp Channel!**",
-    "setback": "**This Channel Is Saved For BackUp Channel!**",
+    "setrealm": "**✦ This Chat Is Saved For Realm Chat!**",
+    "setback": "**✦ This Channel Is Saved For BackUp Channel!**",
 }
 
 @client.Command(command="SetRealm")
 async def realm(event):
     await event.edit(client.STRINGS["wait"])
     if not event.is_group:
-        return await event.edit(STRINGS["norealm"])
+        return await event.edit(client.STRINGS["only"]["Group"])
     client.DB.set_key("REALM_CHAT", event.chat_id)
     client.realm = event.chat_id
     await event.edit(STRINGS["setrealm"])
@@ -49,7 +47,7 @@ async def realm(event):
 async def backup(event):
     await event.edit(client.STRINGS["wait"])
     if not event.is_ch:
-        return await event.edit(STRINGS["noback"])
+        return await event.edit(client.STRINGS["only"]["Channel"])
     client.DB.set_key("BACKUP_CHANNEL", event.chat_id)
     client.backch = event.chat_id
     await event.edit(STRINGS["setback"])
