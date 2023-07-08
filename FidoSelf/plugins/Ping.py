fe44163b45f1f59f0da0ec697ae528337d4c98e5
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 19:22:47 +0330

    Update Ping.py

--- FidoSelf/plugins/Ping.py
+++ FidoSelf/plugins/Ping.py
@@ -23,10 +23,10 @@ STRINGS = {
 @client.Command(command="Ping")
 async def ping(event):
     start = datetime.now()
-    await event.edit(STRINGS["bping"])
+    await event.edit(client.getstrings(STRINGS)["bping"])
     end = datetime.now()
     tms = (end - start).microseconds / 10000
     ping = round(tms / 3, 2)
-    text = STRINGS["ping"]
+    text = client.getstrings(STRINGS)["ping"]
     text = text.format(ping=ping)
     await event.edit(text)
