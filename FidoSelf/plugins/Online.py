dfcac13a9ddaf20a9b9d89fbbf66ca77e6fffebc
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-08 18:12:13 +0330

    update FidoSelf/plugins/Online.py

--- FidoSelf/plugins/Online.py
+++ FidoSelf/plugins/Online.py
@@ -16,6 +16,18 @@ __INFO__ = {
 }
 client.functions.AddInfo(__INFO__)
 
+STRINGS = {
+    "change": "**❁ The Online Has Been {}!**",
+}
+
+@client.Command(command="Online (On|Off)")
+async def onlinemode(event):
+    await event.edit(client.STRINGS["wait"])
+    change = event.pattern_match.group(1).upper()
+    client.DB.set_key("ONLINE_MODE", change)
+    ShowChange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
+    await event.edit(STRINGS["change"].format(ShowChange))
+
 @aiocron.crontab("*/5 * * * * *")
 async def autosender():
     onmode = client.DB.get_key("ONLINE_MODE") or "OFF"
