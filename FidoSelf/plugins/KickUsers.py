8e2340f170e9878e700c84de4508fa57decac5a7
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-10 00:57:15 +0330

    update FidoSelf/plugins/KickUsers.py

--- FidoSelf/plugins/KickUsers.py
+++ FidoSelf/plugins/KickUsers.py
@@ -16,22 +16,21 @@ __INFO__ = {
 client.functions.AddInfo(__INFO__)
 
 STRINGS = {
-    "notacs": "**{STR} \u2736 You Do Not Have Access To Kick Users!**",
+    "notacs": "**{STR} You Do Not Have Access To Kick Users!**",
     "kickuser": "**{STR} The User** ( {} ) **Was Kicked In This Chat!**",
     "errorkick": "**{STR} The User** ( {} ) **Is Not Kicked!**\n**Error:** ( `{}` )"
 }
 
-@client.Command(command="Kick ?(.*)?")
+@client.Command(command="Kick", userid=True)
 async def kickuser(event):
     await event.edit(client.STRINGS["wait"])
     if not event.is_group:
         return await event.edit(client.STRINGS["only"]["Group"])
-    userid = await event.userid(event.pattern_match.group(1))
-    if not userid:
+    if not event.userid:
         return await event.edit(client.STRINGS["user"]["all"])
     if not event.checkAdmin(ban_users=True):
         return await event.edit(client.getstrings(STRINGS)["notacs"])
-    info = await client.get_entity(userid)
+    info = await client.get_entity(event.userid)
     mention = client.functions.mention(info)
     try:
         await client.edit_permissions(event.chat_id, info.id, view_messages=False)
