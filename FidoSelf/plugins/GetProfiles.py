98a9b1fe22af6f0638580e2826c8c170900e6c70
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-10 00:58:49 +0330

    update FidoSelf/plugins/GetProfiles.py

--- FidoSelf/plugins/GetProfiles.py
+++ FidoSelf/plugins/GetProfiles.py
@@ -21,15 +21,14 @@ STRINGS = {
     "caption": "**{STR} The Profile Photos For** ( {} ) **Is Sended!**"
 }
 
-@client.Command(command="GProfiles ?(.*)?")
+@client.Command(command="GProfiles", userid=True)
 async def getprofiles(event):
     await event.edit(client.STRINGS["wait"])
-    userid = await event.userid(event.pattern_match.group(1))
-    if not userid:
+    if not event.userid:
         return await event.edit(client.STRINGS["user"]["all"])
-    info = await client.get_entity(userid)
+    info = await client.get_entity(event.userid)
     mention = client.functions.mention(info)
-    photos = await client.get_profile_photos(userid)
+    photos = await client.get_profile_photos(event.userid)
     if not photos:
         return await event.edit(client.getstrings(STRINGS)["not"].format(mention))
     caption = client.getstrings(STRINGS)["caption"].format(mention)
