a4a1ddecd64add80cd97ef108e4fc4977ece7b2f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-09 22:48:05 +0330

    update FidoSelf/plugins/MainHelp.py

--- FidoSelf/plugins/MainHelp.py
+++ FidoSelf/plugins/MainHelp.py
@@ -32,7 +32,7 @@ STRINGS = {
 
 CATEGORYS = {
     "Setting": ["Help", "Panel", "Lists", "Manage", "Save", "Command", "Realm", "BackUp", "Simbel", "Ping", "Reload"],
-    "Manage": ["Quick", "Save", "Auto", "Vip", "Love", "White", "Black", "MarkRead", "Enemy", "Foshs", "Echo", "Timer", "Rank"],
+    "Manage": ["Quick", "Save", "Auto", "Vip", "Love", "White", "Black", "MarkRead", "Enemy", "Foshs", "Echo", "Timer"],
     "Tools": ["Time", "Translate", "RemoveBg", "Ocr", "Logo", "Image Slicer", "Screen Shot", "Open Ai", "Ai Image", "Country Info", "Najva"],
     "Practical": ["Action", "Copy Action", "Edit Modes", "Anti Forward", "Anti Edit", "Auto Delete", "Auto Translate", "Reaction", "Repeat", "Replace", "Emoji", "Poker"],
     "Usage": ["Youtube", "Cover File", "Video Shot", "Trim Video", "Trim Audio", "Rotater", "Extract Audio", "Edit Duration", "Music Info"],
