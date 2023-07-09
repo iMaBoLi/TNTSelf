98d3c13c0b2419bf5d94feb7faca8dd1d103199d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-09 17:11:07 +0330

    update FidoSelf/plugins/Variebels.py

---
+++ FidoSelf/plugins/Variebels.py
@@ -0,0 +1,28 @@
+from FidoSelf import client
+
+VARS = {
+    "TIME": "Use For Replace With Time String ( Ex: 23:59 )",
+    "DATE": "Use For Replace With Date String ( Ex: 2222/22/22 )",
+    "HEART": "Use For Replace With Random Heart ( Ex: ❤️ )",
+    "FIRSTNAME": "Use For Replace With First Name Of User",
+    "MENTION": "Use For Replace With Mention Of User",
+    "USERNAME": "Use For Replace With Username Of User",
+    "TITLE": "Use For Replace With Chat Title",
+    "CHATUSERNAME": "Use For Replace With Chat Username",
+    "COUNT": "Use For Replace With Chat Members Count",
+}
+
+for Var in VARS:
+    __INFO__ = {
+        "Category": "Variebels",
+        "Name": Var,
+        "Info": {
+            "Help": "The Variebel For Use In Messages And ...",
+            "Commands": {
+                Var: {
+                    "Help": VARS[Var],
+                },
+            },
+        },
+    }
+    client.functions.AddInfo(__INFO__)
