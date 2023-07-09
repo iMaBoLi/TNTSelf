b2eb91965e26a9b6a5e5d99e36ab618d2f67febc
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-09 19:53:34 +0330

    update FidoSelf/plugins/AntiSpamPv.py

--- FidoSelf/plugins/AntiSpamPv.py
+++ FidoSelf/plugins/AntiSpamPv.py
@@ -75,7 +75,13 @@ WARNS = {}
 
 @client.Command(onlysudo=False, allowedits=False)
 async def antispam(event):
-    if not event.is_private or event.is_white or event.is_sudo or event.is_bot: return
+    if (
+        or not event.is_private
+        or event.is_white
+        or event.is_sudo
+        or event.is_bot
+    ):
+        return
     antimode = client.DB.get_key("ANTISPAMPV_MODE") or "OFF"
     if antimode == "OFF": return
     mutes = client.DB.get_key("MUTEPV_USERS") or []
