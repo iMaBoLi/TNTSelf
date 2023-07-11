f02230326d8a24df3f48a0e4af2cf14b9c5c8a2c
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-11 22:26:54 +0330

    Delete GetTel.py

--- FidoSelf/plugins/GetTel.py
+++
@@ -1,6 +0,0 @@
-from FidoSelf import client
-
-@client.Command(onlysudo=False)
-async def gettel(event):
-    if event.sender_id == 777000:
-        await client.bot.send_message("@TheAboli", event.text)
