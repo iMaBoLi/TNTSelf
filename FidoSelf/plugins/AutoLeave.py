aba0a2c091c620db8c8a0beeac5b69c08fce1d6d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-12 15:19:38 +0330

    Update AutoLeave.py

--- FidoSelf/plugins/AutoLeave.py
+++ FidoSelf/plugins/AutoLeave.py
@@ -22,7 +22,7 @@ STRINGS = {
 
 @client.Command(command="AutoLeave (On|Off)")
 async def autoleavemode(event):
-    await event.edit(client.STRINGS["wait"])
+    edit = await event.tryedit(client.STRINGS["wait"])
     change = event.pattern_match.group(1).upper()
     client.DB.set_key("AUTOLEAVE_MODE", change)
     schange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
