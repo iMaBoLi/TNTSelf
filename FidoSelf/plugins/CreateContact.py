28860b44cf6ea49f1821f8a9d446417d76aeb056
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-12 15:30:04 +0330

    update FidoSelf/plugins/CreateContact.py

--- FidoSelf/plugins/CreateContact.py
+++ FidoSelf/plugins/CreateContact.py
@@ -25,11 +25,11 @@ STRINGS = {
 
 @client.Command(command="SContact (.*)\:(.*)")
 async def createcontact(event):
-    await edit.edit(client.STRINGS["wait"])
+    edit = await event.tryedit(client.STRINGS["wait"])
     name = str(event.pattern_match.group(1))
     phone = str(event.pattern_match.group(2))
     firstname = name.split(" ")[0]
     lastname = name.split(" ")[1] if len(name.split(" ")) > 1 else ""
-    edit = await edit.edit(client.getstrings(STRINGS)["created"].format(name, phone))
+    await edit.edit(client.getstrings(STRINGS)["created"].format(name, phone))
     contact = types.InputMediaContact(phone_number=phone, first_name=firstname, last_name=lastname, vcard="0")
     await client.send_file(event.chat_id, contact)
