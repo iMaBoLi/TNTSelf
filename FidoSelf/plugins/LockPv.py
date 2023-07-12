9eb80cd43b9d07dc4bfa47080dbf7c2f1d68178b
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-12 15:19:14 +0330

    Update LockPv.py

--- FidoSelf/plugins/LockPv.py
+++ FidoSelf/plugins/LockPv.py
@@ -20,7 +20,7 @@ STRINGS = {
 }
 @client.Command(command="LockPv (On|Off)")
 async def lockpvmode(event):
-    await event.edit(client.STRINGS["wait"])
+    edit = await event.tryedit(client.STRINGS["wait"])
     change = event.pattern_match.group(1).upper()
     client.DB.set_key("LOVKPV_MODE", change)
     showchange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
