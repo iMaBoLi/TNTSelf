7820d3fabcfd4729f5237e9ad4d893129f48e3e0
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-13 10:07:54 +0330

    Update AntiForward.py

--- FidoSelf/plugins/AntiForward.py
+++ FidoSelf/plugins/AntiForward.py
@@ -20,7 +20,7 @@ STRINGS = {
 
 @client.Command(command="AntiForward (On|Off)")
 async def setantiforward(event):
-    edit = await event.tryedit(client.STRINGS["wait"])
+    await event.edit(client.STRINGS["wait"])
     change = event.pattern_match.group(1).upper()
     client.DB.set_key("ANTIFORWARD_MODE", change)
     showchange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
