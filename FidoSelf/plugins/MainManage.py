a0c216dcf862b906be576835efe4dd26cbd2644d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-13 10:08:12 +0330

    Update MainManage.py

--- FidoSelf/plugins/MainManage.py
+++ FidoSelf/plugins/MainManage.py
@@ -53,7 +53,7 @@ async def get_manage_buttons(userid, chatid):
 
 @client.Command(command="Manage", userid=True)
 async def Manage(event):
-    edit = await event.tryedit(client.STRINGS["wait"])
+    await event.edit(client.STRINGS["wait"])
     if not event.userid:
         return await event.edit(client.STRINGS["user"]["all"])
     chatid = event.chat_id
