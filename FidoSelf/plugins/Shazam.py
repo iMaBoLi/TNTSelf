0ab1f4812cd7374c4c14a106ebf9703ad3296424
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-14 13:46:13 +0330

    update FidoSelf/plugins/Shazam.py

--- FidoSelf/plugins/Shazam.py
+++ FidoSelf/plugins/Shazam.py
@@ -8,8 +8,8 @@ __INFO__ = {
         "Help": "To Recognize Your Musics In Shazam!",
         "Commands": {
             "{CMD}SShazam": {
-                "Help": "To Set Duration",
-                "Reply": ["Video", "Music", "Voice"],
+                "Help": "To Search Shazam",
+                "Reply": ["Music", "Video", "Voice"],
             },
         },
     },
@@ -17,13 +17,14 @@ __INFO__ = {
 client.functions.AddInfo(__INFO__)
 
 STRINGS = {
-    "caption": "**{STR} The Music In Shazam Is Finded!**\n\n**{STR} Title:** ( `{}` )",
+    "notfound": "**{STR} The Music Is Not Find In Shazam!**",
+    "caption": "**{STR} The Music In Shazam!**\n\n**{STR} Title:** ( `{}` )\n**{STR} Artist:** ( `{}` )",
 }
 
 @client.Command(command="SShazam")
 async def searchshazam(event):
     await event.edit(client.STRINGS["wait"])
-    if reply:= event.checkReply(["Video", "Music", "Voice"]):
+    if reply:= event.checkReply(["Music", "Video", "Voice"]):
         return await event.edit(reply)
     if event.reply_message.file.size > client.MAX_SIZE:
         return await event.edit(client.STRINGS["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
@@ -31,9 +32,12 @@ async def searchshazam(event):
     file = await event.reply_message.download_media(client.PATH, progress_callback=callback)
     shazam = Shazam()
     track = await shazam.recognize_song(file)
-    title = track["track"]["share"]["subject"]
-    thumb = track["track"]["images"]["background"]
-    caption = STRINGS["caption"].format(title)
+    if not track["matches"]:
+        return await event.edit(client.getstrings(STRINGS)["notfound"])
+    title = track["track"]["title"]
+    subtitle = track["track"]["subtitle"]
+    thumb = track["track"]["images"]["coverarthq"]
+    caption = client.getstrings(STRINGS)["caption"].format(title, subtitle)
     await client.send_file(event.chat_id, thumb, caption=caption)
     os.remove(file)
     await event.delete()
