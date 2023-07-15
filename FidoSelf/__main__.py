a222dd194d8b668f25090332796543487292fe11
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-15 14:25:25 +0330

    update FidoSelf/__main__.py

--- FidoSelf/__main__.py
+++ FidoSelf/__main__.py
@@ -7,9 +7,9 @@ import platform
 async def setup():
     client.LOGS.info("• Adding Coustom Vars To Client ...")
     await AddVarsToClient()
+    client.functions = functions
     client.LOGS.info("• Installing Youtube Downloader ...")
     await client.functions.runcmd("pip install yt-dlp")
-    client.functions = functions
     client.LOGS.info("• Installing Main Plugins ...")
     plugs, notplugs = load_plugins(client.PLUGINS)
     client.LOGS.info(f"• Successfully Installed {len(plugs)} Plugin From Main Plugins!")
