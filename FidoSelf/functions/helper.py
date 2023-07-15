bb1a3feacd44382ff61e1af709a1ca78ee90205e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-15 12:23:22 +0330

    update FidoSelf/functions/helper.py

--- FidoSelf/functions/helper.py
+++ FidoSelf/functions/helper.py
@@ -15,11 +15,11 @@ def progress(event, download=False, upload=False):
 setattr(Message, "progress", progress)
 setattr(client, "progress", progress)
 
-async def fileprogress(event, file, total, download=False, upload=False):
+def fileprogress(event, file, total, download=False, upload=False):
     newtime = time.time()
     current = os.path.getsize(file) if os.path.exists(file) else 0
     while (total >= current):
-        callback = await create_progress(event, current, total, newtime, download, upload)
+        callback = client.loop.create_task(create_progress(event, current, total, newtime, download, upload))
         current = os.path.getsize(file)
 
 setattr(Message, "fileprogress", fileprogress)
