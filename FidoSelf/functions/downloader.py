b8d7a7fea196e2236be54e72de2c010b473d9d06
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-17 10:54:40 +0330

    update FidoSelf/functions/downloader.py

---
+++ FidoSelf/functions/downloader.py
@@ -0,0 +1,19 @@
+from aiohttp import ClientSession
+import time
+
+async def file_download(event, downloadurl, filename=None):
+    if not filename:
+        filename = downloadurl.split("/")[-1]
+    async with ClientSession() as session:
+        async with session.get(downloadurl, timeout=None) as response:
+            total_size = int(response.headers.get("content-length", 0)) or 0
+            downloaded_size = 0
+            start_time = time.time()
+            with open(filename, "wb") as ffile:
+                async for chunk in response.content.iter_chunked(1024):
+                    if chunk:
+                        file.write(chunk)
+                        downloaded_size += len(chunk)
+                        if total_size and downloaded_size:
+                            event.progress(current=downloaded_size, total=total_size, download=True)
+            return filename
