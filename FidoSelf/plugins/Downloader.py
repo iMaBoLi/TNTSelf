fa7e447550e851a55b4cfca035aea0046f4f894b
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-19 18:11:56 +0330

    update FidoSelf/plugins/Downloader.py

--- FidoSelf/plugins/Downloader.py
+++ FidoSelf/plugins/Downloader.py
@@ -1,4 +1,5 @@
 from FidoSelf import client
+import requests
 import os
 
 __INFO__ = {
@@ -7,11 +8,34 @@ __INFO__ = {
     "Info": {
         "Help": "To Download File From Your Links!",
         "Commands": {
-            "{CMD}SFile <Name> <Link>": {
+            "{CMD}DVideo <Link>": {
+                "Help": "To Download Video",
+                "Input": {
+                    "<Link>": "Link Of Video",
+                },
+            },
+            "{CMD}DMusic <Link>": {
+                "Help": "To Download Music",
+                "Input": {
+                    "<Link>": "Link Of Music",
+                },
+            },
+            "{CMD}DPhoto <Link>": {
+                "Help": "To Download Photo",
+                "Input": {
+                    "<Link>": "Link Of Photo",
+                },
+            },
+            "{CMD}DFile <Link>": {
                 "Help": "To Download File",
                 "Input": {
-                    "<Name>": "Name Of File",
                     "<Link>": "Link Of File",
+                },
+            },
+            "{CMD}DApk <Link>": {
+                "Help": "To Download Apk",
+                "Input": {
+                    "<Link>": "Link Of Apk",
                 },
             },
         },
@@ -22,23 +46,40 @@ client.functions.AddInfo(__INFO__)
 STRINGS = {
     "downloading": "**{STR} Downloading File From Your Link ...**\n\n**{STR} Link:** ( `{}` )\n\n**{STR} FileName:** ( `{}` )",
     "errordown": "**{STR} Downloading File Is Not Completed!**\n\n**{STR} Error:** ( `{}` )",
+    "notdown": "**{STR} The File Is Not Downloaded!**\n\n**{STR} Link:** ( `{}` )",
     "caption": "**{STR} Link:** ( `{}` )\n\n**{STR} FileName:** ( `{}` )",
 }
 
-@client.Command(command="SFile (.*) (.*)")
+@client.Command(command="D(Video|Music|Photo|File|Apk) (.*)")
 async def downloadfile(event):
     await event.edit(client.STRINGS["wait"])
-    filename = event.pattern_match.group(1)
+    dtype = event.pattern_match.group(1).title()
     link = event.pattern_match.group(2)
+    TYPES = {
+        "Video": ".mp4",
+        "Music": ".mp3",
+        "Photo": ".jpg",
+        "File": ".txt",
+        "Apk": ".apk",
+    }
+    filename = link.split("/")[-1]
+    filename = filename + TYPES[dtype]
     await event.edit(client.getstrings(STRINGS)["downloading"].format(link, filename))
     filepath = client.PATH + filename
+    try:
+        resp = requests.get(link)
+        filesize = resp.headers["content-length"]
+    except Exception as error:
+        return await event.edit(client.getstrings(STRINGS)["errordown"].format(error))
+    if filesize > client.MAX_SIZE:
+        return await event.edit(client.STRINGS["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
     cmd = f"curl {link} -o {filepath}"
-    result, error = await client.functions.runcmd(cmd)
-    if error:
-        await event.edit(client.getstrings(STRINGS)["errordown"].format(error))
-    if os.path.exists(filepath):
-        caption = client.getstrings(STRINGS)["caption"].format(link, filename)
-        callback = event.progress(upload=True)
-        uploadfile = await client.fast_upload(filepath, progress_callback=callback)
-        await client.send_file(event.chat_id, uploadfile, caption=caption)        
-        os.remove(filepath)
+    await client.functions.runcmd(cmd)
+    if not os.path.exists(filepath):
+        return await event.edit(client.getstrings(STRINGS)["notdown"].format(link))
+    caption = client.getstrings(STRINGS)["caption"].format(link, filename)
+    callback = event.progress(upload=True)
+    uploadfile = await client.fast_upload(filepath, progress_callback=callback)
+    await client.send_file(event.chat_id, uploadfile, caption=caption)        
+    os.remove(filepath)
+    await event.delete()
