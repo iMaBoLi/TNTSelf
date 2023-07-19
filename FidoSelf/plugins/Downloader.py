dc1823c328b772715a8863bdfe4a45fa775df3e1
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-19 18:20:54 +0330

    update FidoSelf/plugins/Downloader.py

--- FidoSelf/plugins/Downloader.py
+++ FidoSelf/plugins/Downloader.py
@@ -26,10 +26,10 @@ __INFO__ = {
                     "<Link>": "Link Of Photo",
                 },
             },
-            "{CMD}DFile <Link>": {
-                "Help": "To Download File",
+            "{CMD}DTxt <Link>": {
+                "Help": "To Download Txt File",
                 "Input": {
-                    "<Link>": "Link Of File",
+                    "<Link>": "Link Of Txt File",
                 },
             },
             "{CMD}DApk <Link>": {
@@ -44,13 +44,13 @@ __INFO__ = {
 client.functions.AddInfo(__INFO__)
 
 STRINGS = {
-    "downloading": "**{STR} Downloading File From Your Link ...**\n\n**{STR} Link:** ( `{}` )\n\n**{STR} FileName:** ( `{}` )",
-    "errordown": "**{STR} Downloading File Is Not Completed!**\n\n**{STR} Error:** ( `{}` )",
-    "notdown": "**{STR} The File Is Not Downloaded!**\n\n**{STR} Link:** ( `{}` )",
+    "downloading": "**{STR} Downloading {} From Your Link ...**\n\n**{STR} Link:** ( `{}` )\n\n**{STR} FileName:** ( `{}` )",
+    "errordown": "**{STR} Downloading {} Is Not Completed!**\n\n**{STR} Error:** ( `{}` )",
+    "notdown": "**{STR} The {} Is Not Downloaded!**\n\n**{STR} Link:** ( `{}` )",
     "caption": "**{STR} Link:** ( `{}` )\n\n**{STR} FileName:** ( `{}` )",
 }
 
-@client.Command(command="D(Video|Music|Photo|File|Apk) (.*)")
+@client.Command(command="D(Video|Music|Photo|Txt|Apk) (.*)")
 async def downloadfile(event):
     await event.edit(client.STRINGS["wait"])
     dtype = event.pattern_match.group(1).title()
@@ -59,24 +59,24 @@ async def downloadfile(event):
         "Video": ".mp4",
         "Music": ".mp3",
         "Photo": ".jpg",
-        "File": ".txt",
+        "Txt": ".txt",
         "Apk": ".apk",
     }
     filename = link.split("/")[-1]
     filename = filename + TYPES[dtype]
-    await event.edit(client.getstrings(STRINGS)["downloading"].format(link, filename))
+    await event.edit(client.getstrings(STRINGS)["downloading"].format(dtype, link, filename))
     filepath = client.PATH + filename
     try:
         resp = requests.get(link)
         filesize = resp.headers["content-length"]
     except Exception as error:
-        return await event.edit(client.getstrings(STRINGS)["errordown"].format(error))
+        return await event.edit(client.getstrings(STRINGS)["errordown"].format(dtype, error))
     if int(filesize) > client.MAX_SIZE:
         return await event.edit(client.STRINGS["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
     cmd = f"curl {link} -o {filepath}"
     await client.functions.runcmd(cmd)
     if not os.path.exists(filepath):
-        return await event.edit(client.getstrings(STRINGS)["notdown"].format(link))
+        return await event.edit(client.getstrings(STRINGS)["notdown"].format(dtype, link))
     caption = client.getstrings(STRINGS)["caption"].format(link, filename)
     callback = event.progress(upload=True)
     uploadfile = await client.fast_upload(filepath, progress_callback=callback)
