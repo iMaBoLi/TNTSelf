10851e5d61e98fe7be980310016d2bc405473f30
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-20 23:41:02 +0330

    update FidoSelf/functions/youtube.py

--- FidoSelf/functions/youtube.py
+++ FidoSelf/functions/youtube.py
@@ -5,23 +5,18 @@ import secrets
 import re
 import os
 
-YOUTUBE_REGEX = re.compile(r"(?:youtube\.com|youtu\.be)/(?:[\w-]+\?v=|embed/|v/|shorts/)?([\w-]{11})")
-
 MAIN = "yt-dlp -o '{outfile}' --write-thumbnail -f {format} --max-filesize {size} {link}"
 THUMB = "yt-dlp -o '{outfile}' --write-thumbnail --skip-download {link}"
+SUBTITLE = "yt-dlp -o '{outfile}' --write-subs --skip-download {link}"
 
 def yt_info(link):
     from yt_dlp import YoutubeDL
     info = YoutubeDL().extract_info(link, download=False)
     return info
 
-def get_videoid(url):
-    match = YOUTUBE_REGEX.search(url)
-    return match.group(1)
-
 async def yt_video(link):
     from yt_dlp import YoutubeDL
-    videoid = get_videoid(link) 
+    videoid = link[-11:]
     token = secrets.token_hex(nbytes=5)
     outfile = client.PATH + "youtube/" + f"{token} - {videoid}.mp4"
     cmd = MAIN.format(outfile=outfile, format="best", size=client.MAX_SIZE, link=link)
@@ -30,7 +25,7 @@ async def yt_video(link):
 
 async def yt_audio(link):
     from yt_dlp import YoutubeDL
-    videoid = get_videoid(link) 
+    videoid = link[-11:]
     token = secrets.token_hex(nbytes=5)
     outfile = client.PATH + "youtube/" + f"{token} - {videoid}.mp3"
     cmd = MAIN.format(outfile=outfile, format="bestaudio", size=client.MAX_SIZE, link=link)
@@ -38,7 +33,7 @@ async def yt_audio(link):
     return outfile
 
 async def yt_thumb(link):
-    videoid = get_videoid(link)
+    videoid = link[-11:]
     token = secrets.token_hex(nbytes=5)
     thumb = client.PATH + "youtube/" + f"{token} - {videoid}"
     cmd = THUMB.format(outfile=thumb, link=link)
@@ -59,3 +54,12 @@ def convert_thumb(file):
 def yt_search(query, limit=50):
     results = VideosSearch(query, limit=limit)
     return results.result()["result"]
+    
+async def yt_subtitle(link):
+    from yt_dlp import YoutubeDL
+    videoid = link[-11:]
+    token = secrets.token_hex(nbytes=5)
+    outfile = client.PATH + "youtube/" + f"{token} - {videoid}.txt"
+    cmd = SUBTITLE.format(outfile=outfile, link=link)
+    await client.functions.runcmd(cmd)
+    return outfile
