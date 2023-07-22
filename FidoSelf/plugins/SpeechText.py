f8e6f7c1e8ff10b2a55506a928291957478a747a
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2023-07-22 15:42:43 +0330

    update FidoSelf/plugins/SpeechText.py

--- FidoSelf/plugins/SpeechText.py
+++ FidoSelf/plugins/SpeechText.py
@@ -9,15 +9,15 @@ __INFO__ = {
     "Info": {
         "Help": "To Convert Your Voice And Speech To Text!",
         "Commands": {
-            "{CMD}ToText <Lang>": {
+            "{CMD}SText": {
+                "Help": "To Convert To Text",
+                "Reply": ["Music", "Voice"],
+            },
+            "{CMD}SText <Lang>": {
                 "Help": "To Convert To Text",
                 "Input": {
                     "<Lang>": "Text Language",
                 },
-                "Reply": ["Music", "Voice"],
-            },
-            "{CMD}ToText": {
-                "Help": "To Convert To Text",
                 "Reply": ["Music", "Voice"],
             },
         },
@@ -28,13 +28,14 @@ client.functions.AddInfo(__INFO__)
 STRINGS = {
     "converting": "**{STR} Converting Your Speech To Text With Language** ( `{}` ) **...**",
     "errortext": "**{STR} The Convert Is Not Completed!**\n\n**{STR} Error:** ( `{}` )",
+    "errorlang": "**{STR} The Language** ( `{}` ) **Is Not Available!**",
     "speechtext": "**{STR} The Speech Converted To Text!**\n\n**{STR} Language:** ( `{}` )\n**{STR} Results:** ( `{}` )",
 }
 
 API_KEY = "MxnMTXmF9LfkF09by0OQEHD0OgqsjwDc"
 
-@client.Command(command="ToText ?(.*)?")
-async def generatephoto(event):
+@client.Command(command="SText ?(.*)?")
+async def speechtext(event):
     await event.edit(client.STRINGS["wait"])
     client.loop.create_task(convert(event))
     
@@ -50,11 +51,16 @@ async def convert(event):
     settings = ConnectionSettings(url="https://asr.api.speechmatics.com/v2", auth_token=API_KEY)
     config = {
         "type": "transcription",
+        "transcription_config": {
+            "language": lang.lower(),
+        },
     }
     with BatchClient(settings) as bclient:
         try:
-            job_id = bclient.submit_job(audio=file, transcription_config=config)
-            transcript = bclient.wait_for_completion(job_id, transcription_format='txt')
+            jobid = bclient.submit_job(audio=file, transcription_config=config)
+            transcript = bclient.wait_for_completion(jobid, transcription_format="txt")
         except Exception as error:
+            if "400 Bad Request" in error:
+                return await event.edit(client.getstrings(STRINGS)["errorlang"].format(lang))
             return await event.edit(client.getstrings(STRINGS)["errortext"].format(error))
     await event.edit(client.getstrings(STRINGS)["speechtext"].format(lang, transcript))
