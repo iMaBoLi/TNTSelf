fb30120d1c1e101541680e777d6ff9e355c8974d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-09-09 08:38:38 +0330

    update FidoSelf/plugins/Vcinvite.py

--- FidoSelf/plugins/Vcinvite.py
+++ FidoSelf/plugins/Vcinvite.py
@@ -7,14 +7,8 @@ __INFO__ = {
     "Info": {
         "Help": "To Invite Users To Voice Chat!",
         "Commands": {
-            "{CMD}InvVc": {
+            "{CMD}InviteVc": {
                 "Help": "To Invite Users",
-            },
-            "{CMD}InvVc <Users>": {
-                "Help": "To Invite Inputed Users ( Use , To Split Answers )",
-                "Input": {
-                    "<Users>": "List Of UserIDs/Usernames",
-                },
             },
         },
     },
@@ -25,33 +19,31 @@ STRINGS = {
     "notcall": "**{STR} The Voice Chat Is Not Founded For This Chat!**",
     "notuser": "**{STR} The Users To Invite In To Voice Chat Is Not Founded!**",
     "notflood": "**{STR} The Flood Wait Error Is Coming Please Wait And Try Again!**",
-    "inviting": "**{STR} Inviting Users To Voice Chat ...**",
-    "invited": "**{STR} Invite Users To Voice Chat Completed!**"
+    "inviting": "**{STR} Inviting** ( `{}` ) **Users To Voice Chat ...**",
+    "invited": "**{STR} Invited** ( `{}` ) **Users To Voice Chat Completed!**"
 }
 
-@client.Command(command="InvVc ?(.*)?")
+@client.Command(command="InviteVc")
 async def invitevc(event):
     await event.edit(client.STRINGS["wait"])
-    users = str(event.pattern_match.group(1) or "")
     if event.chat.megagroup or event.chat.broadcast:
         info = (await client(functions.channels.GetFullChannelRequest(event.chat_id))).full_chat
     else:
         info = (await client(functions.messages.GetFullChatRequest(event.chat_id))).full_chat
     if not info.call:
         return await event.edit(client.getstrings(STRINGS)["notcall"])
-    if users:
-        usernames = users.replace("@", "").split(",")[:50]
-    else:
-        usernames = []
-        async for user in client.iter_participants(event.chat_id, filter=types.ChannelParticipantsRecent, limit=50):
-            usernames.append(user.id)
-    if not usernames:
+    users = []
+    async for user in client.iter_participants(event.chat_id, filter=types.ChannelParticipantsRecent, limit=50):
+        users.append(user.id)
+    if not users:
         return await event.edit(client.getstrings(STRINGS)["notuser"])
-    await event.edit(client.getstrings(STRINGS)["inviting"])
+    await event.edit(client.getstrings(STRINGS)["inviting"].format(len(users)))
+    invited = 0
     try:
-        result = await client(functions.phone.InviteToGroupCallRequest(call=info.call, users=usernames))
+        result = await client(functions.phone.InviteToGroupCallRequest(call=info.call, users=users))
+        invited += 1
     except errors.FloodWaitError:
         return await event.edit(client.getstrings(STRINGS)["notflood"])
     except:
         pass
-    await event.edit(client.getstrings(STRINGS)["invited"])
+    await event.edit(client.getstrings(STRINGS)["invited"].format(len(invited)))
