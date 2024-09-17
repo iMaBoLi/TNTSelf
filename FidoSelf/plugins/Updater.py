f1d5d45682e78ded2f25e67da425f3b9090120fa
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-09-17 05:04:15 +0330

    update FidoSelf/plugins/Updater.py

--- FidoSelf/plugins/Updater.py
+++ FidoSelf/plugins/Updater.py
@@ -9,14 +9,11 @@ STRINGS = {
 async def update(event):
     await event.edit(client.STRINGS["wait"])
     git = client.functions.Git()
-    link = git.repo.get_archive_link("zipball", "dev")
+    link = git.repo.get_archive_link("zipball", "master")
     await client.functions.runcmd(f"curl {link} -o Fido.zip")
-    await client.functions.runcmd("unzip Fido.zip")
-    shutil.rmtree("/FidoSelf/")
-    path = glob.glob("iMaBoLi*")[0]
-    newpath = path + "/FidoSelf/"
-    shutil.copytree(newpath, "/FidoSelf/")
+    await client.functions.runcmd("unzip Fido.zip -d ../")
+    path = glob.glob("../iMaBoLi*")[0]
+    os.rename(path, "../app")
     await event.edit(client.getstrings(STRINGS)["complete"])
-    shutil.rmtree(path)
     os.remove("Fido.zip")
     await client.functions.runcmd("python3 -m FidoSelf")
