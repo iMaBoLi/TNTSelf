4506c194609d6306f81a6aa4556e1320cc290623
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-09-22 08:30:07 +0330

    update FidoSelf/plugins/AiImage.py

--- FidoSelf/plugins/AiImage.py
+++ FidoSelf/plugins/AiImage.py
@@ -1,5 +1,7 @@
 from FidoSelf import client
 from somnium import Somnium
+from telethon.tl.types import InputMediaWebPage
+from FidoSelf.functions import STYLES
 
 __INFO__ = {
     "Category": "Tools",
@@ -17,33 +19,54 @@ __INFO__ = {
             "{CMD}GStyles": {
                 "Help": "To Get Styles",
             }
-        },
+        },1
     },
 }
 client.functions.AddInfo(__INFO__)
 
 STRINGS = {
-    "generating": "**{STR} Creating Image For** ( `{}` ) **And Style** ( `{}` ) **...**",
-    "caption": "**{STR} The Image For** ( `{}` ) **And Style** ( `{}` ) **Created!**",
-    "styles": "**{STR} The Styles For Creating Photo:**\n\n( {} )"
+    "notid": "**{STR} The Image Style Id** ( `{}` ) **Is Not Available!**",
+    "setid": "**{STR} The Image Style Id Was Set To** ( `{}` - `{}` )",
+    "styles": "**{STR} The Styles Name And ID:**\n\n"
+    "generating": "**{STR} Generating Image For Prompt** ( `{}` ) **...**\n\n**{STR} Style:** ( `{}` - `{}` )",
+    "caption": "**{STR} The Ai Image For Prompt** ( `{}` ) **Was Created!**\n\n**{STR} Style:** ( `{}` - `{}` )",
 }
 
-@client.Command(command="CPhoto (\d*),(.*)")
+@client.Command(command="SStyle (\\d*)")
+async def setstyle(event):
+    await event.edit(client.STRINGS["wait"])
+    styleid = event.pattern_match.group(1)
+    if str(styleid) not in STYLES:
+        return await event.edit(client.getstrings(STRINGS)["notid"].format(styleid))
+    client.DB.set_key("STYLEID_IMAGE", styleid)
+    name = STYLES[str(styleid)]["Name"]
+    link = STYLES[str(styleid)]["Link"]
+    media = InputMediaWebPage(url=link)
+    await event.respond(client.getstrings(STRINGS)["setid"].format(name, styleid), file=media)
+    await event.delete()
+
+@client.Command(command="GStyles")
+async def getstyles(event):
+    await event.edit(client.STRINGS["wait"])
+    text = client.getstrings(STRINGS)["styles"]
+    count = 1
+    for style in STYLES:
+        name = STYLES[style]["Name"]
+        text += f"**{count} -** ( `{name}` ) [ `{style}` ]\n"
+        count += 1
+    await event.edit(text)
+
+@client.Command(command="GPhoto ([\s\S]*)")
 async def generatephoto(event):
     await event.edit(client.STRINGS["wait"])
     client.loop.create_task(generate(event))
     
 async def generate(event):
-    style = int(event.pattern_match.group(1))
-    query = str(event.pattern_match.group(2))
-    await event.edit(client.getstrings(STRINGS)["generating"].format(query, style))
-    file = Somnium.Generate(query, style)
-    caption = client.getstrings(STRINGS)["caption"].format(query, style)
+    prompt = str(event.pattern_match.group(2))
+    styleid = client.DB.get_key("STYLEID_IMAGE") or 1
+    sname = STYLES[str(styleid)]["Name"]
+    await event.edit(client.getstrings(STRINGS)["generating"].format(prompt, name, styleid))
+    file = Somnium.Generate(prompt, int(styleid))
+    caption = client.getstrings(STRINGS)["caption"].format(prompt, name, styleid)
     await client.send_file(event.chat_id, file, caption=caption)
     await event.delete()
-    
-@client.Command(command="GStyles")
-async def getstyles(event):
-    await event.edit(client.STRINGS["wait"])
-    link = "https://graph.org/List-Of-Styles-06-05"
-    await event.edit(client.getstrings(STRINGS)["styles"].format(link))
