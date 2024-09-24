a77b4ec3255baba4843a331163f586b00b6d0fb3
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-09-24 14:35:52 +0330

    update FidoSelf/plugins/GetProfiles.py

--- FidoSelf/plugins/GetProfiles.py
+++ FidoSelf/plugins/GetProfiles.py
@@ -17,8 +17,14 @@ __INFO__ = {
 client.functions.AddInfo(__INFO__)
 
 STRINGS = {
-    "not": "**{STR} The Profile Photos For** ( {} ) **Is Not Found!**",
-    "caption": "**{STR} The Profile Photos For** ( {} ) **Is Sended!**"
+    "EN": {
+        "not": "**{STR} The Profile Photos For** ( {} ) **Is Not Found!**",
+        "caption": "**{STR} The Profile Photos For** ( {} ) **Is Sended!**"
+    },
+    "FA": {
+        "not": "**{STR} هیچ عکس پروفایلی برای** ( {} ) **پیدا نشد!**",
+        "caption": "**{STR} عکس های پروفایل** ( {} ) **ارسال شد!**"
+    },        
 }
 
 @client.Command(command="GProfiles", userid=True)
@@ -30,8 +36,8 @@ async def getprofiles(event):
     mention = client.functions.mention(info)
     photos = await client.get_profile_photos(event.userid)
     if not photos:
-        return await event.edit(client.getstrings(STRINGS)["not"].format(mention))
-    caption = client.getstrings(STRINGS)["caption"].format(mention)
+        return await event.edit(client.getstring(STRINGS, "not").format(mention))
+    caption = client.getstring(STRINGS, "caption").format(mention)
     for phs in client.functions.chunks(photos, 9):
         await event.respond(caption, file=phs)
     await event.delete()
