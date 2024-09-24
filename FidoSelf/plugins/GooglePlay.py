12a0ec79dc9410ff0f0288cf22b9b0fcf220485e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-09-24 17:12:39 +0330

    update FidoSelf/plugins/GooglePlay.py

--- FidoSelf/plugins/GooglePlay.py
+++ FidoSelf/plugins/GooglePlay.py
@@ -1,6 +1,7 @@
 from FidoSelf import client
 from google_play_scraper import app, exceptions
-from telethon.tl.types import InputMediaWebPage
+import requests
+import os
 
 __INFO__ = {
     "Category": "Usage",
@@ -22,11 +23,13 @@ client.functions.AddInfo(__INFO__)
 STRINGS = {
     "EN": {
         "notapp": "**{STR} The Google Play App With ID** ( `{}` ) **Is Not Founded!**",
-        "appinfo": "**{STR} App Name:** ( `{}` - `{}` )\n\n**{STR} Score:** ( `{}` )\n**{STR} Installs:** ( `{}` )\n**{STR} Rating:** ( `{}` )\n**{STR} Reviews:** ( `{}` )\n**{STR} Free:** ( `{}` )\n**{STR} Developer:** ( `{}` )\n\n**{STR} Description:** ( `{}` )",
+        "appinfo": "**{STR} App Name:** ( `{}` - `{}` )\n\n**{STR} Genre:** ( `{}` )\n**{STR} Score:** ( `{}` )\n**{STR} Installs:** ( `{}` )\n**{STR} Rating:** ( `{}` )\n**{STR} Reviews:** ( `{}` )\n**{STR} Free:** ( `{}` )\n**{STR} Developer:** ( `{}` )\n\n**{STR} Description:** ( `{}` )",
+        "shotcap": "**{STR} More Photos For Google Play App:** ( `{}` )",
     },
     "FA": {
         "notapp": "**{STR} هیچ اپلیکیشنی با آیدی** ( `{}` ) **پیدا نشد!**",
-        "appinfo": "**{STR} اسم اپلیکیشن:** ( `{}` - `{}` )\n\n**{STR} امتیاز:** ( `{}` )\n**{STR} تعداد نصب:** ( `{}` )\n**{STR} رتبه:** ( `{}` )\n**{STR} بازدیدها:** ( `{}` )\n**{STR} رایگان:** ( `{}` )\n**{STR} سازنده:** ( `{}` )\n\n**{STR} توضیحات:** ( `{}` )",
+        "appinfo": "**{STR} اسم اپلیکیشن:** ( `{}` - `{}` )\n\n**{STR} موضوع:** ( `{}` )\n**{STR} امتیاز:** ( `{}` )\n**{STR} تعداد نصب:** ( `{}` )\n**{STR} رتبه:** ( `{}` )\n**{STR} بازدیدها:** ( `{}` )\n**{STR} رایگان:** ( `{}` )\n**{STR} سازنده:** ( `{}` )\n\n**{STR} توضیحات:** ( `{}` )",
+        "shotcap": "**{STR} عکس هذی بیشتر برای اپلیکیشن:** ( `{}` )",
     },
 }
 
@@ -40,7 +43,20 @@ async def googlepinfo(event):
         return await event.edit(client.getstring(STRINGS, "notapp").format(appID))
     free = "✅" if result["free"] else "❌"
     description = result["description"][:1000] + "...."
-    caption = client.getstring(STRINGS, "appinfo").format(result["title"], result["appId"], result["score"], result["installs"], result["ratings"], result["reviews"], free, result["developer"], description)
-    icon = InputMediaWebPage(url=result["icon"])
-    await event.respond(caption, file=icon)
+    caption = client.getstring(STRINGS, "appinfo").format(result["title"], result["appId"], result["genre"], (round(result["score"], 1) + " ★"), result["installs"], result["ratings"], result["reviews"], free, result["developer"], description)
+    icon = client.PATH + result["title"] + ".jpg"
+    with open(icon, "wb") as f:
+        f.write(requests.get(result["icon"]).content)
+    info = await client.send_file(event.chat_id, icon, caption=caption)
+    shots = []
+    for i, shot in enumerate(result["screenshots"]):
+        shname = client.PATH + result["title"] + "-" + str(i) + ".jpg"
+        with open(shname, "wb") as f:
+            f.write(requests.get(shot).content)
+        shots.append(shname)
+    caption = client.getstring(STRINGS, "shotcap").format(result["title"])
+    await info.reply(caption, file=shots)
     await event.delete()
+    os.remove(icon)
+    for shot in shots:
+        os.remove(shot)
