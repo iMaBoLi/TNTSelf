63606d6d398c1a753d4b759cbcbf6ab3fec2e2b0
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-09-24 08:12:31 +0330

    update FidoSelf/plugins/Language.py

---
+++ FidoSelf/plugins/Language.py
@@ -0,0 +1,19 @@
+from FidoSelf import client
+from FidoSelf.functions.strings import STRINGS
+
+STRINGS = {
+    "EN": {
+        "ping": "**{STR} The Self Language Was Set To English!**",
+    },
+    "FA": {
+        "ping": "**{STR} زبان ربات با موفقیت روی فارسی تنظیم شد!**",
+    },
+}
+
+@client.Command(command="SetLang (En|Fa)")
+async def setlanguage(event):
+    await event.edit(client.STRINGS["wait"])
+    lang = event.pattern_match.group(1).upper()
+    client.DB.set_key("LANGUAGE", lang)
+    await event.edit(client.getstring(STRINGS, "setlang"))
+    #setattr(client, "STRINGS", STRINGS[lang])
