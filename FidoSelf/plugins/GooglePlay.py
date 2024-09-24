fc72d0e5ce2af65b3fcab1413d176345cfd5773f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-09-24 20:57:49 +0330

    update FidoSelf/plugins/GooglePlay.py

--- FidoSelf/plugins/GooglePlay.py
+++ FidoSelf/plugins/GooglePlay.py
@@ -29,15 +29,13 @@ client.functions.AddInfo(__INFO__)
 STRINGS = {
     "EN": {
         "notapp": "**{STR} The Google Play App With ID** ( `{}` ) **Is Not Founded!**",
-        "appinfo": "**{STR} App Name:** ( `{}` - `{}` )\n\n**{STR} Genre:** ( `{}` )\n**{STR} Score:** ( `{}` )\n**{STR} Downloads:** ( `{}` )\n**{STR} Rating:** ( `{}` )\n**{STR} Reviews:** ( `{}` )\n**{STR} Free:** ( `{}` )\n**{STR} Developer:** ( `{}` )\n\n**{STR} Description:** ( `{}` )",
-        "shotcap": "**{STR} More Photos For Google Play App:** ( `{}` )",
+        "appinfo": "**{STR} App Name:** ( `{}` - `{}` )\n\n**{STR} Genre:** ( `{}` )\n**{STR} Score:** ( `{}` )\n**{STR} Downloads:** ( `{}` )\n**{STR} Rating:** ( `{}` )\n**{STR} Reviews:** ( `{}` )\n**{STR} Free:** ( `{}` )\n**{STR} Developer:** ( `{}` )\n\n**{STR} Description:** ( `{}` )\n\n\n**{STR} More Photos:**",
         "notsearch": "**{STR} The Google Play App With Name** ( `{}` ) **Is Not Founded!**",
         "appsearch": "**{STR} Google Play Apps With Name:** ( `{}` )\n\n",
     },
     "FA": {
         "notapp": "**{STR} هیچ اپلیکیشنی با آیدی** ( `{}` ) **پیدا نشد!**",
-        "appinfo": "**{STR} اسم اپلیکیشن:** ( `{}` - `{}` )\n\n**{STR} موضوع:** ( `{}` )\n**{STR} امتیاز:** ( `{}` )\n**{STR} تعداد دانلود:** ( `{}` )\n**{STR} رتبه:** ( `{}` )\n**{STR} بازدیدها:** ( `{}` )\n**{STR} رایگان:** ( `{}` )\n**{STR} سازنده:** ( `{}` )\n\n**{STR} توضیحات:** ( `{}` )",
-        "shotcap": "**{STR} عکس های بیشتر برای اپلیکیشن:** ( `{}` )",
+        "appinfo": "**{STR} اسم اپلیکیشن:** ( `{}` - `{}` )\n\n**{STR} موضوع:** ( `{}` )\n**{STR} امتیاز:** ( `{}` )\n**{STR} تعداد دانلود:** ( `{}` )\n**{STR} رتبه:** ( `{}` )\n**{STR} بازدیدها:** ( `{}` )\n**{STR} رایگان:** ( `{}` )\n**{STR} سازنده:** ( `{}` )\n\n**{STR} توضیحات:** ( `{}` )\n\n\n**{STR} عکس های بیشتر:**",
         "notsearch": "**{STR} هیچ اپلیکیشنی با نام** ( `{}` ) **پیدا نشد!**",
         "appsearch": "**{STR} اپلیکیشن های گوگل پلی با نام:** ( `{}` )\n\n",
     },
@@ -51,7 +49,7 @@ async def googlepinfo(event):
         result = app(appID)
     except exceptions.NotFoundError:
         return await event.edit(client.getstring(STRINGS, "notapp").format(appID))
-    free = "✅" if result["free"] else "❌"
+    free = "✅ - " + str(result["price"]) if result["free"] else "❌"
     description = result["description"][:1000] + "...."
     caption = client.getstring(STRINGS, "appinfo").format(result["title"], result["appId"], result["genre"], (str(round(result["score"], 1)) + " ★"), result["installs"], result["ratings"], result["reviews"], free, result["developer"], description)
     icon = client.PATH + appID + ".jpg"
@@ -59,7 +57,7 @@ async def googlepinfo(event):
         f.write(requests.get(result["icon"]).content)
     info = await client.send_file(event.chat_id, icon, caption=caption)
     shots = []
-    for i, shot in enumerate(result["screenshots"][:10]):
+    for i, shot in enumerate(result["screenshots"]):
         shname = client.PATH + appID + str(i) + ".jpg"
         with open(shname, "wb") as f:
             f.write(requests.get(shot).content)
