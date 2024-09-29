dfd5bdab91066b3acd5163f76a58849e9cccfa12
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-09-29 14:04:58 +0330

    update FidoSelf/plugins/BankCard.py

--- FidoSelf/plugins/BankCard.py
+++ FidoSelf/plugins/BankCard.py
@@ -6,7 +6,7 @@ __INFO__ = {
     "Info": {
         "Help": "To Save Your Bank Card In Self And Get Information!",
         "Commands": {
-            "{CMD}SetCard \'<Number>\' <Name>": {
+            "{CMD}SetCard '<Number>' <Name>": {
                "Help": "To Set Card Info",
                 "Input": {
                     "<Number>" : "Your Card Number",
@@ -25,18 +25,10 @@ __INFO__ = {
 client.functions.AddInfo(__INFO__)
 
 STRINGS = {
-    "EN": {
-        "setcard": "**{STR} The Bank Card Saved!**\n\n**• Card Number:** ( `{}` )\n**• Card Name:** ( `{}` )",
-        "delcard": "**{STR} The Saved Bank Card Was Deleted!**",
-        "notcard": "**{STR} The Bank Card Is Not Saved!**",
-        "card": "**{STR} Bank Card Information:**\n\n**• Card Number:** ( `{}` )\n**• Card Name:** ( `{}` )",
-    },
-    "FA": {
-        "setcard": "**{STR} کارت بانکی ذخیره شد!**\n\n**• شماره کارت:** ( `{}` )\n**• صاحب کارت:** ( `{}` )",
-        "delcard": "**{STR} کارت بانکی ذخیره شده حذف شد!**",
-        "notcard": "**{STR} کارت بانکی ذخیره نشده است!**",
-        "card": "**{STR} اطلاعات کارت بانکی:**\n\n**• شماره کارت:** ( `{}` )\n**• صاحب کارت:** ( `{}` )",
-    },
+    "setcard": "**{STR} The Bank Card Saved!**\n\n**• Card Number:** ( `{}` )\n**• Card Name:** ( `{}` )",
+    "delcard": "**{STR} The Saved Bank Card Was Deleted!**",
+    "notcard": "**{STR} The Bank Card Is Not Saved!**",
+    "card": "**{STR} Bank Card Information:**\n\n**• Card Number:** ( `{}` )\n**• Card Name:** ( `{}` )",
 }
 
 @client.Command(command="SetCard \'(.*)\' (.*)")
@@ -46,18 +38,18 @@ async def setcard(event):
     cname = str(event.pattern_match.group(2))
     newcard = {"NUMBER": cnumber, "NAME": cname}
     client.DB.set_key("BANK_CARD", newcard)
-    await event.edit(client.getstrings(STRINGS, "setcard").format(cnumber, cname))
+    await event.edit(client.getstrings(STRINGS)["setcard"].format(cnumber, cname))
     
 @client.Command(command="DelCard")
 async def delcard(event):
     await event.edit(client.STRINGS["wait"])
     client.DB.set_key("BANK_CARD", {})
-    await event.edit(client.getstrings(STRINGS, "delcard"))
+    await event.edit(client.getstrings(STRINGS)["delcard"])
 
 @client.Command(command="Card")
 async def getcard(event):
     card = client.DB.get_key("BANK_CARD") or {}
     if not card:
-        return await event.edit(client.getstrings(STRINGS, "notcard"))
-    text = client.getstrings(STRINGS, "card").format(card["NUMBER"], card["NAME"])
+        return await event.edit(client.getstrings(STRINGS)["notcard"])
+    text = client.getstrings(STRINGS)["card"].format(card["NUMBER"], card["NAME"])
     await event.edit(text)
