b9d6b1c907c2925868bbb9b5183eb56b3b34f750
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-10-15 14:39:44 +0330

    update FidoSelf/config.py

--- FidoSelf/config.py
+++ FidoSelf/config.py
@@ -3,14 +3,14 @@ import os
 os.system("python3 ../tmp/setenvs.py")
 
 #Infos
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-SESSION = os.environ.get("SESSION", None)
-BOT_SESSION = os.environ.get("BOT_SESSION", None)
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+SESSION = os.environ.get("SESSION")
+BOT_SESSION = os.environ.get("BOT_SESSION")
 
 #Database
-REDIS_URL = os.environ.get("REDIS_URL", None)
-REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
+REDIS_URL = os.environ.get("REDIS_URL")
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
 
 #Other
-MAX_SIZE = int(os.environ.get("MAX_SIZE", (104857600 * 5)))
+MAX_SIZE = int(os.environ.get("MAX_SIZE"))
