99cdc4ce4786a4137a39746f2862160ae4b8c1a1
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-10-17 01:03:50 +0330

    Create EmojiGame.py

---
+++ TNTSelf/plugins/EmojiGame.py
@@ -0,0 +1,41 @@
+from FidoSelf import client
+from telethon.tl import types
+
+__INFO__ = {
+    "Category": "Funs",
+    "Name": "Emojis",
+    "Info": {
+        "Help": "To Send Emoji Games In Chats!",
+        "Commands": {
+            "{CMD}SDice": None,
+            "{CMD}SDart": None,
+            "{CMD}SBasket": None,
+            "{CMD}SFoot": None,
+            "{CMD}SBoll": None,
+            "{CMD}SSlot": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+@client.Command(command="S(Dice|Dart|Basket|Foot|Boll|Slot) ?(1|2|3|4|5|6)?")
+async def gameemojis(event):
+    await event.delete()
+    emojis = {
+        "Dice": "🎲",
+        "Dart": "🎯",
+        "Basket": "🏀",
+        "Foot": "⚽️",
+        "Boll": "🎳",
+        "Slot": "🎰",
+    }
+    emoji = event.pattern_match.group(1).title()
+    number = event.pattern_match.group(2)
+    emoji = emojis[emoji]
+    send = await client.send_file(event.chat_id, types.InputMediaDice(emoji))
+    if number:
+        sendnumber = send.media.value
+        while sendnumber != int(number):
+            await send.delete()
+            send = await client.send_file(event.chat_id, types.InputMediaDice(emoji))
+            sendnumber = send.media.value
