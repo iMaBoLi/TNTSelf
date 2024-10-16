6ed7ae0f7a977e2cbc0a8d54d4bf0e7dc4ba1309
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-10-17 01:02:44 +0330

    Create KickUsers.py

---
+++ TNTSelf/plugins/KickUsers.py
@@ -0,0 +1,41 @@
+from FidoSelf import client
+
+__INFO__ = {
+    "Category": "Groups",
+    "Name": "Kick",
+    "Info": {
+        "Help": "To Kick Users In Chats!",
+        "Commands": {
+            "{CMD}Kick": {
+                "Help": "To Kick User",
+                "Getid": "You Must Reply To User Or Input UserID/UserName",
+            },
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+STRINGS = {
+    "notacs": "**{STR} You Do Not Have Access To Kick Users!**",
+    "kickuser": "**{STR} The User** ( {} ) **Was Kicked In This Chat!**",
+    "errorkick": "**{STR} The User** ( {} ) **Is Not Kicked!**\n**Error:** ( `{}` )"
+}
+
+@client.Command(command="Kick", userid=True)
+async def kickuser(event):
+    await event.edit(client.STRINGS["wait"])
+    if not event.is_group:
+        return await event.edit(client.STRINGS["only"]["Group"])
+    if not event.userid:
+        return await event.edit(client.STRINGS["user"]["all"])
+    if not event.checkAdmin(ban_users=True):
+        return await event.edit(client.getstrings(STRINGS)["notacs"])
+    info = await client.get_entity(event.userid)
+    mention = client.functions.mention(info)
+    try:
+        await client.edit_permissions(event.chat_id, info.id, view_messages=False)
+        await client.edit_permissions(event.chat_id, info.id)
+    except Exception as error:
+        return await event.edit(client.getstrings(STRINGS)["errorkick"].format(mention, error))
+    text = client.getstrings(STRINGS)["kickuser"].format(mention)
+    await event.edit(text)
