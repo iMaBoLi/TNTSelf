209acd3a2f4009b09a50bde24753c5ba11b4a4ad
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-10-17 00:59:18 +0330

    Create Inline.py

---
+++ TNTSelf/events/Inline.py
@@ -0,0 +1,22 @@
+from FidoSelf import client
+from telethon import events
+from traceback import format_exc
+
+def Inline(
+    pattern=None,
+    onlysudo=True,
+    **kwargs,
+):
+    def decorator(func):
+        async def wrapper(event):
+            try:
+                event.is_sudo = True if event.sender_id == client.me.id else False
+                if onlysudo and not event.is_sudo:
+                    text = client.STRINGS["OtherInline"]
+                    return await event.answer([event.builder.article("FidoSelf - NotForYou", text=text)])
+                await func(event)
+            except:
+                client.LOGS.error(format_exc())
+        client.bot.add_event_handler(wrapper, events.InlineQuery(pattern=pattern, **kwargs))
+        return wrapper
+    return decorator
