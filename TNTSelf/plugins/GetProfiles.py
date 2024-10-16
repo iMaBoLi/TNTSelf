b5de7983922f0d46b52a3ff039516a87c3edd3d3
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-10-17 01:02:25 +0330

    Create GetProfiles.py

---
+++ TNTSelf/plugins/GetProfiles.py
@@ -0,0 +1,43 @@
+from FidoSelf import client
+from telethon import functions, types
+
+__INFO__ = {
+    "Category": "Users",
+    "Name": "Get Profiles",
+    "Info": {
+        "Help": "To Get Profile Photos Of Users!",
+        "Commands": {
+            "{CMD}GProfiles": {
+                "Help": "To Get Profiles",
+                "Getid": "You Must Reply To User Or Input UserID/UserName",
+            },
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+STRINGS = {
+    "EN": {
+        "not": "**{STR} The Profile Photos For** ( {} ) **Is Not Found!**",
+        "caption": "**{STR} The Profile Photos For** ( {} ) **Is Sended!**"
+    },
+    "FA": {
+        "not": "**{STR} هیچ عکس پروفایلی برای** ( {} ) **پیدا نشد!**",
+        "caption": "**{STR} عکس های پروفایل** ( {} ) **ارسال شد!**"
+    },        
+}
+
+@client.Command(command="GProfiles", userid=True)
+async def getprofiles(event):
+    await event.edit(client.STRINGS["wait"])
+    if not event.userid:
+        return await event.edit(client.STRINGS["user"]["all"])
+    info = await client.get_entity(event.userid)
+    mention = client.functions.mention(info)
+    photos = await client.get_profile_photos(event.userid)
+    if not photos:
+        return await event.edit(client.getstring(STRINGS, "not").format(mention))
+    caption = client.getstring(STRINGS, "caption").format(mention)
+    for phs in client.functions.chunks(photos, 9):
+        await event.respond(caption, file=phs)
+    await event.delete()
