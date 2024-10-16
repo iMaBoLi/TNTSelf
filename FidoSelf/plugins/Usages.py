26f2d65ab00f7e5c7769229a8353ad5c8e83f737
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-10-16 13:48:47 +0330

    update FidoSelf/plugins/Usages.py

--- FidoSelf/plugins/Usages.py
+++ FidoSelf/plugins/Usages.py
@@ -18,7 +18,9 @@ import time
 async def logs(event):
     await event.edit(client.STRINGS["wait"])
     if os.path.exists("Fido.log"):
-        await event.respond("**The Console Logs File!**", file="Fido.log")
+        text = open("Fido.log", "r").read()
+        open("Fido.txt", "w").write(str(text))
+        await event.respond("**The Console Logs File!**", file="Fido.txt")
         await event.delete()
     else:
         await event.edit("**The Log File Is Not Available!**")
