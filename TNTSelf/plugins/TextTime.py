608966c26681f5dca1123be038be011559e408d3
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-10-17 01:02:09 +0330

    Create TextTime.py

---
+++ TNTSelf/plugins/TextTime.py
@@ -0,0 +1,69 @@
+from FidoSelf import client
+
+__INFO__ = {
+    "Category": "Time",
+    "Name": "Text Time",
+    "Info": {
+        "Help": "To Save Your Texts For Time In Photo!",
+        "Commands": {
+            "{CMD}NewTtime <Text>": None,
+            "{CMD}DelTtime <Text>": None,
+            "{CMD}TtimeList": None,
+            "{CMD}CleanTtimeList": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+STRINGS = {
+    "newnot": "**{STR} The Text Time** ( `{}` ) **Already In Text Time List!**",
+    "newadd": "**{STR} The Text Time** ( `{}` ) **Added To Text Time List!**",
+    "delnot": "**{STR} The Text Time** ( `{}` ) **Not In Text Time List!**",
+    "del": "**{STR} The Text Time** ( `{}` ) **Deleted From Text Time List!**",
+    "empty": "**{STR} The Text Time List Is Empty!**",
+    "list": "**{STR} The Text Time List:**\n\n",
+    "aempty": "**{STR} The Text Time List Is Already Empty!**",
+    "clean": "**{STR} The Text Time List Is Cleaned!**"
+}
+
+@client.Command(command="NewTtime ([\\s\\S]*)")
+async def addttime(event):
+    await event.edit(client.STRINGS["wait"])
+    ttimes = client.DB.get_key("TEXTTIME_LIST") or []
+    newttime = str(event.pattern_match.group(1))
+    if newttime in ttimes:
+        return await event.edit(client.getstrings(STRINGS)["newnot"].format(newttime))  
+    ttimes.append(newttime)
+    client.DB.set_key("TEXTTIME_LIST", ttimes)
+    await event.edit(client.getstrings(STRINGS)["newadd"].format(newttime))
+    
+@client.Command(command="DelTtime ([\\s\\S]*)")
+async def delttime(event):
+    await event.edit(client.STRINGS["wait"])
+    ttimes = client.DB.get_key("TEXTTIME_LIST") or []
+    newttime = str(event.pattern_match.group(1))
+    if newttime not in ttimes:
+        return await event.edit(client.getstrings(STRINGS)["delnot"].format(newttime))  
+    ttimes.remove(newttime)
+    client.DB.set_key("TEXTTIME_LIST", ttimes)
+    await event.edit(client.getstrings(STRINGS)["del"].format(newttime))
+
+@client.Command(command="TtimeList")
+async def ttimelist(event):
+    await event.edit(client.STRINGS["wait"])
+    ttimes = client.DB.get_key("TEXTTIME_LIST") or []
+    if not ttimes:
+        return await event.edit(client.getstrings(STRINGS)["empty"])
+    text = client.getstrings(STRINGS)["list"]
+    for row, ttime in enumerate(ttimes):
+        text += f"**{row + 1} -** `{ttime}`\n"
+    await event.edit(text)
+
+@client.Command(command="CleanTtimeList")
+async def cleanttimes(event):
+    await event.edit(client.STRINGS["wait"])
+    ttimes = client.DB.get_key("TEXTTIME_LIST") or []
+    if not ttimes:
+        return await event.edit(client.getstrings(STRINGS)["aempty"])
+    client.DB.del_key("TEXTTIME_LIST")
+    await event.edit(client.getstrings(STRINGS)["clean"])
