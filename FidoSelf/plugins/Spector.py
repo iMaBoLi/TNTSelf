0796c974e8acc5fef79dace1726faaf5e22b1876
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-10-16 13:47:16 +0330

    update FidoSelf/plugins/Spector.py

--- FidoSelf/plugins/Spector.py
+++ FidoSelf/plugins/Spector.py
@@ -1,6 +1,7 @@
 from FidoSelf import client
-from telethon import functions, Button
+from telethon import events, functions, types, Button
 import asyncio
+import datetime
 
 __INFO__ = {
     "Category": "Manage",
@@ -19,6 +20,7 @@ client.functions.AddInfo(__INFO__)
 
 STRINGS = {
     "spector": "**❊ Welcome To Spector Menu:**\n\n    {STR} Select Options Below To Manage Spector Modes:**\n    **{STR} User:** ( {} )",
+    "specstatus": "**{STR} User** ( {} - `{}` ) **Is {} Now!** ( `{}` )",
     "closespector": "**{STR} The Spector Panel Successfuly Closed!**",
 }
 
@@ -88,3 +90,17 @@ async def setspector(event):
 async def closespector(event):
     text = client.getstrings(STRINGS)["closespector"]
     await event.edit(text=text)
+
+@client.on(events.UserUpdate)
+async def statusspec(event):
+    if event.user_id == client.me.id or not event.status: return
+    lists = client.DB.get_key("SPECTOR_STATUS") or []
+    status = event.status.to_dict()["_"]
+    if status not in ["UserStatusOnline", "UserStatusOffline"]: return
+    if event.user_id in lists:
+        info = await client.get_entity(event.user_id)
+        mention = client.functions.mention(info)
+        localtime = datetime.datetime.now()
+        time = localtime.strftime("%H:%M:%S")
+        text = client.getstrings(STRINGS)["specstatus"].format(mention, event.user_id, status, time)
+        await client.bot.send_message(client.REALM, text)
