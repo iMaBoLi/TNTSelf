34802d5628018f81867d0b04f842df3715d68c84
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-10-17 01:01:49 +0330

    Create TextSay.py

---
+++ TNTSelf/plugins/TextSay.py
@@ -0,0 +1,32 @@
+from FidoSelf import client
+import asyncio
+
+__INFO__ = {
+    "Category": "Funs",
+    "Name": "Say",
+    "Info": {
+        "Help": "To Say Your Message Texts!",
+        "Commands": {
+            "{CMD}TSay <Text>": None,
+            "{CMD}TSay <Reply(Text)>": None,
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
+STRINGS = {
+    "not": "**{STR} Please Reply To Text Or Enter Text!**"
+}
+
+@client.Command(command="TSay ?([\\S\\s]*)?")
+async def tsay(event):
+    await event.edit(client.STRINGS["wait"])
+    inputtext = str(event.pattern_match.group(1) or "")
+    if not inputtext and not (event.reply_message or event.reply_message.text):
+        return await event.edit(client.getstrings(STRINGS)["not"])
+    OText = inputtext if inputtext else event.reply_message.text
+    Text = ""
+    for Part in OText:
+        Text += Part
+        await event.edit(Text)
+        await asyncio.sleep(0.5)
