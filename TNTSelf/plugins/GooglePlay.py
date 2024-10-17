7343373cf1c58a3ab566378bfe0295d50567b6fd
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-10-17 09:46:32 +0330

    update TNTSelf/plugins/GooglePlay.py

--- TNTSelf/plugins/GooglePlay.py
+++ TNTSelf/plugins/GooglePlay.py
@@ -27,18 +27,10 @@ __INFO__ = {
 client.functions.AddInfo(__INFO__)
 
 STRINGS = {
-    "EN": {
-        "notapp": "**{STR} The Google Play App With ID** ( `{}` ) **Is Not Founded!**",
-        "appinfo": "**{STR} App Name:** ( `{}` - `{}` )\n\n**{STR} Genre:** ( `{}` )\n**{STR} Score:** ( `{}` )\n**{STR} Downloads:** ( `{}` )\n**{STR} Rating:** ( `{}` )\n**{STR} Reviews:** ( `{}` )\n**{STR} Free:** ( `{}` )\n**{STR} Developer:** ( `{}` )\n\n**{STR} Description:** ( `{}` )\n\n\n**{STR} More Photos:**",
-        "notsearch": "**{STR} The Google Play App With Name** ( `{}` ) **Is Not Founded!**",
-        "appsearch": "**{STR} Google Play Apps With Name:** ( `{}` )\n\n",
-    },
-    "FA": {
-        "notapp": "**{STR} هیچ اپلیکیشنی با آیدی** ( `{}` ) **پیدا نشد!**",
-        "appinfo": "**{STR} اسم اپلیکیشن:** ( `{}` - `{}` )\n\n**{STR} موضوع:** ( `{}` )\n**{STR} امتیاز:** ( `{}` )\n**{STR} تعداد دانلود:** ( `{}` )\n**{STR} رتبه:** ( `{}` )\n**{STR} بازدیدها:** ( `{}` )\n**{STR} رایگان:** ( `{}` )\n**{STR} سازنده:** ( `{}` )\n\n**{STR} توضیحات:** ( `{}` )\n\n\n**{STR} عکس های بیشتر:**",
-        "notsearch": "**{STR} هیچ اپلیکیشنی با نام** ( `{}` ) **پیدا نشد!**",
-        "appsearch": "**{STR} اپلیکیشن های گوگل پلی با نام:** ( `{}` )\n\n",
-    },
+    "notapp": "**{STR} The Google Play App With ID** ( `{}` ) **Is Not Founded!**",
+    "appinfo": "**{STR} App Name:** ( `{}` - `{}` )\n\n**{STR} Genre:** ( `{}` )\n**{STR} Score:** ( `{}` )\n**{STR} Downloads:** ( `{}` )\n**{STR} Rating:** ( `{}` )\n**{STR} Reviews:** ( `{}` )\n**{STR} Free:** ( `{}` )\n**{STR} Developer:** ( `{}` )\n\n**{STR} Description:** ( `{}` )\n\n\n**{STR} More Photos:**",
+    "notsearch": "**{STR} The Google Play App With Name** ( `{}` ) **Is Not Founded!**",
+    "appsearch": "**{STR} Google Play Apps With Name:** ( `{}` )\n\n",
 }
 
 @client.Command(command="GPInfo (.*)")
@@ -48,10 +40,10 @@ async def googlepinfo(event):
     try:
         result = app(appID)
     except exceptions.NotFoundError:
-        return await event.edit(client.getstring(STRINGS, "notapp").format(appID))
+        return await event.edit(client.getstrings(STRINGS)["notapp"].format(appID))
     free = "✅" if result["free"] else "❌ - " + str(result["price"]) 
     description = result["description"][:1000] + "...."
-    caption = client.getstring(STRINGS, "appinfo").format(result["title"], result["appId"], result["genre"], (str(round(result["score"], 1)) + " ★"), result["installs"], result["ratings"], result["reviews"], free, result["developer"], description)
+    caption = client.getstrings(STRINGS)["appinfo"].format(result["title"], result["appId"], result["genre"], (str(round(result["score"], 1)) + " ★"), result["installs"], result["ratings"], result["reviews"], free, result["developer"], description)
     icon = client.PATH + appID + ".jpg"
     with open(icon, "wb") as f:
         f.write(requests.get(result["icon"]).content)
@@ -75,8 +67,8 @@ async def googlepsearch(event):
     try:
         result = search(appname, n_hits=20)
     except TypeError:
-        return await event.edit(client.getstring(STRINGS, "notsearch").format(appname))
-    sres = client.getstring(STRINGS, "appsearch").format(appname)
+        return await event.edit(client.getstrings(STRINGS)["notsearch"].format(appname))
+    sres = client.getstrings(STRINGS)["appsearch"].format(appname)
     count = 1
     for app in result:
         title = app["title"]
