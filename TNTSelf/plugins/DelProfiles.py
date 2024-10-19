fd26dae98e200b4daeffc8237fe2f2592b52bb0e
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-10-19 17:28:27 +0330

    update TNTSelf/plugins/DelProfiles.py

--- TNTSelf/plugins/DelProfiles.py
+++ TNTSelf/plugins/DelProfiles.py
@@ -15,12 +15,27 @@ __INFO__ = {
 }
 client.functions.AddInfo(__INFO__)
 
+__INFO__ = {
+    "Category": "Account",
+    "Name": "Clean Profiles",
+    "Info": {
+        "Help": "To Clean Your Profile Photos!",
+        "Commands": {
+            "{CMD}CleanProfile": {
+                "Help": "Clean Profiles!",
+            },
+        },
+    },
+}
+client.functions.AddInfo(__INFO__)
+
 STRINGS = {
     "not": "**{STR} The Profile Photos Is Not Found!**",
     "last": "**{STR} The Last Profile Photo Has Been Deleted!**",
     "count": "**{STR} The** ( `{}` ) **Of Your Profile Photos Has Been Deleted!**",
     "nota": "**{STR} This Profile Photo Is Not Available!**",
-    "one": "**{STR} The Profile Photo** ( `{}` ) **Has Been Deleted!**"
+    "one": "**{STR} The Profile Photo** ( `{}` ) **Has Been Deleted!**",
+    "nota": "**{STR} The All Profile Photos Is Cleaned!**",
 }
 
 @client.Command(command="DelProfile ?((\\-)?\\d*)?")
@@ -48,3 +63,13 @@ async def delprofiles(event):
         pphoto = pphoto[int(prof)-1]
         await client(functions.photos.DeletePhotosRequest(id=[types.InputPhoto(id=pphoto.id, access_hash=pphoto.access_hash, file_reference=pphoto.file_reference)]))
         await event.edit(client.getstrings(STRINGS)["one"].format(prof))
+
+@client.Command(command="CleanProfile")
+async def cleanprofiles(event):
+    await event.edit(client.STRINGS["wait"])
+    pphotos = await client.get_profile_photos("me")
+    if not pphotos:
+        return await event.edit(client.getstrings(STRINGS)["not"])
+    for pphoto in pphotos:
+        await client(functions.photos.DeletePhotosRequest(id=[types.InputPhoto(id=pphoto.id, access_hash=pphoto.access_hash, file_reference=pphoto.file_reference)]))
+    await event.edit(client.getstrings(STRINGS)["clean"])
