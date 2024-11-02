a7b8f16844536460917692cd30671225bf487f74
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-11-02 14:54:39 +0330

    update TNTSelf/plugins/Calculator.py

--- TNTSelf/plugins/Calculator.py
+++ TNTSelf/plugins/Calculator.py
@@ -40,51 +40,38 @@ BUTTONS = {
     "÷": "/",
 }
     
-def get_calc_buttons(chatid, msgid):
-    buttons = [[Button.inline("🆑", data=f"ClearCalc:{chatid}:{msgid}"), Button.inline("⌫", data=f"DelCalc:{chatid}:{msgid}")]]
+def get_calc_buttons():
+    buttons = [[Button.inline("🆑", data=f"ClearCalc"), Button.inline("⌫", data=f"DelCalc")]]
     otherbuttons = []
     for othbts in OPERS:
-        otherbuttons.append(Button.inline(othbts, data=f"AddCalc:{chatid}:{msgid}:{othbts}"))
+        otherbuttons.append(Button.inline(othbts, data=f"AddCalc:{othbts}"))
     otherbuttons = list(client.functions.chunks(otherbuttons, 4))
     numbuttons = []
     for num in NUMS:
-        numbuttons.append(Button.inline(num, data=f"AddCalc:{chatid}:{msgid}:{num}"))
+        numbuttons.append(Button.inline(num, data=f"AddCalc:{num}"))
     numbuttons = list(client.functions.chunks(numbuttons, 3))
-    resbutton = [[Button.inline("=", data=f"CalcRes:{chatid}:{msgid}")]]
+    resbutton = [[Button.inline("=", data=f"CalcRes")]]
     buttons += otherbuttons + numbuttons + resbutton
     return buttons
-
-async def get_calc_data(chatid, msgid):
-    message = await client.get_messages(chatid, ids=msgid)
-    if not message:
-        return "Empty"
-    return message.text
 
 @client.Command(command="Calc")
 async def calculator(event):
     await event.edit(client.STRINGS["wait"])
-    message = await client.bot.send_message(client.REALM, "Empty")
-    chatid = message.chat_id
-    msgid = message.id
-    res = await client.inline_query(client.bot.me.username, f"Calc:{chatid}:{msgid}")
+    res = await client.inline_query(client.bot.me.username, "Calc")
     await res[0].click(event.chat_id)
     await event.delete()
 
-@client.Inline(pattern="Calc\\:(.*)\\:(.*)")
+@client.Inline(pattern="Calc")
 async def inlinecalculator(event):
-    chatid = int(event.pattern_match.group(1))
-    msgid = int(event.pattern_match.group(2))
-    data = await get_calc_data(chatid, msgid)
+    data = client.DB.get_key("CALCULATOR") or "Empty"
     text = client.getstrings(STRINGS)["calc"].format(data)
-    buttons = get_calc_buttons(chatid, msgid)
+    buttons = get_calc_buttons()
     await event.answer([event.builder.article("TNTSelf - Calculator", text=text, buttons=buttons)])
 
-@client.Callback(data="AddCalc\\:(.*)\\:(.*)\\:(.*)")
+@client.Callback(data="AddCalc\\:(.*)")
 async def addcalculator(event):
-    chatid = int(event.data_match.group(1).decode('utf-8'))
-    msgid = int(event.data_match.group(2).decode('utf-8'))
-    string = str(event.data_match.group(3).decode('utf-8'))
-    getdata = await get_calc_data(chatid, msgid)
+    string = str(event.data_match.group(1).decode('utf-8'))
+    getdata = client.DB.get_key("CALCULATOR") or "Empty"
     if getdata == "Empty":
         if string in OPERS or string == "0":
             return await event.answer(client.getstrings(STRINGS)["uncalc"], alert=True)
@@ -93,47 +80,45 @@ async def addcalculator(event):
         if str(getdata)[-1] in OPERS and (string in OPERS or string == "0"):
             return await event.answer(client.getstrings(STRINGS)["uncalc"], alert=True)
         data = str(getdata) + string
+    client.DB.set_key("CALCULATOR", data)
     text = client.getstrings(STRINGS)["calc"].format(data)
-    buttons = get_calc_buttons(chatid, msgid)
+    buttons = get_calc_buttons()
     await event.edit(text=text, buttons=buttons)
-    await client.bot.edit_message(chatid, msgid, data)
-    
+
 @client.Callback(data="DelCalc\\:(.*)\\:(.*)")
 async def delcalculator(event):
     chatid = int(event.data_match.group(1).decode('utf-8'))
     msgid = int(event.data_match.group(2).decode('utf-8'))
-    getdata = await get_calc_data(chatid, msgid)
+    getdata = client.DB.get_key("CALCULATOR") or "Empty"
     if getdata == "Empty":
         return await event.answer(client.getstrings(STRINGS)["notcalc"], alert=True)
     data = str(getdata)[:-1]
     data = data if data else "Empty"
+    client.DB.set_key("CALCULATOR", data)
     text = client.getstrings(STRINGS)["calc"].format(data)
-    buttons = get_calc_buttons(chatid, msgid)
+    buttons = get_calc_buttons()
     await event.edit(text=text, buttons=buttons)
-    await client.bot.edit_message(chatid, msgid, data)
 
 @client.Callback(data="CalcRes\\:(.*)\\:(.*)")
 async def rescalculator(event):
     chatid = int(event.data_match.group(1).decode('utf-8'))
     msgid = int(event.data_match.group(2).decode('utf-8'))
-    data = await get_calc_data(chatid, msgid)
+    data = client.DB.get_key("CALCULATOR") or "Empty"
     if data == "Empty":
         return await event.answer(client.getstrings(STRINGS)["notcalc"], alert=True)
     newdata = data
     for element in BUTTONS:
         newdata = newdata.replace(element, BUTTONS[element])
     result = eval(newdata)
+    client.DB.set_key("CALCULATOR", "Empty")
     text = client.getstrings(STRINGS)["rescalc"].format(data, result)
     await event.edit(text=text)
-    resdata = data + " = " + str(result)
-    await client.bot.edit_message(chatid, msgid, resdata)
-    
+
 @client.Callback(data="ClearCalc\\:(.*)\\:(.*)")
 async def clearcalculator(event):
     chatid = int(event.data_match.group(1).decode('utf-8'))
     msgid = int(event.data_match.group(2).decode('utf-8'))
-    await client.bot.edit_message(chatid, msgid, "Empty")
-    data = await get_calc_data(chatid, msgid)
-    text = client.getstrings(STRINGS)["calc"].format(data)
-    buttons = get_calc_buttons(chatid, msgid)
+    client.DB.set_key("CALCULATOR", "Empty")
+    text = client.getstrings(STRINGS)["calc"].format("Empty")
+    buttons = get_calc_buttons()
     await event.edit(text=text, buttons=buttons)
