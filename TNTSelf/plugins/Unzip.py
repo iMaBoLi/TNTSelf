a354eb0710bc0302400ea89d1f5cae003b7928c3
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-20 03:06:55 +0330

    update TNTSelf/plugins/Unzip.py

--- TNTSelf/plugins/Unzip.py
+++ TNTSelf/plugins/Unzip.py
@@ -1,6 +1,7 @@
 from TNTSelf import client
 from zipfile import ZipFile
 import os
+import secrets
 
 __INFO__ = {
     "Category": "Tools",
@@ -23,7 +24,7 @@ STRINGS = {
     "caption": "**{STR} File Location:** ( `{}` )",
 }
 
-@client.Command(command="Unziipp")
+@client.Command(command="Unzip")
 async def unzip(event):
     await event.edit(client.STRINGS["wait"])
     if reply:= event.checkReply(["File"]):
@@ -34,19 +35,20 @@ async def unzip(event):
 
 async def unzipfile(event):
     callback = event.progress(download=True)
-    zfile = client.PATH + "MainZip.zip"
-    await event.reply_message.download_media(zfile, progress_callback=callback)
+    zfile = await event.reply_message.download_media(client.path, progress_callback=callback)
     await event.edit(client.getstrings(STRINGS)["unziping"])
     zipObj = ZipFile(zfile)
-    zipObj.extractall(client.PATH)
+    token = secrets.token_hex(nbytes=3)
+    zippath = client.PATH + token + "/"
+    zipObj.extractall(zippath)
     zipObj.close()
-    zippath = client.PATH + "Zip/"
     files = [os.path.join(dirpath, file) for (dirpath, dirnames, filenames) in os.walk(zippath) for file in filenames]
     await event.edit(client.getstrings(STRINGS)["sending"].format(len(files)))
     for file in files:
         callback = event.progress(upload=True)
-        caption = client.getstrings(STRINGS)["caption"].format(file)
-        await client.send_file(event.chat_id, file, caption=caption, progress_callback=callback)
+        nfile = file.replace(zippath, "")
+        caption = client.getstrings(STRINGS)["caption"].format(nfile)
+        await client.send_file(event.chat_id, file, caption=caption, force_document=True, allow_cache=True, progress_callback=callback)
     os.remove(zfile)
     await client.functions.runcmd(f"rm -rf {zippath}")
     await event.delete()
