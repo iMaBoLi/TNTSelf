6f07f7f43058de7f02f7f13a8d04856eaca421e3
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-22 13:01:37 +0330

    update TNTSelf/__init__.py

--- TNTSelf/__init__.py
+++ TNTSelf/__init__.py
@@ -1,5 +1,5 @@
 from telethon import TelegramClient
-from TNTSelf.MultiClient import TelClient
+from TNTSelf.client import TelClient
 from telethon.sessions import StringSession
 from logging import INFO, getLogger, basicConfig, FileHandler, StreamHandler
 from TNTSelf import config
