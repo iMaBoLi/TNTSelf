e7448593cc087bc0cd144cc9eae57417164bfb43
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 01:45:06 +0330

    update TNTSelf/plugins/Ping.py

--- TNTSelf/plugins/Ping.py
+++ TNTSelf/plugins/Ping.py
@@ -1,4 +1,4 @@
-from TNTSelf import client
+from TNTSelf import TLclient
 from datetime import datetime
 import time
 
@@ -14,19 +14,19 @@ __INFO__ = {
         },
     },
 }
-#client.functions.AddInfo(__INFO__)
+TLclient.functions.AddInfo(__INFO__)
 
 STRINGS = {
     "ping": "**{STR} PonG !!** ( `{}` )\n**{STR} Uptime:** ( `{}` )",
 }
 
-@client.Command(command="Ping")
+@TLclient.Command(command="Ping")
 async def ping(event):
     start = datetime.now()
-    await event.edit(client.STRINGS["wait"])
+    await event.edit(TLclient.STRINGS["wait"])
     end = datetime.now()
     tms = (end - start).microseconds / 1000
     ping = round(tms / 3, 2)
-    uptime = time.time() - client.START_TIME
-    uptime = client.functions.convert_time(uptime)
-    await event.edit(client.getstrings(STRINGS)["ping"].format(ping, uptime))
+    uptime = time.time() - TLclient.START_TIME
+    uptime = TLclient.functions.convert_time(uptime)
+    await event.edit(TLclient.getstrings(STRINGS)["ping"].format(ping, uptime))
