0e02b5310026cdc13f07d6ff873358c5edacf2fd
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 00:38:38 +0330

    update TNTSelf/functions/__init__.py

--- TNTSelf/functions/__init__.py
+++ TNTSelf/functions/__init__.py
@@ -1,40 +1,22 @@
-from TNTSelf import client
-from TNTSelf.functions.database import *
-from TNTSelf.functions.helper import *
-from TNTSelf.functions.utils import *
-from TNTSelf.functions.core import *
-from TNTSelf.functions.loader import *
-from TNTSelf.functions.tools import *
-from TNTSelf.functions.youtube import *
+from TNTSelf import tlclient
+from TNTSelf.events.Command import Command
+from TNTSelf.events.Callback import Callback
+from TNTSelf.events.Inline import Inline
 from TNTSelf.functions.strings import STRINGS
-from TNTSelf.functions.data import *
-from TNTSelf.functions.fasttelethon import download_file, upload_file
+import os
+import time
+import jdatetime
 
-async def AddVarsToClient():
-    setattr(client, "PLUGINS", get_plugins())
-    setattr(client, "DB", DB)
+def AddVarsToClient():
     setattr(client, "STRINGS", STRINGS)
-    setattr(client, "getstrings", getstrings)
-    setattr(client, "COMMANDS", [])
     setattr(client, "HELP", {})
     setattr(client, "MAX_SIZE", 500000000)
     setattr(client, "PATH", "downloads/")
-    setattr(client, "fast_download", download_file)
-    setattr(client, "fast_upload", upload_file)
-    setattr(client.bot, "me", (await client.bot.get_me()))
-    setattr(client, "REALM", client.DB.get_key("REALM_CHAT"))
-    setattr(client, "BACKUP", client.DB.get_key("BACKUP_CHANNEL"))
-    AddHandlersToClient()
-    SetTimeZone()
-
-def AddHandlersToClient():
-    from TNTSelf.events import Command, Callback, Inline
+    
     setattr(client, "Command", Command)
     setattr(client, "Callback", Callback)
     setattr(client, "Inline", Inline)
-    
-def SetTimeZone():
-    import os, time, jdatetime
+
     os.environ["TZ"] = "Asia/Tehran"
     time.tzset()
     jdatetime.set_locale("fa_IR")
