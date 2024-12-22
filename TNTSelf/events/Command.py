b43f5d3fda95d781becff33305e82f421a2dd13d
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 00:51:03 +0330

    update TNTSelf/events/Command.py

--- TNTSelf/events/Command.py
+++ TNTSelf/events/Command.py
@@ -1,4 +1,4 @@
-from TNTSelf import client
+from TNTSelf import tlclient
 from telethon import events, types
 from traceback import format_exc
 import re
@@ -15,13 +15,13 @@ def Command(
     **kwargs,
 ):
     if command and not pattern:
-        CMD = client.DB.get_key("CMD_SIMBEL") or "."
+        CMD = tlclient.DB.get_key("CMD_SIMBEL") or "."
         if userid or chatid:
             pattern = f"(?i)^\\{CMD}{command}"
         else:
             pattern = f"(?i)^\\{CMD}{command}$"
-    if pattern and pattern not in client.COMMANDS:
-        client.COMMANDS.append(pattern)
+    if pattern and pattern not in tlclient.COMMANDS:
+        tlclient.COMMANDS.append(pattern)
     def decorator(func):
         async def wrapper(event):
             try:
@@ -30,12 +30,12 @@ def Command(
                 if onlysudo and not (event.is_sudo or event.out): return
                 event.reply_message = await event.get_reply_message()
                 event.is_bot = event.sender.bot if isinstance(event.sender, types.User) else False
-                event.userid = await client.functions.getuserid(event) if userid else 0
-                event.chatid = await client.functions.getchatid(event) if chatid else 0
-                if checkCmd and event.text and client.functions.checkCmd(event.text): return
-                blacks = client.DB.get_key("BLACK_LIST") or []
+                event.userid = await tlclient.functions.getuserid(event) if userid else 0
+                event.chatid = await tlclient.functions.getchatid(event) if chatid else 0
+                if checkCmd and event.text and tlclient.functions.checkCmd(event.text): return
+                blacks = event.client.DB.get_key("BLACK_LIST") or []
                 event.is_black = True if event.sender_id in blacks else False
-                whites = client.DB.get_key("WHITE_LIST") or []
+                whites = event.client.DB.get_key("WHITE_LIST") or []
                 event.is_white = True if event.sender_id in whites else False
                 if not event.is_sudo and event.is_black: return
                 if event.via_bot_id: return
@@ -43,9 +43,10 @@ def Command(
             except:
                 client.LOGS.error(format_exc())
                 errortext = f"**• Error :**\n\n`{format_exc()}`"
-                await client.bot.send_message(client.REALM, errortext)
-        client.add_event_handler(wrapper, events.NewMessage(pattern=pattern, **kwargs))
-        if allowedits:
-            client.add_event_handler(wrapper, events.MessageEdited(pattern=pattern, **kwargs))
+                await event.client.bot.send_message(event.client.REALM, errortext)
+        for client in tlclient.clients:  
+            client.add_event_handler(wrapper, events.NewMessage(pattern=pattern, **kwargs))
+            if allowedits:
+                client.add_event_handler(wrapper, events.MessageEdited(pattern=pattern, **kwargs))
         return wrapper
     return decorator
