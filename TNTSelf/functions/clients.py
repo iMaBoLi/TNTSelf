57e12e702176208fe788e8bc5851a603bffff38c
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 02:36:30 +0330

    update TNTSelf/functions/clients.py

--- TNTSelf/functions/clients.py
+++ TNTSelf/functions/clients.py
@@ -1,4 +1,4 @@
-from telethon import TelegramClient
+#from telethon import TelegramClient
 from telethon.sessions import StringSession
 from TNTSelf.functions.database import DATABASE
 from TNTSelf.events.Command import Command
