a9929fb5831610ebc9d8da7feba8700b289b4d4a
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 00:55:36 +0330

    update TNTSelf/__main__.py

--- TNTSelf/__main__.py
+++ TNTSelf/__main__.py
@@ -1,4 +1,4 @@
-from . import tlclient
+from . import client
 from telethon import __version__ as telever
 from TNTSelf import functions
 from traceback import format_exc
@@ -18,26 +18,26 @@ def load_plugins(files):
             notplugs.update({os.path.basename(file): format_exc()})
     return notplugs
 
-tlclient.LOGS.info("• Starting Setup Plugins ...")
-tlclient.functions = functions
-functions.add_vars(tlclient)
-tlclient.LOGS.info("• Installing Plugins ...")
+client.LOGS.info("• Starting Setup Plugins ...")
+client.functions = functions
+functions.add_vars(client)
+client.LOGS.info("• Installing Plugins ...")
 PLUGINS = sorted(glob.glob(f"TNTSelf/plugins/*.py"))
 notplugs = load_plugins(PLUGINS)
-tlclient.LOGS.info(f"• Successfully Installed {len(plugs)} Plugin From Main Plugins!")
-tlclient.LOGS.info(f"• Not Installed {len(notplugs)} Plugin From Main Plugins!")
+client.LOGS.info(f"• Successfully Installed {len(plugs)} Plugin From Main Plugins!")
+client.LOGS.info(f"• Not Installed {len(notplugs)} Plugin From Main Plugins!")
 for plug in notplugs:
-    tlclient.LOGS.info(f"• {plug} --->  {notplugs[plug]}")
-tlclient.LOGS.info(f"• Python Version: {platform.python_version()}")
-tlclient.LOGS.info(f"• Telethon Version: {telever}")
-tlclient.LOGS.info(f"• TNTSelf Version: {tlclient.__version__}")
-tlclient.LOGS.info("\n----------------------------------------\n  • Starting TNTSelf Was Successful!\n----------------------------------------")
+    client.LOGS.info(f"• {plug} --->  {notplugs[plug]}")
+client.LOGS.info(f"• Python Version: {platform.python_version()}")
+client.LOGS.info(f"• Telethon Version: {telever}")
+client.LOGS.info(f"• TNTSelf Version: {client.__version__}")
+client.LOGS.info("\n----------------------------------------\n  • Starting TNTSelf Was Successful!\n----------------------------------------")
 
-tlclient.run_all_clients()
+client.run_all_clients()
 
-async def send_setup(client):
-    message = f"**👋 TNT Self Has Been Start Now !**\n\n**🧒 User :** {tlclient.functions.mention(client.me)}\n**🤖 Manager :** {tlclient.functions.mention(client.bot.me)}"
-    await client.bot.send_message(client.REALM, message)
+async def send_setup(sinclient):
+    message = f"**👋 TNT Self Has Been Start Now !**\n\n**🧒 User :** {client.functions.mention(sinclient.me)}\n**🤖 Manager :** {client.functions.mention(sinclient.bot.me)}"
+    await sinclient.bot.send_message(sinclient.REALM, message)
 
-for client in tlclient.clients:
-    client.loop.run_until_complete(send_setup(client))
+for sinclient in client.clients:
+    sinclient.loop.run_until_complete(send_setup(sinclient))
