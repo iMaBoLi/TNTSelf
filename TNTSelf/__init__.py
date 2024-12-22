f86084fce4dc2e53a424037baabf01b6dae7e63b
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 00:01:11 +0330

    update TNTSelf/__init__.py

--- TNTSelf/__init__.py
+++ TNTSelf/__init__.py
@@ -6,7 +6,7 @@ from traceback import format_exc
 import time
 import sys
 
-__version__ = "2.10.6"
+__version__ = "3.10.6"
 
 LOGS = getLogger("TNTSelf")
 basicConfig(
@@ -16,38 +16,22 @@ basicConfig(
     handlers=[FileHandler("TNT.log"), StreamHandler()],
 )
 
-LOGS.info("• Login Account ...")
+LOGS.info("• Login To Accounts And Bots ...")
 
 MAINCONFIG = "../tmp/config.txt"
 DATA = open(MAINCONFIG, "r").read()
 SESSIONS = eval(DATA)
 
 try:
-    client = TelClient(
+    tlclient = TelClient(
         sessions=SESSIONS,
         app_version=__version__,
     )
 except Exception as error:
-    LOGS.error("• Login To Accounts Was Unsuccessful!")
+    LOGS.error("• Error In Logins:")
     LOGS.error(format_exc())
 
-LOGS.info("• Login Api Bot ...")
-
-BOT_SESSION = "1BJWap1sBuyCO-P8-X9ejsMyf7ukard_RJNXXCGlP-_KiEmSU7jH_diwqrUMFiaKUJcujbwErb4VoyRwyoS9JA4Ti8EH80ZCvk6WacNJ0D0gG6RO0hD7XxsvPjy0EBgxFyspNQZA4fiKEMKRutSVL4yRnHsNK18nB9k3sL08XrImo7WZk2LNxBDyLDQkHKkA71TVh_cw7Lofv818KhTqL46jAix90Rndo3KCu20RABqCymbX7ZM_l3b6CbIdhbNtObVrXU4JltXvBmrTE3b_cFrjciZyoSS0DhDct68Z8VGcdsd_qMFX13teZG1_xwcS1H9wew5k8zRBf8svOMoaNGjTh7-YpGvE="
-BOT_APIID = 22317398
-BOT_APIHASH = "f9bfaf9bad8b7787e0159f32ee9ef88f"
-
-try:
-    client.bot = TelegramClient(
-        session=StringSession(BOT_SESSION),
-        api_id=BOT_APIID,
-        api_hash=BOT_APIHASH,
-    ).start()
-except Exception as error:
-    LOGS.error("• Login To Bot Was Unsuccessful!")
-    LOGS.error(error)
-
-LOGS.info("• Logins Was Completed!")
+LOGS.info("• Logins To Account And Bots Was Completed!")
 
 client.LOGS = LOGS
 client.__version__ = __version__
