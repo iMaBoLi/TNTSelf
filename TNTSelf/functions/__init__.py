9c58c40e963d6813bca896f3e5cda459501414e9
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-22 13:04:41 +0330

    update TNTSelf/functions/__init__.py

--- TNTSelf/functions/__init__.py
+++ TNTSelf/functions/__init__.py
@@ -1,5 +1,4 @@
 from TNTSelf import client
-from TNTSelf import config
 from TNTSelf.functions.database import *
 from TNTSelf.functions.helper import *
 from TNTSelf.functions.utils import *
@@ -14,12 +13,11 @@ from TNTSelf.functions.fasttelethon import download_file, upload_file
 async def AddVarsToClient():
     setattr(client, "PLUGINS", get_plugins())
     setattr(client, "DB", DB)
-    setattr(client, "Config", config)
     setattr(client, "STRINGS", STRINGS)
     setattr(client, "getstrings", getstrings)
     setattr(client, "COMMANDS", [])
     setattr(client, "HELP", {})
-    setattr(client, "MAX_SIZE", config.MAX_SIZE)
+    setattr(client, "MAX_SIZE", 500000000)
     setattr(client, "PATH", "downloads/")
     setattr(client, "fast_download", download_file)
     setattr(client, "fast_upload", upload_file)
