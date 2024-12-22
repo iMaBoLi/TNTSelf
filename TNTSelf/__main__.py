0a36fe2dfdf97fdc99d5c657daf52bf94a185cf5
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 01:10:20 +0330

    update TNTSelf/__main__.py

--- TNTSelf/__main__.py
+++ TNTSelf/__main__.py
@@ -35,8 +35,11 @@ client.LOGS.info(f"• TNTSelf Version: {client.__version__}")
 client.LOGS.info("\n----------------------------------------\n  • Starting TNTSelf Was Successful!\n----------------------------------------")
 
 async def send_setup(sinclient):
-    message = f"**👋 TNT Self Has Been Start Now !**\n\n**🧒 User :** {client.functions.mention(sinclient.me)}\n**🤖 Manager :** {client.functions.mention(sinclient.bot.me)}"
-    await sinclient.bot.send_message(sinclient.REALM, message)
+    try:
+        message = f"**👋 TNT Self Has Been Start Now !**\n\n**🧒 User :** {client.functions.mention(sinclient.me)}\n**🤖 Manager :** {client.functions.mention(sinclient.bot.me)}"
+        await sinclient.bot.send_message(sinclient.REALM, message)
+    except:
+        pass
 
 for sinclient in client.clients:
     sinclient.loop.run_until_complete(send_setup(sinclient))
