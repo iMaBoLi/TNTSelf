4180e8eb3323e6231eb80bf456afcb96953b94ec
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 16:26:24 +0330

    update TNTSelf/plugins/Vcinvite.py

--- TNTSelf/plugins/Vcinvite.py
+++ TNTSelf/plugins/Vcinvite.py
@@ -27,20 +27,20 @@ STRINGS = {
 async def invitevc(event):
     await event.edit(client.STRINGS["wait"])
     if event.chat.megagroup or event.chat.broadcast:
-        info = (await client(functions.channels.GetFullChannelRequest(event.chat_id))).full_chat
+        info = (await event.client(functions.channels.GetFullChannelRequest(event.chat_id))).full_chat
     else:
-        info = (await client(functions.messages.GetFullChatRequest(event.chat_id))).full_chat
+        info = (await event.client(functions.messages.GetFullChatRequest(event.chat_id))).full_chat
     if not info.call:
         return await event.edit(client.getstrings(STRINGS)["notcall"])
     users = []
-    async for user in client.iter_participants(event.chat_id, filter=types.ChannelParticipantsRecent, limit=50):
+    async for user in event.client.iter_participants(event.chat_id, filter=types.ChannelParticipantsRecent, limit=50):
         users.append(user.id)
     if not users:
         return await event.edit(client.getstrings(STRINGS)["notuser"])
     await event.edit(client.getstrings(STRINGS)["inviting"].format(len(users)))
     invited = 0
     try:
-        result = await client(functions.phone.InviteToGroupCallRequest(call=info.call, users=users))
+        result = await event.client(functions.phone.InviteToGroupCallRequest(call=info.call, users=users))
         invited += 1
     except errors.FloodWaitError:
         return await event.edit(client.getstrings(STRINGS)["notflood"])
