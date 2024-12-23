48b33adb29937a022f0b032eaa3d0d404ae6d8c4
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 23:23:27 +0330

    update TNTSelf/plugins/Auto.py

--- TNTSelf/plugins/Auto.py
+++ TNTSelf/plugins/Auto.py
@@ -146,7 +146,7 @@ async def setautosleep(event):
     event.client.DB.set_key("AUTO_SLEEP", sleep)
     await event.edit(client.getstrings(STRINGS)["setsleep"].format(minsleep))
 
-@aiocron.crontab("*/30 * * * * *")
+@aiocron.crontab("*/1 * * * *")
 async def autosender():
     for sinclient in client.clients:
         amode = sinclient.DB.get_key("AUTO_MODE") or "OFF"
