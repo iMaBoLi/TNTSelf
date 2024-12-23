e185a9075728341c9b868830f19f55026dae45c0
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 18:09:51 +0330

    update TNTSelf/plugins/Auto.py

--- TNTSelf/plugins/Auto.py
+++ TNTSelf/plugins/Auto.py
@@ -39,7 +39,7 @@ __INFO__ = {
             "{CMD}SetAutoSleep <Time>": {
                 "Help": "To Set Sleep For Auto Message",
                 "Input": {
-                    "<Time>": "Sleep Time ( 1-120 Minutes )",
+                    "<Time>": "Sleep Time ( 1-30 Minutes )",
                 },
             },
         },
@@ -57,7 +57,6 @@ STRINGS = {
     "list": "**{STR} The Auto List:**\n\n",
     "aempty": "**{STR} The Auto List Is Already Empty**",
     "clean": "**{STR} The Auto List Has Been Cleaned!**",
-    "nosleep": "**{STR} The Auto Sleep Must Be Between** ( `{}` ) **And** ( `{}` )",
     "setsleep": "**{STR} The Auto Sleep Was Set To** ( `{}` )",
     "saveauto": "**{STR} The Auto Message Was Saved!**",
     "notauto": "**{STR} The Auto Message Is Not Saved!**"
@@ -67,7 +66,7 @@ STRINGS = {
 async def automode(event):
     await event.edit(client.STRINGS["wait"])
     change = event.pattern_match.group(1).upper()
-    client.DB.set_key("AUTO_MODE", change)
+    event.client.DB.set_key("AUTO_MODE", change)
     showchange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
     await event.edit(client.getstrings(STRINGS)["change"].format(showchange))
 
@@ -76,12 +75,12 @@ async def addauto(event):
     await event.edit(client.STRINGS["wait"])
     if not event.chatid:
         return await event.edit(client.STRINGS["chat"]["all"])
-    autos = client.DB.get_key("AUTO_CHATS") or {}
-    info = await client.get_entity(event.chatid)
+    autos = event.client.DB.get_key("AUTO_CHATS") or {}
+    info = await event.client.get_entity(event.chatid)
     if event.chatid in autos:
         return await event.edit(client.getstrings(STRINGS)["notall"].format(info.title))
     autos.update({event.chatid: time.time()})
-    client.DB.set_key("AUTO_CHATS", autos)
+    event.client.DB.set_key("AUTO_CHATS", autos)
     await event.edit(client.getstrings(STRINGS)["addchat"].format(info.title))
     
 @client.Command(command="DelAuto", chatid=True)
@@ -89,18 +88,18 @@ async def delauto(event):
     await event.edit(client.STRINGS["wait"])
     if not event.chatid:
         return await event.edit(client.STRINGS["chat"]["all"])
-    autos = client.DB.get_key("AUTO_CHATS") or {}
-    info = await client.get_entity(event.chatid)
+    autos = event.client.DB.get_key("AUTO_CHATS") or {}
+    info = await event.client.get_entity(event.chatid)
     if event.chatid not in autos:
         return await event.edit(client.getstrings(STRINGS)["notin"].format(info.title))  
     del autos[event.chatid]
-    client.DB.set_key("AUTO_CHATS", autos)
+    event.client.DB.set_key("AUTO_CHATS", autos)
     await event.edit(client.getstrings(STRINGS)["delchat"].format(info.title))
     
 @client.Command(command="AutoList")
 async def autolist(event):
     await event.edit(client.STRINGS["wait"])
-    autos = client.DB.get_key("AUTO_CHATS") or []
+    autos = event.client.DB.get_key("AUTO_CHATS") or []
     if not autos:
         return await event.edit(client.getstrings(STRINGS)["empty"])
     text = client.getstrings(STRINGS)["list"]
@@ -113,10 +112,10 @@ async def autolist(event):
 @client.Command(command="CleanAutoList")
 async def cleanautolist(event):
     await event.edit(client.STRINGS["wait"])
-    autos = client.DB.get_key("AUTO_CHATS") or []
+    autos = event.client.DB.get_key("AUTO_CHATS") or []
     if not autos:
         return await event.edit(client.getstrings(STRINGS)["aempty"])
-    client.DB.del_key("AUTO_CHATS")
+    event.client.DB.del_key("AUTO_CHATS")
     await event.edit(client.getstrings(STRINGS)["clean"])
 
 @client.Command(command="SetAuto")
@@ -125,16 +124,16 @@ async def setauto(event):
     if reply:= event.checkReply():
         return await event.edit(reply)
     info = await event.reply_message.save()
-    client.DB.set_key("AUTO_MESSAGE", info)
+    event.client.DB.set_key("AUTO_MESSAGE", info)
     await event.edit(client.getstrings(STRINGS)["saveauto"])
  
 @client.Command(command="GetAuto")
 async def getauto(event):
     await event.edit(client.STRINGS["wait"])
-    info = client.DB.get_key("AUTO_MESSAGE")
+    info = event.client.DB.get_key("AUTO_MESSAGE")
     if not info:
         return await event.edit(client.getstrings(STRINGS)["notsave"])
-    getmsg = await client.get_messages(int(info["chat_id"]), ids=int(info["msg_id"]))
+    getmsg = await event.client.get_messages(int(info["chat_id"]), ids=int(info["msg_id"]))
     await event.respond(getmsg)
     await event.delete()
     
@@ -142,33 +141,33 @@ async def getauto(event):
 async def setautosleep(event):
     await event.edit(client.STRINGS["wait"])
     sleep = int(event.pattern_match.group(1))
-    if 1 > sleep > 120:
-        return await event.edit(client.getstrings(STRINGS)["nosleep"].format(1, 60))
+    sleep = sleep if sleep <= 30 else 30
     sleep = sleep * 60
-    client.DB.set_key("AUTO_SLEEP", sleep)
+    event.client.DB.set_key("AUTO_SLEEP", sleep)
     await event.edit(client.getstrings(STRINGS)["setsleep"].format(sleep))
 
-@aiocron.crontab("*/30 * * * * *")
+@aiocron.crontab("*/20 * * * * *")
 async def autosender():
-    amode = client.DB.get_key("AUTO_MODE") or "OFF"
-    achats = client.DB.get_key("AUTO_CHATS") or []
-    amessage = client.DB.get_key("AUTO_MESSAGE") or {}
-    if not amessage: return
-    if amode == "ON":
-        for chatid in achats:
-            ltime = achats[chatid]
-            sleep = client.DB.get_key("AUTO_SLEEP") or 600
-            if time.time() >= (ltime + int(sleep)):
-                getmsg = await client.get_messages(int(amessage["chat_id"]), ids=int(amessage["msg_id"]))
-                jtime = datetime.now()
-                VARS = {
-                    "TIME": jtime.strftime("%H:%M"),
-                    "DATE": jtime.strftime("%Y") + "/" + jtime.strftime("%m") + "/" + jtime.strftime("%d"),
-                    "HEART": random.choice(client.functions.HEARTS),
-                }
-                for VAR in VARS:
-                    getmsg.text = getmsg.text.replace(VAR, VARS[VAR])
-                await client.send_message(chatid, getmsg)
-                achats[chatid] = time.time()
-                client.DB.set_key("AUTO_CHATS", achats)
-                await asyncio.sleep(0.5)
+    for sinclient in client.clients:
+        amode = sinclient.DB.get_key("AUTO_MODE") or "OFF"
+        achats = sinclient.DB.get_key("AUTO_CHATS") or []
+        amessage = sinclient.DB.get_key("AUTO_MESSAGE") or {}
+        if not amessage: return
+        if amode == "ON":
+            for chatid in achats:
+                ltime = achats[chatid]
+                sleep = sinclient.DB.get_key("AUTO_SLEEP") or 600
+                if time.time() >= (ltime + int(sleep)):
+                    getmsg = await sinclient.get_messages(int(amessage["chat_id"]), ids=int(amessage["msg_id"]))
+                    jtime = datetime.now()
+                    VARS = {
+                        "TIME": jtime.strftime("%H:%M"),
+                        "DATE": jtime.strftime("%Y") + "/" + jtime.strftime("%m") + "/" + jtime.strftime("%d"),
+                        "HEART": random.choice(client.functions.HEARTS),
+                    }
+                    for VAR in VARS:
+                        getmsg.text = getmsg.text.replace(VAR, VARS[VAR])
+                    await sinclient.send_message(chatid, getmsg)
+                    achats[chatid] = time.time()
+                    sinclient.DB.set_key("AUTO_CHATS", achats)
+                    await asyncio.sleep(0.5)
