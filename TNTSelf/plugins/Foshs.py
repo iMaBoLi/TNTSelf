cb3265d394fc10b80328e6bdbf7561e14ff9a19a
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 17:48:20 +0330

    update TNTSelf/plugins/Foshs.py

--- TNTSelf/plugins/Foshs.py
+++ TNTSelf/plugins/Foshs.py
@@ -12,12 +12,6 @@ __INFO__ = {
                     "<Text>": "Text For Add",
                 },
             },
-            "{CMD}DelFosh <Text>": {
-                "Help": "To Delete Fosh",
-                "Input": {
-                    "<Text>": "Text For Delete",
-                },
-            },
             "{CMD}FoshList": {
                 "Help": "To Get Fosh List",
             },
@@ -32,7 +26,6 @@ client.functions.AddInfo(__INFO__)
 STRINGS = {
     "newadd": "**{STR} The Foshs** ( `{}` ) **Added To Fosh List!**",
     "delnot": "**{STR} The Fosh** ( `{}` ) **Not In Fosh List!**",
-    "del": "**{STR} The Fosh** ( `{}` ) **Deleted From Fosh List!**",
     "empty": "**{STR} The Fosh List Is Empty!**",
     "list": "**{STR} The Fosh List:**\n\n",
     "aempty": "**{STR} The Fosh List Is Already Empty!**",
@@ -41,35 +34,27 @@ STRINGS = {
 
 FOSH_LIMIT = 500
 
-@client.Command(command="NewFosh (.*)")
+@client.Command(command="AddFosh (.*)")
 async def addfosh(event):
     await event.edit(client.STRINGS["wait"])
     foshs = event.client.DB.get_key("FOSH_LIST") or []
-    newfoshs = str(event.pattern_match.group(1))
+    newfosh = str(event.pattern_match.group(1))
     newfoshs = newfoshs.split(",")[:50]
     if len(foshs) >= FOSH_LIMIT:
         return await event.edit(client.getstrings(STRINGS)["limadd"].format(FOSH_LIMIT))
     newfoshs = newfoshs if len(foshs) + len(newfoshs) <= FOSH_LIMIT else newfoshs[:FOSH_LIMIT - len(foshs)]
+    addfoshs = ""
     for fosh in newfoshs:
         foshs.append(fosh)
+        addfoshs += f"{fosh} - "
     event.client.DB.set_key("FOSH_LIST", foshs)
-    await event.edit(client.getstrings(STRINGS)["newadd"].format(newfoshs))
-    
-@client.Command(command="DelFosh (.*)")
-async def delfosh(event):
-    await event.edit(client.STRINGS["wait"])
-    foshs = event.client.DB.get_key("FOSH_LIST") or []
-    newfosh = str(event.pattern_match.group(1))
-    if newfosh not in foshs:
-        return await event.edit(client.getstrings(STRINGS)["delnot"].format(newfosh))  
-    foshs.remove(newfosh)
-    event.client.DB.set_key("FOSH_LIST", foshs)
-    await event.edit(client.getstrings(STRINGS)["del"].format(newfosh))
+    addfoshs = addfoshs[:-3]
+    await event.edit(client.getstrings(STRINGS)["newadd"].format(addfoshs))
 
 @client.Command(command="FoshList")
 async def foshlist(event):
     await event.edit(client.STRINGS["wait"])
-    foshs = event.client.DB.get_key("FOSH_LIST") or []
+    foshs = event.client.DB.get_key("FOSH_LIST") or client.functions.FOSHS
     if not foshs:
         return await event.edit(client.getstrings(STRINGS)["empty"])
     text = client.getstrings(STRINGS)["list"]
