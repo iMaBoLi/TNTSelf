f908b303903e635048445797384fb96b8f41a7c3
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 17:58:27 +0330

    update TNTSelf/plugins/EmojiEdit.py

--- TNTSelf/plugins/EmojiEdit.py
+++ TNTSelf/plugins/EmojiEdit.py
@@ -6,8 +6,16 @@ __INFO__ = {
     "Info": {
         "Help": "To Add Emoji On Your Text Messages!",
         "Commands": {
-            "{CMD}Emoji <On-Off>": None,
-            "{CMD}SetEmoji <Emoji>-<Emoji>": None,
+            "{CMD}Emoji <On-Off>": {
+                "Help": "To Turn On-Off Emoji Mode",
+            },
+            "{CMD}SetEmoji <Emoji1>-<Emoji2>": {
+                "Help": "To Set Emojis",
+                "Input": {
+                    "<Emoji1>": "First Emoji",
+                    "<Emoji2>": "Second Emoji",
+                },
+            },
         },
     },
 }
@@ -22,7 +30,7 @@ STRINGS = {
 async def emojimode(event):
     await event.edit(client.STRINGS["wait"])
     change = event.pattern_match.group(1).upper()
-    client.DB.set_key("EMOJI_MODE", change)
+    event.client.DB.set_key("EMOJI_MODE", change)
     showchange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
     await event.edit(client.getstrings(STRINGS)["change"].format(showchange))
 
@@ -32,15 +40,15 @@ async def setemoji(event):
     emoji1 = event.pattern_match.group(1)
     emoji2 = event.pattern_match.group(2)
     emojis = emoji1 + "-" + emoji2
-    client.DB.set_key("EDIT_EMOJIS", emojis)
+    event.client.DB.set_key("EDIT_EMOJIS", emojis)
     text = client.getstrings(STRINGS)["setemoji"].format(emojis)
     await event.edit(text)
 
 @client.Command(allowedits=False, checkCmd=True)
 async def emoji(event):
     if not event.text: return
-    mode = client.DB.get_key("EMOJI_MODE") or "OFF"
-    emojis = client.DB.get_key("EDIT_EMOJIS") or ""
+    mode = event.client.DB.get_key("EMOJI_MODE") or "OFF"
+    emojis = event.client.DB.get_key("EDIT_EMOJIS") or ""
     if mode == "OFF" or not emojis: return
     emoji1 = emojis.split("-")[0]
     emoji2 = emojis.split("-")[1]
