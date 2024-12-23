3e3c76fa80e1e4bbc2dc5a672f52647cf9831510
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 13:18:35 +0330

    update TNTSelf/events/Inline.py

--- TNTSelf/events/Inline.py
+++ TNTSelf/events/Inline.py
@@ -10,13 +10,14 @@ def Inline(
     def decorator(func):
         async def wrapper(event):
             try:
-                event.is_sudo = True if event.sender_id in client.users else False
+                event.is_sudo = True if event.sender_id == event.client.me.id else False
                 if onlysudo and not event.is_sudo:
                     text = client.STRINGS["OtherInline"]
                     return await event.answer([event.builder.article("TNTSelf - NotForYou", text=text)])
                 await func(event)
             except:
                 client.LOGS.error(format_exc())
-        client.bot.add_event_handler(wrapper, events.InlineQuery(pattern=pattern, **kwargs))
+        for sinclient in client.clients:
+            sinclient.bot.add_event_handler(wrapper, events.InlineQuery(pattern=pattern, **kwargs))
         return wrapper
     return decorator
