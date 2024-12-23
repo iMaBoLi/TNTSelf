e52b4109f5d74722727ab2744b9952ffafafb4fc
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 18:00:38 +0330

    update TNTSelf/plugins/LockPv.py

--- TNTSelf/plugins/LockPv.py
+++ TNTSelf/plugins/LockPv.py
@@ -22,13 +22,13 @@ STRINGS = {
 async def lockpvmode(event):
     await event.edit(client.STRINGS["wait"])
     change = event.pattern_match.group(1).upper()
-    client.DB.set_key("LOVKPV_MODE", change)
+    event.client.DB.set_key("LOVKPV_MODE", change)
     showchange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
     await event.edit(client.getstrings(STRINGS)["change"].format(showchange))
 
 @client.Command(onlysudo=False, allowedits=False)
 async def lockepv(event):
     if not event.is_private or event.is_white or event.is_sudo or event.is_bot: return
-    mode = client.DB.get_key("LOVKPV_MODE") or "OFF"
+    mode = event.client.DB.get_key("LOVKPV_MODE") or "OFF"
     if mode == "ON":
         await client(functions.contacts.BlockRequest(event.sender_id))
