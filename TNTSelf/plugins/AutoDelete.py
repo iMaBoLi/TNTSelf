dec71834655c3902b490012b75536b77f76b1a51
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-23 15:49:42 +0330

    update TNTSelf/plugins/AutoDelete.py

--- TNTSelf/plugins/AutoDelete.py
+++ TNTSelf/plugins/AutoDelete.py
@@ -8,8 +8,15 @@ __INFO__ = {
     "Info": {
         "Help": "To Setting Auto Delete Messages In Chats!",
         "Commands": {
-            "{CMD}AutoDelete <On-Off>": None,
-            "{CMD}SetDeleteSleep <1-120min>": None,
+            "{CMD}AutoDelete <On-Off>": {
+                "Help": "To Turn On-Off Auto Delete",
+            }
+            "{CMD}SetDeleteSleep <Time>": {
+                "Help": "To Set Auto Delete Sleep",
+                "Input": {
+                    "<Time>": "Time For Set (1-120)mins",
+                },
+            },
         },
     },
 }
@@ -25,7 +32,7 @@ STRINGS = {
 async def delautomode(event):
     await event.edit(client.STRINGS["wait"])
     change = event.pattern_match.group(1).upper()
-    client.DB.set_key("AUTODELETE_MODE", change)
+    event.client.DB.set_key("AUTODELETE_MODE", change)
     showchange = client.STRINGS["On"] if change == "ON" else client.STRINGS["Off"]
     await event.edit(client.getstrings(STRINGS)["change"].format(showchange))
     
@@ -36,31 +43,32 @@ async def setautodeletesleep(event):
     if 1 > sleep > 120:
         return await event.edit(client.getstrings(STRINGS)["nosleep"].format(1, 60))
     sleep = sleep * 60
-    client.DB.set_key("AUTODELETE_SLEEP", sleep)
+    event.client.DB.set_key("AUTODELETE_SLEEP", sleep)
     await event.edit(client.getstrings(STRINGS)["setsleep"].format(sleep))
 
 @client.Command(allowedits=False, checkCmd=True)
 async def autodelete(event):
-    automode = client.DB.get_key("AUTODELETE_MODE") or "OFF"
+    automode = event.client.DB.get_key("AUTODELETE_MODE") or "OFF"
     if automode == "ON":
-        MSGS = client.DB.get_key("AUTODELETE_MSGS") or {}
+        MSGS = event.client.DB.get_key("AUTODELETE_MSGS") or {}
         msginfo = str(event.chat_id) + ":" + str(event.id)
         MSGS.update({msginfo: time.time()})
-        client.DB.set_key("AUTODELETE_MSGS", MSGS)
+        event.client.DB.set_key("AUTODELETE_MSGS", MSGS)
 
-@aiocron.crontab("*/10 * * * * *")
+@aiocron.crontab("*/5 * * * * *")
 async def autodeleter():
-    MSGS = client.DB.get_key("AUTODELETE_MSGS") or {}
-    if not MSGS: return
-    for msg in MSGS:
-        ltime = MSGS[msg]
-        sleep = client.DB.get_key("AUTODELETE_SLEEP") or 600
-        if time.time() >= (ltime + int(sleep)):
-            info = msg.split(":")
-            try:
-                getmsg = await client.get_messages(int(info[0]), ids=int(info[1]))
-                await getmsg.delete()
-            except:
-                pass
-            del MSGS[msg]
-            client.DB.set_key("AUTODELETE_MSGS", MSGS)
+    for sinclient in client.clients:
+        MSGS = sinclient.DB.get_key("AUTODELETE_MSGS") or {}
+        if not MSGS: return
+        for msg in MSGS:
+            ltime = MSGS[msg]
+            sleep = sinclient.DB.get_key("AUTODELETE_SLEEP") or 600
+            if time.time() >= (ltime + int(sleep)):
+                info = msg.split(":")
+                try:
+                    getmsg = await sinclient.get_messages(int(info[0]), ids=int(info[1]))
+                    await getmsg.delete()
+                except:
+                    pass
+                del MSGS[msg]
+                sinclient.DB.set_key("AUTODELETE_MSGS", MSGS)
