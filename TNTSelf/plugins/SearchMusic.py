c02050e49e7d868cfdf3c6c65e458f074412d21f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-25 00:34:14 +0330

    update TNTSelf/plugins/SearchMusic.py

--- TNTSelf/plugins/SearchMusic.py
+++ TNTSelf/plugins/SearchMusic.py
@@ -26,11 +26,11 @@ STRINGS = {
 async def searchmusic(event):
     await event.edit(client.STRINGS["wait"])
     query = event.pattern_match.group(1)
-    result = await client.inline_query("@DeezerMusicBot", query)
+    result = await event.client.inline_query("@DeezerMusicBot", query)
     if not result.result.results:
         return await event.edit(client.getstrings(STRINGS)["notres"].format(query))
     result = result.result.results[0]
     size = client.functions.convert_bytes(result.document.size)
     caption = client.getstrings(STRINGS)["caption"].format(result.title, result.description, size)
-    await client.send_file(event.chat_id, result.document, caption=caption)
+    await event.client.send_file(event.chat_id, result.document, caption=caption)
     await event.delete()
