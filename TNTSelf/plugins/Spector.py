91cfc40efcde2ef81f0dfa6a3968f317e01fbaae
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-25 00:43:02 +0330

    update TNTSelf/plugins/Spector.py

--- TNTSelf/plugins/Spector.py
+++ TNTSelf/plugins/Spector.py
@@ -40,10 +40,10 @@ SPECS = [
     "LEAVE_GROUPS",
 ]
 
-async def get_spector_buttons(userid, chatid):
+async def get_spector_buttons(event, userid, chatid):
     buttons = []
     for spec in SPECS:
-        lists = client.DB.get_key(f"SPECTOR_{spec}") or []
+        lists = event.client.DB.get_key(f"SPECTOR_{spec}") or []
         smode = client.STRINGS["inline"]["On"] if userid in lists else client.STRINGS["inline"]["Off"]
         cmode = "del" if userid in lists else "add"
         show = spec.replace("_", " ").title()
@@ -58,7 +58,7 @@ async def Spector(event):
     if not event.userid:
         return await event.edit(client.STRINGS["user"]["all"])
     chatid = event.chat_id
-    res = await event.client.inline_query(client.bot.me.username, f"Spector:{chatid}:{event.userid}")
+    res = await event.client.inline_query(event.client.bot.me.username, f"Spector:{chatid}:{event.userid}")
     if event.is_reply:
         await res[0].click(event.chat_id, reply_to=event.reply_message.id)
     else:
@@ -69,20 +69,20 @@ async def Spector(event):
 async def inlinespector(event):
     chatid = int(event.pattern_match.group(1))
     userid = int(event.pattern_match.group(2))
-    info = await client.get_entity(userid)
+    info = await event.client.get_entity(userid)
     mention = client.functions.mention(info)
     text = client.getstrings(STRINGS)["spector"].format(mention)
-    buttons = await get_spector_buttons(userid, chatid)
+    buttons = await get_spector_buttons(event, userid, chatid)
     await event.answer([event.builder.article("TNTSelf - Spector", text=text, buttons=buttons)])
 
 @client.Callback(pattern="Spector\\:(.*)\\:(.*)")
 async def callspector(event):
     chatid = int(event.data_match.group(1).decode('utf-8'))
     userid = int(event.data_match.group(2).decode('utf-8'))
-    info = await client.get_entity(userid)
+    info = await event.client.get_entity(userid)
     mention = client.functions.mention(info)
     text = client.getstrings(STRINGS)["spector"].format(mention)
-    buttons = await get_spector_buttons(userid, chatid)
+    buttons = await get_spector_buttons(event, userid, chatid)
     await event.edit(text, buttons=buttons)
 
 @client.Callback(data="SetSpector\\:(.*)\\:(.*)\\:(.*)\\:(.*)")
@@ -91,16 +91,16 @@ async def setspector(event):
     userid = int(event.data_match.group(2).decode('utf-8'))
     mode = event.data_match.group(3).decode('utf-8')
     change = event.data_match.group(4).decode('utf-8')
-    info = await client.get_entity(userid)
+    info = await event.client.get_entity(userid)
     mode = "SPECTOR_" + mode
-    lists = client.DB.get_key(mode) or []
+    lists = event.client.DB.get_key(mode) or []
     if change == "add":
         lists.append(userid)
-        client.DB.set_key(mode, lists)
+        event.client.DB.set_key(mode, lists)
     elif change == "del":
         lists.remove(userid)
-        client.DB.set_key(mode, lists)
-    buttons = await get_spector_buttons(userid, chatid)
+        event.client.DB.set_key(mode, lists)
+    buttons = await get_spector_buttons(event, userid, chatid)
     await event.edit(buttons=buttons)
 
 @client.Callback(data="CloseSpector")
@@ -111,9 +111,9 @@ async def closespector(event):
 @client.on(events.NewMessage)
 async def sendmessagespec(event):
     if event.sender_id == event.client.me.id: return
-    lists = client.DB.get_key("SPECTOR_SEND_MESSAGE") or []
+    lists = event.client.DB.get_key("SPECTOR_SEND_MESSAGE") or []
     if event.sender_id in lists:
-        info = await client.get_entity(event.sender_id)
+        info = await event.client.get_entity(event.sender_id)
         mention = client.functions.mention(info)
         localtime = datetime.datetime.now()
         time = localtime.strftime("%H:%M:%S")
@@ -124,14 +124,14 @@ async def sendmessagespec(event):
             meslink = "https://t.me/c/" + str(event.chat_id).replace("-100", "") + "/" + str(event.id)
             where = f"[In A Chat]({meslink})"
             text = client.getstrings(STRINGS)["specsendmessage"].format(mention, event.sender_id, where, time)
-        await client.bot.send_message(client.REALM, text)
+        await event.client.bot.send_message(client.REALM, text)
 
 @client.on(events.MessageEdited)
 async def editmessagespec(event):
     if event.sender_id == event.client.me.id: return
-    lists = client.DB.get_key("SPECTOR_EDIT_MESSAGE") or []
+    lists = event.client.DB.get_key("SPECTOR_EDIT_MESSAGE") or []
     if event.sender_id in lists:
-        info = await client.get_entity(event.sender_id)
+        info = await event.client.get_entity(event.sender_id)
         mention = client.functions.mention(info)
         localtime = datetime.datetime.now()
         time = localtime.strftime("%H:%M:%S")
@@ -142,21 +142,21 @@ async def editmessagespec(event):
             meslink = "https://t.me/c/" + str(event.chat_id).replace("-100", "") + "/" + str(event.id)
             where = f"[In A Chat]({meslink})"
             text = client.getstrings(STRINGS)["specsendmessage"].format(mention, event.sender_id, where, time)
-        await client.bot.send_message(client.REALM, text)
+        await event.client.bot.send_message(client.REALM, text)
  
 @client.on(events.UserUpdate)
 async def statusspec(event):
     if event.user_id == event.client.me.id or not event.status: return
     status = event.status.to_dict()["_"]
     if status not in ["UserStatusOnline", "UserStatusOffline", "UserStatusRecently"]: return
-    lists = client.DB.get_key("SPECTOR_STATUS") or []
+    lists = event.client.DB.get_key("SPECTOR_STATUS") or []
     if event.user_id in lists:
-        info = await client.get_entity(event.user_id)
+        info = await event.client.get_entity(event.user_id)
         mention = client.functions.mention(info)
         localtime = datetime.datetime.now()
         time = localtime.strftime("%H:%M:%S")
         text = client.getstrings(STRINGS)["specstatus"].format(mention, event.user_id, status.replace("UserStatus", ""), time)
-        await client.bot.send_message(client.REALM, text)
+        await event.client.bot.send_message(client.REALM, text)
         
 @client.on(events.UserUpdate)
 async def actionspec(event):
@@ -174,55 +174,55 @@ async def actionspec(event):
         "SendMessageRecordRoundAction": "Recording Round Video",
     }
     if action not in actions: return
-    lists = client.DB.get_key("SPECTOR_ACTION") or []
+    lists = event.client.DB.get_key("SPECTOR_ACTION") or []
     if event.user_id in lists:
-        info = await client.get_entity(event.user_id)
+        info = await event.client.get_entity(event.user_id)
         mention = client.functions.mention(info)
         localtime = datetime.datetime.now()
         time = localtime.strftime("%H:%M:%S")
         where = "In Your Pv" if "UpdateUser" in event.original_update.to_dict()["_"] else "In A Chat"
         text = client.getstrings(STRINGS)["specaction"].format(mention, event.user_id, actions[action], where, time)
-        await client.bot.send_message(client.REALM, text)
+        await event.client.bot.send_message(client.REALM, text)
         
 @client.on(events.MessageRead)
 async def readpvspec(event):
     if event.chat_id == event.client.me.id or not event.is_private: return
     userid = event.original_update.peer.user_id
-    lists = client.DB.get_key("SPECTOR_READ_PV") or []
+    lists = event.client.DB.get_key("SPECTOR_READ_PV") or []
     if userid in lists:
-        info = await client.get_entity(userid)
+        info = await event.client.get_entity(userid)
         mention = client.functions.mention(info)
         localtime = datetime.datetime.now()
         time = localtime.strftime("%H:%M:%S")
         text = client.getstrings(STRINGS)["specreadpv"].format(mention, userid, time)
-        await client.bot.send_message(client.REALM, text)
+        await event.client.bot.send_message(client.REALM, text)
         
 @client.on(events.ChatAction)
 async def joingroupspec(event):
     if not event.user_joined and not event.added_by: return
     userid = (await event.get_user()).id
     if userid == event.client.me.id: return
-    lists = client.DB.get_key("SPECTOR_JOIN_GROUPS") or []
+    lists = event.client.DB.get_key("SPECTOR_JOIN_GROUPS") or []
     if userid in lists:
-        info = await client.get_entity(userid)
+        info = await event.client.get_entity(userid)
         mention = client.functions.mention(info)
         chat = await event.get_chat()
         localtime = datetime.datetime.now()
         time = localtime.strftime("%H:%M:%S")
         text = client.getstrings(STRINGS)["specjoingroup"].format(mention, userid, chat.title, chat.id, time)
-        await client.bot.send_message(client.REALM, text)
+        await event.client.bot.send_message(client.REALM, text)
         
 @client.on(events.ChatAction)
 async def leavegroupspec(event):
     if not event.user_left and not event.user_kicked: return
     userid = (await event.get_user()).id
     if userid == event.client.me.id: return
-    lists = client.DB.get_key("SPECTOR_LEAVE_GROUPS") or []
+    lists = event.client.DB.get_key("SPECTOR_LEAVE_GROUPS") or []
     if userid in lists:
-        info = await client.get_entity(userid)
+        info = await event.client.get_entity(userid)
         mention = client.functions.mention(info)
         chat = await event.get_chat()
         localtime = datetime.datetime.now()
         time = localtime.strftime("%H:%M:%S")
         text = client.getstrings(STRINGS)["specleavegroup"].format(mention, userid, chat.title, chat.id, time)
-        await client.bot.send_message(client.REALM, text)
+        await event.client.bot.send_message(client.REALM, text)
