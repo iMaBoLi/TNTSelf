b14a97d29bd66b21eddacebcf2bd45a5062427a9
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-12-25 00:31:50 +0330

    update TNTSelf/plugins/Rotater.py

--- TNTSelf/plugins/Rotater.py
+++ TNTSelf/plugins/Rotater.py
@@ -36,22 +36,22 @@ async def rotate(event):
     if event.reply_message.file.size > client.MAX_SIZE:
         return await event.edit(client.STRINGS["LargeSize"].format(client.functions.convert_bytes(client.MAX_SIZE)))
     if "Photo" in mtype:
-        file = await event.reply_message.download_media(client.PATH)
-        newfile = client.PATH + f"RotatedImage-{str(darge)}.jpg"
+        file = await event.reply_message.download_media(event.client.PATH)
+        newfile = event.client.PATH + f"RotatedImage-{str(darge)}.jpg"
         img = Image.open(file)
         newimg = img.rotate(darge)
         newimg.save(newfile)
         await event.respond(client.getstrings(STRINGS)["proted"].format(str(darge)), file=newfile)        
     elif "Video" in mtype:
         callback = event.progress(download=True)
-        file = await event.reply_message.download_media(client.PATH, progress_callback=callback)
+        file = await event.reply_message.download_media(event.client.PATH, progress_callback=callback)
         await event.edit(client.getstrings(STRINGS)["vrot"].format(str(darge)))
-        newfile = client.PATH + f"RotatedVideo-{str(darge)}.mp4"
+        newfile = event.client.PATH + f"RotatedVideo-{str(darge)}.mp4"
         cmd = f'ffmpeg -i {file} -vf "rotate={darge}" {newfile}'
         await client.functions.runcmd(cmd)
         callback = event.progress(upload=True)
         caption = client.getstrings(STRINGS)["vroted"].format(str(darge))
-        await client.send_file(event.chat_id, newfile, caption=caption, progress_callback=callback)        
+        await event.client.send_file(event.chat_id, newfile, caption=caption, progress_callback=callback)        
     os.remove(file)
     os.remove(newfile)
     await event.delete()
